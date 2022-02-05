7611a0af5319f562a736ddae99663a168942a28e
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-05 14:26:58 +0900

    DFS를 이용한 부분집합 구하기 (수정)

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Node {
-    int data;
-    Node lt, rt;
-
-    public Node(int val) {
-        this.data = val;
-        lt = rt = null;
-    }
-}
-
 public class Main {
     static int num;
     static int[] ch;
     public static void DFS(int L) {
         if(L == num+1) {
-            StringBuilder sb = new StringBuilder("");
-            for(int i = 0; i <= num; i++)
+            StringBuilder sb = new StringBuilder();
+            for(int i = 1; i <= num; i++)
                 if(ch[i] == 1) {
                     sb.append(i);
                     sb.append(" ");
                 }
-            if(!sb.equals(""))
+            if(sb.length() != 0)
                 System.out.println(sb);
         } else {
             // 사용함
