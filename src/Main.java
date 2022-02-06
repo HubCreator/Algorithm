c87cae50aa50b325f3015a85a380907e5719aae0
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-06 10:47:31 +0900

    피보나치 (재귀)

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-    static int num;
-    static int[] ch;
-    public static void DFS(int L) {
-        if(L == num+1) {
-            StringBuilder sb = new StringBuilder();
-            for(int i = 1; i <= num; i++)
-                if(ch[i] == 1) {
-                    sb.append(i);
-                    sb.append(" ");
-                }
-            if(sb.length() != 0)
-                System.out.println(sb);
-        } else {
-            // 사용함
-            ch[L] = 1;
-            DFS(L+1);
-            // 사용하지 않음
-            ch[L] = 0;
-            DFS(L+1);
-        }
+    public static int recursive(int n) {
+        if (n == 1) return 1;
+        else if (n == 2) return 2;
+        else return recursive(n - 2) + recursive(n - 1);
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        num = Integer.parseInt(br.readLine());
-        ch = new int[num+1];
-        DFS(1);
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++)
+            System.out.print(recursive(i) + " ");
     }
 }
