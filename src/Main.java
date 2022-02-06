0375e0a80ce2d92c32fb73931b58ebaed0a146dc
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-06 10:47:57 +0900

    피보나치 (재귀+배열)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import java.io.*;
 
 public class Main {
+    public static int[] fibo;
     public static int recursive(int n) {
-        if (n == 1) return 1;
-        else if (n == 2) return 2;
-        else return recursive(n - 2) + recursive(n - 1);
+        if (n == 1) return fibo[n] = 1;
+        else if (n == 2) return fibo[n] = 1;
+        else return fibo[n] = recursive(n-2) + recursive(n-1);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        fibo = new int[n+1];
 
-        for (int i = 1; i <= n; i++)
-            System.out.print(recursive(i) + " ");
+        recursive(n);
+        for(int i = 1; i <= n; i++)
+            System.out.print(fibo[i] + " ");
     }
 }
