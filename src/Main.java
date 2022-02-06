63f93edb8414fe61c54e95c284f07103fb1576c6
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-07 08:36:50 +0900

    결정 알고리즘1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,41 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] fibo;
-    public static int recursive(int n) {
-        if(fibo[n] > 0) return fibo[n];
-        if (n == 1) return fibo[n] = 1;
-        else if (n == 2) return fibo[n] = 1;
-        else return fibo[n] = recursive(n-2) + recursive(n-1);
+    public static int count(int[] arr, int capacity) {
+        int cnt = 1, sum = 0; // cnt는 DVD 장수
+        for(int x : arr) {
+            if(sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else sum += x;
+        }
+        return cnt;
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        fibo = new int[n+1];
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st1.nextToken());
+        int M = Integer.parseInt(st1.nextToken());
+        int[] arr = new int[N];
 
-        recursive(n);
-        for(int i = 1; i <= n; i++)
-            System.out.print(fibo[i] + " ");
+        for(int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st2.nextToken());
+
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt(),
+                rt = Arrays.stream(arr).sum();
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if(count(arr, mid) <= M) {
+                answer = mid;
+                rt = mid - 1;
+            } else lt = mid + 1;
+        }
+
+        System.out.print(answer);
     }
 }
