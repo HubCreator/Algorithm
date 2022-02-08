bf58186802bb07209f80f65f57143521e8d20d60
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-08 17:34:53 +0900

    이분 정렬

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,27 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int count(int[] arr, int dist) { // 유효한지 판별
-        int cnt = 1;        // 배치한 말의 마리 수
-        int ep = arr[0];    // 말을 배치한 좌표
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i] - ep >= dist) {
-                cnt++;
-                ep = arr[i];
-            }
-        }
-        return cnt;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
         StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st1.nextToken());
-        int C = Integer.parseInt(st1.nextToken()) ;
+        int M = Integer.parseInt(st1.nextToken());
         int[] arr = new int[N];
+        int lt = 0, rt = N-1, answer = 0;
+
         for(int i = 0; i < N; i++)
             arr[i] = Integer.parseInt(st2.nextToken());
 
-        Arrays.sort(arr); // 오름차순 정렬
-        int lt = 1, rt = arr[arr.length-1];
-        int answer = 0;
-
-        while(lt <= rt) { // 이분 검색
+        Arrays.sort(arr);
+        while(lt <= rt) {
             int mid = (lt + rt) / 2;
-            if(count(arr, mid) >= C) {
-                answer = mid;
-                lt = mid + 1;
-            } else rt = mid - 1;
+            if(arr[mid] == M) {
+                answer = mid + 1;
+                break;
+            }
+            else if (arr[mid] > M) rt = mid-1;
+            else lt = mid+1;
         }
 
         System.out.println(answer);
