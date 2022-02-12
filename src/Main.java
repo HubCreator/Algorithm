2fe3de2fcf39eb54a7f667a5d73e4156690f5ad1
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-12 13:05:27 +0900

    정렬 기준 설정

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
-class Main {
-    static int N, M;
-    static ArrayList<ArrayList<Integer>> graph;
-    static int[] dist, ch;
+public class Main {
+    static class Point implements Comparable<Point> {
+        public int x, y;
+        // this가 더 작다고 가정해야 하나?
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if(this.x == o.x) return this.y - o.y; // 오름차순 : 음수값이 return 되도록 함
+            else return this.x - o.x;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st1.nextToken()); // 정점의 수
-        M = Integer.parseInt(st1.nextToken()); // 간선의 수
-        dist = new int[N + 1];
-        ch = new int[N + 1];
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Point> arr = new ArrayList<>();
 
-        StringTokenizer[] st2 = new StringTokenizer[M];
-        for (int i = 0; i < M; i++)
-            st2[i] = new StringTokenizer(br.readLine(), " ");
-
-        graph = new ArrayList<ArrayList<Integer>>();
-        for(int i = 0; i <= N; i++)
-            graph.add(new ArrayList<Integer>());
-
-        for (int i = 0; i < M; i++) {
-            int a = Integer.parseInt(st2[i].nextToken());
-            int b = Integer.parseInt(st2[i].nextToken());
-            graph.get(a).add(b);
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr.add(new Point(Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())));
         }
 
-        BFS(1);
-        for(int i = 2; i < N; i++)
-            System.out.println(i + " : " + dist[i]);
-    }
-
-    private static void BFS(int v) {
-        Queue<Integer> queue = new LinkedList<>();
-        ch[v] = 1;
-        dist[v] = 0;
-        queue.offer(v);
-        while (!queue.isEmpty()) {
-            int cv = queue.poll(); // current vertext
-            for(int nv : graph.get(cv)) {
-                if(ch[nv] == 0) {
-                    ch[nv] = 1;
-                    queue.offer(nv);
-                    dist[nv] = dist[cv]+1;
-                }
-            }
-        }
+        Collections.sort(arr);
+        arr.forEach(p->System.out.println(p.x + " " + p.y));
     }
 }
