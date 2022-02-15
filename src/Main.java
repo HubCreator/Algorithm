f0cdae26bd3d36b412de2cdfd2e57e01e3d91d0e
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-15 21:52:36 +0900

    요소 걸러내기

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,26 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Point implements Comparable<Point> {
-        public int x, y;
-        // this가 더 작다고 가정해야 하나?
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public int compareTo(Point o) {
-            if(this.x == o.x) return this.y - o.y; // 오름차순 : 음수값이 return 되도록 함
-            else return this.x - o.x;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        ArrayList<Point> arr = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[N];
+        ArrayList<Integer> answer = new ArrayList<>();
 
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            arr.add(new Point(Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken())));
-        }
+        for(int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
 
-        Collections.sort(arr);
-        arr.forEach(p->System.out.println(p.x + " " + p.y));
+        int[] arr_sorted = arr.clone(); // 깊은 복사
+        Arrays.sort(arr_sorted);
+
+        for(int i = 0; i < N; i++)
+            if(arr[i] != arr_sorted[i])
+                answer.add(i+1);
+
+        answer.forEach(x->System.out.print(x+" "));
     }
 }
