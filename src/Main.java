0ff8a1619e068ebfa731bee8e60162116ed5bec6
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-24 23:30:58 +0900

    LRU (삽입 정렬)

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,33 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+
+        int S = Integer.parseInt(st1.nextToken());
+        int N = Integer.parseInt(st1.nextToken());
         int[] arr = new int[N];
-        boolean flag = true;
-
+        int[] cache = new int[S];
         for(int i = 0; i < N; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st2.nextToken());
 
-        Arrays.sort(arr);
-        for(int i = 0; i < arr.length-1; i++)
-            if(arr[i] == arr[i+1]) {
-                flag = false;
-                break;
+
+        for(int x : arr) {
+            int pos = -1;
+            for(int i = 0; i < S; i++)
+                if(cache[i] == x) pos = i; // 같은 요소가 있는지 확인
+
+            if(pos == -1) { // 같은 요소가 없다면 하나씩 오른쪽으로 밀어라
+                for(int i = S-1; i >= 1; i--)
+                    cache[i] = cache[i-1];
+                cache[0] = x; // 맨 왼쪽에 요소 삽입
+            } else { // 같은 요소가 있다면
+                for(int i = pos; i >= 1; i--) // 그 요소의 인덱스부터 밀어라
+                    cache[i] = cache[i-1];
+                cache[0] = x;
             }
+        }
 
-        System.out.print(flag ? "U" : "D");
+        Arrays.stream(cache).forEach(x->System.out.print(x + " "));
     }
 }
