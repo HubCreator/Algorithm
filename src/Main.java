cca98a36f2e72344d7b5c0626895360c189e6093
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-02-26 22:36:42 +0900

    삽입 정렬

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,30 @@
 import java.io.*;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer input_nums = new StringTokenizer(br.readLine(), " ");
+        int[] nums = new int[N];
+        for(int i = 0; i < nums.length; i++)
+            nums[i] = Integer.parseInt(input_nums.nextToken());
 
-        int S = Integer.parseInt(st1.nextToken());
-        int N = Integer.parseInt(st1.nextToken());
-        int[] arr = new int[N];
-        LinkedList<Integer> answer = new LinkedList<>();
+        for(int i = 0; i < N; i++) {
+            int index = i; // 기준값
+            for(int j = i+1; j < N; j++) // 기준값 이후의 값들을 비교
+                if(nums[j] < nums[index]) index = j; // index는 가장 작은 요소값을 찾아감
 
-        for(int i = 0; i < arr.length; i++)
-            arr[i] = Integer.parseInt(st2.nextToken());
-
-        for(int i = 0; i < arr.length; i++) {
-            if(answer.size() < S) {
-                if(!answer.contains(arr[i]))
-                    answer.push(arr[i]);
-                else
-                    answer.push(answer.remove(answer.indexOf(arr[i])));
-            }
-            else {
-                if(!answer.contains(arr[i])) {
-                    answer.remove(answer.size()-1);
-                    answer.push(arr[i]);
-                }
-                else
-                    answer.push(answer.remove(answer.indexOf(arr[i])));
-            }
+            int tmp = nums[index]; // 가장 작은 요소와 현재 기준값을 swap
+            nums[index] = nums[i];
+            nums[i] = tmp;
         }
-        answer.forEach(x->System.out.print(x + " "));
+				/*
+					13 5 11 7 23 15
+					7 5 11 13 23 15
+
+				*/
+        Arrays.stream(nums).forEach(x->System.out.print(x+" "));
     }
 }
