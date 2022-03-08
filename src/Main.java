774c22769016ba57d99dd79bfe271cf9f0be148f
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-08 20:34:14 +0900

    선택 정렬

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,20 @@ public class Main {
         for(int i = 0; i < nums.length; i++)
             nums[i] = Integer.parseInt(input_nums.nextToken());
 
-        for(int i = 0; i < N-1; i++) {
-            for(int j = 0; j < N-i-1; j++) { // 바로 옆에 있는 값들을 비교
-                if(nums[j] > nums[j+1]) {   // 가장 큰 값을 맨 오른쪽에 위치시킴
-                    int tmp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = tmp;
-                }
-            }
-        }
+        for(int i = 0; i < N; i++) {
+            int index = i; // 기준값
+            for(int j = i+1; j < N; j++) // 기준값 이후의 값들을 비교
+                if(nums[j] < nums[index]) index = j; // index는 가장 작은 요소값을 찾아감
 
+            int tmp = nums[index]; // 가장 작은 요소와 현재 기준값을 swap
+            nums[index] = nums[i];
+            nums[i] = tmp;
+        }
+				/*
+					13 5 11 7 23 15
+					7 5 11 13 23 15
+
+				*/
         Arrays.stream(nums).forEach(x->System.out.print(x+" "));
     }
 }
