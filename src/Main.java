42dd35a6ec8de3331b19a4979dbd35d5236b9a54
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-09 22:44:53 +0900

    괄호 문자 제거 (스택)

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,28 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer input_nums = new StringTokenizer(br.readLine(), " ");
-        int[] nums = new int[N];
-        for(int i = 0; i < nums.length; i++)
-            nums[i] = Integer.parseInt(input_nums.nextToken());
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < N; i++) {
-            int index = i; // 기준값
-            for(int j = i+1; j < N; j++) // 기준값 이후의 값들을 비교
-                if(nums[j] < nums[index]) index = j; // index는 가장 작은 요소값을 찾아감
-
-            int tmp = nums[index]; // 가장 작은 요소와 현재 기준값을 swap
-            nums[index] = nums[i];
-            nums[i] = tmp;
+        for(int i = 0; i < str.length(); i++) {
+            char x = str.charAt(i);
+            if (x == '(') stack.push(x);
+            else if (x == ')') {
+                int j = i-1;
+                while(stack.peek() != '(') {
+                    if(!stack.isEmpty())
+                        stack.pop();
+                    j = j-1;
+                }
+                if(!stack.isEmpty())
+                    stack.pop();
+            }
+            else
+                stack.push(x);
         }
-				/*
-					13 5 11 7 23 15
-					7 5 11 13 23 15
-
-				*/
-        Arrays.stream(nums).forEach(x->System.out.print(x+" "));
+        stack.forEach(System.out::print);
     }
 }
