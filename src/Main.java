2443db2a09b26a2c037bd3a4b97e9f1dd770616c
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-11 21:00:57 +0900

    스택을 이용한 크레인

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,55 @@
 import java.io.*;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        Stack<Character> stack = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer[] tmp = new StringTokenizer[N];
+        int[][] arr = new int[N][N];
+        Stack<Integer>[] stacks = new Stack[N];
+        int answer = 0;
+        Stack<Integer> bucket = new Stack<>();
 
-        for(char x : str.toCharArray()) {
-            if(x == ')')
-                while(stack.pop() != '('); // 조건을 실행하고, 만족시키지 않으면 break;
-            else stack.push(x);
+        for(int i = 0; i < N; i++)
+            tmp[i] = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                arr[i][j] = Integer.parseInt(tmp[i].nextToken());
+
+        int M = Integer.parseInt(br.readLine());
+        int[] moves = new int[M];
+        StringTokenizer tmp2 = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < M; i++)
+            moves[i] = Integer.parseInt(tmp2.nextToken());
+
+        for(int i = 0; i < N; i++) // stack 생성
+            stacks[i] = new Stack<>();
+
+        for(int i = 0; i < N; i++) { // stack에 값 push
+            for (int j = N-1; j >= 0; j--) {
+                if (arr[j][i] == 0) continue;
+                stacks[i].add(arr[j][i]);
+            }
         }
-        stack.forEach(System.out::print);
+
+        for(int i = 0; i < M; i++) {
+            if(stacks[moves[i]-1].isEmpty()) continue;
+
+            int target = stacks[moves[i]-1].pop();
+            if(!bucket.isEmpty()) {
+                if(target == bucket.peek()) {
+                    bucket.pop();
+                    answer += 2;
+                }
+                else
+                    bucket.push(target);
+            } else
+                bucket.push(target);
+        }
+        System.out.println(answer);
     }
 }
