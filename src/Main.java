f8e8731ae36a07578b52714122ccfa4788684541
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-14 23:00:38 +0900

    스택을 이용한 쇠막대기 자르기

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String tmp = br.readLine();
-        Stack<Integer> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
+        int answer = 0;
 
-        for(char x : tmp.toCharArray()) {
-            if(Character.isDigit(x)) stack.push(Integer.parseInt(String.valueOf(x)));
+        for(int i = 0; i < tmp.length(); i++) {
+            if(tmp.charAt(i) == '(') stack.push('(');
             else {
-                int rt = stack.pop();
-                int lt = stack.pop();
-                switch (x) {
-                    case '+': stack.push(lt + rt); break;
-                    case '-': stack.push(lt - rt); break;
-                    case '*': stack.push(lt * rt); break;
-                    case '/': stack.push(lt / rt); break;
-                }
+                stack.pop();
+                if(tmp.charAt(i-1) == '(') answer += stack.size(); // 원본과의 비교
+                else answer++;
             }
         }
 
-        System.out.println(stack.peek());
+        System.out.println(answer);
     }
 }
