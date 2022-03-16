392cd8d0cc8b9b00745b75f9912e8269306caa02
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-16 19:32:28 +0900

    큐 활용

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,29 @@
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        Queue<Integer> queue = new LinkedList<>();
+        String fixed = br.readLine();
+        String planned = br.readLine();
+        Queue<Character> queue = new LinkedList<>();
+        boolean answer = true;
 
-        for(int i = 1; i <= N; i++)
-            queue.offer(i);
+        for(char x : fixed.toCharArray())
+            queue.offer(x);
 
-        while(queue.size() != 1) {
-            for(int i = 0; i < K-1; i++)
-                queue.offer(queue.poll());
-            queue.poll();
+        for(char x : planned.toCharArray()) {
+            if(queue.contains(x)) {
+                if (queue.poll() != x) { // 참이든 아니든 poll이 실행됨
+                    answer = false;
+                    break;
+                }
+                // else queue.poll(); // 필요 없음
+            }
         }
-        System.out.println(queue.peek());
+        // 선수과목을 모두 듣지 않아도 false
+        if(!queue.isEmpty()) answer = false;
+        System.out.println(answer ? "YES" : "NO");
     }
 }
