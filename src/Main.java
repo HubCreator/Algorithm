3d04ab98d789fe3c2000c690ffdb468d02521c9e
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-19 11:14:26 +0900

    StringBuilder

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,28 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static class Person {
-        int index, priority;
-        public Person(int index, int priority) {
-            this.index = index;
-            this.priority = priority;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer in_options = new StringTokenizer(br.readLine(), " ");
-        StringTokenizer in_nums = new StringTokenizer(br.readLine(), " ");
-        Queue<Person> queue = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(in_options.nextToken());
-        int M = Integer.parseInt(in_options.nextToken());
-        int answer = 0;
-        for(int i = 0; i < N; i++)
-            queue.offer(new Person(i, Integer.parseInt(in_nums.nextToken())));
+        StringBuilder[] p = new StringBuilder[201];
 
-        while(!queue.isEmpty()) {
-            Person target = queue.poll();
-            for(Person x : queue) {
-                if(x.priority > target.priority) {
-                    queue.offer(target);
-                    target = null;
-                    break;
-                }
-            }
-            if(target != null) {
-                answer++;
-                if(target.index == M) break; // 루프 종료 조건
-            }
+        for(int i = 0; i < p.length; i++) {
+            p[i] = new StringBuilder();
         }
-        System.out.println(answer);
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            p[age].append(age).append(' ').append(name).append('\n'); // age를 index로
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(StringBuilder val : p) {
+            sb.append(val);
+        }
+        System.out.println(sb);
     }
 }
