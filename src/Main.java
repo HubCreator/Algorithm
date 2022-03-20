eee41641819cc025cfe36c2bc3f946d4c0a85afc
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-20 23:16:37 +0900

    중복 문자열 제거

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,17 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        StringBuilder[] p = new StringBuilder[201];
-
-        for(int i = 0; i < p.length; i++) {
-            p[i] = new StringBuilder();
-        }
-
-        for(int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int age = Integer.parseInt(st.nextToken());
-            String name = st.nextToken();
-            p[age].append(age).append(' ').append(name).append('\n'); // age를 index로
-        }
-
+        String str = br.readLine();
         StringBuilder sb = new StringBuilder();
-        for(StringBuilder val : p) {
-            sb.append(val);
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.indexOf(str.charAt(i)) == i) {
+                sb.append(str.charAt(i));
+            }
         }
+
         System.out.println(sb);
     }
 }
