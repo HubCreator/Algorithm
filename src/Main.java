b6091c040bb56a0f1d098c4e690334446cdf1fc8
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-25 14:11:21 +0900

    회문 문자열 (대소문자 무시)

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(str).reverse();
 
-        for(int i = 0; i < str.length(); i++) {
-            if(str.indexOf(str.charAt(i)) == i) {
-                sb.append(str.charAt(i));
-            }
-        }
-
-        System.out.println(sb);
+        if(!str.equalsIgnoreCase(sb.toString())) {
+            System.out.println("NO");
+        } else
+            System.out.println("YES");
     }
 }
