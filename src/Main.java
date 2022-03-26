30e5731c6d0bc4c41938497a17163c3254659dd5
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-26 22:27:37 +0900

    가장 짧은 문자 거리

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,35 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringBuilder sb = new StringBuilder(str).reverse();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        char[] str = st.nextToken().toCharArray();
+        char chr = st.nextToken().charAt(0);
+        int[] answer = new int[str.length];
 
-        if(!str.equalsIgnoreCase(sb.toString())) {
-            System.out.println("NO");
-        } else
-            System.out.println("YES");
+        int cnt = 1000;
+        for(int i = 0; i < str.length; i++) {
+            if(str[i] == chr) {
+                cnt = 0;
+                answer[i] = 0;
+            } else {
+                cnt += 1;
+                answer[i] = cnt;
+            }
+        }
+        cnt = 1000;
+        for (int i = str.length-1; i >= 0; i--) {
+            if(str[i] == chr) cnt = 0;
+            else {
+                cnt += 1;
+                answer[i] = Math.min(cnt, answer[i]);
+            }
+        }
+        for(int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i] + " ");
+        }
     }
 }
