5983fb1cfc03de1290fb1485140aa0f00133afde
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-28 23:26:27 +0900

    문자열 압축

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,22 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        char[] str = st.nextToken().toCharArray();
-        char chr = st.nextToken().charAt(0);
-        int[] answer = new int[str.length];
+        String str = br.readLine()+" ";
+        StringBuilder sb = new StringBuilder();
 
-        int cnt = 1000;
-        for(int i = 0; i < str.length; i++) {
-            if(str[i] == chr) {
-                cnt = 0;
-                answer[i] = 0;
-            } else {
-                cnt += 1;
-                answer[i] = cnt;
-            }
-        }
-        cnt = 1000;
-        for (int i = str.length-1; i >= 0; i--) {
-            if(str[i] == chr) cnt = 0;
+        int cnt = 1;
+        for(int i = 0; i < str.length()-1; i++) {
+            if(str.charAt(i) == str.charAt(i+1))
+                cnt++;
             else {
-                cnt += 1;
-                answer[i] = Math.min(cnt, answer[i]);
+                sb.append(str.charAt(i));
+                if(cnt > 1) sb.append(cnt);
+                cnt = 1;
             }
         }
-        for(int i = 0; i < answer.length; i++) {
-            System.out.print(answer[i] + " ");
-        }
+
+        System.out.println(sb);
     }
 }
