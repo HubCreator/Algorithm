d1d86609829005f5161014d6f78ddc0fb67022f8
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-29 23:15:33 +0900

    이진수 -> 십진수 변환 (replace 사용)

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,18 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine()+" ";
-        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        StringBuilder res = new StringBuilder();
 
         int cnt = 1;
-        for(int i = 0; i < str.length()-1; i++) {
-            if(str.charAt(i) == str.charAt(i+1))
-                cnt++;
-            else {
-                sb.append(str.charAt(i));
-                if(cnt > 1) sb.append(cnt);
-                cnt = 1;
-            }
+        for(int i = 0; i < n; i++) {
+            String tmp = str.substring(7*(cnt-1), 7*cnt)
+                    .replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2); // 2진수를 10진수로 변환
+            res.append((char)num);
+            cnt++;
         }
-
-        System.out.println(sb);
+        System.out.println(res);
     }
 }
