81d80c03ea17324fbbbd4b2f6205ee72312500a8
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-04-18 23:34:15 +0900

    공통 원소 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,40 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size1 = Integer.parseInt(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        int[] arr1 = new int[size1];
+        int size2 = Integer.parseInt(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int[] arr2 = new int[size2];
 
-        int arr_size1 = Integer.parseInt(br.readLine());
-        int[] arr1 = new int[arr_size1];
-        StringTokenizer st1 = new StringTokenizer(br.readLine()," ");
-        int arr_size2 =Integer.parseInt(br.readLine());
-        int[] arr2 = new int[arr_size2];
-        StringTokenizer st2 = new StringTokenizer(br.readLine()," ");
-
-        for(int i = 0; i < arr1.length; i++)
+        for(int i = 0; i < size1; i++)
             arr1[i] = Integer.parseInt(st1.nextToken());
-        for(int i = 0; i < arr2.length; i++)
+        for(int i = 0; i < size2; i++)
             arr2[i] = Integer.parseInt(st2.nextToken());
 
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
         ArrayList<Integer> answer = new ArrayList<>();
         int p1 = 0, p2 = 0;
-        while(p1 < arr_size1 && p2 < arr_size2) {
-            if(arr1[p1] > arr2[p2]) {
-                answer.add(arr2[p2++]);
-            } else {
-                answer.add(arr1[p1++]);
-            }
-        }
-        while(p1 < arr_size1) answer.add(arr1[p1++]);
-        while(p2 < arr_size2) answer.add(arr2[p2++]);
 
-        answer.forEach(x -> System.out.print(x + " "));
+        while(p1 < size1 && p2 < size2) {
+            if(arr1[p1] == arr2[p2]) {
+                answer.add(arr1[p1++]);
+                p2++;
+            }
+            else if(arr1[p1] < arr2[p2])
+                p1++;
+            else
+                p2++;
+        }
+
+        answer.stream().forEach(x -> System.out.print(x + " "));
     }
 }
