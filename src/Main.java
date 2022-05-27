3339019f853cdae311565b4c66809b0b21bbdebe
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-05-27 22:56:46 +0900

    특정 문자열 뒤집기

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
-import java.io.*;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String tmp = br.readLine();
-        char answer = ' ';
-
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char x : tmp.toCharArray())
-            map.put(x, map.getOrDefault(x, 0)+1); // 해당 value가 없다면 0 반환
-
-        int max = Integer.MIN_VALUE;
-        for(char key : map.keySet()) { // keySet() -> 존재하는 키들을 모두 순회
-            /*System.out.println(key + " " + map.get(key));*/
-            if(map.get(key) > max) {
-                max = map.get(key);
-                answer = key;
+        char[] chars = br.readLine().toCharArray();
+        List<Character> list = List.of('#', '!', '*', '@');
+        int length = chars.length;
+        for (int i = 0; i < length / 2; i++) {
+            if (!list.contains(chars[i])) {
+                char tmp = chars[i];
+                chars[i] = chars[length - 1 - i];
+                chars[length - 1 - i] = tmp;
             }
         }
-
-        System.out.println(answer);
+        for (Character character : chars) {
+            System.out.print(character);
+        }
     }
 }
