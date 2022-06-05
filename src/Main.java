bea62f617e9765f027133121549cef11e2f9401a
HubCreator <chocomilk8604@gmail.com>
2022-06-05 13:58:00 +0900

    피보나치 for

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,20 @@
-import java.awt.print.Pageable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int[] a = new int[n];
-        int[] b = new int[n];
-        String answer = "";
+        int[] result = new int[n];
 
         for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st1.nextToken());
-            b[i] = Integer.parseInt(st2.nextToken());
+            if(i == 0 || i == 1) result[i] = 1;
+            else
+                result[i] = result[i - 1] + result[i - 2];
         }
-
-        for (int i = 0; i < n; i++) {
-            if (a[i] == b[i]) answer += "D";
-            else if (a[i] == 1 && b[i] == 3) answer += "A";
-            else if (a[i] == 2 && b[i] == 1) answer += "A";
-            else if (a[i] == 3 && b[i] == 2) answer += "A";
-            else answer += "B";
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
         }
-        System.out.println(answer);
-
     }
 }
