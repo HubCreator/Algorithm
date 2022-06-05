fc0182a495af5a273edb926bdea27ff374620329
HubCreator <chocomilk8604@gmail.com>
2022-06-05 14:36:27 +0900

    소수의 갯수 출력하기, 에라토스테네스 체

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+// 소수의 갯수 출력하기
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] result = new int[n];
+        int[] arr = new int[n + 1];
+        int result = 0;
 
-        for (int i = 0; i < n; i++) {
-            if(i == 0 || i == 1) result[i] = 1;
-            else
-                result[i] = result[i - 1] + result[i - 2];
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 0) {
+                result++;
+                for (int j = i; j <= n; j += i) arr[j] = 1;
+            }
         }
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
-        }
+        System.out.println(result);
     }
 }
