113e1209bf1e021bf4f048a9da418af654b0392e
HubCreator <chocomilk8604@gmail.com>
2022-06-08 14:26:39 +0900

    뒤집은 소수

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
-// 소수의 갯수 출력하기
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n + 1];
-        int result = 0;
-
-        for (int i = 2; i <= n; i++) {
-            if (arr[i] == 0) {
-                result++;
-                for (int j = i; j <= n; j += i) arr[j] = 1;
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        List<Integer> result = new ArrayList<>();
+        StringBuilder[] sb = new StringBuilder[n];
+        int length = st.countTokens();
+        for (int i = 0; i < length; i++) {
+            sb[i] = new StringBuilder(st.nextToken()).reverse();
         }
-        System.out.println(result);
+        for (StringBuilder s : sb) {
+            int t = Integer.parseInt(String.valueOf(s));
+            int cnt = 0;
+            for (int j = 1; j <= t; j++) {
+//                if(cnt > 2) break;
+                if (t % j == 0) cnt++;
+            }
+            if (cnt == 2) result.add(t);
+        }
+
+        for (Integer integer : result) {
+            System.out.print(integer + " ");
+        }
     }
 }
