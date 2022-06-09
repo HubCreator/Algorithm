d7913f6253e68aef1731f229613cf084ae44309a
HubCreator <chocomilk8604@gmail.com>
2022-06-09 13:06:25 +0900

    등수 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
+// 등수 구하기
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[n];
-        int length = st.countTokens();
-        int score = 0;
-        int tmp = 1;
-        for (int i = 0; i < length; i++) {
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < length; i++) {
-            if (arr[i] == 1) {
-                score += tmp++;
-            } else {
-                tmp = 1;
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if(arr[j] > arr[i]) cnt++;
             }
+            answer[i] = cnt;
         }
-        System.out.println(score);
+        for (int i : answer) {
+            System.out.print(i + " ");
+        }
     }
 }
