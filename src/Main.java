97e2fb963de5ba4ab16c985f22ffe1fbf1c8c29c
HubCreator <chocomilk8604@gmail.com>
2022-06-11 11:16:28 +0900

    최대 매출액 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
+// n일 중에서 k일 동안의 매출액 중 가장 높은 매출액
 public class Main {
-    // 공통 원소 오름차순 출력
-    public List<Integer> solution(int n1, int[] arr1, int n2, int[] arr2) {
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n1; i++) {
-            map.put(arr1[i], 1);
-        }
-        for (int i = 0; i < n2; i++) {
-            if (map.get(arr2[i]) == null) {
-                map.put(arr2[i], 1);
-            } else
-                map.put(arr2[i], map.get(arr2[i]) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 1) {
-                result.add(entry.getKey());
+    public int solution(int n, int k, int[] arr) {
+        int max = 0;
+        int target = 0;
+        for (int i = 0; i < n - k; i++) {
+            for (int j = i; j < i + k; j++) {
+                target += arr[j];
             }
+            max = Math.max(max, target);
+            target = 0;
         }
-
-        return result;
+        return max;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n1 = Integer.parseInt(br.readLine());
         StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        int n2 = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(st1.nextToken());
+        int k = Integer.parseInt(st1.nextToken());
         StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-        int[] arr1 = new int[n1];
-        int[] arr2 = new int[n2];
-        for (int i = 0; i < n1; i++) {
-            arr1[i] = Integer.parseInt(st1.nextToken());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st2.nextToken());
         }
-        for (int i = 0; i < n2; i++) {
-            arr2[i] = Integer.parseInt(st2.nextToken());
-        }
-
-        for (Integer x : T.solution(n1, arr1, n2, arr2)) {
-            System.out.print(x + " ");
-        }
+        System.out.println(T.solution(n, k, arr));;
     }
 }
