ba1b3c72755c9e0fc0790cc62541d4d23bf956c6
HubCreator <chocomilk8604@gmail.com>
2022-06-12 21:10:08 +0900

    최대 길이의 연속 부분 수열

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-// 연속된 자연수의 합
 public class Main {
-    private int solution(int n) {
-        int answer = 0, cnt = 1;
-        n--; // 1을 뺌
-        while (n > 0) {
-            cnt++;
-            n = n - cnt; // 2를 뺀 이후에 하나씩 loop를 돌며 뺌
-            if (n % cnt == 0) answer++; // cnt는 size와 같음
+    private int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if(arr[rt] == 0) cnt++;
+            while (cnt > k) {
+                if(arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt - lt + 1);
         }
         return answer;
     }
@@ -18,7 +20,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(T.solution(n));
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st1.nextToken());
+        int k = Integer.parseInt(st1.nextToken());
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        System.out.println(T.solution(n, k, arr));
     }
 }
