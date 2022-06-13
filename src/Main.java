6355adc7d06b05c264c58890dd5402acd3be7fb5
HubCreator <chocomilk8604@gmail.com>
2022-06-13 16:36:46 +0900

    K 번째 큰 수 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
-// 모든 아나그램 찾기 (해시, 투포인터, 슬라이딩윈도우), 대소문자 구분, 부분 문자열은 연속되어야 함
+// K 번째 큰 수 구하기
 public class Main {
-    private int solution(String str1, String str2) {
-        char[] chars1 = str1.toCharArray();
-        char[] chars2 = str2.toCharArray();
-        int answer = 0, lt = 0;
-        Map<Character, Integer> window = new HashMap<>();
-        Map<Character, Integer> target = new HashMap<>();
-
-
-        for (char c : chars2) {
-            window.put(c, window.getOrDefault(c, 0) + 1);
+    private int solution(int n, int k, int[] arr) {
+        TreeSet<Integer> tset = new TreeSet<>(Collections.reverseOrder()); // 내림차순 자동 정렬
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    tset.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
         }
-        for (int rt = 0; rt < chars2.length - 1; rt++) { // 초기 윈도우값
-            target.put(chars1[rt], target.getOrDefault(chars1[rt], 0) + 1);
+        int cnt = 0;
+        for (Integer x : tset) {
+            cnt++;
+            if(cnt == k) return x;
         }
-        for (int rt = chars2.length - 1; rt < chars1.length; rt++) {
-            target.put(chars1[rt], target.getOrDefault(chars1[rt], 0) + 1);
-            if (window.equals(target)) answer++;
-            target.put(chars1[lt], target.get(chars1[lt]) - 1);
-            if (target.get(chars1[lt]) == 0) target.remove(chars1[lt]);
-            lt++;
-        }
-        return answer;
+        return -1;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str1 = br.readLine();
-        String str2 = br.readLine();
-        System.out.println(T.solution(str1, str2));
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st1.nextToken());
+        int k = Integer.parseInt(st1.nextToken());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st2.nextToken());
+        }
+        System.out.println(T.solution(n, k, arr));
+        
     }
 }
