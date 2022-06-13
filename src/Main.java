b11e3ced2a8f0bc29531a9fbaa8bbe57b3738a18
HubCreator <chocomilk8604@gmail.com>
2022-06-13 16:10:02 +0900

    모든 아나그램 찾기 (해시, 투포인터, 슬라이딩윈도우)

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-class Main {
-    public List<Integer> solution(int n, int k, int[] arr) {
-        List<Integer> answer = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < k - 1; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+// 모든 아나그램 찾기 (해시, 투포인터, 슬라이딩윈도우), 대소문자 구분, 부분 문자열은 연속되어야 함
+public class Main {
+    private int solution(String str1, String str2) {
+        char[] chars1 = str1.toCharArray();
+        char[] chars2 = str2.toCharArray();
+        int answer = 0, lt = 0;
+        Map<Character, Integer> window = new HashMap<>();
+        Map<Character, Integer> target = new HashMap<>();
+
+
+        for (char c : chars2) {
+            window.put(c, window.getOrDefault(c, 0) + 1);
         }
-        int lt = 0;
-        for (int rt = k - 1; rt < n; rt++) {
-            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
-            answer.add(map.size());
-            map.put(arr[lt], map.get(arr[lt]) - 1);
-            if (map.get(arr[lt]) == 0) map.remove(arr[lt]);
+        for (int rt = 0; rt < chars2.length - 1; rt++) { // 초기 윈도우값
+            target.put(chars1[rt], target.getOrDefault(chars1[rt], 0) + 1);
+        }
+        for (int rt = chars2.length - 1; rt < chars1.length; rt++) {
+            target.put(chars1[rt], target.getOrDefault(chars1[rt], 0) + 1);
+            if (window.equals(target)) answer++;
+            target.put(chars1[lt], target.get(chars1[lt]) - 1);
+            if (target.get(chars1[lt]) == 0) target.remove(chars1[lt]);
             lt++;
         }
         return answer;
@@ -25,18 +33,8 @@ class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st1.nextToken());
-        int k = Integer.parseInt(st1.nextToken());
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
-        }
-
-        List<Integer> solution = T.solution(n, k, arr);
-        for (Integer x : solution) {
-            System.out.print(x + " ");
-        }
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        System.out.println(T.solution(str1, str2));
     }
 }
