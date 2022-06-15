cad1a356583f35ab07084029763b8e424553f380
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-15 19:37:01 +0900

    쇠 막대기 커팅

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
 
-// 크레인 인형뽑기
+// 쇠 막대기 커팅
 public class Main {
-    private int solution(int[][] board, int[] moves) {
-        Stack<Integer> bucket = new Stack<>();
-        int answer = 0;
-
-        for (int pos : moves) {
-            for (int i = 0; i < board.length; i++) {
-                if (board[i][pos - 1] != 0) { // 0이 아닐때지 내려옴
-                    int target = board[i][pos - 1];
-                    board[i][pos - 1] = 0; // board의 해당 위치의 값을 0으로 바꿈
-                    if (!bucket.isEmpty() && target == bucket.peek()) {
-                        bucket.pop();
-                        answer += 2;
-                    }
-                    else
-                        bucket.push(target);
-                    break;
+    private int solution(char[] chars) {
+        int answer = 0, pt = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < chars.length - 1; i++) {
+            char lt = chars[i];
+            char rt = chars[i + 1];
+            if (lt == '(') {
+                stack.push(lt);
+                if (rt == ')') {
+                    stack.pop();
+                    answer += stack.size();
+                    pt = i + 1;
                 }
+            } else { // lt == ')'
+                if (i == pt) continue;
+                stack.pop();
+                answer++;
             }
         }
         return answer;
     }
 
     public static void main(String[] args) throws IOException {
-        Main t = new Main();
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer[] stArr = new StringTokenizer[n];
-        int[][] board = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            stArr[i] = new StringTokenizer(br.readLine(), " ");
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(stArr[i].nextToken());
-            }
-        }
-        int moveCnt = Integer.parseInt(br.readLine());
-        int[] moves = new int[moveCnt];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < moves.length; i++) {
-            moves[i] = Integer.parseInt(st.nextToken());
-        }
-        System.out.println(t.solution(board, moves));
+        String str = br.readLine() + " ";
+        System.out.println(T.solution(str.toCharArray()));
     }
 }
