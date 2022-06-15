72272883d37c8a7c3b386d7c60f35b4db3189911
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-15 19:39:06 +0900

    후위 연산

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-// 쇠 막대기 커팅
+// 후위 연산
 public class Main {
-    private int solution(char[] chars) {
-        int answer = 0, pt = 0;
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < chars.length - 1; i++) {
-            char lt = chars[i];
-            char rt = chars[i + 1];
-            if (lt == '(') {
-                stack.push(lt);
-                if (rt == ')') {
-                    stack.pop();
-                    answer += stack.size();
-                    pt = i + 1;
-                }
-            } else { // lt == ')'
-                if (i == pt) continue;
-                stack.pop();
-                answer++;
+    private int solution(String s) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            String t = String.valueOf(s.charAt(i));
+            switch (t) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b - a);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    stack.push(stack.pop() / stack.pop());
+                    break;
+                default:
+                    stack.push(Integer.parseInt(t));
             }
         }
-        return answer;
+        return stack.pop();
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine() + " ";
-        System.out.println(T.solution(str.toCharArray()));
+        String s = br.readLine();
+        System.out.println(T.solution(s));
     }
 }
