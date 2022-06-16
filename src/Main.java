86dde7f3490df1276f002fafd8db6b86039e2487
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-16 12:13:08 +0900

    LRU 구현 (Deque)

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
-// 삽입 정렬
+// LRU 구현
 public class Main {
-    private int[] solution(int n, int[] arr) {
-        for (int i = 1; i < n; i++) {
-            int tmp = arr[i], j;
-            for (j = i - 1; j >= 0; j--) {              // i기준 왼쪽 요소들을 체크
-                if(arr[j] > tmp) arr[j + 1] = arr[j];   // i번째 요소보다 크면 오른쪽으로 한 칸 밀어라
-                else break;
+    private Deque<Integer> solution(int s, int n, int[] arr) {
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (deque.size() < s) {
+                if (!deque.contains(arr[i]))
+                    deque.offerFirst(arr[i]);
+                else {
+                    if (deque.remove(arr[i])) deque.offerFirst(arr[i]);
+                }
+            } else {
+                if (!deque.contains(arr[i])) {
+                    deque.pollLast();
+                    deque.offerFirst(arr[i]);
+                } else {
+                    if (deque.remove(arr[i])) deque.offerFirst(arr[i]);
+                }
             }
-            arr[j + 1] = tmp;
         }
-        return arr;
+        return deque;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int s = Integer.parseInt(st1.nextToken());
+        int n = Integer.parseInt(st1.nextToken());
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st2.nextToken());
         }
-        for (int i : T.solution(n, arr)) {
-            System.out.print(i + " ");
-        }
+        for (Integer x : T.solution(s, n, arr)) {
+            System.out.print(x + " ");
+        };
     }
 }
