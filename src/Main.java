91f10e760e5a4759075ad24358cb752f8f12ee99
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-16 13:16:24 +0900

    배열 깊은복사, 정렬 비교

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
-// 중복 확인
+// 배열 깊은복사, 정렬 비교
 public class Main {
-    private boolean solution(int n, int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
+    private List<Integer> solution(int n, int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
         for (int i = 0; i < n; i++) {
-            if (!map.containsKey(arr[i])) {
-                map.put(arr[i], 1);
-            } else
-                return false;
+            if(arr[i] != sorted[i])
+                answer.add(i + 1);
         }
-        return true;
+        return answer;
     }
+
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[n];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(T.solution(n, arr) ? "U" : "D");
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
     }
 }
