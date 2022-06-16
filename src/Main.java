ac44461459ce7e60095b872c1643d3d65acf9534
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-16 13:32:10 +0900

    좌표 정렬

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
-// 배열 깊은복사, 정렬 비교
-public class Main {
-    private List<Integer> solution(int n, int[] arr) {
-        List<Integer> answer = new ArrayList<>();
-        int[] sorted = arr.clone();
-        Arrays.sort(sorted);
-
-        for (int i = 0; i < n; i++) {
-            if(arr[i] != sorted[i])
-                answer.add(i + 1);
+// 좌표 정렬
+public  class Main {
+    static class Coord implements Comparable<Coord> {
+        public int x, y;
+        Coord(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        return answer;
+        @Override
+        public int compareTo(Coord o) {
+            if(this.x == o.x) return this.y - o.y;
+            else return this.x - o.x;
+        }
     }
-
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[n];
+        List<Coord> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            list.add(new Coord(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
-
-        for (int x : T.solution(n, arr)) {
-            System.out.print(x + " ");
-        }
+        Collections.sort(list);
+        list.forEach(o -> System.out.println(o.x + " " + o.y));
     }
 }
