22904ce177eea6e2398f27340e793774e25616a1
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-16 13:46:34 +0900

    이분 탐색

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 좌표 정렬
-public  class Main {
-    static class Coord implements Comparable<Coord> {
-        public int x, y;
-        Coord(int x, int y) {
-            this.x = x;
-            this.y = y;
+// 이분 탐색
+public class Main {
+    private int solution(int n, int m, int[] arr) {
+        int lt = 0, rt = n - 1, answer = 0;
+        Arrays.sort(arr);
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+            else if (arr[mid] > m) rt = mid - 1;
+            else lt = mid + 1;
         }
-        @Override
-        public int compareTo(Coord o) {
-            if(this.x == o.x) return this.y - o.y;
-            else return this.x - o.x;
-        }
+        return answer;
     }
     public static void main(String[] args) throws IOException {
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        List<Coord> list = new ArrayList<>();
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st1.nextToken());
+        int m = Integer.parseInt(st1.nextToken());
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            list.add(new Coord(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            arr[i] = Integer.parseInt(st2.nextToken());
         }
-        Collections.sort(list);
-        list.forEach(o -> System.out.println(o.x + " " + o.y));
+
+        System.out.println(T.solution(n, m, arr));
     }
 }
