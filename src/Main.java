0280a9ae3276c62ffe006c72ab5945dca920760d
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-16 09:40:05 +0900

    선택 정렬

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.StringTokenizer;
 
-// 후위 연산
+// 선택정렬
 public class Main {
-    private int solution(String s) {
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            String t = String.valueOf(s.charAt(i));
-            switch (t) {
-                case "+":
-                    stack.push(stack.pop() + stack.pop());
-                    break;
-                case "-":
-                    int a = stack.pop();
-                    int b = stack.pop();
-                    stack.push(b - a);
-                    break;
-                case "*":
-                    stack.push(stack.pop() * stack.pop());
-                    break;
-                case "/":
-                    stack.push(stack.pop() / stack.pop());
-                    break;
-                default:
-                    stack.push(Integer.parseInt(t));
+    private int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
             }
         }
-        return stack.pop();
+        return arr;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        System.out.println(T.solution(s));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] solution = T.solution(n, arr);
+        for (int i : solution) {
+            System.out.print(i + " ");
+        }
     }
 }
