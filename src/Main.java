6e1e97f6c86aca02524269e043ba977660bff4e9
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-18 17:28:07 +0900

    특정 문자열 뒤집기

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-// 이분 탐색
 public class Main {
-    private int solution(int n, int m, int[] arr) {
-        int lt = 0, rt = n - 1, answer = 0;
-        Arrays.sort(arr);
-        while (lt <= rt) {
-            int mid = (lt + rt) / 2;
-            if (arr[mid] == m) {
-                answer = mid + 1;
-                break;
+    private char[] solution(char[] chars) {
+        for (int i = 0; i < chars.length / 2; i++) {
+            char tmp;
+            if (Character.isAlphabetic(chars[i])) {
+                tmp = chars[i];
+                chars[i] = chars[chars.length - 1 - i];
+                chars[chars.length - 1 - i] = tmp;
             }
-            else if (arr[mid] > m) rt = mid - 1;
-            else lt = mid + 1;
         }
-        return answer;
+        return chars;
     }
+
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st1.nextToken());
-        int m = Integer.parseInt(st1.nextToken());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
-        }
-
-        System.out.println(T.solution(n, m, arr));
+        StringBuilder sb = new StringBuilder(br.readLine());
+        System.out.println(T.solution(sb.toString().toCharArray()));
     }
 }
---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = -1;
+        Arrays.sort(A);
+        for (int i = 0; i < A.length; i++) {
+
+        }
+        return answer;
+    }
+}
