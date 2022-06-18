b8aa40c40b064e81afeb894d1e0acaa58d02bae1
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-18 20:53:57 +0900

    재귀 팩토리얼

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 재귀 -> 이진수 출력
+// 재귀 -> 팩토리얼
 public class Main {
-    private void DFS(int n) {
-        if(n == 0) return;
-        else {
-            DFS(n / 2);
-            System.out.print(n % 2);
-        }
+    private int DFS(int n) {
+        if(n == 1) return 1;
+        else return n * DFS(n - 1);
     }
     
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        T.DFS(n);
+        System.out.println(T.DFS(n));;
     }
 }
