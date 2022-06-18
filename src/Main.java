cfbfc3bddff18d42efafd1eb3f72be2bde88becc
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-18 21:29:53 +0900

    메모이제이션

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 재귀 -> 피보나치
+// 메모이제이션
 public class Main {
-    private int recursive(int n) {
-        if (n == 0 || n == 1) return 1;
-        else if (n == 2) return 2;
-        else return recursive(n - 2) + recursive(n - 1);
+    public static int[] fibo;
+
+    private int DFS(int n) {
+        if (fibo[n] > 0) return fibo[n]; // 값을 이미 구해놨으면 재귀를 돌지말고 바로 값 리턴
+        if (n == 1) return fibo[n] = 1;
+        else if (n == 2) return fibo[n] = 1;
+        else return fibo[n] = DFS(n - 2) + DFS(n - 1);
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            System.out.print(T.recursive(i) + " ");
+        fibo = new int[n + 1];
+        T.DFS(n);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibo[i] + " ");
         }
     }
 }
