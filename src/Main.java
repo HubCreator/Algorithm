6c4a3c1c46c7833cef2fba75606bd3588b79287a
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-18 20:51:17 +0900

    이진수 출력

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 재귀
+// 재귀 -> 이진수 출력
 public class Main {
-    private void solution(int n) {
+    private void DFS(int n) {
         if(n == 0) return;
-        solution(n - 1);
-        System.out.print(n + " ");
+        else {
+            DFS(n / 2);
+            System.out.print(n % 2);
+        }
     }
+    
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        T.solution(n);
+        T.DFS(n);
     }
 }
