2bebb3ed009e60e9ae3db71af6c2c7992be7a119
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-18 21:06:00 +0900

    재귀 피보나치

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 재귀 -> 팩토리얼
+// 재귀 -> 피보나치
 public class Main {
-    private int DFS(int n) {
-        if(n == 1) return 1;
-        else return n * DFS(n - 1);
+    private int recursive(int n) {
+        if (n == 0 || n == 1) return 1;
+        else if (n == 2) return 2;
+        else return recursive(n - 2) + recursive(n - 1);
     }
-    
+
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(T.DFS(n));;
+        for (int i = 0; i < n; i++) {
+            System.out.print(T.recursive(i) + " ");
+        }
     }
 }
