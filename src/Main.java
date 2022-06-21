4524b9ef89ae8587a8c6833325cb8480121a1182
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-21 22:50:51 +0900

    합이 같은 부분집합 구하기. (DFS)

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+// 합이 같은 부분집합 (DFS)
 public class Main {
-    int[] dis = {1, -1, 5};
-    int[] ch;
-    Queue<Integer> queue = new LinkedList<>();
-    public int BFS(int s, int e) {
-        ch = new int[10001];
-        ch[s] = 1;
-        queue.offer(s);
-        int level = 0;
-        while (!queue.isEmpty()) {
-            int len = queue.size();
-            for (int i = 0; i < len; i++) {
-                Integer poll = queue.poll();
-                for (int j = 0; j < dis.length; j++) {
-                    int next = poll + dis[j];
-                    if (next == e) return level + 1;
-                    if (next >= 1 && next <= 10000 && ch[next] == 0) {
-                        ch[next] = 1;
-                        queue.offer(next);
-                    }
+    static int arr[];
+    static int max;
+    static int n;
+    static int ch[];
+    static boolean answer = false;
+
+    private int DFS(int L) {
+        if (L == n + 1) { // 깊이가 하나 더 초과한다면..
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) {
+                    sum += arr[i];
                 }
             }
-            level++;
+            if (sum * 2 == max) {
+                answer = true;
+                return 1;
+            }
+        } else {
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
         }
         return 0;
     }
@@ -33,9 +36,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int s = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        System.out.println(T.BFS(s, e));
+        arr = new int[n + 1];
+        max = Arrays.stream(arr).max().getAsInt();
+        for (int i = 1; i <= arr.length - 1; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        ch = new int[n + 1];
+        T.DFS(1);
+
+        if (answer) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
