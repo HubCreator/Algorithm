bb9bfd401bb6c0ed749737b872c9121cb7d06556
HubCreator <chocomilk8604@gmail.com>
2022-06-23 15:42:16 +0900

    말단 노드까지의 최단거리 DFS 수정

--- src/Main.java
+++ src/Main.java
@@ -11,38 +11,23 @@ class Node {
 }
 
 public class Main {
-    static int level = 1;
-    static int[] ltArr = new int[1000];
-    static int[] rtArr = new int[1000];
-
-    private void DFS(Node root) {
-        if (root.lt != null) {
-            ltArr[level++] = 1;
-            DFS(root.lt);
-        }
-        if (root.rt != null) {
-            rtArr[level++] = 1;
-            DFS(root.rt);
+    Node root;
+    private int DFS(int level, Node root) {
+        if(root.lt == null && root.rt == null) return level;
+        else {
+            return Math.min(DFS(level + 1, root.lt), DFS(level + 1, root.rt));
         }
     }
 
     public static void main(String[] args) {
         Main T = new Main();
-        Node root = new Node(1);
-        root.lt = new Node(2);
-        root.rt = new Node(3);
+        T.root = new Node(1);
+        T.root.lt = new Node(2);
+        T.root.rt = new Node(3);
 
-        root.lt.lt = new Node(4);
-        root.lt.rt = new Node(5);
+        T.root.lt.lt = new Node(4);
+        T.root.lt.rt = new Node(5);
 
-        T.DFS(root);
-        int answer = -1;
-        for (int i = 1; i < ltArr.length; i++) {
-            if (ltArr[i] == 0 || rtArr[i] == 0) {
-                answer = i;
-                break;
-            }
-        }
-        System.out.println(answer);
+        System.out.println(T.DFS(0, T.root));
     }
 }
