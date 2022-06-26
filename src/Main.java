7faeb443ab6b59166664e3c747cb3dcc3858d0ec
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-06-26 21:58:11 +0900

    그리디 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    static int[] score, time, ch;
-    static int n, m;
-    static List<Integer> answer = new ArrayList<>();
+class Person implements Comparable<Person>{
+    int w;
+    int h;
+    Person(int w, int h) {
+        this.w = w;
+        this.h = h;
+    }
 
-    private void DFS(int L) {
-        if (L == n + 1) {
-            int scoreSum = 0, timeSum = 0;
-            for (int i = 1; i <= n; i++) {
-                if (ch[i] == 1) {
-                    scoreSum += score[i];
-                    timeSum += time[i];
-                }
+    @Override
+    public int compareTo(Person o) {
+        // o : 자식, this : 부모 (부모가 더 크다고 생각하자. 부모 아래에 자식 있음)
+        return o.h - this.h; // 자식에서 부모를 뺐으므로 내림차순 정렬 (음수)
+    }
+}
+
+// 정렬 해놓고 해결해 나감
+public class Main {
+    private int solution(int n, List<Person> people) {
+        int cnt = 0, max = Integer.MIN_VALUE;
+        Collections.sort(people); // overriding된 compareTo를 활용한 정렬
+        for (Person person : people) {
+            if (person.w > max) {
+                max = person.w;
+                cnt++;
             }
-            if (timeSum <= m)
-                answer.add(scoreSum);
-        } else {
-            ch[L] = 1;
-            DFS(L + 1);
-            ch[L] = 0;
-            DFS(L + 1);
         }
+        return cnt;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken()); // 문제의 갯수
-        m = Integer.parseInt(st.nextToken()); // 제한 시간
-        StringTokenizer[] stArr = new StringTokenizer[n + 1];
-        score = new int[n + 1];
-        time = new int[n + 1];
-        ch = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            stArr[i] = new StringTokenizer(br.readLine(), " ");
-            int s = Integer.parseInt(stArr[i].nextToken());
-            int t = Integer.parseInt(stArr[i].nextToken());
-            score[i] = s;
-            time[i] = t;
+        int n = Integer.parseInt(br.readLine());
+        List<Person> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            list.add(new Person(h, w));
         }
-        T.DFS(1);
-        System.out.println(Collections.max(answer));
+        System.out.println(T.solution(n, list));
     }
 }
