fc2dbde176a43b6fb08468782c14b1cf69dc7c07
HubCreator <chocomilk8604@gmail.com>
2022-06-26 16:28:27 +0900

    DFS 활용2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int c, n;
-    static int[] arr, ch;
+    static int[] score, time, ch;
+    static int n, m;
     static List<Integer> answer = new ArrayList<>();
 
     private void DFS(int L) {
         if (L == n + 1) {
-            int sum = 0;
+            int scoreSum = 0, timeSum = 0;
             for (int i = 1; i <= n; i++) {
-                if (ch[i] == 1)
-                    sum += arr[i];
+                if (ch[i] == 1) {
+                    scoreSum += score[i];
+                    timeSum += time[i];
+                }
             }
-            if (sum < c) answer.add(sum);
+            if (timeSum <= m)
+                answer.add(scoreSum);
         } else {
             ch[L] = 1;
             DFS(L + 1);
@@ -31,12 +31,18 @@ public class Main {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        c = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        arr = new int[n + 1];
+        n = Integer.parseInt(st.nextToken()); // 문제의 갯수
+        m = Integer.parseInt(st.nextToken()); // 제한 시간
+        StringTokenizer[] stArr = new StringTokenizer[n + 1];
+        score = new int[n + 1];
+        time = new int[n + 1];
         ch = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            stArr[i] = new StringTokenizer(br.readLine(), " ");
+            int s = Integer.parseInt(stArr[i].nextToken());
+            int t = Integer.parseInt(stArr[i].nextToken());
+            score[i] = s;
+            time[i] = t;
         }
         T.DFS(1);
         System.out.println(Collections.max(answer));
