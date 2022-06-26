eb32cedd5f3015b2fb7c809f77a2a21a1c1648af
HubCreator <chocomilk8604@gmail.com>
2022-06-26 14:13:25 +0900

    경로 탐색 (인접 리스트)

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-// 경로 탐색 (인접 행렬)
 public class Main {
-    static int n, m;
-    static int[][] graph;
-    static int[] check;
-    static int answer = 0;
+    public static List<List<Integer>> list;
+    public static int v, e;
+    public static int ch[];
+    public static int answer = 0;
 
     private void DFS(int val) {
-        if (val == n) {
+        if (val == v) {
             answer++;
         } else {
-            for (int i = 1; i <= n; i++) {
-                if (graph[val][i] == 1 && check[i] == 0) {
-                    check[i] = 1;
-                    DFS(i);
-                    check[i] = 0;
+            for (int t : list.get(val)) {
+                if (ch[t] == 0) {
+                    ch[t] = 1;
+                    DFS(t);
+                    ch[t] = 0;
                 }
             }
         }
@@ -26,18 +27,24 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
+        list = new ArrayList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        graph = new int[n + 1][n + 1];
-        check = new int[n + 1];
-        StringTokenizer[] stArr = new StringTokenizer[m + 1];
-        for (int i = 1; i <= m; i++) {
-            stArr[i] = new StringTokenizer(br.readLine(), " ");
-            graph[Integer.parseInt(stArr[i].nextToken())][Integer.parseInt(stArr[i].nextToken())] = 1;
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        ch = new int[v + 1];
+        for (int i = 1; i <= v; i++) {
+            list.add(new ArrayList<>(i));
         }
-        check[1] = 1;
+        StringTokenizer[] stArr = new StringTokenizer[e];
+        for (int i = 0; i < e; i++) {
+            stArr[i] = new StringTokenizer(br.readLine(), " ");
+            int _v = Integer.parseInt(stArr[i].nextToken());
+            int _e = Integer.parseInt(stArr[i].nextToken());
+            list.get(_v).add(_e);
+        }
+
+        ch[1] = 1;
         T.DFS(1);
         System.out.println(answer);
     }
