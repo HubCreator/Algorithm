cb9935a84098e466fdc265a032dd29f9bd08040a
HubCreator <chocomilk8604@gmail.com>
2022-06-27 12:08:11 +0900

    거스름 돈 걸러주기 DFS

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-// 중복 순열 구하기
+// 거스름 돈 걸러주기
 public class Main {
     static int n, m;
+    static int answer = Integer.MAX_VALUE;
+    static Integer[] arr;
     static int[] ch;
 
-    private void DFS(int L) {
-        if (L == m) {
-            for (int x : ch) System.out.print(x + " ");
-            System.out.println();
+    private void DFS(int L, int sum) {
+        if(sum > m || L >= answer) return;
+        if (sum == m) {
+            answer = Math.min(answer, L);
         } else {
-            for (int i = 1; i <= n; i++) {
-                ch[L] = i;
-                DFS(L + 1);
+            for (int i = 0; i < n; i++) {
+                DFS(L + 1, sum + arr[i]);
             }
         }
     }
@@ -23,10 +26,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        ch = new int[m];
-        T.DFS(0);
+        arr = new Integer[n];
+        ch = new int[n + 1];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr, Collections.reverseOrder());
+        m = Integer.parseInt(br.readLine());
+        T.DFS(0, 0);
+        System.out.println(answer);
     }
 }
