6e8e82d04658be49048597d630f946148040c84d
HubCreator <chocomilk8604@gmail.com>
2022-06-29 13:54:05 +0900

    미로 최단경로 찾기 BFS

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
+
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Main {
     static int BOARD_SIZE = 7;
-    static int[][] board = new int[BOARD_SIZE][BOARD_SIZE];
-    static int[][] ch = new int[BOARD_SIZE][BOARD_SIZE];
-    static int answer = 0;
+    static int[][] board = new int[BOARD_SIZE + 1][BOARD_SIZE + 1];
+    static int[][] dis = new int[BOARD_SIZE + 1][BOARD_SIZE + 1];
+    int[] _x = {-1, 0, 1, 0};
+    int[] _y = {0, 1, 0, -1};
 
-    private void DFS(int x, int y) {
-        if (x == BOARD_SIZE - 1 && y == BOARD_SIZE - 1) {
-            answer++;
-        } else {
-            // 상하좌우 방향으로 체크
-            if (x - 1 > 0 && ch[x - 1][y] == 0 && board[x - 1][y] == 0) {
-                ch[x - 1][y] = 1;
-                DFS(x - 1, y);
-                ch[x - 1][y] = 0;
-            }
-            if (x + 1 < BOARD_SIZE && ch[x + 1][y] == 0 && board[x + 1][y] == 0) {
-                ch[x + 1][y] = 1;
-                DFS(x + 1, y);
-                ch[x + 1][y] = 0;
-            }
-            if (y - 1 > 0 && ch[x][y - 1] == 0 && board[x][y - 1] == 0) {
-                ch[x][y - 1] = 1;
-                DFS(x, y - 1);
-                ch[x][y - 1] = 0;
-            }
-            if (y + 1 < BOARD_SIZE && ch[x][y + 1] == 0 && board[x][y + 1] == 0) {
-                ch[x][y + 1] = 1;
-                DFS(x, y + 1);
-                ch[x][y + 1] = 0;
+    private void BFS(int x, int y) {
+        Queue<Point> queue = new LinkedList<>();
+        board[x][y] = 1;
+        queue.offer(new Point(x, y));
+        while (!queue.isEmpty()) {
+            Point poll = queue.poll();
+            for (int j = 0; j < 4; j++) {
+                int nx = poll.x + _x[j];
+                int ny = poll.y + _y[j];
+                if ((nx >= 1 && nx <= BOARD_SIZE && ny >= 1 && ny <= BOARD_SIZE) // board의 범위 내에 있는지 확인
+                        && board[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                    queue.offer(new Point(nx, ny));
+                    dis[nx][ny] = dis[poll.x][poll.y] + 1;
+                }
             }
         }
     }
@@ -40,11 +43,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int i = 1; i <= BOARD_SIZE; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < BOARD_SIZE; j++) board[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= BOARD_SIZE; j++) board[i][j] = Integer.parseInt(st.nextToken());
         }
-        T.DFS(0, 0); // 0, 0에서 출발
-        System.out.println(answer);
+
+        T.BFS(1, 1);
+        if (dis[BOARD_SIZE][BOARD_SIZE] == 0) System.out.println(-1);
+        else System.out.println(dis[BOARD_SIZE][BOARD_SIZE]);
     }
 }
