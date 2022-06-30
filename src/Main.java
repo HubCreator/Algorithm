b9f9bd66a05457c752ec804a7d4c047b80b489d4
HubCreator <chocomilk8604@gmail.com>
2022-06-30 15:29:50 +0900

    계단 오르기 동적계획

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-
-class Time implements Comparable<Time> {
-    int time;
-    char status;
-
-    Time(int time, char status) {
-        this.time = time;
-        this.status = status;
-    }
-
-    @Override
-    public int compareTo(Time o) {
-        if (this.time == o.time) return this.status - o.status;
-        else return this.time - o.time; // 끝나는 시간으로 정렬
-    }
-}
 
 public class Main {
-    private int solution(int n, List<Time> list) {
-        Collections.sort(list);
-        int answer = Integer.MIN_VALUE;
-        int cnt = 0;
-        for (Time time : list) {
-            if(time.status == 's') cnt++;
-            else cnt--;
-            answer = Math.max(answer, cnt);
+    static int[] dy;
+    private void solution(int n) {
+        if(n == 1) dy[n] = 1;
+        else if(n == 2) dy[n] = 2;
+        else {
+            dy[n] = dy[n - 2] + dy[n - 1];
         }
-        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Time> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            list.add(new Time(start, 's'));
-            list.add(new Time(end, 'e'));
+        dy = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            T.solution(i);
         }
-        System.out.println(T.solution(n, list));
+        System.out.println(dy[n]);
     }
 }
