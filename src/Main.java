39c0be67679a747984ee4585b8217cceb3e7c256
HubCreator <chocomilk8604@gmail.com>
2022-06-30 16:25:44 +0900

    최대 부분수열 동적계획

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dy;
+    static int[] arr, dy;
     private void solution(int n) {
-        if(n == 1) dy[n] = 1;
-        else if(n == 2) dy[n] = 2;
+        if(n == 0) dy[n] = 1;
         else {
-            dy[n] = dy[n - 2] + dy[n - 1];
+            int cnt = 1;
+            for (int i = 0; i < n; i++) {
+                if(arr[n] > arr[i]) cnt = Math.max(cnt, dy[i] + 1);
+            }
+            dy[n] = cnt;
         }
     }
 
@@ -16,10 +21,11 @@ public class Main {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        dy = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            T.solution(i);
-        }
-        System.out.println(dy[n]);
+        arr = new int[n];
+        dy = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) T.solution(i);
+        System.out.println(Arrays.stream(dy).max().getAsInt());;
     }
 }
