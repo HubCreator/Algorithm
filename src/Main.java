c0801251db0b5a95982e84a6a854faad7ae37195
HubCreator <chocomilk8604@gmail.com>
2022-07-01 17:55:01 +0900

    문자열 압축

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    private int[] solution(String str, char c) {
-        int[] answer = new int[str.length()];
-        Arrays.fill(answer, 10000);
-
-        int index = 10000;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == c) {
-                index = i;
-                answer[i] = 0;
-            } else {
-                answer[i] = Math.abs(index - i);
+    private String solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        int sum = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) sum++;
+            else {
+                if (sum > 1) {
+                    sb.append(sum);
+                    sb.append(str.charAt(i));
+                    sum = 1;
+                } else sb.append(str.charAt(i));
             }
         }
-
-        index = 10000;
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if (str.charAt(i) == c) {
-                index = i;
-                answer[i] = 0;
-            } else {
-                answer[i] = Math.min(Math.abs(index - i), answer[i]);
-            }
-        }
-        return answer;
+        return sb.toString();
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        String str = st.nextToken();
-        char c = st.nextToken().toCharArray()[0];
-        for (int x : T.solution(str, c)) System.out.print(x + " ");
+        String str = br.readLine() + " ";
+        System.out.println(T.solution(str));;
     }
 }
