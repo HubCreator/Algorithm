6318859226c5104bc909c64d60dd592275aa5c95
HubCreator <chocomilk8604@gmail.com>
2022-07-01 10:41:28 +0900

    섬의 갯수 구하기 BFS

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int[][] board, dis;
-    static int n;
-    int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
-    int[] dy = {1, -1, -1, 0, 1, 1, -1, 0};
+class Point {
+    int x, y;
 
-    private void DFS(int _x, int _y) {
-        for (int i = 0; i < 8; i++) { // 8 방향
-            int nx = _x + dx[i];
-            int ny = _y + dy[i];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[ny][nx] == 1) {
-                board[ny][nx] = 0;
-                DFS(nx, ny);
+    Point(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+}
+public class Main {
+    int[] dy = {1, -1, -1, 0, 1, 1, -1, 0};
+    int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
+    static int n;
+    static int[][] board;
+
+    private void BFS(int y, int x) {
+        Queue<Point> queue = new LinkedList<>();
+        board[y][x] = 0;
+        queue.offer(new Point(y, x));
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+            for (int i = 0; i < 8; i++) {
+                int ny = p.y + dy[i];
+                int nx = p.x + dx[i];
+                if (ny >= 0 && ny < n && nx >= 0 && nx < n && board[ny][nx] == 1) {
+                    board[ny][nx] = 0;
+                    queue.offer(new Point(ny, nx));
+                }
             }
         }
+    }
+
+    private int solution() {
+        int answer = 0;
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                if (board[y][x] == 1) {
+                    answer++;
+                    BFS(y, x);
+                }
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
@@ -25,26 +54,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         board = new int[n][n];
-        dis = new int[n][n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < n; j++) board[i][j] = Integer.parseInt(st.nextToken());
         }
-        int answer = 0;
-        boolean flag;
-        do {
-            flag = false;
-            for (int y = 0; y < n; y++) {
-                for (int x = 0; x < n; x++) {
-                    if (board[y][x] == 1) {
-                        board[y][x] = 0;
-                        T.DFS(x, y);
-                        answer++;
-                        flag = true;
-                    }
-                }
-            }
-        } while (flag);
-        System.out.println(answer);
+        System.out.println(T.solution());
     }
 }
