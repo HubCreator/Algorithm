e14501bdfba93a031f6ac7702769449d84c77d4c
HubCreator <chocomilk8604@gmail.com>
2022-07-01 22:32:32 +0900

    뒤집은 소수

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    private String solution(String str) {
-        StringBuilder sb = new StringBuilder();
-        int sum = 1;
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (str.charAt(i) == str.charAt(i + 1)) sum++;
-            else {
-                if (sum > 1) {
-                    sb.append(sum);
-                    sb.append(str.charAt(i));
-                    sum = 1;
-                } else sb.append(str.charAt(i));
-            }
+    private boolean isPrime(int v) {
+        if(v == 1) return false;
+        for (int i = 2; i < v; i++) {
+            if(v % i == 0) return false; // 나누어 떨어지는 것이 있으면 소수가 아님
         }
-        return sb.toString();
+        return true;
+    }
+
+    private List<Integer> solution(int n, int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) { // 소수인지 체크
+            if(isPrime(arr[i])) answer.add(arr[i]);
+        }
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine() + " ";
-        System.out.println(T.solution(str));;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder(st.nextToken());
+            arr[i] = Integer.parseInt(String.valueOf(sb.reverse()));
+        }
+        for (Integer x : T.solution(n, arr)) System.out.print(x + " ");
     }
 }
