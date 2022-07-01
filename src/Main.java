889f07d37fa1fcc8b4bb6382e543a09304224262
HubCreator <chocomilk8604@gmail.com>
2022-07-01 15:07:58 +0900

    최대 수입 스케줄 greedy

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Point {
-    int x, y;
+class Work implements Comparable<Work> {
+    int money;
+    int date;
 
-    Point(int y, int x) {
-        this.y = y;
-        this.x = x;
+    public Work(int money, int date) {
+        this.money = money;
+        this.date = date;
+    }
+
+    @Override
+    public int compareTo(Work o) {
+        return o.date - this.date;
     }
 }
 public class Main {
-    int[] dy = {1, -1, -1, 0, 1, 1, -1, 0};
-    int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
-    static int n;
-    static int[][] board;
-
-    private void BFS(int y, int x) {
-        Queue<Point> queue = new LinkedList<>();
-        board[y][x] = 0;
-        queue.offer(new Point(y, x));
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();
-            for (int i = 0; i < 8; i++) {
-                int ny = p.y + dy[i];
-                int nx = p.x + dx[i];
-                if (ny >= 0 && ny < n && nx >= 0 && nx < n && board[ny][nx] == 1) {
-                    board[ny][nx] = 0;
-                    queue.offer(new Point(ny, nx));
-                }
-            }
-        }
-    }
-
-    private int solution() {
+    private int solution(List<Work> list, int n) {
         int answer = 0;
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                if (board[y][x] == 1) {
-                    answer++;
-                    BFS(y, x);
-                }
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder()); // 큰 값의 우선순위가 제일 높게
+        Collections.sort(list); // 날짜 내림차순 정렬
+        int max = list.get(0).date;
+        int j = 0;
+        for (int i = max; i > 0; i--) {
+            for (; j < n; j++) {
+                if(list.get(j).date < i) break;
+                pQueue.offer(list.get(j).money);
             }
+            if(!pQueue.isEmpty()) answer += pQueue.poll();
         }
         return answer;
     }
@@ -52,12 +37,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        board = new int[n][n];
+        int n = Integer.parseInt(br.readLine());
+        List<Work> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < n; j++) board[i][j] = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            list.add(new Work(m, d));
         }
-        System.out.println(T.solution());
+        System.out.println(T.solution(list, n));
     }
 }
