0bcd3b2bbd0f4407692f4f914a90ec9fc8e20ccf
HubCreator <chocomilk8604@gmail.com>
2022-07-01 23:14:54 +0900

    격자판 최대합

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    private boolean isPrime(int v) {
-        if(v == 1) return false;
-        for (int i = 2; i < v; i++) {
-            if(v % i == 0) return false; // 나누어 떨어지는 것이 있으면 소수가 아님
+    private int solution(int n, int[][] board) {
+        int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += board[i][j];
+                sum2 += board[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
         }
-        return true;
-    }
 
-    private List<Integer> solution(int n, int[] arr) {
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < n; i++) { // 소수인지 체크
-            if(isPrime(arr[i])) answer.add(arr[i]);
+        sum1 = sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += board[i][i];
+            sum2 += board[n - i - 1][n - i - 1];
         }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
         return answer;
     }
 
@@ -26,12 +32,11 @@ public class Main {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int board[][] = new int[n][n];
         for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder(st.nextToken());
-            arr[i] = Integer.parseInt(String.valueOf(sb.reverse()));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) board[i][j] = Integer.parseInt(st.nextToken());
         }
-        for (Integer x : T.solution(n, arr)) System.out.print(x + " ");
+        System.out.println(T.solution(n, board));
     }
 }
