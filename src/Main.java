779ff45e85926b61806b4bc27b28474190e15e62
HubCreator <chocomilk8604@gmail.com>
2022-07-02 11:48:54 +0900

    두 배열 합치기

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    private int solution(int n, int[][] board) {
-        int answer = Integer.MIN_VALUE;
-        int sum1, sum2;
-        for (int i = 0; i < n; i++) {
-            sum1 = sum2 = 0;
-            for (int j = 0; j < n; j++) {
-                sum1 += board[i][j];
-                sum2 += board[j][i];
-            }
-            answer = Math.max(answer, sum1);
-            answer = Math.max(answer, sum2);
+    private List<Integer> solution(int[] arr1, int[] arr2) {
+        List<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < arr1.length && p2 < arr2.length) {
+            if (arr1[p1] < arr2[p2]) answer.add(arr1[p1++]);
+            else answer.add(arr2[p2++]);
         }
-
-        sum1 = sum2 = 0;
-        for (int i = 0; i < n; i++) {
-            sum1 += board[i][i];
-            sum2 += board[n - i - 1][n - i - 1];
-        }
-        answer = Math.max(answer, sum1);
-        answer = Math.max(answer, sum2);
-
+        while (p1 < arr1.length) answer.add(arr1[p1++]);
+        while (p2 < arr2.length) answer.add(arr2[p2++]);
         return answer;
     }
 
@@ -32,11 +22,13 @@ public class Main {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int board[][] = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < n; j++) board[i][j] = Integer.parseInt(st.nextToken());
-        }
-        System.out.println(T.solution(n, board));
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[m];
+        for (int i = 0; i < n; i++) arr1[i] = Integer.parseInt(st1.nextToken());
+        for (int i = 0; i < m; i++) arr2[i] = Integer.parseInt(st2.nextToken());
+        for (Integer x : T.solution(arr1, arr2)) System.out.print(x + " ");
     }
 }
