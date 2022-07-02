93df6623ed21784b06160332970eb8f7211772ac
HubCreator <chocomilk8604@gmail.com>
2022-07-02 14:41:16 +0900

    최대 매출 슬라이딩 윈도우

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private int solution(int n, int t, int[][] arr) {
-        int answer = 0;
-        for (int a = 0; a < n - 1; a++) {
-            for (int b = a + 1; b < n; b++) { // a 번째 학생과 b 번째 학생을 비교해나감
-                int st1 = arr[0][a]; // a 번째 학생
-                int st2 = arr[0][b]; // b 번째 학생
-                int st1Inx = 0, st2Inx = 0;
-                boolean flag = true;
-                for (int i = 1; i < t; i++) { // 첫 번째 테스트는 건너 뛰고, 각 테스트를 돔
-                    for (int j = 0; j < n; j++) { // a, b 학생이 각 테스트마다 몇 번째 인덱스에 위치해 있는지 파악
-                        if (arr[i][j] == st1) st1Inx = j;
-                        if (arr[i][j] == st2) st2Inx = j;
-                    }
-                    if (st1Inx > st2Inx) { // 순위가 어그러지면
-                        flag = false;
-                        break; // 해당 학생들의 케이스에 대한 비교는 break
-                    }
-                }
-                if (flag) answer++;
-            }
+    private int solution(int n, int k, int[] arr) {
+        int answer = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < k - 1; i++) sum += arr[i]; // 초기 윈도우값 설정
+        for (int i = k - 1; i < n - k + 1; i++) {
+            sum += arr[i];
+            answer = Math.max(answer, sum);
+            sum -= arr[i - (k - 1)];
         }
         return answer;
     }
@@ -31,14 +19,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int t = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[t][n];
-        for (int i = 0; i < t; i++) {
-            StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < n; j++) arr[i][j] = Integer.parseInt(tmp.nextToken());
-        }
-        System.out.println(T.solution(n, t, arr));
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st1.nextToken());
+        int k = Integer.parseInt(st1.nextToken());
+        int[] arr = new int[n];
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st2.nextToken());
+        System.out.println(T.solution(n, k, arr));
     }
 }
