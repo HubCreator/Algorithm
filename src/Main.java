440cfe4253cd4590ccd3e07dc9251a2991d5fc23
HubCreator <chocomilk8604@gmail.com>
2022-07-04 18:38:31 +0900

    K 번째 큰 수 DFS 조합

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
-    private int solution(char[] s, char[] t) {
-        Map<Character, Integer> target = new HashMap<>();
-        Map<Character, Integer> w = new HashMap<>();
-        int lt = 0, answer = 0;
-        for (int i = 0; i < t.length; i++) { // target 값 설정
-            target.put(t[i], target.getOrDefault(t[i], 0) + 1);
+    static int n, k;
+    static int[] arr, ch;
+    static TreeSet<Integer> tSet = new TreeSet<>(Collections.reverseOrder());
+
+    private void DFS(int L, int s) {
+        if (L == k) {
+            int sum = 0;
+            for (int x : ch) sum += arr[x];
+            tSet.add(sum);
+        } else {
+            for (int i = s; i < arr.length; i++) {
+                ch[L] = i;
+                DFS(L + 1, i + 1);
+            }
         }
-        // 슬라이딩 윈도우 초기값 설정
-        for (int i = 0; i < t.length - 1; i++) {
-            w.put(s[i], w.getOrDefault(s[i], 0) + 1);
-        }
-        for (int rt = t.length - 1; rt < s.length; rt++) {
-            w.put(s[rt], w.getOrDefault(s[rt], 0) + 1); // rt를 늘려
-            if(target.equals(w)) answer++; // 아나그램인지 체크
-            w.put(s[lt], w.get(s[lt]) - 1); // 다음 w 구성을 위해 lt의 요소 값을 제거
-            if(w.get(s[lt]) == 0) w.remove(s[lt]); // 제거했는데 value가 0이면 key도 제거
-            lt++; // lt 하나 증가
+    }
+
+    private int solution() {
+        int cnt = 0;
+        for (Integer x : tSet) {
+            cnt++;
+            if(cnt == k) return x;
         }
 
-        return answer;
+        return -1;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        String t = br.readLine();
-        System.out.println(T.solution(s.toCharArray(), t.toCharArray()));
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st1.nextToken());
+        k = Integer.parseInt(st1.nextToken());
+        arr = new int[n];
+        ch = new int[k];
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st2.nextToken());
+        T.DFS(0, 0);
+        System.out.println(T.solution());;
+
     }
 }
