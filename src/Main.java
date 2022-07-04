7e688f8238c5c42ddc4c36e746d4b01cbbe7cdd0
HubCreator <chocomilk8604@gmail.com>
2022-07-04 18:13:30 +0900

    모든 아나그램 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    private int solution(int n, int k, int[] arr) {
-        int lt = 0, cnt = 0, answer = Integer.MIN_VALUE;
-
-        for (int rt = 0; rt < n; rt++) {
-            if(arr[rt] == 0) cnt++;
-            while (cnt > k) {
-                if(arr[lt] == 0) cnt--;
-                lt++;
-            }
-            answer = Math.max(answer, rt - lt + 1);
+    private int solution(char[] s, char[] t) {
+        Map<Character, Integer> target = new HashMap<>();
+        Map<Character, Integer> w = new HashMap<>();
+        int lt = 0, answer = 0;
+        for (int i = 0; i < t.length; i++) { // target 값 설정
+            target.put(t[i], target.getOrDefault(t[i], 0) + 1);
         }
+        // 슬라이딩 윈도우 초기값 설정
+        for (int i = 0; i < t.length - 1; i++) {
+            w.put(s[i], w.getOrDefault(s[i], 0) + 1);
+        }
+        for (int rt = t.length - 1; rt < s.length; rt++) {
+            w.put(s[rt], w.getOrDefault(s[rt], 0) + 1); // rt를 늘려
+            if(target.equals(w)) answer++; // 아나그램인지 체크
+            w.put(s[lt], w.get(s[lt]) - 1); // 다음 w 구성을 위해 lt의 요소 값을 제거
+            if(w.get(s[lt]) == 0) w.remove(s[lt]); // 제거했는데 value가 0이면 key도 제거
+            lt++; // lt 하나 증가
+        }
+
         return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(tmp.nextToken());
-        System.out.println(T.solution(n, k, arr));
+        String s = br.readLine();
+        String t = br.readLine();
+        System.out.println(T.solution(s.toCharArray(), t.toCharArray()));
     }
 }
