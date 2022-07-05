bda2ca9cce7247c7d18f3d37ca0e4cbfb06b2695
HubCreator <chocomilk8604@gmail.com>
2022-07-05 14:32:34 +0900

    크레인 인형뽑기

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    private String solution(char[] arr) {
-        Stack<Character> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        for (char c : arr) {
-            if (c != ')') stack.push(c);
-            else while (stack.pop() != '(') continue;
+    private int solution(int n, int[][] board, int m, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < m; i++) {
+            int tCol = moves[i];
+            for (int j = 1; j <= n; j++) {
+                if (board[j][tCol] != 0) {
+                    if (!stack.isEmpty() && stack.peek() == board[j][tCol]) {
+                        stack.pop();
+                        answer += 2;
+                    }
+                    else stack.push(board[j][tCol]);
+                    board[j][tCol] = 0;
+                    break;
+                }
+            }
         }
-        for (Character x : stack) sb.append(x);
-        return sb.toString();
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        System.out.println(T.solution(str.toCharArray()));
+        int n = Integer.parseInt(br.readLine()); // board size
+        int[][] board = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j <= n; j++) board[i][j] = Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine());
+        int[] moves = new int[m];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < m; i++) moves[i] = Integer.parseInt(st.nextToken());
+        System.out.println(T.solution(n, board, m, moves));
     }
 }
