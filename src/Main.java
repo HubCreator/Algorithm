aa56933428548fcfd7eb9fdee129c3c64d2ed2c0
HubCreator <chocomilk8604@gmail.com>
2022-07-05 15:14:35 +0900

    선수과목 판단

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
 import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Queue;
 
 public class Main {
-    private int solution(int n, int k) {
-        Deque<Integer> deque = new LinkedList<>();
-        for (int i = 1; i <= n; i++) deque.addLast(i);
-        while (deque.size() != 1) {
-            for (int i = 0; i < k - 1; i++) {
-                Integer x = deque.removeFirst();
-                deque.addLast(x);
+    private boolean solution(String t, String plan) {
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : t.toCharArray()) queue.offer(c);
+        for (char x : plan.toCharArray()) {
+            if (queue.contains(x) ) {
+                if(queue.peek() == x)
+                    queue.poll();
+                else return false;
             }
-            deque.removeFirst();
         }
-        return deque.getFirst();
+        return queue.size() == 0;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        System.out.println(T.solution(n, k));;
+        String t = br.readLine();
+        String plan = br.readLine();
+        System.out.println(T.solution(t, plan) ? "YES" : "NO");
     }
 }
