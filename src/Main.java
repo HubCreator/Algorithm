d8a92d93bbd5ff283f9ec1e6a372ec4738cc924a
HubCreator <chocomilk8604@gmail.com>
2022-07-05 15:02:20 +0900

    공주 구하기 Dequeue

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-    private int solution(int n, int[][] board, int m, int[] moves) {
-        int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < m; i++) {
-            int tCol = moves[i];
-            for (int j = 1; j <= n; j++) {
-                if (board[j][tCol] != 0) {
-                    if (!stack.isEmpty() && stack.peek() == board[j][tCol]) {
-                        stack.pop();
-                        answer += 2;
-                    }
-                    else stack.push(board[j][tCol]);
-                    board[j][tCol] = 0;
-                    break;
-                }
+    private int solution(int n, int k) {
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 1; i <= n; i++) deque.addLast(i);
+        while (deque.size() != 1) {
+            for (int i = 0; i < k - 1; i++) {
+                Integer x = deque.removeFirst();
+                deque.addLast(x);
             }
+            deque.removeFirst();
         }
-        return answer;
+        return deque.getFirst();
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // board size
-        int[][] board = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 1; j <= n; j++) board[i][j] = Integer.parseInt(st.nextToken());
-        }
-        int m = Integer.parseInt(br.readLine());
-        int[] moves = new int[m];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < m; i++) moves[i] = Integer.parseInt(st.nextToken());
-        System.out.println(T.solution(n, board, m, moves));
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        System.out.println(T.solution(n, k));;
     }
 }
