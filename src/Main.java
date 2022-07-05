30cbfa89c44aa49a94f9de55c3a7a4a8241c46d7
HubCreator <chocomilk8604@gmail.com>
2022-07-05 12:54:25 +0900

    친구인가 Union & Find

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
-    static int n, k;
-    static int[] arr, ch;
-    static TreeSet<Integer> tSet = new TreeSet<>(Collections.reverseOrder());
+    static int[] unf;
 
-    private void DFS(int L, int s) {
-        if (L == k) {
-            int sum = 0;
-            for (int x : ch) sum += arr[x];
-            tSet.add(sum);
-        } else {
-            for (int i = s; i < arr.length; i++) {
-                ch[L] = i;
-                DFS(L + 1, i + 1);
-            }
-        }
+    int Find(int v) {
+        if (v == unf[v]) return unf[v];
+        else return unf[v] = Find(unf[v]);
     }
 
-    private int solution() {
-        int cnt = 0;
-        for (Integer x : tSet) {
-            cnt++;
-            if(cnt == k) return x;
-        }
-
-        return -1;
+    private void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+        if (fa != fb) unf[fa] = fb;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st1.nextToken());
-        k = Integer.parseInt(st1.nextToken());
-        arr = new int[n];
-        ch = new int[k];
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st2.nextToken());
-        T.DFS(0, 0);
-        System.out.println(T.solution());;
-
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        unf = new int[n + 1];
+        for (int i = 1; i <= n; i++) unf[i] = i;
+        for (int i = 1; i <= m; i++) {
+            StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(tmp.nextToken());
+            int b = Integer.parseInt(tmp.nextToken());
+            T.Union(a, b);
+        }
+        StringTokenizer q = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(q.nextToken());
+        int b = Integer.parseInt(q.nextToken());
+        System.out.println(T.Find(a) == T.Find(b) ? "YES" : "NO");
     }
 }
