424ede9d260d32588c0383f1aad2ffefe4d2972f
HubCreator <chocomilk8604@gmail.com>
2022-07-06 20:55:16 +0900

    배열 정렬 비교

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    private List<Integer> solution(int s, int n, int[] arr) {
-        Deque<Integer> deque = new LinkedList<>();
-        for (int i = 0; i < s; i++) deque.offerFirst(arr[i]); // 초기값 설정
-        for (int i = s; i < n; i++) {
-            int t = arr[i];
-            if (deque.contains(t)) {
-                deque.remove(t);
-                deque.offerFirst(t);
-            } else {
-                deque.pollLast();
-                deque.offerFirst(t);
-            }
+    private List<Integer> solution(int n, int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        int[] cloned = arr.clone();
+        Arrays.sort(cloned);
+        for (int i = 0; i < n; i++) {
+            if(arr[i] != cloned[i]) answer.add(i + 1);
         }
-
-        return new ArrayList<>(deque);
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
-        int s = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(tmp.nextToken());
-        for (Integer x : T.solution(s, n, arr)) System.out.print(x + " ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for (Integer x : T.solution(n, arr)) System.out.print(x + " ");
     }
 }
