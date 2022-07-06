2cd07b44acd5f946f37fff379344ffea55cc1acc
HubCreator <chocomilk8604@gmail.com>
2022-07-06 14:07:56 +0900

    신고 결과 받기

--- src/Solution.java
+++ src/Solution.java
@@ -1,68 +1,22 @@
-
-class Point {
-    int x, y;
-
-    public Point(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-}
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 class Solution {
-    public String solution(int[] numbers, String hand) {
-        StringBuilder answer = new StringBuilder();
-        Point left = new Point(3, 0);
-        Point right = new Point(3, 2);
-        int[][] board = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9},
-                {-1, 0, -1}
-        };
-
-        for (int n : numbers) {
-            for (int y = 0; y < 4; y++) { // board를 돌면서
-                for (int x = 0; x < 3; x++) {
-                    if (board[y][x] == n) {
-                        Point t = new Point(y, x);
-                        if (x == 0) { // left
-                            answer.append("L");
-                            left = t;
-                        } else if (x == 2) { // right
-                            answer.append("R");
-                            right = t;
-                        } else { // middle
-                            // 가까운 손을 비교해야 함
-                            int l = Math.abs(left.x - t.x) + Math.abs(left.y - t.y);
-                            int r = Math.abs(right.x - t.x) + Math.abs(right.y - t.y);
-                            if (l < r) {
-                                answer.append("L");
-                                left = t;
-                            } else if (l > r) {
-                                answer.append("R");
-                                right = t;
-                            } else {
-                                if (hand.equals("left")) {
-                                    answer.append("L");
-                                    left = t;
-                                } else {
-                                    answer.append("R");
-                                    right = t;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+    public int[] solution(String[] id_list, String[] report, int k) {
+        List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList()); // 중복된 report 제거
+        Map<String, Integer> count = new HashMap<>(); // 누가 몇 번 신고당했는지
+        for (String s : list) {
+            String target = s.split(" ")[1];
+            count.put(target, count.getOrDefault(target, 0) + 1);
         }
-        return answer.toString();
-    }
 
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
-        String hand = "left";
-        System.out.println(T.solution(numbers, hand).equals("LRLLRRLLLRR") ? "YES" : "NO");
-
+        return Arrays.stream(id_list).map(_user -> {
+            final String user = _user;
+            List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
+            return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
+        }).mapToInt(Long::intValue).toArray();
     }
 }
