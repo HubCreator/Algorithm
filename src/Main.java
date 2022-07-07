bf5d9d11f665d0a26b6d34322f40d16511df79d5
HubCreator <chocomilk8604@gmail.com>
2022-07-07 16:56:00 +0900

    최단 경로 BFS

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-// 재귀 피보나치
 public class Main {
-    int[] fiboArr;
+    int[] dx = {1, 5, -1};
+    int[] ch;
 
-    private int fibo(int v) {
-        if(fiboArr[v] > 0) return fiboArr[v];
-        if(v == 1) return fiboArr[v] =  1;
-        else if(v == 2) return fiboArr[v] = 1;
-        else return fiboArr[v] = fibo(v - 2) + fibo(v - 1);
-    }
-
-    private void solution(int n) {
-        fiboArr = new int[n + 1];
-        fibo(n);
-        for (int i = 1; i < n; i++) System.out.print(fiboArr[i] + " ");
+    private int BFS(int s, int e) {
+        int answer = 0;
+         ch = new int[10001];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(s); // 초기값
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Integer p = queue.poll();
+                for (int j = 0; j < 3; j++) {
+                    int nx = p + dx[j];
+                    if (nx == e) return answer + 1;
+                    if (nx >= 1 && nx <= 10000 && ch[nx] == 0) {
+                        queue.offer(nx);
+                        ch[nx] = 1;
+                    }
+                }
+            }
+            answer++;
+        }
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        T.solution(n);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int s = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        System.out.println(T.BFS(s, e));
     }
 }
