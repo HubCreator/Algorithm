8180da3074d9ffc5015eb4c4b3a6f355b7914c13
HubCreator <chocomilk8604@gmail.com>
2022-07-07 13:12:44 +0900

    카카오 신규 아이디 추천

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,65 @@
+import java.util.List;
+
 class Solution {
-    int[] ch, arr;
-    int answer = 0;
+    public String solution(String new_id) {
+        List<Character> reg = List.of('-', '_', '.');
 
-    boolean isPrime(int v) {
-        for (int i = 2; i < v - 1; i++) {
-            if (v % i == 0) return false;
+        String answer = new_id.toLowerCase(); // 1 단계 - 소문자로
+        StringBuilder sb = new StringBuilder();
+        System.out.println("1 " + answer);
+
+        for (int i = 0; i < answer.length(); i++) { // 2 단계 - 필요없는 특수문자 제거
+            char t = answer.charAt(i);
+            if (!Character.isDigit(t) && !Character.isAlphabetic(t) && !reg.contains(t))
+                answer = answer.replace(t, ' ');
         }
-        return true;
-    }
+        answer = answer.replaceAll(" ", "");
+        System.out.println("2 " + answer);
 
-    private void DFS(int L, int s) {
-        if (L == 3) {
-            int sum = 0;
-            for (int x : ch) sum += arr[x];
-            if (isPrime(sum)) answer++;
+        answer += " ";
+        for (int i = 0; i < answer.length(); i++) { // 3 단계 - '.'을 하나로 압축
+            if (answer.charAt(i) == '.' && answer.charAt(i) == answer.charAt(i + 1)) continue;
+            sb.append(answer.charAt(i));
+        }
+        answer = sb.toString().trim();
+        sb.delete(0, sb.length());
+        System.out.println("3 " + answer);
 
-        } else {
-            for (int i = s; i < arr.length; i++) {
-                ch[L] = i;
-                DFS(L + 1, i + 1);
+        if (answer.charAt(0) == '.') { // 4 단계 - 맨 앞, 맨 뒤에 있는 '.' 제거
+            answer = answer.substring(1);
+        }
+        if (answer.length() > 1 && answer.charAt(answer.length() - 1) == '.') {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+        System.out.println("4 " + answer);
+
+        if (answer.length() == 0) answer = "a"; // 5 단계
+        System.out.println("5 " + answer);
+
+
+        if (answer.length() > 15) answer = answer.substring(0, 15).trim(); // 6 단계
+        if (answer.length() > 1 && answer.charAt(answer.length() - 1) == '.') {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+
+
+        System.out.println("6 " + answer);
+
+
+        if (answer.length() <= 2) { // 7 단계
+            sb.append(answer);
+            while (sb.length() < 3) {
+                sb.append(answer.charAt(answer.length() - 1));
             }
+            answer = sb.toString();
         }
-    }
-
-    public int solution(int[] nums) {
-        arr = nums;
-        ch = new int[3];
-        DFS(0, 0);
+        System.out.println("7 " + answer);
 
         return answer;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        int[] asdf = {1, 2, 7, 6, 4};
-        System.out.println(T.solution(asdf));
+        System.out.println(T.solution("=.="));
     }
 }
