fd275c554d519a2392cb93f980b3a356386d9630
HubCreator <chocomilk8604@gmail.com>
2022-07-07 13:19:18 +0900

    카카오 신규 아이디 추천 보완 수정

--- src/Solution.java
+++ src/Solution.java
@@ -1,60 +1,28 @@
-import java.util.List;
 
 class Solution {
     public String solution(String new_id) {
-        List<Character> reg = List.of('-', '_', '.');
+        String answer = "";
+        String temp = new_id.toLowerCase(); // 1 단계 - 소문자로 변환
 
-        String answer = new_id.toLowerCase(); // 1 단계 - 소문자로
-        StringBuilder sb = new StringBuilder();
-        System.out.println("1 " + answer);
+        temp = temp.replaceAll("[^-_.a-z0-9]", ""); // 2 단계 - 숫자, 알파벳, 특정 기호를 제외하고 제거
 
-        for (int i = 0; i < answer.length(); i++) { // 2 단계 - 필요없는 특수문자 제거
-            char t = answer.charAt(i);
-            if (!Character.isDigit(t) && !Character.isAlphabetic(t) && !reg.contains(t))
-                answer = answer.replace(t, ' ');
-        }
-        answer = answer.replaceAll(" ", "");
-        System.out.println("2 " + answer);
+        temp = temp.replaceAll("[.]{2,}", "."); // 3 단계 - 연속된 '.'을 제거
 
-        answer += " ";
-        for (int i = 0; i < answer.length(); i++) { // 3 단계 - '.'을 하나로 압축
-            if (answer.charAt(i) == '.' && answer.charAt(i) == answer.charAt(i + 1)) continue;
-            sb.append(answer.charAt(i));
-        }
-        answer = sb.toString().trim();
-        sb.delete(0, sb.length());
-        System.out.println("3 " + answer);
+        temp = temp.replaceAll("^[.]|[.]$", ""); // 4 단계 - 처음과 끝에 '.'을 제거
 
-        if (answer.charAt(0) == '.') { // 4 단계 - 맨 앞, 맨 뒤에 있는 '.' 제거
-            answer = answer.substring(1);
-        }
-        if (answer.length() > 1 && answer.charAt(answer.length() - 1) == '.') {
-            answer = answer.substring(0, answer.length() - 1);
-        }
-        System.out.println("4 " + answer);
+        if (temp.equals("")) // 5 단계 - 빈 문자열이면 a를 추가
+            temp += "a";
 
-        if (answer.length() == 0) answer = "a"; // 5 단계
-        System.out.println("5 " + answer);
-
-
-        if (answer.length() > 15) answer = answer.substring(0, 15).trim(); // 6 단계
-        if (answer.length() > 1 && answer.charAt(answer.length() - 1) == '.') {
-            answer = answer.substring(0, answer.length() - 1);
+        if (temp.length() >= 16) { // 6 단계 - 길이가 16 이상이면 16 이후의 수를 제거, 제거한 이후에 끝자리가 '.'이면 제거
+            temp = temp.substring(0, 15);
+            temp = temp.replaceAll("^[.]|[.]$", "");
         }
 
+        if (temp.length() <= 2) // 7 단계 - 길이가 2 이하라면, 마지막 문자를 길이가 3까지 반복하여 길이를 늘림
+            while (temp.length() < 3)
+                temp += temp.charAt(temp.length() - 1);
 
-        System.out.println("6 " + answer);
-
-
-        if (answer.length() <= 2) { // 7 단계
-            sb.append(answer);
-            while (sb.length() < 3) {
-                sb.append(answer.charAt(answer.length() - 1));
-            }
-            answer = sb.toString();
-        }
-        System.out.println("7 " + answer);
-
+        answer = temp;
         return answer;
     }
 
