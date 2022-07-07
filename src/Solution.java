9c3c705e7d89d70f03189dd133f5262e00b9dbb4
HubCreator <chocomilk8604@gmail.com>
2022-07-07 11:17:16 +0900

    소수 구하기, 조합

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,39 @@
 class Solution {
-    public int solution(String s) {
-        String[] ch = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for (int i = 0; i < ch.length; i++) {
-            s = s.replaceAll(ch[i], String.valueOf(i));
+    int[] ch, arr;
+    int answer = 0;
+
+    boolean isPrime(int v) {
+        for (int i = 2; i < v - 1; i++) {
+            if (v % i == 0) return false;
         }
-        return Integer.parseInt(s);
+        return true;
+    }
+
+    private void DFS(int L, int s) {
+        if (L == 3) {
+            int sum = 0;
+            for (int x : ch) sum += arr[x];
+            if (isPrime(sum)) answer++;
+
+        } else {
+            for (int i = s; i < arr.length; i++) {
+                ch[L] = i;
+                DFS(L + 1, i + 1);
+            }
+        }
+    }
+
+    public int solution(int[] nums) {
+        arr = nums;
+        ch = new int[3];
+        DFS(0, 0);
+
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        System.out.println(T.solution("one4seveneight"));
+        int[] asdf = {1, 2, 7, 6, 4};
+        System.out.println(T.solution(asdf));
     }
 }
