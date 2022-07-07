d78e3a662dbd669d25bcae4415ae27ff9e0739d3
HubCreator <chocomilk8604@gmail.com>
2022-07-07 10:58:21 +0900

    카카오 숫자와 영단어 보완

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,10 @@
 class Solution {
     public int solution(String s) {
-        StringBuilder sb = new StringBuilder();
         String[] ch = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for (int i = 0; i < s.length(); i++) {
-            char t = s.charAt(i);
-            if (Character.isDigit(t)) sb.append(t);
-            else {
-                for (int j = 0; j < ch.length; j++) {
-                    if (s.substring(i).startsWith(ch[j])) {
-                        sb.append(j);
-                        i += ch[j].length() - 1;
-                    }
-                }
-            }
+        for (int i = 0; i < ch.length; i++) {
+            s = s.replaceAll(ch[i], String.valueOf(i));
         }
-        return Integer.parseInt(sb.toString());
+        return Integer.parseInt(s);
     }
 
     public static void main(String[] args) {
