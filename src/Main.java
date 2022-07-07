16c64671e926b93d3fb6d72985e7573de9a2c349
HubCreator <chocomilk8604@gmail.com>
2022-07-07 16:22:28 +0900

    재귀 피보나치 메모이제이션

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
+// 재귀 피보나치
 public class Main {
-    private int canPut(int[] arr, int c) { // m : cd의 갯수, c : cd 하나의 용량
-        int cnt = 1, sum = 0;
-        int idx = 0;
-        while (idx < arr.length) {
-            sum += arr[idx];
-            if (sum > c) {
-                sum = arr[idx];
-                cnt++;
-            }
-            idx++;
-        }
-        return cnt;
+    int[] fiboArr;
+
+    private int fibo(int v) {
+        if(fiboArr[v] > 0) return fiboArr[v];
+        if(v == 1) return fiboArr[v] =  1;
+        else if(v == 2) return fiboArr[v] = 1;
+        else return fiboArr[v] = fibo(v - 2) + fibo(v - 1);
     }
 
-    private int solution(int n, int m, int[] arr) {
-        Arrays.sort(arr);
-        int lt = Arrays.stream(arr).max().getAsInt();
-        int rt = Arrays.stream(arr).sum();
-        int mid = 0;
-        while (lt <= rt) {
-            mid = (lt + rt) / 2;
-            int t = canPut(arr, mid);
-            if (t == m) return mid;
-            else if (t > m) lt = mid + 1; // 용량을 더 키움
-            else rt = mid - 1;
-        }
-        return mid;
+    private void solution(int n) {
+        fiboArr = new int[n + 1];
+        fibo(n);
+        for (int i = 1; i < n; i++) System.out.print(fiboArr[i] + " ");
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(tmp.nextToken());
-        System.out.println(T.solution(n, m, arr));
+        int n = Integer.parseInt(br.readLine());
+        T.solution(n);
     }
 }
