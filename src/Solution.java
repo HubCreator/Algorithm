59cf585b561d531c94224a5a9fc502615dd6f669
HubCreator <chocomilk8604@gmail.com>
2022-07-08 16:30:30 +0900

    기능 개발 배포

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,31 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Stack;
 
+class Function{
+    int progress, speeds;
+
+    public Function(int progress, int speeds) {
+        this.progress = progress;
+        this.speeds = speeds;
+    }
+}
 class Solution {
-
-    public int solution(int[] scoville, int K) {
-        int answer = 0;
-        PriorityQueue<Integer> pQueue = new PriorityQueue<>();
-        for (int x : scoville) pQueue.add(x);
-
-        while (pQueue.size() > 1 && pQueue.peek() < K) {
-            pQueue.add(pQueue.poll() + pQueue.poll() * 2);
-            answer++;
+    public int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Stack<Function> stack = new Stack<>();
+        int cnt = 0;
+        for (int i = progresses.length - 1; i >= 0; i--) {
+            stack.push(new Function(progresses[i], speeds[i]));
         }
-        return !pQueue.isEmpty() && pQueue.peek() >= K ? answer : -1;
+        while (!stack.isEmpty()) {
+            while (!stack.isEmpty() && stack.peek().progress >= 100) {
+                cnt++;
+                stack.pop();
+            }
+            for (Function x : stack) x.progress += x.speeds;
+            if(cnt != 0) answer.add(cnt);
+            cnt = 0;
+        }
+        return answer.stream().mapToInt(i -> i.intValue()).toArray();
     }
 }
