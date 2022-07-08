bd54c30441e7bbb4156e291e57b369ad0dec71d5
HubCreator <chocomilk8604@gmail.com>
2022-07-08 13:05:39 +0900

    여벌의 체육복 greedy 수정 보완

--- src/Solution.java
+++ src/Solution.java
@@ -1,76 +1,31 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Objects;
-
-class Student implements Comparable<Student> {
-    int id;
-    boolean lost, isChecked = false;
-
-    public Student(int id, boolean lost) {
-        this.id = id;
-        this.lost = lost;
-    }
-
-    @Override
-    public int compareTo(Student o) {
-        return this.id - o.id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, lost, isChecked);
-    }
-}
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int lostSize = lost.length;
-        ArrayList<Student> lostList = new ArrayList<>();
-        ArrayList<Student> reserveList = new ArrayList<>();
-        ArrayList<Student> mergedList = new ArrayList<>();
-        for (int i : lost) lostList.add(new Student(i, true));
-        for (int i : reserve) reserveList.add(new Student(i, false));
-        mergedList.addAll(lostList);
-        for (Student s : reserveList) {
-            if (mergedList.contains(s)) {
-                mergedList.remove(s);
-                lostSize--;
-            } else {
-                mergedList.add(s);
+        int[] people = new int[n + 2]; // 맨 왼쪽 오른쪽을 비워둠
+        int answer = n;
+
+        for (int l : lost) people[l]--;
+        for (int r : reserve) people[r]++;
+
+        for (int i = 1; i < people.length - 1; i++) {
+            if (people[i] == -1) { // 잃어버린 놈
+                if (people[i - 1] == 1) { // 잃어버린 놈과 빌려주는 놈을 모두 0으로 만듬
+                    people[i]++;
+                    people[i - 1]--;
+                } else if (people[i + 1] == 1) {
+                    people[i]++;
+                    people[i + 1]--;
+                } else
+                    answer--;
             }
         }
-//        mergedList.addAll(reserveList);
-        Collections.sort(mergedList);
+        return answer;
+    }
 
-        for (int i = 0; i < mergedList.size(); i++) {
-            Student ts = mergedList.get(i);
-            if (mergedList.get(i).lost) {
-                if (i - 1 >= 0) {
-                    Student s = mergedList.get(i - 1);
-                    if (!s.isChecked && !s.lost && Math.abs(s.id - ts.id) == 1) {
-                        s.isChecked = true;
-                        lostSize--;
-                        continue;
-                    }
-                }
-                if (i + 1 < mergedList.size()) {
-                    Student s = mergedList.get(i + 1);
-                    if (!s.isChecked && !s.lost && Math.abs(s.id - ts.id) == 1) {
-                        s.isChecked = true;
-                        lostSize--;
-                    }
-                }
-            }
-        }
-
-        return n - lostSize;
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        System.out.println(T.solution(n, lost, reserve));
     }
 }
