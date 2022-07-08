3019f8c776aa57c677efa3526530fd8bc99f0ab9
HubCreator <chocomilk8604@gmail.com>
2022-07-08 13:24:01 +0900

    예산 짜기

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int[] people = new int[n + 2]; // 맨 왼쪽 오른쪽을 비워둠
-        int answer = n;
-
-        for (int l : lost) people[l]--;
-        for (int r : reserve) people[r]++;
-
-        for (int i = 1; i < people.length - 1; i++) {
-            if (people[i] == -1) { // 잃어버린 놈
-                if (people[i - 1] == 1) { // 잃어버린 놈과 빌려주는 놈을 모두 0으로 만듬
-                    people[i]++;
-                    people[i - 1]--;
-                } else if (people[i + 1] == 1) {
-                    people[i]++;
-                    people[i + 1]--;
-                } else
-                    answer--;
-            }
+    public int solution(int[] d, int budget) {
+        int answer = 0, sum = 0;
+        Arrays.sort(d);
+        for (int x : d) {
+            sum += x;
+            if (sum <= budget) answer++;
+            else break;
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int n = 5;
-        int[] lost = {2, 4};
-        int[] reserve = {1, 3, 5};
-        System.out.println(T.solution(n, lost, reserve));
     }
 }
