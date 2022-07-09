b6a330d60331c5f161478dd140c9d41e29f6f415
HubCreator <chocomilk8604@gmail.com>
2022-07-09 13:50:21 +0900

    카카오 괄호 변환

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,42 @@
-import java.util.ArrayList;
-import java.util.Stack;
-
-class Function{
-    int progress, speeds;
-
-    public Function(int progress, int speeds) {
-        this.progress = progress;
-        this.speeds = speeds;
-    }
-}
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        Stack<Function> stack = new Stack<>();
+    public int getDividedIdx(String str) {
+        int ch = 0;
+        for (int i = 0; i < str.length(); i++) {
+            ch += (str.charAt(i) == '(') ? 1 : -1;
+            if (ch == 0) return i + 1;
+        }
+        return -1;
+    }
+
+    public boolean isCorrect(String str) {
         int cnt = 0;
-        for (int i = progresses.length - 1; i >= 0; i--) {
-            stack.push(new Function(progresses[i], speeds[i]));
+        for (int i = 0; i < str.length(); i++) {
+            cnt += str.charAt(i) == '(' ? 1 : -1;
+            if (cnt < 0) return false;
         }
-        while (!stack.isEmpty()) {
-            while (!stack.isEmpty() && stack.peek().progress >= 100) {
-                cnt++;
-                stack.pop();
-            }
-            for (Function x : stack) x.progress += x.speeds;
-            if(cnt != 0) answer.add(cnt);
-            cnt = 0;
+        return cnt == 0;
+    }
+
+    private String change(String u, String v) {
+        StringBuilder s = new StringBuilder("(" + getAnswer(v) + ")");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < u.length() - 1; i++) {
+            sb.append(u.charAt(i) == ')' ? '(' : ')');
         }
-        return answer.stream().mapToInt(i -> i.intValue()).toArray();
+        s.append(sb);
+        return s.toString();
+    }
+
+    private String getAnswer(String w) {
+        if (w.equals("")) return "";
+        int idx = getDividedIdx(w);
+        String u = w.substring(0, idx);
+        String v = w.substring(idx);
+        if (isCorrect(u)) return u + getAnswer(v);
+        else return change(u, v);
+    }
+
+    public String solution(String p) {
+        return getAnswer(p);
     }
 }
