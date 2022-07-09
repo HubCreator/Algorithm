01ff5f71cb87161a4e4d05cc8a1ae00d2a585389
HubCreator <chocomilk8604@gmail.com>
2022-07-10 00:12:38 +0900

    약수의 갯수와 덧셈

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,10 @@
 class Solution {
-    public int solution(int n) {
-        StringBuilder a = new StringBuilder();
-
-        while (n > 0) {
-            a.append(n % 3);
-            n /= 3;
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            if (i % Math.sqrt(i) == 0) answer -= i; // 제곱수인 경우 약수의 개수가 홀수
+            else answer += i; // 제곱수가 아닌 경우 약수의 개수가 짝수
         }
-        return Integer.parseInt(a.toString(), 3);
+        return answer;
     }
 }
