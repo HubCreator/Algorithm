85d3bb1df226ce9e2ea8c4599b7d6547790a6477
HubCreator <chocomilk8604@gmail.com>
2022-07-09 20:52:59 +0900

    3진법 뒤집기 보완

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,11 @@
-import java.util.Stack;
-
 class Solution {
     public int solution(int n) {
-        Stack<Integer> stack = new Stack<>();
-        StringBuilder ternary = new StringBuilder();
+        StringBuilder a = new StringBuilder();
 
-        do {
-            stack.push(n % 3);
-            n = n / 3;
-        } while (n > 0);
-        while (!stack.isEmpty()) ternary.append(stack.pop()); // 10진법을 3진법으로
-
-        ternary.reverse(); // 뒤집기
-
-        return Integer.parseInt(ternary.toString(), 3); // 3진법을 10진법으로
+        while (n > 0) {
+            a.append(n % 3);
+            n /= 3;
+        }
+        return Integer.parseInt(a.toString(), 3);
     }
 }
