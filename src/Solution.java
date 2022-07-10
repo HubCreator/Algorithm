9d3f08d75c59691de23b967d0c9c2b68c3c0f5e6
HubCreator <chocomilk8604@gmail.com>
2022-07-10 20:16:19 +0900

    조이스틱

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
-    public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Arrays.sort(phone_book);
+    public int solution(String name) {
+        int answer = 0;
+        int[] diff = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        for (char c : name.toCharArray()) // 상하 조작
+            answer += diff[c - 'A'];
 
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            String cur = phone_book[i];
-            String next = phone_book[i + 1];
-            if(next.startsWith(cur)) return false;
+        int length = name.length();
+        int min = length - 1;
+
+        for (int i = 0; i < length; i++) {
+            int next = i + 1;
+            while (next < length && name.charAt(next) == 'A') {
+                next++; // A가 연속되어 나온다면 이를 카운트하고 바로 그 다음까지 인덱스를 올려라
+            }
+            min = Math.min(min, i + length - next + Math.min(i, length - next));
         }
-        return answer;
+
+        return answer + min;
     }
 }
