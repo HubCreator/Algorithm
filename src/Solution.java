32c9013bc53ec5849b5dd9ce4770d36db044a42b
HubCreator <chocomilk8604@gmail.com>
2022-07-10 16:13:42 +0900

    전화번호 목록 접두사 체크

--- src/Solution.java
+++ src/Solution.java
@@ -1,44 +1,15 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
-class Entry {
-    int idx, priority;
-
-    public Entry(int idx, int priority) {
-        this.idx = idx;
-        this.priority = priority;
-    }
-}
+import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] priorities, int location) {
-        Queue<Entry> queue = new LinkedList<>();
-        int answer = 0;
-        for (int i = 0; i < priorities.length; i++) {
-            queue.offer(new Entry(i, priorities[i]));
-        }
-        while (!queue.isEmpty()) {
-            Entry p = queue.poll();
-            for (Entry entry : queue) {
-                if (entry.priority > p.priority) {
-                    queue.offer(p);
-                    p = null;
-                    break;
-                }
-            }
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
 
-            if (p != null) {
-                answer++;
-                if (p.idx == location) return answer;
-            }
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            String cur = phone_book[i];
+            String next = phone_book[i + 1];
+            if(next.startsWith(cur)) return false;
         }
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int[] p = {2, 1, 3, 2};
-        int l = 2;
-        System.out.println(T.solution(p, l));
+        return answer;
     }
 }
