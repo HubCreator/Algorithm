732ad8bb3cdacb878d09b566b36e8e483d6b1388
HubCreator <chocomilk8604@gmail.com>
2022-07-10 13:02:16 +0900

    그래프 최단거리 BFS

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int n, m;
+    static int[] answer;
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static int n, answer = 0;
-    static int[] ch;
 
-    private void DFS(int val) {
-        if (val == n) {
-            answer++;
-        } else {
-            for (Integer x : graph.get(val)) {
-                if (ch[x] == 0) {
-                    ch[x] = 1;
-                    DFS(x);
-                    ch[x] = 0;
+    private void BFS(int val) {
+        int level = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> ch = new ArrayList<>();
+        for (Integer x : graph.get(1)) queue.offer(x); // 1번 노드에서 갈 수 있는 노드들을 queue에 추가 (초기 설정)
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Integer p = queue.poll(); // 한놈 뽑아서
+                if (p == val) {
+                    answer[val] = level + 1; // 그게 val값이면 우리가 찾는 답이다
+                    return;
+                }
+                ch.add(p); // 뽑은 놈을 ch에 넣어 중복 경로 제거
+                for (Integer t : graph.get(p)) { // 뽑은 놈에서의 가지들을 체크
+                    if (!ch.contains(t) && !queue.contains(t)) { // 이미 뽑았던 놈이거나 큐에 존재하는 것들은 중복 경로 이므로 제외
+                        queue.offer(t);
+                    }
                 }
             }
+            level++;
         }
     }
 
@@ -28,17 +38,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        for (int i = 1; i <= n; i++) graph.add(new ArrayList<>());
-        ch = new int[n + 1];
+        m = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+        answer = new int[n + 1];
         for (int i = 0; i < m; i++) {
             StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
             int from = Integer.parseInt(tmp.nextToken());
             int to = Integer.parseInt(tmp.nextToken());
             graph.get(from).add(to);
         }
-        ch[1] = 1;
-        T.DFS(1);
-        System.out.println(answer);
+        for (int i = 2; i <= n; i++) T.BFS(i);
+        for (int i = 2; i <= n; i++) System.out.println(i + " : " + answer[i]);
+
     }
 }
