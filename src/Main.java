9b3c53e42fa9956c85913dd90581004709d8e747
HubCreator <chocomilk8604@gmail.com>
2022-07-10 13:56:57 +0900

    최대 점수 구하기 DFS

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int c, n, answer = Integer.MIN_VALUE;
-    static int[] arr, ch;
+    static int n, m, answer = Integer.MIN_VALUE;
+    static int[] score, limit, ch;
 
     private void DFS(int L) {
-        if (L == n) {
-            int sum = 0;
-            for (int i = 0; i < ch.length; i++) {
-                if (ch[i] == 1) sum += arr[i];
+        if (L == n) { // 모두 다 돌고, 제한 시간 안에 다 풀었다면
+            int limitSum = 0, scoreSum = 0;
+            for (int i = 0; i < n; i++) {
+                if (ch[i] == 1) {
+                    limitSum += limit[i];
+                    scoreSum += score[i];
+                }
             }
-            if (sum <= c) answer = Math.max(answer, sum);
+            if (limitSum <= m) answer = Math.max(answer, scoreSum);
         } else {
             ch[L] = 1;
             DFS(L + 1);
@@ -26,11 +29,16 @@ public class Main {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        c = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        arr = new int[n];
-        ch = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 문제 개수
+        m = Integer.parseInt(st.nextToken()); // 제한 시간
+        score = new int[n + 1];
+        limit = new int[n + 1];
+        ch = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
+            score[i] = Integer.parseInt(tmp.nextToken());
+            limit[i] = Integer.parseInt(tmp.nextToken());
+        }
         T.DFS(0);
         System.out.println(answer);
     }
