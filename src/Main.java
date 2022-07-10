196f60802184b7c08b466e43bb2295428468c596
HubCreator <chocomilk8604@gmail.com>
2022-07-10 14:22:07 +0900

    씨름 선수 greedy

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int n, m, answer = Integer.MIN_VALUE;
-    static int[] score, limit, ch;
+class Person implements Comparable<Person> {
+    int h, w;
 
-    private void DFS(int L) {
-        if (L == n) { // 모두 다 돌고, 제한 시간 안에 다 풀었다면
-            int limitSum = 0, scoreSum = 0;
-            for (int i = 0; i < n; i++) {
-                if (ch[i] == 1) {
-                    limitSum += limit[i];
-                    scoreSum += score[i];
-                }
+    public Person(int h, int w) {
+        this.h = h;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (o.h == this.h) return o.w - this.w;
+        else return o.h - this.h;
+    }
+}
+
+public class Main {
+    private int solution(ArrayList<Person> list) {
+        int answer = 0, max = Integer.MIN_VALUE;
+        Collections.sort(list);
+        for (Person p : list) {
+            if (p.w > max) {
+                max = p.w;
+                answer++;
             }
-            if (limitSum <= m) answer = Math.max(answer, scoreSum);
-        } else {
-            ch[L] = 1;
-            DFS(L + 1);
-            ch[L] = 0;
-            DFS(L + 1);
         }
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken()); // 문제 개수
-        m = Integer.parseInt(st.nextToken()); // 제한 시간
-        score = new int[n + 1];
-        limit = new int[n + 1];
-        ch = new int[n + 1];
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Person> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
-            score[i] = Integer.parseInt(tmp.nextToken());
-            limit[i] = Integer.parseInt(tmp.nextToken());
+            int h = Integer.parseInt(tmp.nextToken());
+            int w = Integer.parseInt(tmp.nextToken());
+            list.add(new Person(h, w));
         }
-        T.DFS(0);
-        System.out.println(answer);
+        System.out.println(T.solution(list));
     }
 }
