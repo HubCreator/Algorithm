41afc2f7c0992f70924327d0ae85ee61ec882db1
HubCreator <chocomilk8604@gmail.com>
2022-07-10 20:24:33 +0900

    두 개 뽑아서 더하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,13 @@
+import java.util.TreeSet;
+
 class Solution {
-    public int solution(String name) {
-        int answer = 0;
-        int[] diff = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        for (char c : name.toCharArray()) // 상하 조작
-            answer += diff[c - 'A'];
-
-        int length = name.length();
-        int min = length - 1;
-
-        for (int i = 0; i < length; i++) {
-            int next = i + 1;
-            while (next < length && name.charAt(next) == 'A') {
-                next++; // A가 연속되어 나온다면 이를 카운트하고 바로 그 다음까지 인덱스를 올려라
+    public int[] solution(int[] numbers) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                treeSet.add(numbers[i] + numbers[j]);
             }
-            min = Math.min(min, i + length - next + Math.min(i, length - next));
         }
-
-        return answer + min;
+        return treeSet.stream().mapToInt(Integer::intValue).toArray();
     }
 }
