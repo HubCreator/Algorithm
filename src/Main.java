23c917905ea830dbbcf2e7b20cdc97b708aaa607
HubCreator <chocomilk8604@gmail.com>
2022-07-11 18:07:21 +0900

    토마토 BFS with dis[]

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -15,23 +16,21 @@ class Point {
 }
 
 public class Main {
-    public static final int BOARD_SIZE = 7;
-    public static int[][] board = new int[BOARD_SIZE + 1][BOARD_SIZE + 1];
-    public static int[][] dis = new int[BOARD_SIZE + 1][BOARD_SIZE + 1];
+    static int xL, yL;
+    static int[][] box, dis;
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
 
-    public int[] dx = {-1, 0, 1, 0};
-    public int[] dy = {0, 1, 0, -1};
-
-    private void BFS(int y, int x) {
+    private void BFS(ArrayList<Point> startAt) {
         Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(y, x));
+        for (Point point : startAt) queue.offer(point);
         while (!queue.isEmpty()) {
             Point p = queue.poll();
-            for (int j = 0; j < 4; j++) {
-                int ny = p.y + dy[j];
-                int nx = p.x + dx[j];
-                if (ny > 0 && ny <= BOARD_SIZE && nx > 0 && nx <= BOARD_SIZE && board[ny][nx] == 0) {
-                    board[ny][nx] = 1;
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dy[i];
+                int nx = p.x + dx[i];
+                if (ny >= 0 && ny < yL && nx >= 0 && nx < xL && box[ny][nx] == 0) {
+                    box[ny][nx] = 1;
                     queue.offer(new Point(ny, nx));
                     dis[ny][nx] = dis[p.y][p.x] + 1;
                 }
@@ -42,12 +41,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 1; i <= BOARD_SIZE; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 1; j <= BOARD_SIZE; j++) board[i][j] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        xL = Integer.parseInt(st.nextToken());
+        yL = Integer.parseInt(st.nextToken());
+        box = new int[yL][xL];
+        dis = new int[yL][xL];
+        ArrayList<Point> startAt = new ArrayList<>();
+        for (int i = 0; i < yL; i++) {
+            StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < xL; j++) {
+                int t = Integer.parseInt(tmp.nextToken());
+                if (t == 1) startAt.add(new Point(i, j));
+                box[i][j] = t;
+            }
         }
-        T.BFS(1, 1);
-        System.out.println(dis[BOARD_SIZE][BOARD_SIZE]);
+        T.BFS(startAt);
+
+
+        int max = Integer.MIN_VALUE;
+        boolean flag = true;
+
+        Loop1:
+        for (int y = 0; y < yL; y++) {
+            for (int x = 0; x < xL; x++) {
+                max = Math.max(max, dis[y][x]);
+                if (dis[y][x] == 0 && box[y][x] == 0) {
+                    flag = false;
+                    break Loop1;
+                }
+            }
+        }
+
+        if (flag) System.out.println(max);
+        else System.out.println(-1);
     }
 }
