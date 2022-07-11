7666da31bd304604dd8cd7c1cf5a2ffaba323b81
HubCreator <chocomilk8604@gmail.com>
2022-07-11 14:34:46 +0900

    소수 찾기. 순열 DFS

--- src/Solution.java
+++ src/Solution.java
@@ -1,48 +1,56 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
-class Point {
-    int y, x;
-
-    public Point(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-}
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    static int[][] board;
-    static int width, height;
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
+    String nums;
+    List<Integer> primeList = new ArrayList<>();
+    int[] ch;
+    int len;
 
-    private int BFS(int y, int x) {
-        Queue<Point> queue = new LinkedList<>();
-        int level = 0;
-        queue.offer(new Point(y, x));
-        while (!queue.isEmpty()) {
-            int len = queue.size();
-            for (int i = 0; i < len; i++) {
-                Point p = queue.poll();
-                if (p.y == height - 1 && p.x == width - 1) return level + 1;
-                for (int j = 0; j < 4; j++) {
-                    int ny = p.y + dy[j];
-                    int nx = p.x + dx[j];
-                    if (ny >= 0 && ny < height && nx >= 0 && nx < width && board[ny][nx] == 1) {
-                        board[ny][nx] = 0;
-                        queue.offer(new Point(ny, nx));
-                    }
-                }
-            }
-            level++;
+    public boolean isPrime(int v) {
+        if (v <= 1) return false;
+        for (int i = 2; i < v; i++) {
+            if (v % i == 0) return false;
         }
-        return -1;
+        return true;
     }
 
-    public int solution(int[][] maps) {
-        board = maps;
-        height = maps.length;
-        width = maps[0].length;
-        return BFS(0, 0);
+    private void DFS(int L) {
+        if (L == len) {
+            List<Integer> visited = new ArrayList<>();
+            StringBuilder tmp = new StringBuilder();
+            for (int x : ch) {
+                if (visited.contains(x)) return;
+                visited.add(x);
+                tmp.append(nums.charAt(x));
+            }
+            Integer t = Integer.parseInt(String.valueOf(tmp));
+            if (!primeList.contains(t) && isPrime(t)) primeList.add(t);
+        } else {
+            for (int i = 0; i < nums.length(); i++) {
+                ch[L] = i;
+                DFS(L + 1);
+            }
+        }
+    }
+
+    public int solution(String numbers) {
+        nums = numbers;
+        for (int i = 0; i < numbers.length(); i++) {
+            int o = Integer.parseInt(String.valueOf(numbers.charAt(i)));
+            if (!primeList.contains(o) && isPrime(o))
+                primeList.add(o);
+        }
+        for (int i = 2; i <= numbers.length(); i++) {
+            len = i;
+            ch = new int[len];
+            DFS(0);
+        }
+        return primeList.size();
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution("17"));
     }
 }
