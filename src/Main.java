aea6d0118e355cb8afc16d1268eb240d95882fb3
HubCreator <chocomilk8604@gmail.com>
2022-07-11 15:23:04 +0900

    중복 순열 DFS

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-class Person implements Comparable<Person> {
-    int h, w;
-
-    public Person(int h, int w) {
-        this.h = h;
-        this.w = w;
-    }
-
-    @Override
-    public int compareTo(Person o) {
-        if (o.h == this.h) return o.w - this.w;
-        else return o.h - this.h;
-    }
-}
-
 public class Main {
-    private int solution(ArrayList<Person> list) {
-        int answer = 0, max = Integer.MIN_VALUE;
-        Collections.sort(list);
-        for (Person p : list) {
-            if (p.w > max) {
-                max = p.w;
-                answer++;
+    static int n, m;
+    static int[] ch;
+
+    private void DFS(int L) {
+        if (L == m) {
+            for (int x : ch) System.out.print(x + " ");
+            System.out.println();
+
+        } else {
+            for (int i = 1; i <= n; i++) { // 1부터 시작해서 n까지 돌아야 하니까
+                ch[L] = i; // L은 0부터 인덱스의 숫자와 함께 올라감
+                DFS(L + 1);
             }
         }
-        return answer;
+
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        ArrayList<Person> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
-            int h = Integer.parseInt(tmp.nextToken());
-            int w = Integer.parseInt(tmp.nextToken());
-            list.add(new Person(h, w));
-        }
-        System.out.println(T.solution(list));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        ch = new int[m];
+
+        T.DFS(0);
+
     }
 }
