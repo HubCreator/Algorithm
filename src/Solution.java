5e42a9e370b417c809f613afd0c4027a7513ead0
HubCreator <chocomilk8604@gmail.com>
2022-07-11 10:16:57 +0900

    지갑을 만들 수 있는 최소 직사각형 크기

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-class Log {
-    String status, userId;
-
-    public Log(String status, String userId) {
-        this.status = status;
-        this.userId = userId;
-    }
-}
-
 class Solution {
-    List<Log> list;
-    HashMap<String, String> map;
-    private void writeLog(String r) {
-        String[] split = r.split(" ");
-        if(r.startsWith("Enter")) {
-            list.add(new Log(split[0], split[1])); // status와 userId를 기록
-            map.put(split[1], split[2]); // userId에 해당하는 userName을 저장
-        } else if (r.startsWith("Change")) {
-            map.put(split[1], split[2]); // userId에 해당하는 userName을 수정
-        } else if (r.startsWith("Leave")) {
-            list.add(new Log(split[0], split[1])); // status와 userId를 기록
-        }
-    }
+    public int solution(int[][] sizes) {
+        int a = Integer.MIN_VALUE, b = Integer.MIN_VALUE;
 
-    public String[] solution(String[] record) {
-        ArrayList<String> answer = new ArrayList<>();
-        list = new ArrayList<>();
-        map = new HashMap<>();
-
-        for (String r : record) writeLog(r);
-        for (Log log : list) {
-            StringBuilder sb = new StringBuilder();
-            if (log.status.equals("Enter")) {
-                sb.append(map.get(log.userId)).append("님이 들어왔습니다.");
-            } else {
-                sb.append(map.get(log.userId)).append("님이 나갔습니다.");
-            }
-            answer.add(sb.toString());
+        for (int[] size : sizes) a = Math.max(a, Math.max(size[0], size[1]));
+        for (int[] size : sizes) {
+            int tmp = Math.min(size[0], size[1]);
+            b = Math.max(b, tmp);
         }
-        return answer.toArray(new String[0]);
+        return a * b;
     }
 }
