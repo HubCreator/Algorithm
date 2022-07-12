f4b326ef1869b25b99567457c13fba0831b326f3
HubCreator <chocomilk8604@gmail.com>
2022-07-12 16:34:34 +0900

    튜플

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,35 @@
-class Solution {
-    public String solution(int n) {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
 
-        return new String(new char[n / 2 + 1]).replace("\0", "수박").substring(0, n);
+class Entry implements Comparable<Entry>{
+    int key, cnt;
+
+    public Entry(int key, int cnt) {
+        this.key = key;
+        this.cnt = cnt;
+    }
+
+    @Override
+    public int compareTo(Entry o) {
+        return o.cnt - this.cnt;
+    }
+}
+class Solution {
+    public int[] solution(String s) {
+        HashMap<String, Integer> map = new HashMap<>();
+        ArrayList<Entry> list = new ArrayList<>();
+        s = s.replaceAll("[{|}]{1,}", "");
+        String[] sub = s.split(",");
+        for(int i = 0; i < sub.length; i++) {
+            map.put(sub[i], map.getOrDefault(sub[i], 0) + 1);
+        }
+        for(Map.Entry<String, Integer> e : map.entrySet()) {
+            list.add(new Entry(Integer.parseInt(e.getKey()), e.getValue()));
+        }
+        Collections.sort(list);
+
+        return list.stream().map(m -> m.key).mapToInt(i -> i.intValue()).toArray();
     }
 }
