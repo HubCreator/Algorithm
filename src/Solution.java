fef6dbdd38e76e171f47576837771ea99a283572
HubCreator <chocomilk8604@gmail.com>
2022-07-12 15:20:51 +0900

    수박수박수

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,6 @@
 class Solution {
-    public String solution(String s, int n) {
-        StringBuilder answer = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
-            if(Character.isAlphabetic(s.charAt(i))) {
-                char t = s.charAt(i);
-                int tmp = t + n;
+    public String solution(int n) {
 
-                if(Character.isUpperCase(t)) {
-                    if(tmp > 'Z') tmp -= 26;
-                } else if(Character.isLowerCase(t)) {
-                    if(tmp > 'z') tmp -= 26;
-                }
-                answer.append((char) tmp);
-            } else answer.append(" ");
-        }
-        return answer.toString();
+        return new String(new char[n / 2 + 1]).replace("\0", "수박").substring(0, n);
     }
 }
