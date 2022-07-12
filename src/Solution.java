7f81b7c7ddded325c80fa3d1903f4764b4f8ef80
HubCreator <chocomilk8604@gmail.com>
2022-07-12 10:53:55 +0900

    연속된 숫자 제거 Stack

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Stack;
 
-class Log {
-    String status, userId;
-
-    public Log(String status, String userId) {
-        this.status = status;
-        this.userId = userId;
-    }
-}
-
-class Solution {
-    List<Log> list;
-    HashMap<String, String> map;
-    private void writeLog(String r) {
-        String[] split = r.split(" ");
-        if(r.startsWith("Enter")) {
-            list.add(new Log(split[0], split[1])); // status와 userId를 기록
-            map.put(split[1], split[2]); // userId에 해당하는 userName을 저장
-        } else if (r.startsWith("Change")) {
-            map.put(split[1], split[2]); // userId에 해당하는 userName을 수정
-        } else if (r.startsWith("Leave")) {
-            list.add(new Log(split[0], split[1])); // status와 userId를 기록
-        }
-    }
-
-    public String[] solution(String[] record) {
-        ArrayList<String> answer = new ArrayList<>();
-        list = new ArrayList<>();
-        map = new HashMap<>();
-
-        for (String r : record) writeLog(r);
-        for (Log log : list) {
-            StringBuilder sb = new StringBuilder();
-            if (log.status.equals("Enter")) {
-                sb.append(map.get(log.userId)).append("님이 들어왔습니다.");
-            } else {
-                sb.append(map.get(log.userId)).append("님이 나갔습니다.");
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        for(int x : arr) {
+            if(stack.isEmpty()) stack.push(x);
+            else if(!stack.isEmpty() && stack.peek() != x) {
+                stack.push(x);
             }
-            answer.add(sb.toString());
         }
-        return answer.toArray(new String[0]);
+        return stack.stream().mapToInt(Integer::intValue).toArray();
     }
 }
