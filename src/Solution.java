d5f9c9affd5c26386e740bf3596626ad424c7a61
HubCreator <chocomilk8604@gmail.com>
2022-07-12 21:32:08 +0900

    다리를 지나는 트럭

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,27 @@
-public class Solution {
-    public int[] solution(int[][] v) {
-        int[] answer = new int[2];
-        answer[0] = v[0][0] ^ v[1][0] ^ v[2][0];
-        answer[1] = v[0][1] ^ v[1][1] ^ v[2][1];
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 1, curIdx = 1;
+        Queue<Integer> bridge = new LinkedList<>();
+        bridge.offer(truck_weights[0]);
+
+        int sum;
+        do {
+            if (bridge.size() >= bridge_length) bridge.poll();
+
+            sum = 0;
+            for (Integer x : bridge) sum += x;
+            if (curIdx < truck_weights.length && sum + truck_weights[curIdx] <= weight) {
+                bridge.offer(truck_weights[curIdx++]);
+            } else {
+                bridge.offer(0);
+            }
+            answer++;
+            sum = 0;
+            for (Integer x : bridge) sum += x;
+        } while (sum != 0);
 
         return answer;
     }
