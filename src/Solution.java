336b8e81d4fec798ae8b37dc949317562637b6e2
HubCreator <chocomilk8604@gmail.com>
2022-07-12 17:33:45 +0900

    온전한 사각형

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,9 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ArrayList;
+public class Solution {
+    public int[] solution(int[][] v) {
+        int[] answer = new int[2];
+        answer[0] = v[0][0] ^ v[1][0] ^ v[2][0];
+        answer[1] = v[0][1] ^ v[1][1] ^ v[2][1];
 
-class Entry implements Comparable<Entry>{
-    int key, cnt;
-
-    public Entry(int key, int cnt) {
-        this.key = key;
-        this.cnt = cnt;
-    }
-
-    @Override
-    public int compareTo(Entry o) {
-        return o.cnt - this.cnt;
-    }
-}
-class Solution {
-    public int[] solution(String s) {
-        HashMap<String, Integer> map = new HashMap<>();
-        ArrayList<Entry> list = new ArrayList<>();
-        s = s.replaceAll("[{|}]{1,}", "");
-        String[] sub = s.split(",");
-        for(int i = 0; i < sub.length; i++) {
-            map.put(sub[i], map.getOrDefault(sub[i], 0) + 1);
-        }
-        for(Map.Entry<String, Integer> e : map.entrySet()) {
-            list.add(new Entry(Integer.parseInt(e.getKey()), e.getValue()));
-        }
-        Collections.sort(list);
-
-        return list.stream().map(m -> m.key).mapToInt(i -> i.intValue()).toArray();
+        return answer;
     }
 }
