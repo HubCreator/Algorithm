7c69cdf16d1b3a9ed3ca260624c072d4f13425e2
HubCreator <chocomilk8604@gmail.com>
2022-07-12 11:54:11 +0900

    문자열 마음대로 정렬

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,28 @@
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.Comparator;
 
-public class Solution {
-    public int[] solution(int []arr) {
-        Stack<Integer> stack = new Stack<>();
-        for(int x : arr) {
-            if(stack.isEmpty()) stack.push(x);
-            else if(!stack.isEmpty() && stack.peek() != x) {
-                stack.push(x);
-            }
-        }
-        return stack.stream().mapToInt(Integer::intValue).toArray();
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = strings.clone();
+
+        Comparator<String> comp = (s1, s2) -> {
+            char str1 = s1.charAt(n);
+            char str2 = s2.charAt(n);
+
+            if (str1 > str2)
+                return 1;
+            else if (str1 < str2)
+                return -1;
+            else
+                return 0;
+        };
+
+        Arrays.sort(strings);
+        Arrays.sort(strings, comp);
+
+        for (int i = 0; i < strings.length; i++)
+            answer[i] = strings[i];
+
+        return answer;
     }
 }
