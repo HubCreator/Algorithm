0c6d045d969ba25193f02e74dfa2901475602891
HubCreator <chocomilk8604@gmail.com>
2022-07-13 14:27:33 +0900

    문자열 내림차순 정렬

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,10 @@
 import java.util.Arrays;
-import java.util.Collections;
 
-class Solution {
-    public int solution(int[] citations) {
-        Integer[] intArray = new Integer[citations.length];
-
-        for(int i = 0; i < citations.length; i++)
-            intArray[i] = citations[i];
-
-        Arrays.sort(intArray, Collections.reverseOrder());
-
-        for(int i = 0; i < intArray.length; i++) {
-            if(intArray[i] <= i+1)
-                return i;
-        }
-
-        return intArray.length;
+public class Solution {
+    public String reverseStr(String str){
+        char[] sol = str.toCharArray();
+        Arrays.sort(sol);
+        return new StringBuilder(new String(sol)).reverse().toString();
     }
+
 }
