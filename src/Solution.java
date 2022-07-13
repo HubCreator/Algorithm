0b0dbcfe32d2f777f66683a95228f4e30dbe097c
HubCreator <chocomilk8604@gmail.com>
2022-07-13 12:14:04 +0900

    자연수 뒤집어 배열로

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,11 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        char[][] board = new char[n][n];
-
-        for(int i = 0; i < n; i++) {
-            Long t1 = Long.parseLong(Integer.toBinaryString(arr1[i]).toString());
-            Long t2 = Long.parseLong(Integer.toBinaryString(arr2[i]).toString());
-
-            final String format = "%0"+n+"d";
-            String tmp1 = String.format(format, t1);
-            String tmp2 = String.format(format, t2);
-
-            for(int j = 0; j < n; j++) {
-                if(tmp1.charAt(j) == '1' || tmp2.charAt(j) == '1') board[i][j] = '#';
-                else if(tmp1.charAt(j) == '0' && tmp2.charAt(j) == '0') board[i][j] = ' ';
-            }
+    public int[] solution(long n) {
+        int[] answer = new int[(int)Math.log10(n) + 1];
+        String s = String.valueOf(n);
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = Integer.parseInt(String.valueOf(s.charAt(answer.length - 1 - i)));
         }
 
-        for(int i = 0; i < n; i++) {
-            answer[i] = new String(board[i]);
-        }
         return answer;
     }
 }
