35c0516c7c2447293b77ae089a5966e454e618ba
HubCreator <chocomilk8604@gmail.com>
2022-07-15 21:34:06 +0900

    이상한 문자열 만들기

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,13 @@
 class Solution {
     public String solution(String s) {
-        if(s.length() % 2 == 0) return new StringBuilder()
-                .append(s.charAt(s.length() / 2 - 1))
-                .append(s.charAt(s.length() / 2)).toString();
-        else return new StringBuilder()
-                .append(s.charAt(s.length() / 2)).toString();
+        char[] arr = s.toCharArray();
+        int tmp = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(tmp % 2 == 0) arr[i] = Character.toUpperCase(arr[i]);
+            else arr[i] = Character.toLowerCase(arr[i]);
+            tmp++;
+            if(Character.isSpace(arr[i])) tmp = 0;
+        }
+        return new String(arr);
     }
 }
