6afcf992406196f0a55729cda9f75f422a8857b4
HubCreator <chocomilk8604@gmail.com>
2022-07-17 13:28:57 +0900

    카카오 다트게임

--- src/Solution.java
+++ src/Solution.java
@@ -1,52 +1,35 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
-class Dungeon {
-    int requiredStem, stemina;
-    public Dungeon (int r, int s) {
-        this.requiredStem = r;
-        this.stemina = s;
-    }
-}
 class Solution {
-    int[] pm, ch;
-    int curStem, dungeonSize, answer = Integer.MIN_VALUE;
-    List<Dungeon> list;
-
-    public void DFS(int L) {
-        if (L == pm.length) {
-            int cnt = 1;
-            for (int x : pm) {
-                if(list.get(x - 1).requiredStem > curStem) break;
-                else {
-                    curStem -= list.get(x - 1).stemina;
-                    cnt++;
+    public int solution(String dartResult) {
+        int idx = 0;
+        int[] values = new int[3];
+        for (int i = 0; i < dartResult.length(); i++) {
+            char c = dartResult.charAt(i);
+            if (Character.isDigit(c)) {
+                if (c == '1' && Character.isDigit(dartResult.charAt(i + 1))) {
+                    values[idx] = 10;
+                    i++;
+                    continue;
                 }
-            }
-            answer = Math.max(answer, cnt);
-        } else {
-            for (int i = 1; i <= dungeonSize; i++) {
-                if(ch[i] == 0) {
-                    ch[i] = 1;
-                    pm[L] = i; // L은 0부터 인덱스의 숫자와 함께 올라감
-                    DFS(L + 1);
-                    ch[i] = 0;
+                values[idx] = Integer.parseInt(String.valueOf(c));
+            } else if (Character.isAlphabetic(c)) {
+                switch (c) {
+                    case 'D' -> values[idx] = (int) Math.pow(values[idx], 2);
+                    case 'T' -> values[idx] = (int) Math.pow(values[idx], 3);
                 }
+                if (i + 1 < dartResult.length() && Character.isDigit(dartResult.charAt(i + 1))) idx++;
+            } else {
+                switch (c) {
+                    case '*' -> {
+                        if (idx != 0) values[idx - 1] *= 2;
+                        values[idx] *= 2;
+                    }
+                    case '#' -> values[idx] *= -1;
+                }
+                idx++;
             }
         }
-    }
-
-    public int solution(int k, int[][] dungeons) {
-        curStem = k;
-        dungeonSize = dungeons.length;
-        pm = new int[dungeonSize];
-        ch = new int[dungeonSize + 1];
-        list = new ArrayList<>();
-        for(int[] arr : dungeons) {
-            list.add(new Dungeon(arr[0], arr[1]));
-        }
-        DFS(0);
-
-        return answer;
+        return Arrays.stream(values).sum();
     }
 }
