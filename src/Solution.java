389173b8ff1659322ec1e7da88819438b3484e60
HubCreator <chocomilk8604@gmail.com>
2022-07-22 22:20:07 +0900

    던전 피로도 DFS 보완

--- src/Solution.java
+++ src/Solution.java
@@ -1,52 +1,19 @@
-import java.util.List;
-import java.util.ArrayList;
-
-class Dungeon {
-    int curTired, needTired;
-
-    public Dungeon(int ct, int nt) {
-        this.curTired = ct;
-        this.needTired = nt;
-    }
-}
-
 class Solution {
-    int[] pm, ch;
-    int curStem, dungeonSize, answer = Integer.MIN_VALUE;
-    List<Dungeon> list;
-
-    public void DFS(int L) {
-        if (L == pm.length) {
-            int cnt = 0, tmp = curStem;
-            for (int x : pm) {
-                if (list.get(x - 1).curTired > tmp) break;
-                else {
-                    tmp -= list.get(x - 1).needTired;
-                    cnt++;
-                }
-            }
-            answer = Math.max(answer, cnt);
-        } else {
-            for (int i = 1; i <= dungeonSize; i++) {
-                if (ch[i] == 0) {
-                    ch[i] = 1;
-                    pm[L] = i;
-                    DFS(L + 1);
-                    ch[i] = 0;
-                }
+    int dfs(int k, int[][] dungeons) {
+        int cnt = 0;
+        for (int[] d : dungeons) {
+            int initialTired = d[0], needTired = d[1];
+            if (initialTired <= k) {
+                d[0] = Integer.MAX_VALUE;
+                cnt = Math.max(cnt, dfs(k - needTired, dungeons) + 1);
+                d[0] = initialTired;
             }
         }
+        return cnt;
     }
 
     public int solution(int k, int[][] dungeons) {
-        curStem = k;
-        dungeonSize = dungeons.length;
-        pm = new int[dungeonSize];
-        ch = new int[dungeonSize + 1];
-        list = new ArrayList<>();
-        for (int[] arr : dungeons) list.add(new Dungeon(arr[0], arr[1]));
-        DFS(0);
-        return answer;
+        return dfs(k, dungeons);
     }
 
     public static void main(String[] args) {
