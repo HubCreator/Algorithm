673d17151a64429b8cbff9cedf4aa04f261400f0
HubCreator <chocomilk8604@gmail.com>
2022-07-25 00:47:43 +0900

    정수 내림차순 정렬 stream

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,7 @@
-import java.util.*;
-
 class Solution {
     public long solution(long n) {
         StringBuilder sb = new StringBuilder();
-        String str = String.valueOf(n);
-        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i = 0; i < str.length(); i++)
-            pQ.offer(Integer.parseInt(String.valueOf(str.charAt(i))));
-        for(int i = 0; i < str.length(); i++)
-            sb.append(pQ.poll());
-
-        return Long.parseLong(sb.toString());
+        Long.toString(n).chars().sorted().forEach(c -> sb.append(Character.valueOf((char)c)));
+        return Long.parseLong(sb.reverse().toString());
     }
 }
