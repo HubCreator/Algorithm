0fa477687f7f055bc893bd08960b9b9dd33a0666
HubCreator <chocomilk8604@gmail.com>
2022-07-24 23:56:07 +0900

    정수 제곱근 판별

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,8 @@
 class Solution {
-    int dfs(int k, int[][] dungeons) {
-        int cnt = 0;
-        for (int[] d : dungeons) {
-            int initialTired = d[0], needTired = d[1];
-            if (initialTired <= k) {
-                d[0] = Integer.MAX_VALUE;
-                cnt = Math.max(cnt, dfs(k - needTired, dungeons) + 1);
-                d[0] = initialTired;
-            }
-        }
-        return cnt;
-    }
-
-    public int solution(int k, int[][] dungeons) {
-        return dfs(k, dungeons);
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int k = 80;
-        int[][] dungeons = {
-                {80, 20},
-                {50, 40},
-                {30, 10}
-        };
-        System.out.println(T.solution(k, dungeons));
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+        return (double)(sqrt % 1d) == 0d ?
+                (long)((sqrt + 1) * (sqrt + 1)) :
+                -1l;
     }
 }
