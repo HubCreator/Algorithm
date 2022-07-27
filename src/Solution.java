e651e1e52eff7307eafb5429d13fe583d3ca38d5
HubCreator <chocomilk8604@gmail.com>
2022-07-27 11:55:14 +0900

    가장 큰 정수 구하기 Comparator

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,16 @@
+import java.util.Arrays;
+
 class Solution {
-    int[] nums, pm, ch;
-    int answer = Integer.MIN_VALUE;
-    private void dfs(int L) {
-        if (L == pm.length) {
-            StringBuilder sb = new StringBuilder();
-            for (int x : pm) sb.append(nums[x]);
-            answer = Math.max(answer, Integer.parseInt(sb.toString()));
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if(ch[i] == 0) {
-                    ch[i] = 1;
-                    pm[L] = i;
-                    dfs(L + 1);
-                    ch[i] = 0;
-                }
-            }
-        }
-    }
-
     public String solution(int[] numbers) {
-        nums = numbers;
-        pm = new int[numbers.length];
-        ch = new int[numbers.length];
-        dfs(0);
-        return Integer.toString(answer);
-    }
+        StringBuilder answer = new StringBuilder();
+        Arrays.stream(numbers).boxed().sorted((a, b) -> {
+            int s1 = Integer.parseInt(a.toString().concat(b.toString()));
+            int s2 = Integer.parseInt(b.toString().concat(a.toString()));
+            if (s1 < s2) return 1;
+            else if (s1 > s2) return -1;
+            else return 0;
+        }).forEach(answer::append);
 
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int[] arr = {1, 1, 1, 1, 1, 1, 1, 2, 2, 3};
-        System.out.println(T.solution(arr));
+        return Double.parseDouble(answer.toString()) == 0 ? "0" : answer.toString();
     }
 }
