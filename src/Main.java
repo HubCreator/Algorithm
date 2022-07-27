f95e4baa549ce398c54ee1b55baf0b6603fdfd83
HubCreator <chocomilk8604@gmail.com>
2022-07-27 10:57:46 +0900

    가장 큰 정수 구하기 DFS

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,57 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.function.Function;
+
+class Person {
+    String forename;
+    String surname;
+    LocalDate birthday;
+    String gender;
+    String emailAddress;
+    int age;
+
+    public Person(String forename, String surname, LocalDate birthday, String gender, String emailAddress, int age) {
+        this.forename = forename;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "forename='" + forename + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+@FunctionalInterface
+interface PersonFactory {
+    Person create(String forename, String surname,
+                  LocalDate birthday, String gender, String emailAddress, int age);
+}
+
 
 public class Main {
+    private void initialize(List<Person> list, PersonFactory factory,
+                            String forename, String surname, LocalDate birthday, String gender,
+                            String emailAddress, int age) {
+        for (int i = 0; i < 10; i++) {
+            list.add(factory.create(forename, surname, birthday, gender, emailAddress, age));
+        }
+    }
+
     public static void main(String[] args) {
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
-        set.add(2);
-        set.add(3);
-        set.add(4);
-        Iterator<Integer> it = set.iterator();
-        while (it.hasNext()) {
-            System.out.print(it.next() + " ");
+        Main T = new Main();
+        PersonFactory factory = Person::new;
+        ArrayList<Person> list = new ArrayList<>();
+        T.initialize(list, factory, "a", "b", LocalDate.parse("2000-11-21"), "asdf", "asf@naver.com", 1);
+        for (Person person : list) {
+            System.out.println(person);
         }
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,34 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public int solution(String str1, String str2) {
-        int sum = 0;
-        double common = 0;
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
-        for (int i = 0; i < str1.length() - 1; i++) {
-            String s = str1.substring(i, i + 2);
-            if (s.matches("[a-zA-Z]{2}"))
-                map1.put(s.toLowerCase(), map1.getOrDefault(s.toLowerCase(), 0) + 1);
+    int[] nums, pm, ch;
+    int answer = Integer.MIN_VALUE;
+    private void dfs(int L) {
+        if (L == pm.length) {
+            StringBuilder sb = new StringBuilder();
+            for (int x : pm) sb.append(nums[x]);
+            answer = Math.max(answer, Integer.parseInt(sb.toString()));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if(ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = i;
+                    dfs(L + 1);
+                    ch[i] = 0;
+                }
+            }
         }
-        for (int i = 0; i < str2.length() - 1; i++) {
-            String s = str2.substring(i, i + 2);
-            if (s.matches("[a-zA-Z]{2}"))
-                map2.put(s.toLowerCase(), map2.getOrDefault(s.toLowerCase(), 0) + 1);
-        }
+    }
 
-        if (!map1.isEmpty()) {
-            for (String s : map1.keySet()) {
-                if(!map2.isEmpty() && map2.containsKey(s)) sum += Math.max(map1.get(s), map2.get(s));
-                else sum += map1.get(s);
-            }
-            for (String s : map2.keySet()) {
-                if(!map1.containsKey(s)) sum += map2.get(s);
-            }
-        }
-        if (!map1.isEmpty()) {
-            for (String s : map1.keySet()) {
-                if (!map2.isEmpty() && map2.containsKey(s)) common += Math.min(map1.get(s), map2.get(s));
-            }
-        }
-        return map1.isEmpty() && map2.isEmpty() ? 65536 : (int)(common / sum * 65536);
+    public String solution(int[] numbers) {
+        nums = numbers;
+        pm = new int[numbers.length];
+        ch = new int[numbers.length];
+        dfs(0);
+        return Integer.toString(answer);
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int[] arr = {1, 1, 1, 1, 1, 1, 1, 2, 2, 3};
+        System.out.println(T.solution(arr));
     }
 }
