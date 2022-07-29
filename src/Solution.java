634175604404289031d9b770b7b5a4068c21123f
HubCreator <chocomilk8604@gmail.com>
2022-07-29 18:37:43 +0900

    핸드폰 번호 가리기 regex 보완

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,5 @@
 class Solution {
     public String solution(String phone_number) {
-        int cnt = phone_number.length() - 4;
-        StringBuilder stars = new StringBuilder();
-        for(int i = 1; i <= cnt; i++) stars.append('*');
-        return phone_number.replaceAll("^[0-9]{0,"+cnt+"}", stars.toString());
+        return phone_number.replaceAll(".(?=.{4})", "*");
     }
 }
