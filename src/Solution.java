d897afb08936c4aa8dd5f092e943c9fabe339661
HubCreator <chocomilk8604@gmail.com>
2022-07-29 22:36:44 +0900

    자릿수 더하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,11 @@
-class Solution {
-    public String solution(String phone_number) {
-        return phone_number.replaceAll(".(?=.{4})", "*");
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true){
+            answer += n % 10; // 1의 자리를 answer에 count
+            if (n < 10) break;
+            n = n / 10; // 10으로 나눈 몫으로 갱신
+        }
+        return answer;
     }
 }
