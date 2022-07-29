9edebcb0267e7975cf1e83db6a924e6cfbcc5285
HubCreator <chocomilk8604@gmail.com>
2022-07-29 18:33:18 +0900

    핸드폰 번호 가리기

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,8 @@
 class Solution {
-    public long solution(int n, int[] times) {
-        long lt = 0, rt = (long) 1e18;
-        long answer = (long) 1e18;
-        while (lt <= rt) {
-            long cnt = 0;
-            long mid = (lt + rt) / 2;
-            for (int time : times) {
-                cnt += mid / time;
-                if (cnt >= n) {
-                    rt = mid - 1;
-                    answer = mid;
-                    break;
-                }
-            }
-            if (cnt < n) lt = mid + 1;
-        }
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int[] arr = {1, 5};
-        System.out.println(T.solution(10, arr));
+    public String solution(String phone_number) {
+        int cnt = phone_number.length() - 4;
+        StringBuilder stars = new StringBuilder();
+        for(int i = 1; i <= cnt; i++) stars.append('*');
+        return phone_number.replaceAll("^[0-9]{0,"+cnt+"}", stars.toString());
     }
 }
