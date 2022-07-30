f5523a736733bfe8f248d2a3e383654a061440a9
HubCreator <chocomilk8604@gmail.com>
2022-07-30 17:10:55 +0900

    구명보트 greedy with ArrayList (timeout)

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,24 @@
-class Solution {
-    public int[][] solution(int[][] a, int[][] b) {
-        int[][] answer = new int[a.length][a[0].length];
-        for (int i = 0; i < a.length; i++) answer[i] = a[i].clone();
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
-                answer[i][j] += b[i][j];
-            }
+class Solution {
+    public int solution(int[] people, int limit) {
+        List<Integer> list = new ArrayList<>();
+        for (int p : people) list.add(p);
+        list.sort(Collections.reverseOrder());
+
+        int answer = 0;
+        while (!list.isEmpty()) {
+            Integer last = list.get(list.size() - 1);
+            if (last * 2 > limit) return answer + list.size();
+
+            Integer t = list.remove(0);
+            if (list.isEmpty()) return answer + 1;
+
+            if (t + last <= limit) list.remove(last);
+            answer++;
         }
         return answer;
     }
