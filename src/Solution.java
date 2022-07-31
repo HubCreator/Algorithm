4944ed4120d7765f915015b8d6229cfaaaa466a4
HubCreator <chocomilk8604@gmail.com>
2022-07-31 12:44:28 +0900

    124 나라의 숫자 보완

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,12 @@
 class Solution {
-    static StringBuilder sb = new StringBuilder();
-
-    static void hello(int n) {
-        if (n == 1) {
-            sb.append("1");
-        } else if (n == 2) {
-            sb.append("2");
-        } else if (n == 3 || n == 0) {
-            sb.append("4");
-        } else {
-            hello((n - 1) / 3);
-            hello(n % 3);
-        }
-    }
-
     public String solution(int n) {
-        hello(n);
-        return sb.toString();
-    }
+        String[] num = {"4","1","2"};
+        StringBuilder answer = new StringBuilder();
 
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        System.out.println(T.solution(10));
+        while(n > 0){
+            answer.append(num[n % 3]);
+            n = (n - 1) / 3;
+        }
+        return answer.reverse().toString();
     }
 }
