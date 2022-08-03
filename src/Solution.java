18f2377e1ac5327f3514b4f275478ab60a626c7a
HubCreator <chocomilk8604@gmail.com>
2022-08-03 17:08:50 +0900

    카카오 순위 검색 보완. HashMap with sorting, dfs, binarySearch

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,57 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Person {
-    String lang, area, role, favor;
-    int score;
-
-    public Person(String lang, String area, String role, String favor, int score) {
-        this.lang = lang;
-        this.area = area;
-        this.role = role;
-        this.favor = favor;
-        this.score = score;
-    }
-
-    public boolean isFulfilled(String lang, String area, String role, String tmp) {
-        String favor = tmp.split(" ")[0];
-        int score = Integer.parseInt(tmp.split(" ")[1]);
-
-        if (score > this.score) return false;
-        if (!lang.equals("-") && !lang.equals(this.lang)) return false;
-        if (!area.equals("-") && !area.equals(this.area)) return false;
-        if (!role.equals("-") && !role.equals(this.role)) return false;
-        if (!favor.equals("-") && !favor.equals(this.favor)) return false;
-
-        return true;
-    }
-}
+import java.util.*;
 
 class Solution {
-    List<Person> people;
+    static HashMap<String, ArrayList<Integer>> map;
+    static int score;
+    static String[] strings, sInfoArr;
 
-    public int[] solution(String[] info, String[] query) {
-        people = new ArrayList<>();
+    static void dfs(int L) { // key를 이룰 수 있는 모든 경우의 수를 구함
+        if (L == 4) {
+            String str = String.join("", strings);
+            if (!map.containsKey(str)) map.put(str, new ArrayList<>());
+            map.get(str).add(score);
+        } else {
+            strings[L] = sInfoArr[L];
+            dfs(L + 1);
+            strings[L] = "-";
+            dfs(L + 1);
+        }
+    }
+
+    static int binarySearch(ArrayList<Integer> list, int val) {
+        int left = 0, right = list.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (list.get(mid) < val) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+
+    public static int[] solution(String[] info, String[] query) {
+        int idx = 0;
         int[] answer = new int[query.length];
+        map = new HashMap<>();
+
         for (String s : info) {
-            String[] arr = s.split(" ");
-            people.add(new Person(arr[0], arr[1], arr[2], arr[3], Integer.parseInt(arr[4])));
+            strings = new String[4];
+            sInfoArr = s.split(" ");
+            score = Integer.parseInt(sInfoArr[4]);
+            dfs(0);
         }
 
-        for (int i = 0; i < query.length; i++) {
-            String[] arr = query[i].split(" and ");
-            int cnt = 0;
-            for (Person p : people) {
-                if (p.isFulfilled(arr[0], arr[1], arr[2], arr[3])) cnt++;
+        for (String key : map.keySet()) Collections.sort(map.get(key)); // 점수들이 들어있는 value(ArrayList)에 대해 오름차순 정렬
+
+        for (String q : query) {
+            String[] strs = q.split(" and | ");
+            String key = new StringBuilder(strs[0]).append(strs[1]).append(strs[2]).append(strs[3]).toString();
+
+            if (!map.containsKey(key)) answer[idx++] = 0;
+            else {
+                ArrayList<Integer> list = map.get(key);
+                answer[idx++] = list.size() - binarySearch(list, Integer.parseInt(strs[4]));
             }
-            answer[i] = cnt;
         }
         return answer;
     }
