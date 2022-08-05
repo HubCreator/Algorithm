29ccab77a630db4c538ca0309115b358a25a19c0
HubCreator <chocomilk8604@gmail.com>
2022-08-05 11:26:07 +0900

    x만큼 간격이 있는 n개의 숫자

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,9 @@
-import java.util.ArrayList;
-
 class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        for(int x : arr) {
-            if(x % divisor == 0) answer.add(x);
+    public long[] solution(long x, int n) {
+        long[] answer = new long[n];
+        for (int i = 1; i <= n; i++) {
+            answer[i - 1] = x * i;
         }
-        return answer.isEmpty() ?
-                new int[]{-1} :
-                answer.stream().sorted().mapToInt(i->i.intValue()).toArray();
+        return answer;
     }
 }
