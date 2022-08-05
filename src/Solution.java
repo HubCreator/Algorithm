3408ab0b6712664b367396fb2c1db69c14207c20
HubCreator <chocomilk8604@gmail.com>
2022-08-05 11:17:25 +0900

    나누어 떨어지는 숫자 배열

--- src/Solution.java
+++ src/Solution.java
@@ -1,57 +1,13 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
-class Point {
-    int y, x, val;
-
-    public Point(int y, int x, int val) {
-        this.y = y;
-        this.x = x;
-        this.val = val;
-    }
-}
+import java.util.ArrayList;
 
 class Solution {
-    static int[][] pic;
-    static int[] dy;
-    static int[] dx;
-
-    private int bfs(int m, int n, Point p) {
-        int sum = 0;
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(p);
-        pic[p.y][p.x] = 0;
-
-        while (!queue.isEmpty()) {
-            Point poll = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int ny = poll.y + dy[i];
-                int nx = poll.x + dx[i];
-                if (ny >= 0 && ny < m && nx >= 0 && nx < n && pic[ny][nx] == p.val) {
-                    pic[ny][nx] = 0;
-                    queue.offer(new Point(ny, nx, pic[ny][nx]));
-                }
-            }
-            sum++;
+    public int[] solution(int[] arr, int divisor) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int x : arr) {
+            if(x % divisor == 0) answer.add(x);
         }
-        return sum;
-    }
-
-    public int[] solution(int m, int n, int[][] picture) {
-        int numberOfArea = 0, maxSizeOfOneArea = Integer.MIN_VALUE;
-        dx = new int[]{-1, 0, 1, 0};
-        dy = new int[]{0, 1, 0, -1};
-        pic = new int[picture.length][picture[0].length];
-        for (int i = 0; i < picture.length; i++) pic[i] = picture[i].clone();
-
-        for (int i = 0; i < pic.length; i++) {
-            for (int j = 0; j < pic[i].length; j++) {
-                if (pic[i][j] != 0) {
-                    numberOfArea++;
-                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea, bfs(m, n, new Point(i, j, pic[i][j])));
-                }
-            }
-        }
-        return new int[]{numberOfArea, maxSizeOfOneArea};
+        return answer.isEmpty() ?
+                new int[]{-1} :
+                answer.stream().sorted().mapToInt(i->i.intValue()).toArray();
     }
 }
