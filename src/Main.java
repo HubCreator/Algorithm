678a5775c09092324f0eaee353526dcd7e282d0d
HubCreator <chocomilk8604@gmail.com>
2022-08-10 18:47:57 +0900

    점프와 순간이동

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
-public class Main {
+import java.util.Deque;
+import java.util.LinkedList;
+
+public  class Main {
     public static void main(String[] args) {
-        int a = 10;
-        String b = Integer.toString(a, 2);
-        System.out.println(b);
-        System.out.println(Integer.parseInt(b, 2));
+        Deque<Integer> deque1 = new LinkedList<>();
+        Deque<Integer> deque2 = new LinkedList<>();
+        Deque<Integer> deque3 = new LinkedList<>();
+
+        for (int i = 0; i < 10; i++) deque1.offer(i);
+        for (int i = 0; i < 10; i++) deque2.offerFirst(i);
+        for (int i = 0; i < 10; i++) deque3.offerLast(i);
+
+
+        for (Integer x : deque1) {
+            System.out.print(x + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for (Integer x : deque2) {
+            System.out.print(x + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for (Integer x : deque3) {
+            System.out.print(x + " ");
+        }
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,13 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
-
-class Solution {
-    private final static int CACHE_HIT = 1;
-    private final static int CACHE_MISS = 5;
-
-    public int solution(int cacheSize, String[] cities) {
-        if (cacheSize == 0) return cities.length * CACHE_MISS;
+public class Solution {
+    public int solution(int n) {
         int answer = 0;
-        Deque<String> deque = new LinkedList<>();
-        for (String tmp : cities) {
-            String city = tmp.toLowerCase();
-            if (deque.contains(city)) {
-                deque.remove(city);
-                deque.offerFirst(city);
-                answer += CACHE_HIT;
-            } else {
-                if (deque.size() == cacheSize) deque.pollLast();
-                deque.offerFirst(city);
-                answer += CACHE_MISS;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                n--;
+                answer++;
             }
+            n /= 2;
         }
 
         return answer;
