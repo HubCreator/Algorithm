4ba1687b6d01252a81c3f4ad7a0d8afcfba2c8e2
HubCreator <chocomilk8604@gmail.com>
2022-08-15 20:31:19 +0900

    두 개 이하로 다른 비트 개선

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,8 @@ import java.util.LinkedList;
 
 public  class Main {
     public static void main(String[] args) {
-        Deque<Integer> deque1 = new LinkedList<>();
-        Deque<Integer> deque2 = new LinkedList<>();
-        Deque<Integer> deque3 = new LinkedList<>();
-
-        for (int i = 0; i < 10; i++) deque1.offer(i);
-        for (int i = 0; i < 10; i++) deque2.offerFirst(i);
-        for (int i = 0; i < 10; i++) deque3.offerLast(i);
-
-
-        for (Integer x : deque1) {
-            System.out.print(x + " ");
-        }
-
-        System.out.println();
-        System.out.println();
-
-        for (Integer x : deque2) {
-            System.out.print(x + " ");
-        }
-
-        System.out.println();
-        System.out.println();
-
-        for (Integer x : deque3) {
-            System.out.print(x + " ");
-        }
+        int a = 2, b = 6;
+        String s = Long.toBinaryString(a ^ b);
+        System.out.println("s = " + s);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,58 +1,19 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
-    Map<String, ArrayList<Integer>> map;
-    String[] strings, sInfoArr;
-    int score;
-
-    private void dfs(int L) {
-        if (L == 4) {
-            String str = String.join("", strings); // String 배열을 모두 합쳐 String으로 반환
-            if (!map.containsKey(str)) map.put(str, new ArrayList<>()); // value가 ArrayList<>일 때 핸들링
-            map.get(str).add(score); // 얕은 복사임 (map.put(str, map.get(str).add(score))를 하면 안됨)
-        } else {
-            strings[L] = sInfoArr[L];
-            dfs(L + 1);
-            strings[L] = "-";
-            dfs(L + 1);
-        }
-    }
-
-    private int binarySearch(ArrayList<Integer> list, int val) { // list 요소 중 val 이상인 요소의 최소 인덱스를 찾아라
-        int lt = 0, rt = list.size() - 1;
-        int mid;
-        while (lt <= rt) {
-            mid = (lt + rt) / 2;
-            if (list.get(mid) < val) lt = mid + 1;
-            else rt = mid - 1;
-        }
-        return lt;
-    }
-
-    public int[] solution(String[] info, String[] query) {
-        int idx = 0;
-        int[] answer = new int[query.length];
-        map = new HashMap<>();
-        // 모든 경우의수로 Map을 구성
-        for (String s : info) {
-            strings = new String[4];
-            sInfoArr = s.split(" ");
-            score = Integer.parseInt(sInfoArr[4]);
-            dfs(0);
-        }
-
-        for (String key : map.keySet()) Collections.sort(map.get(key)); // 각 키에 달려있는 ArrayList의 요소들을 오름차순 정렬함
-
-        for (String q : query) {
-            String[] strs = q.split(" and | ");
-            String key = new StringBuilder(strs[0]).append(strs[1]).append(strs[2]).append(strs[3]).toString();
-            if (!map.containsKey(key)) answer[idx++] = 0;
-            else {
-                ArrayList<Integer> list = map.get(key);
-                answer[idx++] = list.size() - binarySearch(list, Integer.parseInt(strs[4]));
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            for (long j = numbers[i] + 1; ; j++) {
+                char[] chars = Long.toBinaryString(numbers[i] ^ j).toCharArray();
+                int sum = 0;
+                for (char c : chars) {
+                    if (c == '1') sum++;
+                    if (sum > 2) break;
+                }
+                if (sum <= 2) {
+                    answer[i] = j;
+                    break;
+                }
             }
         }
         return answer;
