b6df0b058662e02e016eff1817a2e2b103aa231e
HubCreator <chocomilk8604@gmail.com>
2022-08-17 19:59:00 +0900

    메뉴 리뉴얼

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,75 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
-    int[] ch = new int[5];
-    PriorityQueue<String> pqueue = new PriorityQueue<>();
+    int[] ch;
+    List<Character> list = new ArrayList<>();
+    List<String> entry = new ArrayList<>();
 
-    private void dfs(int L) {
+    private void dfs(int L, int s) {
         if (L == ch.length) {
             StringBuilder sb = new StringBuilder();
-            for (int x : ch) {
-                if (x == 1) sb.append('A');
-                else if (x == 2) sb.append('E');
-                else if (x == 3) sb.append('I');
-                else if (x == 4) sb.append('O');
-                else if (x == 5) sb.append('U');
-            }
-            if (!pqueue.contains(sb.toString())) pqueue.offer(sb.toString());
+            for (int x : ch) sb.append(list.get(x));
+            entry.add(sb.toString());
         } else {
-            ch[L] = 0;
-            dfs(L + 1);
-            ch[L] = 1;
-            dfs(L + 1);
-            ch[L] = 2;
-            dfs(L + 1);
-            ch[L] = 3;
-            dfs(L + 1);
-            ch[L] = 4;
-            dfs(L + 1);
-            ch[L] = 5;
-            dfs(L + 1);
+            for (int i = s; i < list.size(); i++) {
+                ch[L] = i;
+                dfs(L + 1, i + 1);
+            }
         }
     }
 
-    public int solution(String word) {
-        int answer = -1;
+    public String[] solution(String[] orders, int[] course) {
+        List<String> answer = new ArrayList<>();
+        List<Integer> courseList = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
 
-        dfs(0);
+        for (int x : course) courseList.add(x);
+        Arrays.sort(orders);
+        for (String order : orders)
+            for (int i = 0; i < order.length(); i++)
+                if (list.isEmpty() || !list.contains(order.charAt(i)))
+                    list.add(order.charAt(i));
 
-        while (!pqueue.isEmpty()) {
-            String poll = pqueue.poll();
-            answer++;
-            if (poll.equals(word)) break;
+        for (int i : course) {
+            ch = new int[i];
+            entry.clear();
+            map.clear();
+            dfs(0, 0);
+
+            for (String e : entry) {
+                String[] split = e.split("");
+                Arrays.sort(split);
+                String join = String.join("", split);
+
+                for (String order : orders) {
+                    if(join.equals(order)) continue;
+                    boolean flag = true;
+                    for (String s : split) {
+                        if (!order.contains(s)) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag) map.put(join, map.getOrDefault(join, 0) + 1);
+                }
+            }
+            if (map.isEmpty()) continue;
+            int max = Collections.max(map.values());
+            for (Map.Entry<String, Integer> e : map.entrySet()) {
+                if (e.getValue().equals(max) && courseList.contains(e.getValue())) answer.add(e.getKey());
+            }
         }
-        return answer;
+
+        Collections.sort(answer);
+        return answer.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        String[] arr1 = {"XYZ", "XWY", "WXA"};
+        int[] arr2 = {2, 3, 4};
+        for (String s : T.solution(arr1, arr2)) {
+            System.out.println("s = " + s);
+        }
     }
 }
