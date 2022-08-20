e4ae3fbe07174b9c9bca3f5b5d441f1f5d507527
HubCreator <chocomilk8604@gmail.com>
2022-08-20 18:23:48 +0900

    순열

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
-import java.util.Deque;
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public  class Main {
-    public static void main(String[] args) {
-        int a = 2, b = 6;
-        String s = Long.toBinaryString(a ^ b);
-        System.out.println("s = " + s);
+public class Main {
+    static private int n, m;
+    static int[] arr;
+
+    private static void pm(int L) {
+        if (L == m) {
+            for (int x : arr) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i = 1; i <= n; i++) {
+                arr[L] = i;
+                pm(L + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        pm(0);
     }
 }
