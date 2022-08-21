49f9039c130cd3b50fd5de1974e2266cff4f49c9
HubCreator <chocomilk8604@gmail.com>
2022-08-21 21:46:21 +0900

    순열

--- src/Solution.java
+++ src/Solution.java
@@ -1,56 +1,38 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
-    List<List<Integer>> list = new ArrayList<>();
-    Map<Integer, Integer> map = new HashMap<>();
-    int max = Integer.MIN_VALUE;
+    private int[] arr, pm, ch;
+    private int n, m;
 
-    private void bfs(int val) {
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[val + 1];
-        visited[1] = true;
-        for (int i = 0; i < list.get(1).size(); i++) { // 1과 연결된 노드들 모두 queue에 집어 넣음
-            Integer t = list.get(1).get(i);
-            visited[t] = true;
-            map.put(t, 1);
-            queue.offer(t);
-        }
-
-        while (!queue.isEmpty()) {
-            Integer p = queue.poll();
-            if (!visited[p]) { // 인접 노드를 찾아야 함
-                List<Integer> tmpList = list.get(p);
-                int tmpMin = Integer.MAX_VALUE;
-                for (Integer x : tmpList) {
-                    if (map.containsKey(x)) {
-                        tmpMin = Math.min(tmpMin, map.get(x));
-                    }
+    private void dfs(int L) {
+        if (L == m) {
+            for (int x : pm) System.out.print(arr[x] + " ");
+            System.out.println();
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (ch[i] == 0) {
+                    ch[L] = 1;
+                    pm[L] = i;
+                    dfs(L + 1);
+                    ch[L] = 0;
                 }
-                map.put(p, tmpMin + 1);
-                max = Math.max(max, tmpMin + 1);
-                visited[p] = true;
-            }
-            for (int j = 0; j < list.get(p).size(); j++) {
-                Integer t = list.get(p).get(j);
-                if (!visited[t] && !queue.contains(t)) queue.offer(t);
             }
         }
     }
 
-    public int solution(int n, int[][] edge) {
-        int answer = 0, tmp = Integer.MIN_VALUE;
+    public void solution(int _n, int _m, int[] _arr) {
+        arr = _arr.clone();
+        n = _n;
+        m = _m;
+        ch = new int[n];
+        pm = new int[m];
 
-        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
-        for (int[] e : edge) {
-            list.get(e[0]).add(e[1]);
-            list.get(e[1]).add(e[0]);
-        }
+        dfs(0);
+    }
 
-        bfs(n);
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        T.solution(3, 2, new int[]{3, 6, 9});
 
-        for (Integer x : map.values()) tmp = Math.max(tmp, x);
-        for (Integer x : map.values()) if (x == tmp) answer++;
-
-        return answer;
     }
 }
