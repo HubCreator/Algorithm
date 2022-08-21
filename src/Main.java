a9f750ab653fa3546c682f61305558e45c44a3f6
HubCreator <chocomilk8604@gmail.com>
2022-08-21 21:05:20 +0900

    거스름돈 걸러주기 dfs 중복 허용

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static private int n, m;
-    static int[] arr;
+    static private Integer[] arr;
+    static private int m, answer = Integer.MAX_VALUE;
 
-    private static void pm(int L) {
-        if (L == m) {
-            for (int x : arr) System.out.print(x + " ");
-            System.out.println();
-        } else {
-            for (int i = 1; i <= n; i++) {
-                arr[L] = i;
-                pm(L + 1);
-            }
+    private void dfs(int L, int sum) {
+        if (sum > m || L >= answer) return;
+        if (sum == m) answer = Math.min(answer, L);
+        else {
+            for (Integer x : arr) dfs(L + 1, sum + x);
         }
     }
 
+    private int solution() {
+        dfs(0, 0);
+
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new Integer[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[m];
-        pm(0);
+        for (int i = 0; i < arr.length; i++) arr[i] = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        System.out.println(T.solution());
     }
 }
