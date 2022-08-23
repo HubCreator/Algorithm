f17dd19b018634c8afb2e4cd1f45f7a8d7c6203b
HubCreator <chocomilk8604@gmail.com>
2022-08-23 11:39:56 +0900

    두 배열 공통 원소 오름차순 정렬. two-pointer

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,24 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public int[] solution(int n, int[] arr) {
+    public int[] solution(int n, int[] _arr1, int m, int[] _arr2) {
         List<Integer> answer = new ArrayList<>();
-        for (int i : arr) answer.add(i);
-        Collections.sort(answer, Collections.reverseOrder());
+        int[] arr1 = _arr1.clone();
+        int[] arr2 = _arr2.clone();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
 
-        for (int i : arr) System.out.print(answer.indexOf(i) + 1 + " ");
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (arr1[p1] == arr2[p2]) {
+                answer.add(arr1[p1]);
+                p1++;
+                p2++;
+            } else if (arr1[p1] < arr2[p2]) p1++;
+            else p2++;
+        }
 
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
@@ -16,7 +26,12 @@ class Solution {
     public static void main(String[] args) {
         Solution T = new Solution();
         int n = 5;
-        int[] arr = {87, 92, 92, 100, 76};
-        for (int x : T.solution(n, arr)) System.out.print(x + " ");
+        int[] arr1 = {1, 3, 9, 5, 2};
+        int m = 5;
+        int[] arr2 = {3, 2, 5, 7, 8};
+
+        for (int x : T.solution(n, arr1, m, arr2)) {
+            System.out.print(x + " ");
+        }
     }
 }
