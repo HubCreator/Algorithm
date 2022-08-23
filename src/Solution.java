92428bb8829385df3a4dc613eb7406173c54f9a3
HubCreator <chocomilk8604@gmail.com>
2022-08-23 11:47:47 +0900

    최대 매출액 슬라이딩 윈도우

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 class Solution {
-    public int[] solution(int n, int[] _arr1, int m, int[] _arr2) {
-        List<Integer> answer = new ArrayList<>();
-        int[] arr1 = _arr1.clone();
-        int[] arr2 = _arr2.clone();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+    // k일간 최대 매출액
+    public int solution(int n, int k, int[] arr) {
+        int sum = 0, max = Integer.MIN_VALUE;
+        // 초기 윈도우 설정
+        for (int i = 0; i < k - 1; i++) sum += arr[i];
 
-        int p1 = 0, p2 = 0;
-        while (p1 < n && p2 < m) {
-            if (arr1[p1] == arr2[p2]) {
-                answer.add(arr1[p1]);
-                p1++;
-                p2++;
-            } else if (arr1[p1] < arr2[p2]) p1++;
-            else p2++;
+        for (int i = k - 1; i < n; i++) {
+            sum += arr[i];
+            max = Math.max(max, sum);
+            sum -= arr[i - k + 1];
         }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return max;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        int n = 5;
-        int[] arr1 = {1, 3, 9, 5, 2};
-        int m = 5;
-        int[] arr2 = {3, 2, 5, 7, 8};
-
-        for (int x : T.solution(n, arr1, m, arr2)) {
-            System.out.print(x + " ");
-        }
+        int n = 10;
+        int k = 3;
+        int[] arr = {20, 15, 11, 20, 25, 10, 20, 19, 13, 15};
+        System.out.println(T.solution(n, k, arr));
     }
 }
