79945035bfef453ae5abcc0f1521c2e5db14b009
HubCreator <chocomilk8604@gmail.com>
2022-08-23 13:45:36 +0900

    괄호 문자 제거

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,27 @@
+import java.util.Stack;
 
 class Solution {
-    // k일간 최대 매출액
-    public int solution(int n, int k, int[] arr) {
-        int sum = 0, max = Integer.MIN_VALUE;
-        // 초기 윈도우 설정
-        for (int i = 0; i < k - 1; i++) sum += arr[i];
-
-        for (int i = k - 1; i < n; i++) {
-            sum += arr[i];
-            max = Math.max(max, sum);
-            sum -= arr[i - k + 1];
+    public String solution(String str) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char t = str.charAt(i);
+            if (Character.isAlphabetic(t) | t == '(') stack.push(t);
+            else {
+                Character pop;
+                do {
+                    pop = stack.pop();
+                } while (pop != '(');
+            }
         }
-        return max;
+        while (!stack.isEmpty()) sb.append(stack.pop());
+
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        int n = 10;
-        int k = 3;
-        int[] arr = {20, 15, 11, 20, 25, 10, 20, 19, 13, 15};
-        System.out.println(T.solution(n, k, arr));
+        String tmp = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+        System.out.println(T.solution(tmp));
     }
 }
