443cd04666608689f8d5861bd162ba2b3eaec66e
HubCreator <chocomilk8604@gmail.com>
2022-08-24 16:02:24 +0900

    숫자 문자열과 영단어

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,10 @@
-
 class Solution {
     public int solution(String s) {
-        int answer = s.length();
-
-        for (int size = 1; size <= s.length() / 2; size++) {
-            int idx = 0, length = s.length(), cnt = 1;
-            String tmp = "";
-            StringBuilder sb = new StringBuilder();
-            while (idx <= s.length()) {
-                int len = Math.min(idx + size, length);
-                String substring = s.substring(idx, len);
-                if (tmp.equals(substring)) cnt++;
-                else {
-                    if (cnt > 1) sb.append(cnt).append(tmp);
-                    else sb.append(tmp);
-                    cnt = 1;
-                }
-                idx += size;
-                tmp = substring;
-            }
-            if (cnt > 1) sb.append(cnt).append(tmp);
-            else sb.append(tmp);
-
-            answer = Math.min(answer, sb.toString().length());
+        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String tmp = s;
+        for (int i = 0; i < 10; i++) {
+            tmp = tmp.replaceAll(nums[i], String.valueOf(i));
         }
-        return answer;
+        return Integer.parseInt(tmp);
     }
 }
