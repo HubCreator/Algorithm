374c1b361656518eacb4bca85811bf074d7185e3
HubCreator <chocomilk8604@gmail.com>
2022-08-24 16:24:39 +0900

    이진 변환 반복하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,15 @@
 class Solution {
-    public int solution(String s) {
-        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String tmp = s;
-        for (int i = 0; i < 10; i++) {
-            tmp = tmp.replaceAll(nums[i], String.valueOf(i));
+    public int[] solution(String s) {
+        int round = 0, cnt = 0;
+        while(!s.equals("1")) {
+            int len = s.length();
+            // 1단계
+            s = s.replaceAll("0", "");
+            cnt += len - s.length();
+            // 2단계
+            s = Integer.toBinaryString(s.length());
+            round++;
         }
-        return Integer.parseInt(tmp);
+        return new int[]{round, cnt};
     }
 }
