db5562eaee70730f7416e339651938cf326d3e3b
HubCreator <chocomilk8604@gmail.com>
2022-08-24 14:44:15 +0900

    카카오 문자열 압축

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
-    public int[] solution(int[] lottos, int[] win_nums) {
-        int cnt = 0, blank = 0;
-        int[] prize = {6, 6, 5, 4, 3, 2, 1};
-        List<Integer> winNumsList = new ArrayList<>();
-        for (int x : win_nums) winNumsList.add(x);
-        for (int lotto : lottos) {
-            if (winNumsList.contains(lotto)) cnt++;
-            if (lotto == 0) blank++;
+    public int solution(String s) {
+        int answer = s.length();
+        Queue<String> queue = new LinkedList<>();
+
+        for (int i = 1; i <= s.length() / 2; i++) {
+            int idx = 0, j = 0;
+            StringBuilder str = new StringBuilder();
+            queue.clear();
+            while (idx < s.length()) {
+                StringBuilder sb = new StringBuilder();
+                int len = j + i;
+                for (; j < len && idx < s.length(); j++) {
+                    sb.append(s.charAt(idx++));
+                }
+                queue.offer(sb.toString());
+            }
+
+            String pop1 = queue.poll();
+            int cnt = 1;
+            while (!queue.isEmpty()) {
+                String pop2 = queue.poll();
+                if (pop1.equals(pop2)) cnt++;
+                else {
+                    if (cnt > 1) str.append(cnt).append(pop1);
+                    else str.append(pop1);
+                    cnt = 1;
+                }
+                pop1 = pop2;
+            }
+            if (cnt > 1) str.append(cnt).append(pop1);
+            else str.append(pop1);
+            answer = Math.min(answer, str.toString().length());
         }
-        return new int[]{prize[cnt + blank], prize[cnt]};
+        return answer;
     }
 }
