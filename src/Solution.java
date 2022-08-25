91e3df997a925cd51b840b670cdfa710dcba070b
HubCreator <chocomilk8604@gmail.com>
2022-08-25 15:36:28 +0900

    두 큐의 합 같게 만들기

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,38 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
-    public int solution(int left, int right) {
-        int answer = 0;
-        for (int i = left; i <= right; i++) {
-            if (i % Math.sqrt(i) == 0) answer -= i;
-            else answer += i;
+    public int solution(int[] queue1, int[] queue2) {
+        long sum1 = 0, sum2 = 0, cnt = 0;
+
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        for (int x : queue1) {
+            sum1 += x;
+            q1.offer(x);
+        }
+        for (int x : queue2) {
+            sum2 += x;
+            q2.offer(x);
         }
 
-        return answer;
+        if (sum1 == sum2) return 0;
+        while (sum1 != sum2 && cnt < queue1.length * 2.6) {
+            Integer p;
+            if (sum1 > sum2) {
+                p = q1.poll();
+                q2.offer(p);
+                sum1 -= p;
+                sum2 += p;
+            } else {
+                p = q2.poll();
+                q1.offer(p);
+                sum1 += p;
+                sum2 -= p;
+            }
+            cnt++;
+        }
+
+        return cnt >= queue1.length * 2.6 ? -1 : (int) cnt;
     }
 }
