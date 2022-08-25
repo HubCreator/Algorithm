c4825ecafe507261cebc3ebbcd68add79234e5c5
HubCreator <chocomilk8604@gmail.com>
2022-08-25 12:50:08 +0900

    기능개발 배포 Queue

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,42 @@
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Job {
+    int progress, speed;
+
+    public Job(int progress, int speed) {
+        this.progress = progress;
+        this.speed = speed;
+    }
+}
 
 class Solution {
-    public int solution(int[] scoville, int K) {
-        int answer = 0;
-        PriorityQueue<Integer> pqueue = new PriorityQueue<>();
-        for (int x : scoville) pqueue.offer(x);
-        while (pqueue.size() >= 2 && pqueue.peek() < K) {
-            pqueue.offer(pqueue.poll() + pqueue.poll() * 2);
-            answer++;
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Job> queue = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+        int cnt;
+        for (int i = 0; i < progresses.length; i++) {
+            queue.offer(new Job(progresses[i], speeds[i]));
         }
-        return pqueue.peek() >= K ? answer : -1;
+        while (!queue.isEmpty()) {
+            cnt = 0;
+            for (Job job : queue) job.progress += job.speed;
+            while (!queue.isEmpty() && queue.peek().progress >= 100) {
+                queue.poll();
+                cnt++;
+            }
+            if (cnt > 0) answer.add(cnt);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int[] arr1 = {95, 90, 99, 99, 80, 99};
+        int[] arr2 = {1, 1, 1, 1, 1, 1};
+        for (int x : T.solution(arr1, arr2)) System.out.print(x + " ");
     }
 }
