a5859cc80d1f21d9f665ac9fff25056ac75c109e
HubCreator <chocomilk8604@gmail.com>
2022-08-26 15:45:08 +0900

    카카오 오픈 채티방 로그 기록

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,39 @@
-import java.util.Arrays;
+import java.util.*;
+
+class User {
+    String uid, action;
+
+    public User(String uid, String action) {
+        this.uid = uid;
+        this.action = action;
+    }
+}
 
 class Solution {
-    public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+    private static final String IN = "님이 들어왔습니다.";
+    private static final String OUT = "님이 나갔습니다.";
+
+    public String[] solution(String[] record) {
+        List<String> answer = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+        List<User> list = new LinkedList<>();
+        for (String s : record) {
+            String[] split = s.split(" ");
+            if (split[0].startsWith("L")) {
+                list.add(new User(split[1], split[0]));
+            } else if (split[0].startsWith("C")) {
+                map.put(split[1], split[2]);
+            } else {
+                map.put(split[1], split[2]);
+                list.add(new User(split[1], split[0]));
+            }
         }
-        return answer;
+        for (User user : list) {
+            if (user.action.startsWith("E"))
+                answer.add(new StringBuilder(map.get(user.uid)).append(IN).toString());
+            else answer.add(new StringBuilder(map.get(user.uid)).append(OUT).toString());
+        }
+
+        return answer.toArray(new String[0]);
     }
 }
