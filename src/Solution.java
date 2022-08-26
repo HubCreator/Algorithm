e011cef8fef8e0598c3284df4a93d536d30dbf9f
HubCreator <chocomilk8604@gmail.com>
2022-08-26 11:59:49 +0900

    무지의 먹방 라이브 객체화로 개선

--- src/Solution.java
+++ src/Solution.java
@@ -1,45 +1,40 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+class Food {
+    int id, time;
+
+    public Food(int id, int time) {
+        this.id = id;
+        this.time = time;
+    }
+
+    public void reduceTime() {
+        this.time--;
+    }
+
+    public boolean isOne() {
+        return this.time == 1;
+    }
+}
 
 class Solution {
     public int solution(int[] times, long k) {
-        int cur = -1;
-        double size = times.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= size; i++) {
-            map.put(i, times[i - 1]);
-            list.add(i);
+        int idx = 0;
+        List<Food> list = new ArrayList<>();
+        for (int i = 0; i < times.length; i++) {
+            list.add(new Food(i + 1, times[i]));
+        }
+        while (k > 0L) {
+            if (list.get(idx).isOne())
+                list.remove(idx--);
+            else
+                list.get(idx).reduceTime();
+            idx++;
+            if (idx == list.size()) idx = 0;
+            k -= 1L;
         }
 
-        while (k >= 0) {
-            cur++;
-            if (cur == size + 1) cur = 1;
-            if (!list.contains(cur)) {
-                double lt = 0;
-                double rt = list.size() - 1;
-                while (lt <= rt) {
-                    int mid = (int) ((lt + rt) / 2);
-                    if (list.get(mid) < cur) lt = mid + 1;
-                    else rt = mid - 1;
-                }
-                cur = list.get((int) lt);
-            }
-            if (map.get(cur) == 1) {
-                map.remove(cur);
-                list.remove((Integer) cur);
-            } else map.put(cur, map.get(cur) - 1);
-            k--;
-        }
-        return cur;
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int[] arr = {4, 2, 3, 6, 7, 1, 5, 8};
-        int k = 27;
-        System.out.println(T.solution(arr, k));
+        return list.isEmpty() ? -1 : list.get(idx).id;
     }
 }
