06f8ae79b80a7ded3f5d39d6383e5b0df9346cb3
HubCreator <chocomilk8604@gmail.com>
2022-08-27 18:56:19 +0900

    문자열 다루기 기본

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 class Solution {
-    public String[] solution(String[] orders, int[] course) {
-        List<String> answer = new ArrayList<>();
-        int idx = 0;
-        List<Integer> courseList = new ArrayList<>();
-        List<String> list = new ArrayList<>();
-        for (int x : course) courseList.add(x);
-        Collections.addAll(list, orders);
-        Collections.sort(list, (a, b) -> a.length() - b.length());
-        while (idx < list.size()) {
-            String s = list.get(idx);
-            int cnt = 0;
-            if (courseList.contains(s.length())) {
-                for (int i = idx + 1; i < list.size(); i++) {
-                    if (list.get(i).contains(s)) cnt++;
-                }
-            }
-            if (cnt > 0) answer.add(s);
-            idx++;
-        }
-        for (String s : answer) {
-            System.out.println("s = " + s);
-        }
-
-
-        return null;
+    public boolean solution(String s) {
+        return s.matches("[0-9]{4}") || s.matches("[0-9]{6}");
     }
 }
