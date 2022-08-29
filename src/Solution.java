cb45e82c455a23a18e2bc101adcbf254cb98cf7d
HubCreator <chocomilk8604@gmail.com>
2022-08-29 14:59:18 +0900

    시저 암호

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,15 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
-    public int solution(String[][] clothes) {
-        int answer = 1;
-        Map<String, List<String>> map = new HashMap<>();
-        for (String[] clothe : clothes) {
-            if (map.get(clothe[1]) == null) map.put(clothe[1], new ArrayList<>());
-            map.get(clothe[1]).add(clothe[0]);
+    public String solution(String s, int n) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isAlphabetic(s.charAt(i))) {
+                char t = s.charAt(i);
+                if (Character.isUpperCase(t) && t + n > 'Z') t -= 26;
+                else if (Character.isLowerCase(t) && t + n > 'z') t -= 26;
+                answer.append((char) (t + n));
+            } else answer.append(" ");
         }
-        for (Map.Entry<String, List<String>> e : map.entrySet()) {
-            answer *= (e.getValue().size() + 1); // 각 옷을 입는 경우의 수와 모든 옷을 입지 않는 경우의 수
-        }
-        return answer - 1; // 옷을 적어도 하나 입고 있어야 하기 때문에, 모두 입고 있지 않는 경우의 수를 하나 빼줌
+
+        return answer.toString();
     }
 }
