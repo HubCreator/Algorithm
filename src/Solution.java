a6044126e68315bdae8987ec3c40476a9b071683
HubCreator <chocomilk8604@gmail.com>
2022-08-30 15:50:05 +0900

    비밀지도

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,28 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
-class Truck {
-    int weight, move;
-
-    public Truck(int weight) {
-        this.weight = weight;
-        this.move = 1;
-    }
-
-    public void gogo() {
-        this.move++;
-    }
-}
-
 class Solution {
-    public int solution(int bridgeLen, int weight, int[] weights) {
-        int curWeight = 0, answer = 0;
-        Queue<Truck> waitQ = new LinkedList<>();
-        Queue<Truck> moveQ = new LinkedList<>();
-        for (int w : weights) waitQ.offer(new Truck(w));
+    private static final int size = 5;
 
-        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
-            answer++;
-            if (moveQ.isEmpty()) {
-                Truck p = waitQ.poll();
-                curWeight += p.weight;
-                moveQ.offer(p);
-                continue;
-            }
-            for (Truck t : moveQ) t.gogo(); // 모두 움직여
-            if (moveQ.peek().move > bridgeLen) { // 제일 앞에 놈이 다리 길이보다 길면 빼야함
-                Truck p = moveQ.poll();
-                curWeight -= p.weight;
-            }
-            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) { // 다리 위에 추가적으로 올릴 수 있으면 올려라
-                Truck p = waitQ.poll();
-                curWeight += p.weight;
-                moveQ.offer(p);
-            }
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String[] str1 = new String[n], str2 = new String[n];
+        String format = "%0" + n + "d";
+        for (int i = 0; i < n; i++) {
+            String s = String.format(format, Long.parseLong(Integer.toBinaryString(arr1[i])));
+            str1[i] = s;
+        }
+        for (int i = 0; i < n; i++) {
+            String s = String.format(format, Long.parseLong(Integer.toBinaryString(arr2[i])));
+            str2[i] = s;
         }
 
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                if (str1[i].charAt(j) == '1' || str2[i].charAt(j) == '1') {
+                    sb.append(1);
+                } else sb.append(0);
+            }
+            answer[i] = sb.toString().replaceAll("1", "#").replaceAll("0", " ");
+        }
         return answer;
     }
 }
