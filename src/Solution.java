aaa9b73a21b187e43d7b1e309cb108a71913f6be
HubCreator <chocomilk8604@gmail.com>
2022-08-30 15:17:04 +0900

    다리를 지나는 트럭

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,46 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Truck {
+    int weight, move;
+
+    public Truck(int weight) {
+        this.weight = weight;
+        this.move = 1;
+    }
+
+    public void gogo() {
+        this.move++;
+    }
+}
 
 class Solution {
-    public int[] solution(String s) {
-        Set<String> set = new HashSet<>();
-        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
-        Arrays.sort(arr, (a, b) -> a.length() - b.length());
-        int[] answer = new int[arr.length];
-        int idx = 0;
-        for (String s1 : arr) {
-            for (String s2 : s1.split(",")) {
-                System.out.print(s2 + " ");
-                if (set.add(s2)) answer[idx++] = Integer.parseInt(s2); // set 컬렉션에 이미 요소가 있으면 false 반환
+    public int solution(int bridgeLen, int weight, int[] weights) {
+        int curWeight = 0, answer = 0;
+        Queue<Truck> waitQ = new LinkedList<>();
+        Queue<Truck> moveQ = new LinkedList<>();
+        for (int w : weights) waitQ.offer(new Truck(w));
+
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+            answer++;
+            if (moveQ.isEmpty()) {
+                Truck p = waitQ.poll();
+                curWeight += p.weight;
+                moveQ.offer(p);
+                continue;
             }
-            System.out.println();
+            for (Truck t : moveQ) t.gogo(); // 모두 움직여
+            if (moveQ.peek().move > bridgeLen) { // 제일 앞에 놈이 다리 길이보다 길면 빼야함
+                Truck p = moveQ.poll();
+                curWeight -= p.weight;
+            }
+            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) { // 다리 위에 추가적으로 올릴 수 있으면 올려라
+                Truck p = waitQ.poll();
+                curWeight += p.weight;
+                moveQ.offer(p);
+            }
         }
+
         return answer;
     }
 }
