2c8e570ab242e6e50c1a427b043abbd45b63514b
HubCreator <chocomilk8604@gmail.com>
2022-08-31 21:42:07 +0900

    스킬트리 개선

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,11 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = skill_trees.length;
-        for (String tree : skill_trees) {
-            int idx = 0, tmp = -1;
-            for (int i = 0; i < skill.length(); i++) {
-                int e = tree.indexOf(skill.charAt(i));
-                if (e != -1) {
-                    if (tree.charAt(e) != skill.charAt(idx)) {
-                        answer--;
-                        break;
-                    }
-                    if (tmp < e) {
-                        tmp = e;
-                        idx++;
-                    } else {
-                        answer--;
-                        break;
-                    }
-                }
-            }
-        }
+
+        for (String tree : skill_trees)
+            if (skill.indexOf(tree.replaceAll("[^" + skill + "]", "")) != 0)
+                answer--;
+
         return answer;
     }
 }
