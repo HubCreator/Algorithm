a60e4d951a43e86885d14022e7ee4cd72fb02bb9
HubCreator <chocomilk8604@gmail.com>
2022-09-01 15:29:34 +0900

    던전 피로도 dfs

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,19 @@
 class Solution {
-    public int solution(String skill, String[] skill_trees) {
-        int answer = skill_trees.length;
+    private int dfs(int k, int[][] dungeons) {
+        int cnt = 0;
+        for (int[] dungeon : dungeons) {
+            int required = dungeon[0];
+            int using = dungeon[1];
+            if (k >= required) {
+                dungeon[0] = Integer.MAX_VALUE; // 해당 던전을 중복하여 돌지 않도록 체크
+                cnt = Math.max(cnt, dfs(k - using, dungeons) + 1);
+                dungeon[0] = required; // 해당 던전을 다시 돌 수 있도록 원복
+            }
+        }
+        return cnt;
+    }
 
-        for (String tree : skill_trees)
-            if (skill.indexOf(tree.replaceAll("[^" + skill + "]", "")) != 0)
-                answer--;
-
-        return answer;
+    public int solution(int k, int[][] dungeons) {
+        return dfs(k, dungeons); // 전역변수를 두지 않고, 매개변수로 넘기는 것도 나름 깔끔하네
     }
 }
