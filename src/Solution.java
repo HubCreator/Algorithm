2a79c0e14631327941708fa50b32c1dd026bb5a4
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-05 21:44:21 +0900

    정수 제곱근 판별

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,8 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(String dartResult) {
-        int idx = 0;
-        int[] values = new int[3];
-        for (int i = 0; i < dartResult.length(); i++) {
-            char c = dartResult.charAt(i);
-            if (Character.isDigit(c)) {
-                if (c == '1' && Character.isDigit(dartResult.charAt(i + 1))) {
-                    values[idx] = 10;
-                    i++;
-                    continue;
-                }
-                values[idx] = Integer.parseInt(String.valueOf(c));
-            } else if (Character.isAlphabetic(c)) {
-                switch (c) {
-                    case 'D' -> values[idx] = (int) Math.pow(values[idx], 2);
-                    case 'T' -> values[idx] = (int) Math.pow(values[idx], 3);
-                }
-                if (i + 1 < dartResult.length() && Character.isDigit(dartResult.charAt(i + 1))) idx++;
-            } else {
-                switch (c) {
-                    case '*' -> {
-                        if (idx != 0) values[idx - 1] *= 2;
-                        values[idx] *= 2;
-                    }
-                    case '#' -> values[idx] *= -1;
-                }
-                idx++;
-            }
+    public long solution(long n) {
+        if (Math.pow((int) Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
         }
-        return Arrays.stream(values).sum();
+        return -1;
     }
 }
