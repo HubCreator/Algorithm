87f7bbd8afb13a0d93030eda86d19c3f2a14b587
HubCreator <chocomilk8604@gmail.com>
2022-09-07 16:00:10 +0900

    가장 큰 정사각형 찾기

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,44 @@
-import java.util.Arrays;
-
 class Solution {
-    public int[] solution(int[] arr) {
-        if (arr.length == 1) return new int[]{-1};
+    private boolean isAllZero(int[][] board) {
+        boolean flag = true;
+        for (int[] col : board) {
+            for (int row : col) {
+                if (row == 1) return false;
+            }
+        }
+        return flag;
+    }
 
-        int[] answer = new int[arr.length - 1];
-        int min = Arrays.stream(arr).min().getAsInt(), idx = 0;
-
-        for (int x : arr) if (x != min) answer[idx++] = x;
-
+    public int solution(int[][] board) {
+        if (isAllZero(board)) return 0;
+        int answer = Integer.MIN_VALUE;
+        int maxSize = Math.min(board.length, board[0].length);
+        for (int i = 1; i <= maxSize; i++) { // 정사각형의 size
+            Loop1:
+            for (int j = 0; j <= board.length - i; j++) { // y 좌표
+                for (int k = 0; k <= board[j].length - i; k++) { // x 좌표
+                    int sum = 0;
+                    Loop2:
+                    for (int l = j; l < j + i; l++) { // 해당 좌표로 부터 정사각형 범위 내에 있는 값들
+                        for (int m = k; m < k + i; m++) {
+                            if (board[l][m] == 0) break Loop2;
+                            sum += board[l][m];
+                        }
+                    }
+                    if (Math.pow(i, 2) == sum) {
+                        answer = sum;
+                        break Loop1;
+                    }
+                }
+            }
+        }
         return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int[][] arr = {{0, 0, 1, 1}, {1, 1, 1, 1}};
+        System.out.println(T.solution(arr));
     }
 }
