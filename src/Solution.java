2f6b5f73a8f3f36e5a2ddfe4baa2ded6fecb8fad
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-09 14:33:15 +0900

    JadenCase 문자열 만들기 개선

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder answer = new StringBuilder(s.toLowerCase());
-        if (Character.isLowerCase(answer.charAt(0)))
-            answer.replace(0, 1, String.valueOf(Character.toUpperCase(answer.charAt(0))));
-        for (int i = 1; i < answer.length(); i++) {
-            if (answer.charAt(i - 1) == ' ' && Character.isLowerCase(answer.charAt(i))) {
-                answer.replace(i, i + 1, String.valueOf(Character.toUpperCase(answer.charAt(i))));
-            }
+        StringBuilder answer = new StringBuilder();
+        String[] arr = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for (String ss : arr) {
+            if (flag) answer.append(ss.toUpperCase());
+            else answer.append(ss);
+            flag = ss.equals(" ");
         }
 
         return answer.toString();
