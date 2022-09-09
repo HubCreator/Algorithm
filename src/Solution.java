743a0fa08e7521cda9624a57a17a9dabba3d133c
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-09 11:58:01 +0900

    뉴스 클러스터링

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    private boolean isAllZero(int[][] board) {
-        for (int[] col : board) {
-            for (int row : col) {
-                if (row == 1) return false;
+    public int solution(String str1, String str2) {
+        int sum = 0;
+        double common = 0;
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+        for (int i = 0; i < str1.length() - 1; i++) {
+            String s = str1.substring(i, i + 2);
+            if (s.matches("[a-zA-Z]{2}"))
+                map1.put(s.toLowerCase(), map1.getOrDefault(s.toLowerCase(), 0) + 1);
+        }
+        for (int i = 0; i < str2.length() - 1; i++) {
+            String s = str2.substring(i, i + 2);
+            if (s.matches("[a-zA-Z]{2}"))
+                map2.put(s.toLowerCase(), map2.getOrDefault(s.toLowerCase(), 0) + 1);
+        }
+
+        if (!map1.isEmpty()) {
+            for (String s : map1.keySet()) {
+                if(!map2.isEmpty() && map2.containsKey(s)) sum += Math.max(map1.get(s), map2.get(s));
+                else sum += map1.get(s);
+            }
+            for (String s : map2.keySet()) {
+                if(!map1.containsKey(s)) sum += map2.get(s);
             }
         }
-        return true;
-    }
-
-    private int isBoardSizeOne(int[][] board) {
-        if (board.length == 1 && board[0].length == 1) {
-            return board[0][0] == 1 ? 1 : 0;
-        }
-        return -1;
-    }
-
-    public int solution(int[][] board) {
-        if (isAllZero(board)) return 0;
-        int boardSizeOne = isBoardSizeOne(board);
-        if (boardSizeOne >= 0) return boardSizeOne;
-        int answer = Integer.MIN_VALUE;
-        for (int i = 1; i < board.length; i++) { // y 좌표
-            for (int j = 1; j < board[0].length; j++) { // x 좌표
-                if (board[i][j] == 0) continue;
-                board[i][j] = Math.min(Math.min(board[i][j - 1], board[i - 1][j]), board[i - 1][j - 1]) + 1;
-                answer = Math.max(answer, board[i][j]);
+        if (!map1.isEmpty()) {
+            for (String s : map1.keySet()) {
+                if (!map2.isEmpty() && map2.containsKey(s)) common += Math.min(map1.get(s), map2.get(s));
             }
         }
-        return (int) Math.pow(answer, 2);
+        return map1.isEmpty() && map2.isEmpty() ? 65536 : (int)(common / sum * 65536);
     }
 }
