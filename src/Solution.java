761324afaff3d0a299b59abd4167af9400602c48
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-11 12:30:47 +0900

    배달 플루이드-워셜

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,36 @@
 class Solution {
-    public String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        String[] arr = s.toLowerCase().split("");
-        boolean flag = true;
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+        int[][] arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) {
+                    arr[i][j] = 0;
+                    continue;
+                }
+                arr[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int[] r : road) {
+            int v1 = r[0] - 1;
+            int v2 = r[1] - 1;
+            int cost = r[2];
 
-        for (String ss : arr) {
-            if (flag) answer.append(ss.toUpperCase());
-            else answer.append(ss);
-            flag = ss.equals(" ");
+            arr[v1][v2] = Math.min(arr[v1][v2], cost);
+            arr[v2][v1] = Math.min(arr[v2][v1], cost);
         }
 
-        return answer.toString();
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[0][j] <= K) answer++;
+        }
+        return answer;
     }
 }
