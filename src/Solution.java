5cbe81878ca3780ba43210f5b43749f8167c3347
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-12 20:42:35 +0900

    최솟값 만들기

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
-    public int solution(int N, int[][] road, int K) {
+    public int solution(int[] A, int[] B) {
         int answer = 0;
-        int[][] arr = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i == j) {
-                    arr[i][j] = 0;
-                    continue;
-                }
-                arr[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        for (int[] r : road) {
-            int v1 = r[0] - 1;
-            int v2 = r[1] - 1;
-            int cost = r[2];
-
-            arr[v1][v2] = Math.min(arr[v1][v2], cost);
-            arr[v2][v1] = Math.min(arr[v2][v1], cost);
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[B.length - i - 1];
         }
 
-        for (int k = 0; k < N; k++) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
-                }
-            }
-        }
-
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[0][j] <= K) answer++;
-        }
         return answer;
     }
 }
