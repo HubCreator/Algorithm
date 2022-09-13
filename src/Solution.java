38ad51a23c13f568f4c6e984258771c8c5aaff0a
HubCreator <chocomilk8604@gmail.com>
2022-09-13 13:49:57 +0900

    최고의 집합 개선, 수정

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,14 @@
 class Solution {
-    int[] ch, arr;
-    int cnt = 0, max = Integer.MIN_VALUE, maxEl = 0;
-
-    private void dfs(int L, int s, int target) {
-        if (L == ch.length) {
-            int sum = 0, mult = 1, maxElement = Integer.MIN_VALUE;
-            for (int x : ch) {
-                if (x == 0) break;
-                sum += arr[x];
-                mult *= arr[x];
-                if (x > maxElement) maxElement = x;
-            }
-            if (sum == target) {
-                cnt++;
-                for (int x : ch) System.out.print(x + " ");
-                System.out.println();
-                if (mult > max) {
-                    max = mult;
-                    maxEl = maxElement;
-                }
-            }
-        } else {
-            for (int i = s; i < arr.length; i++) {
-                ch[L] = i;
-                dfs(L + 1, i + 1, target);
-            }
-        }
-    }
-
     public int[] solution(int n, int s) {
         if (n > s) return new int[]{-1};
-        arr = new int[s + 1];
-        for (int i = 0; i <= s; i++) arr[i] = i;
-        ch = new int[n];
-        dfs(0, 0, s);
-        if (s % n == 0) {
-            cnt++;
-            maxEl = s / n;
+        int[] answer = new int[n];
+        int mid = s / n;
+        int sum = s - (mid * n);
+        System.out.println("sum = " + sum);
+        for (int i = 0; i < n; i++) {
+            if (i >= n - sum) answer[i] = mid + 1;
+            else answer[i] = mid;
         }
-        return new int[]{cnt, maxEl};
+        return answer;
     }
 }
