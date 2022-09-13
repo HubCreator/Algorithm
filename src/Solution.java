ad7e190b7ee6f61434f809e4a4371d07b7e1e22e
HubCreator <chocomilk8604@gmail.com>
2022-09-13 13:18:50 +0900

    최고의 집합

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,42 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        for (int i = 1; n - i * (i + 1) / 2 >= 0; i++) { // i = 자릿수
-            int sum = i * (i + 1) / 2;
-            if ((n - sum) % i == 0) answer++;
+    int[] ch, arr;
+    int cnt = 0, max = Integer.MIN_VALUE, maxEl = 0;
+
+    private void dfs(int L, int s, int target) {
+        if (L == ch.length) {
+            int sum = 0, mult = 1, maxElement = Integer.MIN_VALUE;
+            for (int x : ch) {
+                if (x == 0) break;
+                sum += arr[x];
+                mult *= arr[x];
+                if (x > maxElement) maxElement = x;
+            }
+
+            if (sum == target) {
+                cnt++;
+                for (int x : ch) System.out.print(x + " ");
+                System.out.println();
+                if (mult > max) {
+                    max = mult;
+                    maxEl = maxElement;
+                }
+            }
+
+        } else {
+            for (int i = s; i < arr.length; i++) {
+                ch[L] = i;
+                dfs(L + 1, i + 1, target);
+            }
         }
-        return answer;
+    }
+
+    public int[] solution(int n, int s) {
+        if (n > s) return new int[]{-1};
+        arr = new int[s + 1];
+        for (int i = 0; i <= s; i++) arr[i] = i;
+        ch = new int[n];
+        dfs(0, 0, s);
+
+        return new int[]{cnt, maxEl};
     }
 }
