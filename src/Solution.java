a0d8da292a1d7c255cf3038471eeb5db5b9eee77
HubCreator <chocomilk8604@gmail.com>
2022-09-14 14:46:36 +0900

    입국심사 이분검색

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,27 @@
-import java.util.Arrays;
-
 class Solution {
-    public String solution(int[] numbers) {
-        StringBuilder answer = new StringBuilder();
-        Arrays.stream(numbers).boxed().sorted((a, b) -> {
-            Integer s1 = Integer.parseInt(a.toString().concat(b.toString()));
-            Integer s2 = Integer.parseInt(b.toString().concat(a.toString()));
-            return s2.compareTo(s1);
-        }).forEach(answer::append);
+    public long solution(int n, int[] times) {
+        long lt = 0, rt = (long) 1e18;
+        long answer = 0;
+        while (lt <= rt) { // 범위 내에서 계속해서 찾아나감
+            long mid = (lt + rt) / 2; // mid = 총 소요 시간
+            long cnt = 0;             // cnt = 각 심사대에서 몇 명의 사람을 받을 수 있는가 (mid / time)
+            for (int time : times) {
+                cnt += mid / time;
+                if (cnt >= n) {
+                    rt = mid - 1;
+                    answer = mid;
+                    break;
+                }
+            }
+            if (cnt < n) lt = mid + 1;
+        }
+        return answer;
+    }
 
-        return Double.parseDouble(answer.toString()) == 0 ? "0" : answer.toString();
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int n = 6;
+        int[] arr = {7, 10};
+        System.out.println(T.solution(n, arr));
     }
 }
