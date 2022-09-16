059599d61d831386c15ba24a3780ec29a2654ec9
HubCreator <chocomilk8604@gmail.com>
2022-09-16 11:08:37 +0900

    카카오 컬러링북

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        String[] arr = new String[]{"1", "100", "90"};
-        Arrays.sort(arr);
-        for (String s : arr) {
-            System.out.println("s = " + s);
-        }
+        int a = 10;
+        String s = Integer.toString(a, 2);
+        String s1 = Long.toString(a, 2);
+        System.out.println("s = " + s);
+        System.out.println("s1 = " + s1);
+        int x = Integer.parseInt(s, 3);
+        System.out.println("x = " + x);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,52 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        while (n > 0) {
-            answer += n % 10;
-            n /= 10;
+class Point {
+    int y, x, val;
+
+    public Point(int y, int x, int val) {
+        this.y = y;
+        this.x = x;
+        this.val = val;
+    }
+}
+
+class Solution {
+    int[] dy = {-1, 0, 1, 0};
+    int[] dx = {0, 1, 0, -1};
+
+    private int bfs(int y, int x, int[][] picture) {
+        Queue<Point> queue = new LinkedList<>();
+        int cnt = 1;
+        queue.offer(new Point(y, x, picture[y][x]));
+        picture[y][x] = 0;
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dy[i];
+                int nx = p.x + dx[i];
+                if (ny >= 0 && ny < picture.length && nx >= 0 && nx < picture[0].length && picture[ny][nx] == p.val) {
+                    queue.offer(new Point(ny, nx, p.val));
+                    picture[ny][nx] = 0;
+                    cnt++;
+                }
+            }
         }
-        return answer;
+        return cnt;
+    }
+
+    public int[] solution(int m, int n, int[][] picture) {
+        int numberOfArea = 0, maxSizeOfOneArea = Integer.MIN_VALUE;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] > 0) {
+                    numberOfArea++;
+                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea, bfs(i, j, picture));
+                }
+            }
+        }
+
+        return new int[]{numberOfArea, maxSizeOfOneArea};
     }
 }
