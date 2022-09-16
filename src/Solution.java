ea1f9008cff37516c27e9fadbdfcc8b9356b979e
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-16 17:25:53 +0900

    바둑이 dfs 기본

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,34 @@
-import java.util.LinkedList;
-import java.util.Queue;
 
 class Solution {
-    public int solution(int[] queue1, int[] queue2) {
-        long sum1 = 0, sum2 = 0, cnt = 0;
+    int[] ch;
+    int answer = Integer.MIN_VALUE;
 
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        for (int x : queue1) {
-            sum1 += x;
-            q1.offer(x);
-        }
-        for (int x : queue2) {
-            sum2 += x;
-            q2.offer(x);
-        }
-
-        if (sum1 == sum2) return 0;
-        while (sum1 != sum2 && cnt < queue1.length * 2.6) {
-            Integer p;
-            if (sum1 > sum2) {
-                p = q1.poll();
-                q2.offer(p);
-                sum1 -= p;
-                sum2 += p;
-            } else {
-                p = q2.poll();
-                q1.offer(p);
-                sum1 += p;
-                sum2 -= p;
+    private void dfs(int L, int c, int[] arr) {
+        if (L == arr.length) {
+            int sum = 0;
+            for (int i = 0; i < ch.length; i++) {
+                if (ch[i] == 1) sum += arr[i];
             }
-            cnt++;
+            if (sum <= c) answer = Math.max(answer, sum);
+        } else {
+            ch[L] = 1;
+            dfs(L + 1, c, arr);
+            ch[L] = 0;
+            dfs(L + 1, c, arr);
         }
+    }
 
-        return cnt >= queue1.length * 2.6 ? -1 : (int) cnt;
+    public int solution(int c, int[] arr) {
+        ch = new int[c];
+        dfs(0, c, arr);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int c = 259;
+        int[] arr = {81, 58, 42, 33, 61};
+        System.out.println(T.solution(c, arr));
     }
 }
