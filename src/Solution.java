aa7e75cc64da190036d5fb3e0a05ab4d410837b2
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-16 18:28:05 +0900

    순열

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,32 @@
 public class Solution {
-    int[] ch;
+    int[] ch, pm;
 
-    private void dfs(int L, int n, int r) {
-        if (L == r) {
-            for (int x : ch) System.out.print(x + " ");
+    private void dfs(int L, int[] arr) {
+        if (L == pm.length) {
+            for (int x : pm) System.out.print(arr[x] + " ");
             System.out.println();
         } else {
-            for (int i = 1; i <= n; i++) {
-                ch[L] = i;
-                dfs(L + 1, n, r);
+            for (int i = 0; i < arr.length; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = i;
+                    dfs(L + 1, arr);
+                    ch[i] = 0;
+                }
             }
         }
     }
 
-    public void solution(int n, int r) {
-        ch = new int[r];
-        dfs(0, n, r);
+    public void solution(int[] arr, int r) {
+        pm = new int[r];
+        ch = new int[arr.length];
+        dfs(0, arr);
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        T.solution(3, 2);
+        int r = 2;
+        int[] arr = {3, 6, 9};
+        T.solution(arr, r);
     }
 }
