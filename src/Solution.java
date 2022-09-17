98e7cf853bbe21275dc3f0629cc115ac2b317d42
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-17 11:34:34 +0900

    타겟 넘버 dfs

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,28 @@
-import java.util.Stack;
-
 class Solution {
-    public String solution(String number, int k) {
-        char[] result = new char[number.length() - k];
-        Stack<Character> stack = new Stack<>();
+    int[] nums, ch;
+    int t, answer = 0;
 
-        for (int i = 0; i < number.length(); i++) {
-            char c = number.charAt(i);
-            while (!stack.isEmpty() && stack.peek() < c && --k >= 0) {
-                stack.pop();
+    private void dfs(int L) {
+        if (L == nums.length) {
+            int sum = 0;
+            for (int i = 0; i < ch.length; i++) {
+                if (ch[i] == 1) sum -= nums[i];
+                else sum += nums[i];
             }
-            stack.push(c);
+            if (sum == t) answer++;
+        } else {
+            ch[L] = 0;
+            dfs(L + 1);
+            ch[L] = 1;
+            dfs(L + 1);
         }
-        for (int i = 0; i < result.length; i++) {
-            result[i] = stack.get(i);
-        }
-        return new String(result);
+    }
+
+    public int solution(int[] numbers, int target) {
+        nums = numbers;
+        ch = new int[numbers.length];
+        t = target;
+        dfs(0);
+        return answer;
     }
 }
