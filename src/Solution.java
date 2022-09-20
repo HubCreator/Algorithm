226c2356b47e7c973d7d0c1a144c1e9837b180cc
HubCreator <chocomilk8604@gmail.com>
2022-09-20 11:46:33 +0900

    행렬의 곱셈

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,26 @@
-import java.util.Stack;
-
 class Solution {
-    public int[] solution(int n, String[] words) {
-        int who = 0, turn = 0, idx = 0;
-        Stack<String> stack = new Stack<>();
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int arr1RowSize = arr1.length, arr1ColSize = arr1[0].length;
+        int arr2RowSize = arr2.length, arr2ColSize = arr2[0].length;
+        int idx = 0;
+        int[][] answer = new int[arr1RowSize][arr2ColSize];
 
-        for (String word : words) {
-            if (idx % n == 0) turn++;
-            if (stack.isEmpty()) stack.push(word);
-            else if (!stack.contains(word)) {
-                String peek = stack.peek();
-                if (peek.charAt(peek.length() - 1) == word.charAt(0)) stack.push(word);
-                else break;
-            } else break;
-            who = (who + 1) % n;
-            idx++;
+        for (int i = 0; i < arr1RowSize; i++) {
+            if (idx == arr2ColSize) idx = 0;
+            int[] arr1Tmp = arr1[i].clone();
+            int[] arr2Tmp = new int[arr2RowSize];
+
+            for (int j = 0; j < arr2ColSize; j++) {
+                int sum = 0;
+                for (int k = 0; k < arr2RowSize; k++)
+                    arr2Tmp[k] = arr2[k][j];
+
+                for (int k = 0; k < arr1Tmp.length; k++)
+                    sum += arr1Tmp[k] * arr2Tmp[k];
+
+                answer[i][idx++] = sum;
+            }
         }
-
-        if (stack.size() == words.length) return new int[]{0, 0};
-        else return new int[]{who + 1, turn};
+        return answer;
     }
 }
