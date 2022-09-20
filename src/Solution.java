f2990693ab10faaec17282a863df2bfca27af162
HubCreator <chocomilk8604@gmail.com>
2022-09-20 14:58:25 +0900

    행렬의 곱셈 3중 for문

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,13 @@
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        int arr1RowSize = arr1.length, arr1ColSize = arr1[0].length;
-        int arr2RowSize = arr2.length, arr2ColSize = arr2[0].length;
-        int idx = 0;
-        int[][] answer = new int[arr1RowSize][arr2ColSize];
-
-        for (int i = 0; i < arr1RowSize; i++) {
-            if (idx == arr2ColSize) idx = 0;
-            int[] arr1Tmp = arr1[i].clone();
-            int[] arr2Tmp = new int[arr2RowSize];
-
-            for (int j = 0; j < arr2ColSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < arr2RowSize; k++)
-                    arr2Tmp[k] = arr2[k][j];
-
-                for (int k = 0; k < arr1Tmp.length; k++)
-                    sum += arr1Tmp[k] * arr2Tmp[k];
-
-                answer[i][idx++] = sum;
+    public int[][] solution(int[][] A, int[][] B) {
+        int[][] answer = new int[A.length][B[0].length];
+        if (A[0].length == B.length) {
+            for (int i = 0; i < A.length; i++) {
+                for (int j = 0; j < B[0].length; j++) {
+                    for (int k = 0; k < A[0].length; k++) {
+                        answer[i][j] += A[i][k] * B[k][j];
+                    }
+                }
             }
         }
         return answer;
