ba60563507c4d181ad4d8f328fbd58927d6e7de8
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-09-22 23:08:18 +0900

    점프와 순간이동

--- src/Solution.java
+++ src/Solution.java
@@ -1,44 +1,15 @@
-import java.util.*;
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
 
-class Job implements Comparable<Job> {
-    int startAt, duration;
-
-    public Job(int a, int b) {
-        this.startAt = a;
-        this.duration = b;
-    }
-
-    @Override
-    public int compareTo(Job o) {
-        if (this.startAt == o.startAt) return this.duration - o.duration;
-        else return this.startAt - o.startAt;
-    }
-}
-
-class Solution {
-    public int solution(int[][] jobs) {
-        int answer = 0, cur = 0;
-        List<Job> list = new ArrayList<>();
-        for (int[] e : jobs) list.add(new Job(e[0], e[1]));
-        Collections.sort(list);
-        Deque<Job> dq = new LinkedList<>(list);
-        while (!dq.isEmpty()) {
-            Job p = dq.peekFirst();
-            if (cur <= p.startAt) {
-                dq.pollFirst();
-                cur = p.startAt + p.duration;
-            } else {
-                List<Job> tmp = new ArrayList<>();
-                while (!dq.isEmpty() && dq.peekFirst().startAt < cur) {
-                    tmp.add(dq.pollFirst());
-                }
-                Collections.sort(tmp, (a, b) -> b.duration - a.duration);
-                for (Job j : tmp) dq.offerFirst(j);
-                p = dq.pollFirst();
-                cur += p.duration;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                n--;
+                answer++;
             }
-            answer += cur - p.startAt;
+            n /= 2;
         }
-        return answer / jobs.length;
+
+        return answer;
     }
 }
