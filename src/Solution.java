c02bd5c7c8f3837e322c7a1a005c07999256e003
HubCreator <chocomilk8604@gmail.com>
2022-09-24 12:11:21 +0900

    카펫

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    int[] arr;
-
-    private int dfs(int n) {
-        if (arr[n] > 0) return arr[n];
-        if (n == 1) return arr[n] = 1;
-        else if (n == 2) return arr[n] = 1;
-        else return arr[n] = (dfs(n - 2) + dfs(n - 1)) % 1234567;
-    }
-
-    public int solution(int n) {
-        arr = new int[n + 1];
-        return dfs(n);
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        List<int[]> entry = new ArrayList<>();
+        int area = brown + yellow;
+        for (int i = area - 1; i >= 0; i--) {
+            if (area % i == 0) {
+                int divided = area / i;
+                if (i < divided) break;
+                if (divided >= 3)
+                    entry.add(new int[]{i, divided});
+            }
+        }
+        if (entry.size() == 1) return entry.get(0);
+        else {
+            for (int[] arr : entry) {
+                if (area - arr[0] * 2 - arr[1] * 2 + 4 == yellow)
+                    return arr;
+            }
+        }
+        return answer;
     }
 }
