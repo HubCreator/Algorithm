b57aa9daa29d9ff45334c8f179fcf8c9d6aac541
HubCreator <chocomilk8604@gmail.com>
2022-09-24 13:47:00 +0900

    카펫 근의 공식으로 개선

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        List<int[]> entry = new ArrayList<>();
-        int area = brown + yellow;
-        for (int i = area - 1; i >= 0; i--) {
-            if (area % i == 0) {
-                int divided = area / i;
-                if (i < divided) break;
-                if (divided >= 3)
-                    entry.add(new int[]{i, divided});
-            }
-        }
-        if (entry.size() == 1) return entry.get(0);
-        else {
-            for (int[] arr : entry) {
-                if (area - arr[0] * 2 - arr[1] * 2 + 4 == yellow)
-                    return arr;
-            }
-        }
-        return answer;
+        int tmp = (brown + 4) / 2;
+        return new int[]{(int) ((tmp + Math.sqrt(Math.pow(tmp, 2) - 4 * (brown + yellow))) / 2),
+                (int) ((tmp - Math.sqrt(Math.pow(tmp, 2) - 4 * (brown + yellow))) / 2)};
     }
 }
