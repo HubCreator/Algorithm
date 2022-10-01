1ac444e8e5fe8468fa898d8da5ecf5e942c16980
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-10-01 15:26:29 +0900

    유클리드 호제법 재귀로 개선

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,11 @@
 class Solution {
-
-    public int gcd (int a, int b) {
-        return a < b ? b % a : a % b;
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 
-    public int[] solution(int _n, int _m) {
-        int[] answer = new int[2];
-        int n = _n;
-        int m = _m;
-
-        while (n != 0 && m != 0) {
-            int v = gcd(n, m);
-            if (n < m) m = v;
-            else n = v;
-        }
-        answer[0] = Math.max(n, m);
-        answer[1] = _n * _m / answer[0];
-
-        return answer;
+    public int[] solution(int n, int m) {
+        int gcd = gcd(n, m);
+        return new int[]{gcd, n * m / gcd};
     }
 }
