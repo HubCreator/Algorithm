32452a2e925326c1844f81d8484f0bdd9af499c8
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-10-01 15:12:25 +0900

    N 개의 최소공배수

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,18 @@
 class Solution {
-    public long[] solution(long[] numbers) {
-        long[] answer = new long[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            for (long j = numbers[i] + 1; ; j++) {
-                char[] chars = Long.toBinaryString(numbers[i] ^ j).toCharArray();
-                int sum = 0;
-                for (char c : chars) { // 여기서 실행시간 초과 오류가 일어날 것으로 예상
-                    if (c == '1') sum++;
-                    if (sum > 2) break;
-                }
-                if (sum <= 2) {
-                    answer[i] = j;
-                    break;
-                }
-            }
+    public int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public int solution(int[] arr) {
+        int lcm = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            lcm = lcm(arr[i], lcm);
         }
-        return answer;
+        return lcm;
     }
 }
