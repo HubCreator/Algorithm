ff47e6352337943c42ed384ef7c4242771faefef
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-10-02 21:55:18 +0900

    네트워크 연결

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,29 @@
+import java.util.Arrays;
+
 class Solution {
-    private int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+    int[] unf;
+
+    private int find(int v) {
+        if (v == unf[v]) return unf[v];
+        else return unf[v] = find(unf[v]);
     }
 
-    public int[] solution(int n, int m) {
-        int gcd = gcd(n, m);
-        return new int[]{gcd, n * m / gcd};
+    private void union(int a, int b) {
+        int fa = find(a);
+        int fb = find(b);
+        if(fa != fb) unf[fa] = fb;
+    }
+
+    public int solution(int n, int[][] links) {
+        unf = new int[n];
+        for (int i = 0; i < n; i++) unf[i] = i;
+        for (int i = 0; i < links.length; i++) {
+            for (int j = 0; j < links[i].length; j++) {
+                if (i == j) continue;
+                if (links[i][j] == 1) union(i, j);
+            }
+        }
+
+        return Arrays.stream(unf).map(m -> find(m)).distinct().toArray().length;
     }
 }
