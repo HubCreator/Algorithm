c71f9042f9019df3682eafa1c00070afc598f9cd
HubCreator <chocomilk8604@gmail.com>
2022-10-06 11:50:53 +0900

    n^2 배열 자르기 수정

--- src/Solution.java
+++ src/Solution.java
@@ -2,18 +2,20 @@
 class Solution {
     public int[] solution(int n, long left, long right) {
         int[] result = new int[(int) (right - left + 1)];
-        int ltY, ltX, rtY, rtX, idx = 0;
+        long ltY, ltX, rtY, rtX;
+        int idx = 0;
 
-        ltY = (int) (left / (long) n);
-        ltX = (int) (left % (long) n);
-        rtY = (int) (right / (long) n);
-        rtX = (int) (right % (long) n);
+        ltY = (left / (long) n);
+        ltX = (left % (long) n);
+        rtY = (right / (long) n);
+        rtX = (right % (long) n);
 
-        for (int y = ltY; y <= rtY; y++) {
-            for (int x = ltX; y == ltY && x < n; x++) result[idx++] = Math.max(y + 1, x + 1);
-            for (int x = 0; y != ltY && y != rtY && x < n; x++) result[idx++] = Math.max(y + 1, x + 1);
-            for (int x = 0; y == rtY && x <= rtX; x++) result[idx++] = Math.max(y + 1, x + 1);
+        for (long y = ltY; y <= rtY; y++) {
+            for (long x = ltX; y == ltY && x < n; x++) result[idx++] = (int) Math.max(y + 1, x + 1);
+            for (long x = 0; y != ltY && y != rtY && x < n; x++) result[idx++] = (int) Math.max(y + 1, x + 1);
+            for (long x = 0; y == rtY && x <= rtX; x++) result[idx++] = (int) Math.max(y + 1, x + 1);
         }
+
         return result;
     }
 }
