7c0622d1a7ac2290b639e8167d0edce24cc92fbd
HubCreator <chocomilk8604@gmail.com>
2022-10-07 18:10:55 +0900

    야근 지수

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,18 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> pqueue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int work : works) pqueue.offer(work);
 
-    boolean isPrime(long v) {
-        if (v == 0 || v == 1) return false;
-        for (long i = 2; i < (long) Math.sqrt(v) + 1; i++)
-            if (v % i == 0) return false;
-        return true;
-    }
+        for (int i = 0; i < n; i++)
+            pqueue.offer(pqueue.poll() - 1);
 
-    public int solution(int n, int k) {
-        int answer = 0;
-        String s = Long.toString(n, k);
-        StringBuilder sb = new StringBuilder();
-
-        if (s.charAt(s.length() - 1) != '0') s += '0';
-
-        for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
-            if (cur != '0') sb.append(cur);
-            else {
-                if (isPrime(Long.parseLong(sb.toString()))) answer++;
-                sb = new StringBuilder("0"); // 0 또는 1이 연달아 나오는 상황에서 sb가 초기화되어있지 않다면 런타임 에러 발생
-            }
+        for (Integer x : pqueue) {
+            if (x < 0) return 0;
+            answer += Math.pow(x, 2);
         }
         return answer;
     }
