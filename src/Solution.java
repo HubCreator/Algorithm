ecb1e86c027b253d71b4b3fe020613da5301c378
HubCreator <chocomilk8604@gmail.com>
2022-10-08 16:45:50 +0900

    카카오 압축

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,32 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public long solution(int n, int[] works) {
-        long answer = 0;
-        PriorityQueue<Integer> pqueue = new PriorityQueue<>(Collections.reverseOrder());
-        for (int work : works) pqueue.offer(work);
+    public int[] solution(String msg) {
+        List<Integer> answer = new ArrayList<>();
+        List<String> dict = new ArrayList<>();
+        int idx;
+        msg += "!";
 
-        for (int i = 0; i < n; i++)
-            pqueue.offer(pqueue.poll() - 1);
+        for (int i = 'A'; i <= 'Z'; i++) dict.add(String.valueOf((char) i));
 
-        for (Integer x : pqueue) {
-            if (x < 0) return 0;
-            answer += Math.pow(x, 2);
+        for (int i = 0; i < msg.length() - 1; ) {
+            int cnt = 0;
+            idx = i;
+            StringBuilder sb = new StringBuilder();
+            sb.append(msg.charAt(i));
+
+            do {
+                cnt++;
+                sb.append(msg.charAt(++idx));
+            } while (dict.contains(sb.toString()) && idx < msg.length() - 1);
+
+            dict.add(sb.toString());
+            answer.add(dict.indexOf(sb.substring(0, sb.length() - 1)));
+
+            if (cnt > 1) i += cnt;
+            else i++;
         }
-        return answer;
+        return answer.stream().mapToInt(i -> i + 1).toArray();
     }
 }
