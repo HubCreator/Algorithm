a06abe125e64179b75861bedc758d5bc8062eb63
HubCreator <chocomilk8604@gmail.com>
2022-10-10 22:41:27 +0900

    단어 변환 BFS

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,46 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
 
 class Solution {
-    public int[] solution(String msg) {
-        List<Integer> result = new ArrayList<>();
-        List<String> dict = new ArrayList<>();
-        int cnt, idx;
-        msg += "!";
 
-        for (int i = 'A'; i <= 'Z'; i++) dict.add(String.valueOf((char) i));
-
-        for (int i = 0; i < msg.length() - 1; ) {
-            cnt = 0;
-            idx = i;
-
-            StringBuilder sb = new StringBuilder();
-            sb.append(msg.charAt(i));
-
-            do {
-                cnt++;
-                sb.append(msg.charAt(++idx));
-            } while (dict.contains(sb.toString()));
-
-            dict.add(sb.toString());
-            result.add(dict.indexOf(sb.substring(0, sb.length() - 1)));
-
-            if (cnt > 1) i += cnt;
-            else i++;
+    private int bfs(String str, String target, String[] words) {
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(str);
+        int tmp, cnt = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String poll = queue.poll();
+                if (Objects.equals(poll, target)) return cnt;
+                for (String t : words) {
+                    tmp = 0;
+                    for (int k = 0; k < t.length(); k++)
+                        if (poll.charAt(k) != t.charAt(k)) tmp++;
+                    if (tmp == 1) queue.offer(t);
+                }
+            }
+            cnt++;
         }
+        return 0;
+    }
 
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < answer.length; i++) answer[i] = result.get(i) + 1;
+    public int solution(String begin, String target, String[] words) {
+        boolean flag = false;
+        for (String word : words) {
+            if (word.equals(begin)) return 1;
+            if (word.equals(target)) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) return 0;
+        return bfs(begin, target, words);
+    }
 
-        return answer;
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        String[] arr = {"cog", "log", "lot", "dog", "dot", "hot"};
+        System.out.println(T.solution("hit", "cog", arr));
     }
 }
