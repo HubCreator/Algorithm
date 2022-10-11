a0ab4aba4f52bf4c64a395c30435af34e54cdb9f
HubCreator <chocomilk8604@gmail.com>
2022-10-11 14:28:37 +0900

    단어 변환 수정

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,41 @@
 import java.util.LinkedList;
-import java.util.Objects;
 import java.util.Queue;
 
 class Solution {
 
+    private boolean isValid(String poll, String t) {
+        int tmp = 0;
+        for (int i = 0; i < t.length(); i++)
+            if (poll.charAt(i) != t.charAt(i)) tmp++;
+        return tmp == 1;
+    }
+
     private int bfs(String str, String target, String[] words) {
         Queue<String> queue = new LinkedList<>();
         queue.offer(str);
-        int tmp, cnt = 0;
+        int cnt = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 String poll = queue.poll();
-                if (Objects.equals(poll, target)) return cnt;
                 for (String t : words) {
-                    tmp = 0;
-                    for (int k = 0; k < t.length(); k++)
-                        if (poll.charAt(k) != t.charAt(k)) tmp++;
-                    if (tmp == 1) queue.offer(t);
-
+                    if (isValid(poll, t)) {
+                        queue.offer(t);
+                        if (t.equals(target)) return cnt + 1;
+                    }
                 }
             }
-            cnt++;
+            if (queue.size() > 1) cnt += queue.size();
+            else cnt++;
         }
-        return 0;
+        return cnt;
     }
 
     public int solution(String begin, String target, String[] words) {
         boolean flag = false;
         for (String word : words) {
             if (word.equals(begin)) return 1;
-            if (word.equals(target)) {
-                flag = true;
-                break;
-            }
+            if (word.equals(target)) flag = true;
         }
         if (!flag) return 0;
         return bfs(begin, target, words);
@@ -41,7 +43,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        String[] arr = {"cog", "log", "lot", "dog", "dot", "hot"};
+        String[] arr = {"hot", "dot", "dog", "lot", "log"};
         System.out.println(T.solution("hit", "cog", arr));
     }
 }
