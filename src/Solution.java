8442b3d4271176c405e07610ab6f912481cedd55
HubCreator <chocomilk8604@gmail.com>
2022-10-14 14:34:44 +0900

    등굣길 DP 반대로 구하도록 수정

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,27 @@
 class Solution {
 
     int[][] board;
-    long[][] dis;
+    int[][] dis;
 
     private void dfs(int x, int y) {
-        if (x <= 0 || y <= 0 || board[y][x] == -1) return;
-        dis[y][x] = (dis[y][x + 1] + dis[y + 1][x]) % 1000000007;
-        dfs(x, y - 1);
-        dfs(x - 1, y);
+        if (x > dis[0].length - 1 || y > dis.length - 1 || board[y][x] == -1) return;
+        dis[y][x] = (dis[y][x - 1] + dis[y - 1][x]) % 1000000007;
+        dfs(x, y + 1);
+        dfs(x + 1, y);
     }
 
     public int solution(int m, int n, int[][] puddles) {
         board = new int[n + 1][m + 1];
-        dis = new long[n + 2][m + 2];
+        dis = new int[n + 1][m + 1];
         for (int[] puddle : puddles) board[puddle[1]][puddle[0]] = -1;
 
-        dis[n + 1][m] = 1;
-        dfs(m, n);
-        return (int) dis[1][1];
+        dis[0][1] = 1;
+        dfs(1, 1);
+        return dis[n][m];
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution(4, 3, new int[][]{{2, 2}}));
     }
 }
