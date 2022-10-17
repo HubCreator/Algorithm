3cb57a7df6ab2ea70e9b92fc288f5e34a82b9de0
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-10-17 20:26:25 +0900

    Gradle 추가

---
+++ src/test/java/MapTest.java
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MapTest {
+    @Test
+    void test() {
+        Assertions.assertThat(12).isEqualTo(12);
+    }
+}
