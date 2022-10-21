1f5998cc0101573aad474baccd8dbd70cbe9814d
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-10-21 09:43:44 +0900

    연속 부분 수열 합의 개수

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,22 +1,29 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 class Solution {
-    public String solution(String number, int k) {
-        char[] result = new char[number.length() - k];
-        Deque<Character> stack = new ArrayDeque<>();
+    Set<Integer> answer = new HashSet<>();
 
-        for (int i = 0; i < number.length(); i++) {
-            char c = number.charAt(i);
-            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
-                stack.pop();
-            }
-            stack.push(c);
+    private void getSequenceSum(int[] elements, int i) {
+        List<Integer> list = new ArrayList<>();
+        for (int j = 0; j < elements.length * 2; j++) {
+            list.add(elements[j % elements.length]);
         }
+        // 초기 윈도우값 설정
+        int win = 0, idx = 0;
+        for (int j = 0; j < i; j++) {
+            win += list.get(j);
+        }
+        for (int j = i; j < list.size(); j++) {
+            win += list.get(j);
+            answer.add(win);
+            win -= list.get(idx++);
+        }
+    }
 
-        for (int i = 0; i < result.length; i++) {
-            result[i] = stack.pollLast();
+    public int solution(int[] elements) {
+        for (int i = 0; i < elements.length; i++) {
+            getSequenceSum(elements, i);
         }
-        return new String(result);
+        return answer.size();
     }
 }
