8c013eaf9efeff9b8aa4293c758c8beec678bb29
HubCreator <chocomilk8604@gmail.com>
2022-10-24 10:33:34 +0900

    땅따먹기 동적 계획법으로 개선

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,32 +1,49 @@
+import java.util.Arrays;
+
 class Solution {
+    public static final int N = 4;
+    static int[][] cache;
 
-    public static final int COL_LEN = 4;
-
-    public static int solution(int[][] land) {
-        int answer = 0;
-        return logic(land, answer);
+    private static void initCache(int[][] cache) {
+        for (int[] arr : cache) {
+            Arrays.fill(arr, -1);
+        }
     }
 
-    private static int logic(int[][] land, int answer) {
-        for (int i = 1; i < land.length; i++) {
-            int[] priorRow = land[i - 1]; // 이전 행의 배열
-            for (int j = 0; j < COL_LEN; j++) {
-                land[i][j] += getMaxElement(priorRow, j);
-                if (i == land.length - 1) {
-                    answer = Math.max(answer, land[i][j]);
+    private static int move(int[][] land, int row, int col) {
+        if (row == land.length - 1) return cache[row][col] = land[row][col]; // 기저 조건
+        if (cache[row][col] != -1) return cache[row][col]; // 캐시
+
+        for (int i = 0; i < N; i++) {
+            if (i != col) {
+                cache[row][col] = Math.max(cache[row][col], land[row][col] + move(land, row + 1, i));
+            }
+        }
+
+        return cache[row][col];
+    }
+
+    static int solution(int[][] land) {
+        cache = new int[land.length][N];
+        int result = Integer.MIN_VALUE;
+        initCache(cache);
+        for (int i = 0; i < N; i++) {
+            result = Math.max(result, move(land, 0, i));
+            for (int[] ints : cache) {
+                for (int x : ints) {
+                    System.out.print(x + " ");
                 }
+                System.out.println();
             }
+            System.out.println("result = " + result);
+            System.out.println();
         }
-        return answer;
+        return result;
     }
 
-    private static int getMaxElement(int[] priorRow, int col) {
-        int max = 0;
-        for (int k = 0; k < COL_LEN; k++) { // 매 번 구하는게 최선인가..?
-            if (k != col && max < priorRow[k]) {
-                max = priorRow[k];
-            }
-        }
-        return max;
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int[][] arr = {{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}};
+        System.out.println(T.solution(arr));
     }
 }
