420235b5d0b5ba8c93a06c4a03658aadebe148ec
HubCreator <chocomilk8604@gmail.com>
2022-10-25 17:16:54 +0900

    베스트 앨범

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,71 +1,91 @@
-import java.util.Arrays;
+import java.util.*;
 
-class File implements Comparable<File> {
-    final String head;
-    final String number;
-    final String tail;
+class Music implements Comparable<Music> {
+    final Integer id;
+    final Integer play;
 
-    File(String head, String number, String tail) {
-        this.head = head;
-        this.number = number;
-        this.tail = tail;
+    Music(Integer id, Integer play) {
+        this.id = id;
+        this.play = play;
     }
 
     @Override
-    public int compareTo(File o) {
-        Integer thisNum = Integer.parseInt(this.number);
-        Integer objectNum = Integer.parseInt(o.number);
-        if (head.equalsIgnoreCase(o.head) && thisNum.equals(objectNum)) {
-            return 0;
-        } else if (head.equalsIgnoreCase(o.head)) {
-            return thisNum.compareTo(objectNum);
-        } else {
-            return head.compareToIgnoreCase(o.head);
+    public int compareTo(Music o) {
+        return o.play.compareTo(this.play);
+    }
+}
+
+class Genre implements Comparable<Genre> {
+    final String genre;
+    final List<Music> musics = new ArrayList<>();
+
+    Genre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getTotalPlayCnt() {
+        int sum = 0;
+        for (Music music : musics) sum += music.play;
+        return sum;
+    }
+
+    public List<Integer> getTop2Music() {
+        List<Integer> result = new ArrayList<>();
+        Collections.sort(musics);
+        int idx = 0;
+        while (idx < musics.size() && result.size() < 2) {
+            result.add(musics.get(idx++).id);
         }
+        return result;
     }
 
     @Override
-    public String toString() {
-        return head + number + tail;
+    public int compareTo(Genre o) {
+        return o.getTotalPlayCnt().compareTo(this.getTotalPlayCnt());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Genre genre1 = (Genre) o;
+        return Objects.equals(genre, genre1.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(genre);
     }
 }
 
 class Solution {
-    public String[] solution(String[] files) {
-        String[] answer = new String[files.length];
-        File[] fileArr = new File[files.length];
-
-        for (int i = 0; i < files.length; i++) {
-            // 파일 명을 세 부분으로 나눈다. (HEAD, NUMBER, TAIL)
-            StringBuilder head = new StringBuilder();
-            StringBuilder number = new StringBuilder();
-            String tail;
-
-            String file = files[i];
-            int idx = 0, cnt = 0;
-            while (Character.isAlphabetic(file.charAt(idx)) || Character.isSpaceChar(file.charAt(idx)) ||
-                    Character.getType(file.charAt(idx)) == Character.DASH_PUNCTUATION) {
-                head.append(file.charAt(idx++));
-            }
-            while (idx <= file.length() - 1  && Character.isDigit(file.charAt(idx)) && cnt < 5) {
-                number.append(file.charAt(idx++));
-                cnt++;
-            }
-            tail = file.substring(idx);
-            fileArr[i] = new File(head.toString(), number.toString(), tail);
+    public int[] solution(String[] genres, int[] plays) {
+        List<Integer> result = new ArrayList<>();
+        List<Genre> list = new ArrayList<>();
+        Map<String, Genre> map = new HashMap<>();
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+            Genre gen = !map.containsKey(genre) ? new Genre(genre) : map.get(genre);
+            gen.musics.add(new Music(i, play));
+            map.put(genre, gen);
         }
-        Arrays.sort(fileArr);
+
+
+        for (Map.Entry<String, Genre> e : map.entrySet()) {
+            list.add(e.getValue());
+        }
+
+        Collections.sort(list);
+        for (Genre genre : list) {
+            List<Music> musics = genre.musics;
+            Collections.sort(musics);
+            result.addAll(genre.getTop2Music());
+        }
+        int[] answer = new int[result.size()];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = fileArr[i].toString();
+            answer[i] = result.get(i);
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        String[] arr = {"ABC12", "AbC12", "aBc12"};
-        for (String s : T.solution(arr)) {
-            System.out.println("s = " + s);
-        }
     }
 }
