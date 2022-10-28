d39ac0bcad6ee4e1ff83d328cefd621cec79a447
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-10-28 20:39:29 +0900

    할인 행사, Map 슬라이딩 윈도우

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,17 +1,36 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public int solution(int[][] routes) {
-        Arrays.sort(routes, (a, b) -> a[1] - b[1]); // 각 행을 정렬 (종료 지점 기준으로 오름차순 정렬)
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String, Integer> wishList = new HashMap<>();
+        Map<String, Integer> win = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wishList.put(want[i], number[i]);
+        }
 
-        int ans = 0;
-        int camPoint = Integer.MIN_VALUE;
-        for (int[] ints : routes) {
-            if (camPoint < ints[0]) { // 시작지점이 더 크다면
-                ++ans;
-                camPoint = ints[1]; // 종료 지점을 저장
+        // 초기 윈도우 설정
+        for (int i = 0; i < 9; i++) {
+            win.put(discount[i], win.getOrDefault(discount[i], 0) + 1);
+        }
+
+        // 슬라이딩 윈도우
+        int lt = 0;
+        for (int rt = 9; rt < discount.length; rt++, lt++) {
+            win.put(discount[rt], win.getOrDefault(discount[rt], 0) + 1);
+            if (wishList.equals(win)) answer++;
+            win.put(discount[lt], win.get(discount[lt]) - 1);
+            if (win.get(discount[lt]) == 0) {
+                win.remove(discount[lt]);
             }
         }
-        return ans;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        String[] arr = {"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"};
+        System.out.println(T.solution(new String[]{"banana", "apple", "rice", "pork", "pot"}, new int[]{3,2,2,2,1}, arr));
     }
 }
