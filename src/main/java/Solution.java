d534b711b270ccd01be862c2a61f2215c6042e66
HubCreator <chocomilk8604@gmail.com>
2022-10-29 17:53:03 +0900

    기지국 설치

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,36 +1,33 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
+    public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        Map<String, Integer> wishList = new HashMap<>();
-        Map<String, Integer> win = new HashMap<>();
-        for (int i = 0; i < want.length; i++) {
-            wishList.put(want[i], number[i]);
-        }
+        boolean[] arr = new boolean[n + 1];
+        int len = 2 * w + 1;
 
-        // 초기 윈도우 설정
-        for (int i = 0; i < 9; i++) {
-            win.put(discount[i], win.getOrDefault(discount[i], 0) + 1);
-        }
-
-        // 슬라이딩 윈도우
-        int lt = 0;
-        for (int rt = 9; rt < discount.length; rt++, lt++) {
-            win.put(discount[rt], win.getOrDefault(discount[rt], 0) + 1);
-            if (wishList.equals(win)) answer++;
-            win.put(discount[lt], win.get(discount[lt]) - 1);
-            if (win.get(discount[lt]) == 0) {
-                win.remove(discount[lt]);
+        // 초기화
+        for (int station : stations) {
+            for (int i = station - w; i <= station + w; i++) {
+                if (i > arr.length - 1) break;
+                arr[i] = true;
             }
         }
-        return answer;
-    }
 
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        String[] arr = {"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"};
-        System.out.println(T.solution(new String[]{"banana", "apple", "rice", "pork", "pot"}, new int[]{3,2,2,2,1}, arr));
+        int cnt = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i]) {
+                if (cnt > 0) {
+                    answer += (int) Math.ceil((double) (cnt) / (double) len);
+                    cnt = 0;
+                }
+                continue;
+            }
+            cnt++;
+        }
+
+        if (!arr[arr.length - 1] && cnt > 1) {
+            answer += (int) Math.ceil((double) (cnt - 1) / (double) len);
+        }
+
+        return answer;
     }
 }
