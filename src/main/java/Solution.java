652fe0f6949487fd4f21adbe34fa703e4c06fdb2
HubCreator <chocomilk8604@gmail.com>
2022-10-29 18:12:06 +0900

    기지국 설치, List를 두지 않고 바로 answer에 반영하도록 개선

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,24 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
 
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        int answer = 0;
+        double answer = 0;
         int len = 2 * w + 1;
-        List<Integer> list = new ArrayList<>();
 
         int point = 1;
         for (int station : stations) {
-            list.add((station - w) - point);
+            int tmp = (station - w) - point;
+            answer += Math.ceil((double) tmp / len);
             point = station + w + 1;
         }
         if (stations[stations.length - 1] + w < n) {
-            list.add(n - point + 1);
-        }
+            answer += Math.ceil((double) (n - point + 1) / len);
 
-        for (Integer x : list) {
-            answer += (int) Math.ceil((double) x / len);
         }
-        return answer;
+        return (int) answer;
     }
 }
