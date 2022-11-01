624461d99ac9c6ccb495cb5c20121f85da0e7962
HubCreator <chocomilk8604@gmail.com>
2022-11-01 11:58:51 +0900

    n^2 배열 자르기 런타임 에러 해결. 몫과 나머지를 사용해 검증하여 해결

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,37 +1,18 @@
 class Solution {
     public int[] solution(int n, long left, long right) {
-        if (left == right) return new int[]{};
-
         int[] result = new int[(int) ((right - left) + 1)];
-        long ltY, ltX, rtY, rtX, tmp;
-        int idx = 0;
+        int ltY, rtY, idx = 0;
 
-        ltY = (left / n);
-        ltX = (left % n);
-        rtY = (right / n);
-        rtX = (right % n);
+        ltY = (int) (left / n);
+        rtY = (int) (right / n);
 
-        for (long y = ltY; y <= rtY; y++) {
-            tmp = ltX;
-            while (y == ltY && tmp < n) {
-                result[idx++] = (int) Math.max(tmp++, y) + 1;
-            }
-            tmp = 0;
-            while (y != ltY && y != rtY && tmp < n) {
-                result[idx++] = (int) Math.max(tmp++, y) + 1;
-            }
-            tmp = 0;
-            while (y == rtY && tmp <= rtX) {
-                result[idx++] = (int) Math.max(tmp++, y) + 1;
+        for (int y = ltY; y <= rtY; y++) {
+            for (int x = 0; x < n; x++) {
+                if (((long) y * n + x >= left && (long) y * n + x <= right)) {
+                    result[idx++] = Math.max(y, x) + 1;
+                }
             }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        for (int x : T.solution(3, 2, 5)) {
-            System.out.print(x + " ");
-        }
     }
 }
