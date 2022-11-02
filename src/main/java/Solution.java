1233f91dbaf191e09fa67c1ce2dacfa0b81c1fb6
HubCreator <chocomilk8604@gmail.com>
2022-11-02 14:00:59 +0900

    두 개 이하로 다른 비트, 짝수와 홀수 경우의 수를 나눠 개선

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,18 +3,29 @@ class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            long num = numbers[i] + 1;
-            while (Long.bitCount(num ^ numbers[i]) > 2) {
-                num++;
+            // 짝수일 때
+            if (numbers[i] % 2 == 0) {
+                answer[i] = numbers[i] + 1;
+                continue;
             }
-            answer[i] = num;
+            // 홀수일 때
+            StringBuilder target = new StringBuilder(Long.toBinaryString(numbers[i]));
+            int idx = target.lastIndexOf("0");
+            if (idx != -1) {
+                target.replace(idx, idx + 1, "1");
+                target.replace(idx + 1, idx + 2, "0");
+            } else {
+                target.insert(0, '1');
+                target.replace(1, 2, "0");
+            }
+            answer[i] = Long.parseLong(target.toString(), 2);
         }
         return answer;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        for (long l : T.solution(new long[]{2, 7})) {
+        for (long l : T.solution(new long[]{1001})) {
             System.out.println("l = " + l);
         }
     }
