31a1c5644f0451223ccdca634f47975588661b59
HubCreator <chocomilk8604@gmail.com>
2022-11-02 13:18:41 +0900

    두 개 이하로 다른 비트, 비트 연산으로 개선

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,32 +1,21 @@
-import java.util.HashSet;
-import java.util.Set;
 
-public class Solution {
-    private static final Set<Integer> set = new HashSet<>();
-
-    public int solution(String[] user_id, String[] banned_id) {
-        go(0, user_id, banned_id, 0);
-        return set.size();
-    }
-
-    private static void go(int index, String[] user_id, String[] banned_id, int bit) {
-        if (index == banned_id.length) {
-            set.add(bit);
-            return;
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            long num = numbers[i] + 1;
+            while (Long.bitCount(num ^ numbers[i]) > 2) {
+                num++;
+            }
+            answer[i] = num;
         }
-        // 해당 인덱스의 제재 추출
-        String reg = banned_id[index].replaceAll("[*]", ".");
-        for (int i = 0; i < user_id.length; ++i) {
-            // (해당 번째의 비트를 이미 확인했거나 || 해당 regex의 대상이 아니라면) continue;
-            if ((((bit >> i) & 1) == 1) || !user_id[i].matches(reg)) continue;
-            go(index + 1, user_id, banned_id, (bit | 1 << i)); // regex의 대상이라면 해당 bit를 on, index++
-        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution T = new Solution();
-        String[] arr1 = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
-        String[] arr2 = {"fr*d*", "*rodo", "******", "******"};
-        System.out.println(T.solution(arr1, arr2));
+        for (long l : T.solution(new long[]{2, 7})) {
+            System.out.println("l = " + l);
+        }
     }
 }
