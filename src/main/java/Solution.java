36175a2e0bd0862499ece9f5d3838b59538fffb9
HubCreator <chocomilk8604@gmail.com>
2022-11-03 18:34:14 +0900

    카카오 보석 쇼핑, 투 포인터. 정확성 테스트만 통과

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,28 @@
+import java.util.*;
+
 class Solution {
-    public long[] solution(long[] numbers) {
-        long[] answer = numbers.clone();
-        for (int i = 0; i < answer.length; i++) {
-            answer[i]++;
-            answer[i] += (answer[i] ^ numbers[i]) >> 2;
+    public int[] solution(String[] gems) {
+        int[] answer = new int[]{1, Integer.MAX_VALUE};
+        Set<String> entry = new HashSet<>(Arrays.asList(gems));
+        Queue<String> target = new LinkedList<>();
+        int lt = 0, rt = 0;
+
+        while (lt <= rt && rt < gems.length) {
+            target.add(gems[rt++]);
+
+            boolean flag = false;
+            while (entry.equals(new HashSet<>(target))) { // lt를 갱신
+                target.poll();
+                lt++;
+                flag = true;
+            }
+            if (flag && rt - lt < answer[1] - answer[0]) { // lt를 갱신했는데, answer보다 길이가 작다면
+                answer = new int[]{lt, rt}; // 다시 한 번 answer 갱신
+            }
+        }
+
+        if (answer[1] == Integer.MAX_VALUE) {
+            return new int[]{1, gems.length};
         }
         return answer;
     }
--- src/test/java/CodingTest.java
+++ src/test/java/CodingTest.java
@@ -1,7 +1,9 @@
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CodingTest {
@@ -18,4 +20,11 @@ public class CodingTest {
         System.out.println(Integer.toBinaryString((7 ^ 8) >>> 2));
     }
 
+    @Test
+    void setTest() {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3, 3, 3, 4, 4, 5));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(5, 5, 4, 4, 3, 3, 3, 2, 2, 1));
+        assertThat(set1).isEqualTo(set2);
+        assertThat(set1).isEqualTo(new HashSet<>(Arrays.asList(5, 5, 4, 3, 2, 1)));
+    }
 }
