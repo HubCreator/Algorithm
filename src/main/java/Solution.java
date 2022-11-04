2b9d1600a85d7ef2b3459b14ad797715ea575667
HubCreator <chocomilk8604@gmail.com>
2022-11-04 14:40:15 +0900

    스티커 모으기 초안

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,26 +1,54 @@
-import java.util.*;
-
 class Solution {
-    public int[] solution(String[] gems) {
-        int[] answer = new int[2];
-        Set<String> entry = new HashSet<>(Arrays.asList(gems));
-        Map<String, Integer> map = new HashMap<>();
 
-        int lt = 0, length = Integer.MAX_VALUE;
-        for (int rt = 0; rt < gems.length; rt++) {
-            map.put(gems[rt], map.getOrDefault(gems[rt], 0) + 1);
+    private int dfs(int[] arr, int idx) {
+        int sum = arr[idx];
+        arr[idx] = -1; // 해당 인덱스는 -1 표시
 
-            while (map.get(gems[lt]) >= 2) {
-                map.put(gems[lt], map.get(gems[lt]) - 1);
-                lt++;
-            }
+        // 양 옆의 인덱스도 -1 표시
+        if (idx == 0) {
+            arr[arr.length - 1] = -1;
+            arr[idx + 1] = -1;
+        } else if (idx == arr.length - 1) {
+            arr[idx - 1] = -1;
+            arr[0] = -1;
+        } else {
+            arr[idx - 1] = -1;
+            arr[idx + 1] = -1;
+        }
 
-            if (map.size() == entry.size() && length > (rt - lt)) {
-                length = rt - lt;
-                answer[0] = lt + 1;
-                answer[1] = rt + 1;
+        int tmp = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != -1) {
+                tmp = Math.max(tmp, dfs(arr.clone(), i));
             }
         }
+        if (tmp != Integer.MIN_VALUE) sum += tmp;
+        return sum;
+    }
+
+    public int solution(int sticker[]) {
+        // 짝수개일 경우
+        if (sticker.length % 2 == 0) {
+            int even = 0, odd = 0;
+            for (int i = 0; i < sticker.length; i++) {
+                if (i % 2 == 0) even += sticker[i];
+                else odd += sticker[i];
+            }
+            return Math.max(even, odd);
+        }
+
+        // 홀수개일 경우
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < sticker.length; i++) {
+            answer = Math.max(answer, dfs(sticker.clone(), i));
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int[] arr = {1, 3, 2, 5, 4};
+        System.out.println(T.solution(arr));
     }
 }
