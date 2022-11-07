f9e94aa709e45a5b75d92fb7469447b970418d4c
HubCreator <chocomilk8604@gmail.com>
2022-11-07 19:09:31 +0900

    다음 큰 숫자, bitCount() 함수로 개선

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,27 +1,11 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-
 class Solution {
-    public int solution(int[] stones, int k) {
-        PriorityQueue<Integer> entry = new PriorityQueue<>();
-
-        // 초기 윈도우
-        PriorityQueue<Integer> window = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < k - 1; i++) {
-            window.offer(stones[i]);
+    public int solution(int n) {
+        int bitCnt1 = Integer.bitCount(n);
+        int bitCnt2 = -1;
+        int answer = n;
+        while (bitCnt1 != bitCnt2) {
+            bitCnt2 = Integer.bitCount(++answer);
         }
-        int lt = 0;
-        for (int rt = k - 1; rt < stones.length; rt++) {
-            window.offer(stones[rt]);
-            entry.offer(window.peek());
-            window.remove(stones[lt++]);
-        }
-        return entry.peek();
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
-        System.out.println(T.solution(stones, 3));
+        return answer;
     }
 }
