11ca40bf77102b3eeb0f0cfe32d56de1784e0b3b
HubCreator <chocomilk8604@gmail.com>
2022-11-09 14:25:59 +0900

    간단한 소인수분해

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,34 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder answer = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] entry = {2, 3, 5, 7, 11};
 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            answer.append("#").append(test_case).append(" ");
-
-            int length = Integer.parseInt(br.readLine());
-            int[] arr = new int[length];
-            long sum = 0;
-            int max = 0;
-
-            st = new StringTokenizer(br.readLine());
-
-            for (int i = 0; i < length; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            answer.append("#").append(i + 1).append(" ");
+            long num = Long.parseLong(br.readLine());
+            for (int e : entry) {
+                int cnt = 0;
+                long target = num;
+                while (target % e == 0) {
+                    target /= e;
+                    cnt++;
+                }
+                answer.append(cnt).append(" ");
             }
-
-            for (int i = length - 1; i >= 0; i--) {
-                if (max < arr[i]) max = arr[i]; // max 갱신
-                else sum += max - arr[i]; // max가 아니면 차이만큼 sum에 더함
-            }
-            answer.append(sum).append("\n");
+            answer.append("\n");
         }
         System.out.println(answer);
     }
