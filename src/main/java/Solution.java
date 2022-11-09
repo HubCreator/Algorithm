cd2501aec910d198cbb22c1babb74f0507c5cca0
HubCreator <chocomilk8604@gmail.com>
2022-11-09 15:18:24 +0900

    SW 문제 해결 기본. View 문제

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,27 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder answer = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int[] entry = {2, 3, 5, 7, 11};
+        StringTokenizer st;
+        List<Integer> list, window;
+        int length, result;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < 10; i++) {
             answer.append("#").append(i + 1).append(" ");
-            long num = Long.parseLong(br.readLine());
-            for (int e : entry) {
-                int cnt = 0;
-                long target = num;
-                while (target % e == 0) {
-                    target /= e;
-                    cnt++;
-                }
-                answer.append(cnt).append(" ");
+            list = new ArrayList<>();
+            window = new ArrayList<>();
+            length = Integer.parseInt(br.readLine());
+            result = 0;
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < length; j++) {
+                list.add(Integer.parseInt(st.nextToken()));
             }
-            answer.append("\n");
+
+            // 초기 윈도우값 설정
+            for (int j = 0; j < 4; j++) {
+                window.add(list.get(j));
+            }
+
+            for (int j = 4; j < list.size(); j++) {
+                window.add(list.get(j));
+                PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
+                pQueue.addAll(window);
+                Integer target = window.get(2);
+                Integer poll = pQueue.poll();
+                if (poll.equals(target) && poll > pQueue.peek()) {
+                    result += poll - pQueue.peek();
+                }
+                window.remove(0);
+            }
+            answer.append(result).append("\n");
         }
         System.out.println(answer);
     }
