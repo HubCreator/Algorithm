01173bd7f92b1c34b44302090dbb036f4b74a136
HubCreator <chocomilk8604@gmail.com>
2022-11-10 22:19:29 +0900

    격자판 bfs 최적화하여 개선. 구현 완료

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Point {
     int y, x;
@@ -21,12 +19,18 @@ public class Solution {
     private static int[] dy = {0, -1, 0, 1};
     private static int[] dx = {-1, 0, 1, 0};
 
+    private static List<Point> points = new ArrayList<>();
+    private static String[][] board;
+    private static int n, m;
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder answer = new StringBuilder();
         for (int i = 0; i < T; i++) {
             answer.append("#").append(i + 1).append(" ");
+            points.clear();
             answer.append(solution(br) ? "possible" : "impossible").append("\n");
         }
         System.out.println(answer);
@@ -35,44 +39,40 @@ public class Solution {
     private static boolean solution(BufferedReader br) throws IOException {
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        String[][] board = makeBoard(n, m, br);
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        makeBoard(br);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!board[i][j].equals("?") || !board[i][j].equals("!")) {
-                    if (!bfs(board, i, j)) return false;
-                }
-            }
-        }
-        return true;
+        return bfs();
     }
 
-    private static String[][] makeBoard(int n, int m, BufferedReader br) throws IOException {
-        String[][] board = new String[n][m];
+    private static void makeBoard(BufferedReader br) throws IOException {
+        board = new String[n][m];
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < m; j++) {
                 board[i][j] = String.valueOf(str.charAt(j));
+                if (!board[i][j].equals("?")) points.add(new Point(i, j, board[i][j]));
             }
         }
-        return board;
     }
 
-    private static boolean bfs(String[][] board, int row, int col) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(row, col, board[row][col]));
+    private static boolean bfs() {
+        Queue<Point> queue = new LinkedList<>(points);
         while (!queue.isEmpty()) {
             Point poll = queue.poll();
+            if (poll.mark.equals("!")) continue;
             board[poll.y][poll.x] = "!";
             for (int i = 0; i < 4; i++) {
                 int ny = poll.y + dy[i];
                 int nx = poll.x + dx[i];
-                if (ny >= 0 && ny < board.length && nx >= 0 && nx < board[0].length && !board[ny][nx].equals("!")) {
-                    if (!board[ny][nx].equals(poll.mark)) {
-                        board[ny][nx] = poll.mark.equals("#") ? "." : "#";
-                        queue.add(new Point(ny, nx, board[ny][nx]));
+                if (ny >= 0 && ny < n && nx >= 0 && nx < m && !board[ny][nx].equals("!")) {
+                    if (board[ny][nx].equals("?")) {
+                        String mark = poll.mark.equals("#") ? "." : "#";
+                        queue.offer(new Point(ny, nx, mark));
+                        board[ny][nx] = mark;
+                    } else if (!board[ny][nx].equals(poll.mark)) {
+                        queue.offer(new Point(ny, nx, board[ny][nx]));
                     } else {
                         return false;
                     }
