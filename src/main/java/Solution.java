f73b89e9a02b7f9b1d4f2a20c524afc0a0b4510c
HubCreator <chocomilk8604@gmail.com>
2022-11-13 18:03:53 +0900

    평탄화 작업. 매 번 sorting 해줌

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,37 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Solution {
     static StringBuilder answer = new StringBuilder();
-
-    static int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static StringTokenizer st;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int test_case = 1; test_case <= T; test_case++) {
-            answer.append("#").append(test_case).append(" \n");
-            int n = Integer.parseInt(br.readLine());
-            int[][] arr = new int[n][n];
-            int cnt = 1, dirIdx = 0;
-            int[] coords = {0, 0};
-            while (cnt <= Math.pow(n, 2)) {
-                arr[coords[0]][coords[1]] = cnt++;
-                if (coords[0] + dir[dirIdx][0] < 0 || coords[0] + dir[dirIdx][0] >= n ||
-                        coords[1] + dir[dirIdx][1] < 0 || coords[1] + dir[dirIdx][1] >= n || // arr 2차원 배열의 범위를 벗어나는지 체크
-                        arr[coords[0] + dir[dirIdx][0]][coords[1] + dir[dirIdx][1]] != 0) { // 이미 초기화 되었는지 체크
-                    dirIdx = (dirIdx + 1) % dir.length;
-                }
-                coords[0] += dir[dirIdx][0];
-                coords[1] += dir[dirIdx][1];
-            }
-            for (int[] ints : arr) {
-                for (int x : ints) {
-                    answer.append(x).append(" ");
-                }
-                answer.append("\n");
-            }
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            answer.append("#").append(test_case).append(" ");
+            LinkedList<Integer> dQueue = new LinkedList<>();
 
+            int n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int len = st.countTokens();
+            for (int i = 0; i < len; i++) {
+                dQueue.offer(Integer.parseInt(st.nextToken()));
+            }
+            for (int i = 0; i < n; i++) {
+                Collections.sort(dQueue);
+                if (dQueue.getFirst().equals(dQueue.getLast())) break; // 평탄화 완료
+                dQueue.offerLast(dQueue.pollLast() - 1);
+                dQueue.offerFirst(dQueue.pollFirst() + 1);
+            }
+            Collections.sort(dQueue);
+            answer.append(dQueue.getLast() - dQueue.getFirst()).append("\n");
         }
         System.out.println(answer);
     }
