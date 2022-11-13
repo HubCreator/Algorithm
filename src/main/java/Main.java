7266fba80d6c9395d7e3d69d107f60023831d025
HubCreator <chocomilk8604@gmail.com>
2022-11-13 12:39:19 +0900

    중위 표기법을 후위 표기법으로, 후위 표기법 연산

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,61 +1,127 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
+
+class GStack<T> {
+    List<T> list = new ArrayList<>();
+
+    public void push(T item) {
+        list.add(item);
+    }
+
+    public T pop() {
+        if (list.size() == 0) return null;
+        return list.remove(list.size() - 1);
+    }
+
+    public T peek() {
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+}
+
 
 public class Main {
-    public static List<String> answer = new ArrayList<>();
-    public static int[][] cache = new int[101][101];
+    static GStack<String> stringStack = new GStack<>();
+    static GStack<Double> doubleGStack = new GStack<>();
 
-    private static int match(String W, String S, int w, int s) {
-        // 메모이제이션
-        if (cache[w][s] != -1) return 1;
-        // W[w]와 S[s]를 맞춰 나간다
-        while (s < S.length() && w < W.length() &&
-                (W.charAt(w) == '?' || W.charAt(w) == S.charAt(s))) {
-            ++w;
-            ++s;
-        }
-        // 더이상 대응할 수 없으면 왜 while문이 끝났는지 확인
-        // 2. 패턴 끝에 도달해 끝난 경우 : 문자열도 끝났어야 함
-        if (w == W.length()) return cache[w][s] = s == S.length() ? 1 : 0;
-        // 4. *를 만나서 끝난 경우 : *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인
-        if (W.charAt(w) == '*') {
-            for (int skip = 0; skip + s < S.length(); skip++) {
-                if (match(W, S, w + 1, s + skip) == 1) {
-                    return cache[w][s] = 1;
-                }
-            }
-        }
-        // 3. 이외의 경우에는 모두 대응되지 않는다
-        return cache[w][s] = 0;
+    public static void main(String[] args) {
+        String exp = "2.0*3.0-((4.0+5.0)*2.0)-4.0=";
+        String postfix = getPostfix(exp);
+        double evaluation = getEvaluation(postfix);
+        System.out.println("postfix = " + postfix);
+        System.out.println("evaluation = " + evaluation);
     }
 
-    private static void initCache() {
-        for (int[] ints : cache) {
-            Arrays.fill(ints, -1);
-        }
-    }
+    private static double getEvaluation(String exp) {
+        List<Character> operations = Arrays.asList('+', '-', '*', '/');
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int totalCnt = Integer.parseInt(br.readLine());
-        for (int i = 0; i < totalCnt; i++) {
-            String target = br.readLine();
-            int cnt = Integer.parseInt(br.readLine());
-            for (int j = 0; j < cnt; j++) {
-                initCache();
-                String S = br.readLine();
-                if (match(target, S, 0, 0) == 1) {
-                    answer.add(S);
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+            if (!operations.contains(c)) {
+                int cnt = 0;
+                StringBuilder buffer = new StringBuilder();
+                while (cnt < 3) {
+                    buffer.append(exp.charAt(i + cnt));
+                    cnt++;
                 }
+                doubleGStack.push(Double.parseDouble(buffer.toString()));
+                i += 2;
+                continue;
+            }
+
+            switch (c) {
+                case '+':
+                    doubleGStack.push(doubleGStack.pop() + doubleGStack.pop());
+                    break;
+                case '-':
+                    double a = doubleGStack.pop();
+                    double b = doubleGStack.pop();
+                    doubleGStack.push(b - a);
+                    break;
+                case '*':
+                    doubleGStack.push(doubleGStack.pop() * doubleGStack.pop());
+                    break;
+                case '/':
+                    doubleGStack.push(doubleGStack.pop() / doubleGStack.pop());
+                    break;
             }
         }
+        return doubleGStack.pop();
+    }
 
-        for (String s : answer) {
-            System.out.println(s);
+    private static String getPostfix(String exp) {
+        StringBuilder answer = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(exp, "+-*/()=", true);
+        while (st.hasMoreTokens()) {
+            String s = st.nextToken();
+            if (s.equals("=")) break;
+            int priority = getPriority(s);
+
+            switch (s) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    while (!stringStack.isEmpty() && getPriority(stringStack.peek()) >= priority) {
+                        answer.append(stringStack.pop());
+                    }
+                    stringStack.push(s);
+                    break;
+                case "(":
+                    stringStack.push(s);
+                    break;
+                case ")":
+                    while (!stringStack.isEmpty() && !stringStack.peek().equals("(")) {
+                        answer.append(stringStack.pop());
+                    }
+                    stringStack.pop();  // '(' 제거
+                    break;
+                default:  // operand
+                    answer.append(s);
+                    doubleGStack.push(Double.parseDouble(s));
+            }
+        }
+        while (!stringStack.isEmpty()) {
+            answer.append(stringStack.pop());
+        }
+        return answer.toString();
+    }
+
+    public static int getPriority(String s) {
+        switch (s) {
+            case "*":
+            case "/":
+                return 2;
+            case "+":
+            case "-":
+                return 1;
+            default:
+                return 0;
         }
     }
 }
