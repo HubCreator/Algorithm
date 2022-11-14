83089e88a31f2b25a0fd127f6a2f50b4f17c2dfd
HubCreator <chocomilk8604@gmail.com>
2022-11-14 15:03:20 +0900

    Base64 decoder, util 클래스 사용 풀이

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,46 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Base64;
 
 class Solution {
-    static StringBuffer answer = new StringBuffer();
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        for (int test_case = 1; test_case <= T; test_case++) {
-            answer.append("#" + test_case + " ");
-            String enc = br.readLine();
-            StringBuilder transformed = new StringBuilder();
-            transform(enc, transformed);
-            String binaryString = transformed.toString();
-            int i;
-            for (i = 0; i < binaryString.length() - 8; i += 8) {
-                answer.append((char) Integer.parseInt(binaryString.substring(i, i + 8), 2));
-            }
-            answer.append(".\n");
-        }
-        System.out.println(answer);
-    }
-
-    private static void transform(String enc, StringBuilder result) {
-        char[] chars = enc.toCharArray();
-        for (char ch : chars) {
-            if (Character.isUpperCase(ch)) {
-                String s = Integer.toBinaryString((int) ch - 65);
-                result.append(String.format("%06d", Integer.parseInt(s)));
-            } else if (Character.isLowerCase(ch)) {
-                String s = Integer.toBinaryString((int) ch - 71);
-                result.append(String.format("%06d", Integer.parseInt(s)));
-            } else if (Character.isDigit(ch)) {
-                String s = Integer.toBinaryString(Integer.parseInt(String.valueOf(ch)) + 52);
-                result.append(String.format("%06d", Integer.parseInt(s)));
-            } else if (ch == '+') {
-                String s = Integer.toBinaryString(62);
-                result.append(String.format("%06d", Integer.parseInt(s)));
-            } else if (ch == '/') {
-                String s = Integer.toBinaryString(63);
-                result.append(String.format("%06d", Integer.parseInt(s)));
-            }
+        for (int t = 1; t <= T; t++) {
+            System.out.println("#" + t + " " + new String(Base64.getDecoder().decode(br.readLine())));
         }
     }
 }
