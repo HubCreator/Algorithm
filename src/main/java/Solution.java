7897e74ad7dc33561b47581ecd2cce8eeb263784
HubCreator <chocomilk8604@gmail.com>
2022-11-14 10:22:15 +0900

    유효 숫자 수정 및 완성. 채점 사이트에서 MessageFormat 클래스 사용 불가. 효율성 테스트 완료

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,27 +1,23 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.text.MessageFormat;
 
 class Solution {
     static StringBuffer answer = new StringBuffer();
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
         for (int test_case = 1; test_case <= T; test_case++) {
-            answer.append("#").append(test_case).append(" ");
             char[] ch = br.readLine().toCharArray();
-            int length = ch.length - 1;
             int n = Integer.parseInt(ch[0] + "" + ch[1]);
-
             if (ch[2] - '0' >= 5) n += 1;
-            String resultNum = String.valueOf(n);
-            if (resultNum.length() > 2) length = ch.length;
+            int length = ch.length - 1;
 
-            String result = MessageFormat.format("{0}.{1}*10^{2}", resultNum.charAt(0), resultNum.charAt(1), length);
-            answer.append(result).append("\n");
+            String resultNum = n + "";
+            if (resultNum.length() > 2) length++;
+
+            String result = resultNum.charAt(0) + "." + resultNum.charAt(1) + "*10^" + length;
+            answer.append("#" + test_case + " " + result + "\n");
         }
         System.out.println(answer);
     }
