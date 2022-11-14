0cc42ed7cecd5b0c023fb4884de75311fc9ef3c5
HubCreator <chocomilk8604@gmail.com>
2022-11-14 21:07:43 +0900

    N-Queen dfs 풀이 완료. 제한시간 초과

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 class Point {
     int y, x;
@@ -37,10 +35,6 @@ public class Solution {
         int T = Integer.parseInt(br.readLine());
         for (int test_case = 1; test_case <= T; test_case++) {
             int N = Integer.parseInt(br.readLine());
-            if (N == 1) {
-                answer.append("#" + test_case + " " + 1 + "\n");
-                continue;
-            }
             resultCnt = 0;
             board = new boolean[N][N];
             dfs(board, 0);
@@ -66,31 +60,22 @@ public class Solution {
 
     private static boolean isValid(boolean[][] board) {
         List<Point> entry = new ArrayList<>();
+        Set<Integer> ySet = new HashSet<>();
+        Set<Integer> xSet = new HashSet<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (board[i][j]) entry.add(new Point(i, j));
+                if (board[i][j]) {
+                    entry.add(new Point(i, j));
+                    ySet.add(i);
+                    xSet.add(j);
+                }
             }
         }
-        int y = board.length;
-        int x = board.length;
-        // 좌 우 방향으로 수직선 상에 위치하는지 체크
-        for (Point p : entry) {
-            if ((y ^ p.y) == 0) return false;
-            if ((x ^ p.x) == 0) return false;
-            y = p.y;
-            x = p.x;
-        }
-
-        // 대각선 방향으로 체크
-        List<Point> tmp = new ArrayList<>();
-        for (Point point : entry) {
-            for (int i = -board.length; i <= board.length; i++) {
-                if (i == 0) continue;
-                Point p1 = new Point(point.y + i, point.x + i);
-                Point p2 = new Point(point.y - i, point.x - i);
-                if (!tmp.isEmpty() && tmp.contains(p1) && tmp.contains(p2)) return false;
-                tmp.add(p1);
-                tmp.add(p2);
+        if (ySet.size() != board.length || xSet.size() != board.length) return false;
+        for (Point p1 : entry) {
+            for (Point p2 : entry) {
+                if (p1.equals(p2)) continue;
+                if (Math.abs((p1.y - p2.y) / (p1.x - p2.x)) == 1) return false;
             }
         }
         return true;
