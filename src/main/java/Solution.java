628b8199e623e57e73f6bfe8cdf1693ceab66ec2
HubCreator <chocomilk8604@gmail.com>
2022-11-14 16:44:44 +0900

    간단한 369 게임

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,13 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Base64;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
+    static StringBuilder answer = new StringBuilder();
+    static List<Character> entry = Arrays.asList('3', '6', '9');
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 1; t <= T; t++) {
-            System.out.println("#" + t + " " + new String(Base64.getDecoder().decode(br.readLine())));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            String s = i + "";
+            int cnt = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (entry.contains(s.charAt(j))) cnt++;
+            }
+            if (cnt == 0) {
+                answer.append(i + " ");
+                continue;
+            }
+            while (cnt > 0) {
+                answer.append('-');
+                cnt--;
+            }
+            answer.append(" ");
         }
+        System.out.println(answer);
     }
 }
