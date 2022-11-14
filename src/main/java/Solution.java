1409e057b7d903813c6cef6fe787e43ae985f73b
HubCreator <chocomilk8604@gmail.com>
2022-11-14 22:47:57 +0900

    N-Queen 구현 완료

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,82 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-class Point {
-    int y, x;
-
-    public Point(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return y == point.y && x == point.x;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(y, x);
-    }
-}
 
 public class Solution {
+    static int n;
+    static int[] data;
+    static int cnt;
     static StringBuilder answer = new StringBuilder();
-    static boolean board[][];
-    static int resultCnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+
         for (int test_case = 1; test_case <= T; test_case++) {
-            int N = Integer.parseInt(br.readLine());
-            resultCnt = 0;
-            board = new boolean[N][N];
-            dfs(board, 0);
-            answer.append("#" + test_case + " " + resultCnt + "\n");
+            cnt = 0;
+            n = Integer.parseInt(br.readLine()); // n*n크기의 체스판
+            data = new int[n]; // n크기의 1차원 배열
+
+            queen(0); // depth 0을 넣어줌
+            answer.append("#" + test_case + " " + cnt + "\n");
         }
         System.out.println(answer);
     }
 
-    private static void dfs(boolean[][] board, int L) {
-        // 기저 조건 (각 위치마다 8방향으로 뻗어나가며 경로에 다른 말이 없는지 확인)
-        if (L == board.length) {
-            if (isValid(board)) {
-                resultCnt++;
-            }
+    public static void queen(int depth) {
+        if (depth == n) {
+            cnt++;
             return;
         }
-        for (int i = 0; i < board.length; i++) {
-            board[L][i] = true;
-            dfs(board, L + 1);
-            board[L][i] = false;
+        for (int i = 0; i < n; i++) {
+            data[depth] = i;
+            if (check(depth)) queen(depth + 1);
         }
     }
 
-    private static boolean isValid(boolean[][] board) {
-        List<Point> entry = new ArrayList<>();
-        Set<Integer> ySet = new HashSet<>();
-        Set<Integer> xSet = new HashSet<>();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j]) {
-                    entry.add(new Point(i, j));
-                    ySet.add(i);
-                    xSet.add(j);
-                }
-            }
-        }
-        if (ySet.size() != board.length || xSet.size() != board.length) return false;
-        for (Point p1 : entry) {
-            for (Point p2 : entry) {
-                if (p1.equals(p2)) continue;
-                if (Math.abs((p1.y - p2.y) / (p1.x - p2.x)) == 1) return false;
-            }
+    public static boolean check(int depth) {
+        for (int i = 0; i < depth; i++) {
+            if (data[depth] == data[i]) return false;
+            else if (Math.abs(depth - i) == Math.abs(data[depth] - data[i])) return false;
         }
         return true;
     }
