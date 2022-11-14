f6617f7f1a6cbc61b03195d49754a251bf5569b7
HubCreator <chocomilk8604@gmail.com>
2022-11-14 23:28:57 +0900

    N-Queen 주석 추가

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class Solution {
     static int n;
-    static int[] data;
+    static int[] ch;
     static int cnt;
     static StringBuilder answer = new StringBuilder();
 
@@ -15,29 +15,34 @@ public class Solution {
         for (int test_case = 1; test_case <= T; test_case++) {
             cnt = 0;
             n = Integer.parseInt(br.readLine()); // n*n크기의 체스판
-            data = new int[n]; // n크기의 1차원 배열
-
-            queen(0); // depth 0을 넣어줌
+            ch = new int[n]; // n크기의 1차원 배열
+            dfs(0); // depth 0을 넣어줌
             answer.append("#" + test_case + " " + cnt + "\n");
         }
         System.out.println(answer);
     }
 
-    public static void queen(int depth) {
-        if (depth == n) {
+    public static void dfs(int L) {
+        // 기저 조건
+        if (L == n) {
             cnt++;
             return;
         }
         for (int i = 0; i < n; i++) {
-            data[depth] = i;
-            if (check(depth)) queen(depth + 1);
+            ch[L] = i; // check 배열의 각 인덱스엔 몇 번째 col을 넣을지 저장
+            if (isValid(L)) dfs(L + 1);
         }
     }
 
-    public static boolean check(int depth) {
-        for (int i = 0; i < depth; i++) {
-            if (data[depth] == data[i]) return false;
-            else if (Math.abs(depth - i) == Math.abs(data[depth] - data[i])) return false;
+    public static boolean isValid(int L) {
+        for (int i = 0; i < L; i++) {
+            if (ch[L] == ch[i]) {
+                // 같은 열을 선택했는지 체크
+                return false;
+            } else if (Math.abs(L - i) == Math.abs(ch[L] - ch[i])) {
+                 // 대각선 상에 있는지 체크 (기울기 1 : x증가량 == y증가량)
+                return false;
+            }
         }
         return true;
     }
