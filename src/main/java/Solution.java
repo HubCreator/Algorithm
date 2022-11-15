def4f230409592ddde06d789bf85de4130b3e025
HubCreator <chocomilk8604@gmail.com>
2022-11-15 14:20:22 +0900

    보급로, bfs 개선

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,38 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
-class Point {
-    int y, x;
+class Point implements Comparable<Point> {
+    int y, x, value;
 
-    public Point(int y, int x) {
+    public Point(int y, int x, int value) {
         this.y = y;
         this.x = x;
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        return this.value - o.value;
     }
 }
 
 public class Solution {
+    static int N, result;
+    static int[][] map;
+    static boolean[][] visited;
+    static int dy[] = {0, 0, -1, 1};
+    static int dx[] = {-1, 1, 0, 0};
+    static PriorityQueue<Point> pq;
     static StringBuilder answer = new StringBuilder();
-    static int n;
-    static int[][] board, dis;
-    static final int[] dy = {-1, 0, 1, 0};
-    static final int[] dx = {0, 1, 0, -1};
+
+    private static void initialize(BufferedReader br) throws IOException {
+        visited = new boolean[N][N];
+        visited[0][0] = true;
+        result = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = s.charAt(j) - '0'; // char -> int
+            }
+        }
+    }
 
     private static void bfs() {
-        Queue<Point> queue = new ArrayDeque<>();
-        queue.offer(new Point(0, 0));
-        dis[0][0] = 0;
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();
+        pq = new PriorityQueue();
+        pq.offer(new Point(0, 0, 0));
+
+        while (!pq.isEmpty()) {
+            Point p = pq.poll();
+            if (p.y == N - 1 && p.x == N - 1) { // 목적지에 도달했을 때 return
+                result = Math.min(result, p.value);
+                return;
+            }
             for (int i = 0; i < 4; i++) {
                 int ny = p.y + dy[i];
                 int nx = p.x + dx[i];
-                if (ny >= 0 && ny < n && nx >= 0 && nx < n && board[ny][nx] != -1) {
-                    dis[ny][nx] = Math.min(dis[ny][nx], dis[p.y][p.x] + board[ny][nx]);
-                    queue.offer(new Point(ny, nx));
-                    board[p.y][p.x] = -1; // 지나갔던 경로를 -1 표시
+                if (ny >= 0 && ny < N && nx >= 0 && nx < N && !visited[ny][nx]) {
+                    visited[ny][nx] = true;
+                    pq.offer(new Point(ny, nx, p.value + map[ny][nx]));
                 }
             }
         }
@@ -42,24 +64,12 @@ public class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int test_case = 1; test_case <= T; test_case++) {
-            n = Integer.parseInt(br.readLine());
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
             initialize(br);
             bfs();
-            answer.append("#" + test_case + " " + dis[n - 1][n - 1] + "\n");
+            answer.append("#" + test_case + " " + result + "\n");
         }
         System.out.println(answer);
-    }
-
-    private static void initialize(BufferedReader br) throws IOException {
-        dis = new int[n][n];
-        int row = 0;
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < n; j++) {
-                board[row][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
-                dis[i][j] = Integer.MAX_VALUE;
-            }
-            row++;
-        }
     }
 }
