b77656feaa7470d3cc2d05b1a38ed15639a006e4
HubCreator <chocomilk8604@gmail.com>
2022-11-15 12:48:18 +0900

    보급로, bfs

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,49 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Point {
+    int y, x;
+
+    public Point(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+}
 
 public class Solution {
-    static int n;
-    static int[] ch;
-    static int cnt;
     static StringBuilder answer = new StringBuilder();
+    static int n;
+    static int[][] board, dis;
+    static final int[] dy = {-1, 0, 1, 0};
+    static final int[] dx = {0, 1, 0, -1};
+
+    private static void bfs() {
+        Queue<Point> queue = new ArrayDeque<>();
+        queue.offer(new Point(0, 0));
+        dis[0][0] = 0;
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + dy[i];
+                int nx = p.x + dx[i];
+                if (ny >= 0 && ny < n && nx >= 0 && nx < n && board[ny][nx] != -1) {
+                    dis[ny][nx] = Math.min(dis[ny][nx], dis[p.y][p.x] + board[ny][nx]);
+                    queue.offer(new Point(ny, nx));
+                    board[p.y][p.x] = -1; // 지나갔던 경로를 -1 표시
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
         for (int test_case = 1; test_case <= T; test_case++) {
-            cnt = 0;
-            n = Integer.parseInt(br.readLine()); // n*n크기의 체스판
-            ch = new int[n]; // n크기의 1차원 배열
-            dfs(0); // depth 0을 넣어줌
-            answer.append("#" + test_case + " " + cnt + "\n");
+            n = Integer.parseInt(br.readLine());
+            initialize(br);
+            bfs();
+            answer.append("#" + test_case + " " + dis[n - 1][n - 1] + "\n");
         }
         System.out.println(answer);
     }
 
-    public static void dfs(int L) {
-        // 기저 조건
-        if (L == n) {
-            cnt++;
-            return;
-        }
+    private static void initialize(BufferedReader br) throws IOException {
+        dis = new int[n][n];
+        int row = 0;
         for (int i = 0; i < n; i++) {
-            ch[L] = i; // check 배열의 각 인덱스엔 몇 번째 col을 넣을지 저장
-            if (isValid(L)) dfs(L + 1);
-        }
-    }
-
-    public static boolean isValid(int L) {
-        for (int i = 0; i < L; i++) {
-            if (ch[L] == ch[i]) {
-                // 같은 열을 선택했는지 체크
-                return false;
-            } else if (Math.abs(L - i) == Math.abs(ch[L] - ch[i])) {
-                 // 대각선 상에 있는지 체크 (기울기 1 : x증가량 == y증가량)
-                return false;
+            String s = br.readLine();
+            for (int j = 0; j < n; j++) {
+                board[row][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
+                dis[i][j] = Integer.MAX_VALUE;
             }
+            row++;
         }
-        return true;
     }
 }
