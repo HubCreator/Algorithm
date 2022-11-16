a87deda3deeb9ea328bd020cc166ccd0c1fb891b
HubCreator <chocomilk8604@gmail.com>
2022-11-16 16:55:13 +0900

    농작물 수확하기

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,22 +4,41 @@ import java.io.InputStreamReader;
 
 public class Solution {
     static StringBuilder answer = new StringBuilder();
-    static int T;
+    static int T, N;
+    static int[][] board;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine().trim());
         for (int test_case = 1; test_case <= T; test_case++) {
-            answer.append("#" + test_case + " " + solution(br) + "\n");
+            initialize(br);
+            answer.append("#" + test_case + " " + solution() + "\n");
         }
         System.out.println(answer);
     }
 
-    private static int solution(BufferedReader br) throws IOException {
-        String str = br.readLine();
-        for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) return 0;
+    private static void initialize(BufferedReader br) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                board[i][j] = str.charAt(j) - '0';
+            }
         }
-        return 1;
+    }
+
+    private static int solution() {
+        int cnt = 0, level = 0, sum = 0;
+        int mid = (N / 2);
+
+        while (level < N) {
+            for (int j = mid - cnt; j <= mid + cnt; j++) {
+                sum += board[level][j];
+            }
+            cnt = level < mid ? cnt + 1 : cnt - 1;
+            level++;
+        }
+        return sum;
     }
 }
