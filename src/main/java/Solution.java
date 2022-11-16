b61515b0dbe090b4937de8941bdba1ff361141fb
HubCreator <chocomilk8604@gmail.com>
2022-11-16 12:40:32 +0900

    패턴의 마디 길이. 문제 오류

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,42 +5,31 @@ import java.util.StringTokenizer;
 
 public class Solution {
     static StringBuilder answer = new StringBuilder();
-    static StringTokenizer st;
-    static int T, length, min, max, sum;
-    static int[] arr;
+    static int T;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine().trim());
         for (int test_case = 1; test_case <= T; test_case++) {
-            initialize(br);
-            answer.append("#" + test_case + " " + solution() + "\n");
+            answer.append("#" + test_case + " " + solution(br) + "\n");
         }
         System.out.println(answer);
     }
 
-    private static int solution() {
-        for (int x : arr) {
-            if (x != min && x != max) sum += x;
+    private static int solution(BufferedReader br) throws IOException {
+        String str = br.readLine().trim();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            sb.append(str.charAt(i));
+            if (isValid(str, sb)) {
+                return sb.length();
+            }
         }
-        return Math.round(sum / (float) (length - 2));
+        return -1;
     }
 
-    private static void initialize(BufferedReader br) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        length = st.countTokens();
-        arr = new int[length];
-        sum = 0;
-        for (int i = 0; i < length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // 최대 최소값 구하기
-        min = Integer.MAX_VALUE;
-        max = Integer.MIN_VALUE;
-        for (int x : arr) {
-            min = Math.min(min, x);
-            max = Math.max(max, x);
-        }
+    private static boolean isValid(String str, StringBuilder sb) {
+        return new StringTokenizer(str, sb.toString()).countTokens() == 0;
     }
 }
