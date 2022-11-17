5b070f4b612dd6bd7a25e917aca1999664bb47d8
HubCreator <chocomilk8604@gmail.com>
2022-11-17 19:11:45 +0900

    어디에 단어가 들어갈까

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Solution {
     static StringBuilder answer = new StringBuilder();
-    static int T, N;
+    static StringTokenizer st;
+    static int T, N, K;
     static int[][] board;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine().trim());
         for (int test_case = 1; test_case <= T; test_case++) {
+
             initialize(br);
             answer.append("#" + test_case + " " + solution() + "\n");
         }
@@ -18,27 +23,38 @@ public class Solution {
     }
 
     private static void initialize(BufferedReader br) throws IOException {
-        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
         board = new int[N][N];
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                board[i][j] = str.charAt(j) - '0';
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
     }
 
-    private static int solution() {
-        int cnt = 0, level = 0, sum = 0;
-        int mid = (N / 2);
 
-        while (level < N) {
-            for (int j = mid - cnt; j <= mid + cnt; j++) {
-                sum += board[level][j];
+    private static int solution() {
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            int rtRow, ltRow = 0;
+            int rtCol, ltCol = 0;
+            for (rtRow = 0, rtCol = 0; rtRow < N && rtCol < N; rtRow++, rtCol++) {
+                if (board[i][rtRow] == 0) {
+                    if (rtRow - ltRow == K) cnt++;
+                    ltRow = rtRow + 1;
+                }
+
+                if (board[rtCol][i] == 0) {
+                    if (rtCol - ltCol == K) cnt++;
+                    ltCol = rtCol + 1;
+                }
             }
-            cnt = level < mid ? cnt + 1 : cnt - 1;
-            level++;
+            if (rtRow - ltRow == K) cnt++;
+            if (rtCol - ltCol == K) cnt++;
         }
-        return sum;
+        return cnt;
     }
 }
