e446a594eb3ac6b6b865299d9e6225b587769483
HubCreator <chocomilk8604@gmail.com>
2022-11-18 16:45:22 +0900

    회문

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,21 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Solution {
+    static final int BOARD_SIZE = 8;
     static StringBuilder answer = new StringBuilder();
-    static StringTokenizer st;
-    static int T, N, K;
-    static int[][] board;
+    static int N;
+    static char[][] board;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine().trim());
-        for (int test_case = 1; test_case <= T; test_case++) {
-
+        for (int test_case = 1; test_case <= 10; test_case++) {
             initialize(br);
             answer.append("#" + test_case + " " + solution() + "\n");
         }
@@ -23,38 +18,46 @@ public class Solution {
     }
 
     private static void initialize(BufferedReader br) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+        board = new char[BOARD_SIZE][BOARD_SIZE];
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                board[i][j] = str.charAt(j);
             }
         }
     }
 
-
     private static int solution() {
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            int rtRow, ltRow = 0;
-            int rtCol, ltCol = 0;
-            for (rtRow = 0, rtCol = 0; rtRow < N && rtCol < N; rtRow++, rtCol++) {
-                if (board[i][rtRow] == 0) {
-                    if (rtRow - ltRow == K) cnt++;
-                    ltRow = rtRow + 1;
-                }
-
-                if (board[rtCol][i] == 0) {
-                    if (rtCol - ltCol == K) cnt++;
-                    ltCol = rtCol + 1;
-                }
+        int result = 0;
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            // 초기 윈도우 생성
+            StringBuilder sbRow = new StringBuilder();
+            StringBuilder sbCol = new StringBuilder();
+            for (int j = 0; j < N - 1; j++) {
+                sbRow.append(board[i][j]);
+                sbCol.append(board[j][i]);
             }
-            if (rtRow - ltRow == K) cnt++;
-            if (rtCol - ltCol == K) cnt++;
+            // 슬라이딩 윈도우
+            for (int j = N - 1; j < BOARD_SIZE; j++) {
+                sbRow.append(board[i][j]);
+                if (isPalindrome(sbRow.toString())) result++;
+                sbRow.deleteCharAt(0);
+
+                sbCol.append(board[j][i]);
+                if (isPalindrome(sbCol.toString())) result++;
+                sbCol.deleteCharAt(0);
+            }
         }
-        return cnt;
+        return result;
+    }
+
+    private static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
