a61120ec94f28b61ff152905e0d9c27bf570d13e
HubCreator <chocomilk8604@gmail.com>
2022-11-18 17:29:48 +0900

    2차원 배열의 4방향 최대 합

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,56 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static final int BOARD_SIZE = 8;
+    static final int BOARD_SIZE = 100;
     static StringBuilder answer = new StringBuilder();
+    static StringTokenizer st;
     static int N;
-    static char[][] board;
+    static int[][] board;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int test_case = 1; test_case <= 10; test_case++) {
+            N = Integer.parseInt(br.readLine());
             initialize(br);
-            answer.append("#" + test_case + " " + solution() + "\n");
+            answer.append("#" + N + " " + solution() + "\n");
         }
         System.out.println(answer);
     }
 
     private static void initialize(BufferedReader br) throws IOException {
-        board = new char[BOARD_SIZE][BOARD_SIZE];
-        N = Integer.parseInt(br.readLine());
+        board = new int[BOARD_SIZE][BOARD_SIZE];
         for (int i = 0; i < BOARD_SIZE; i++) {
-            String str = br.readLine();
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < BOARD_SIZE; j++) {
-                board[i][j] = str.charAt(j);
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
     }
 
     private static int solution() {
-        int result = 0;
+        int max = 0, diagonalASum = 0, diagonalBSum = 0;
         for (int i = 0; i < BOARD_SIZE; i++) {
-            // 초기 윈도우 생성
-            StringBuilder sbRow = new StringBuilder();
-            StringBuilder sbCol = new StringBuilder();
-            for (int j = 0; j < N - 1; j++) {
-                sbRow.append(board[i][j]);
-                sbCol.append(board[j][i]);
+            int j, rowSum = 0, colSum = 0;
+            for (j = 0; j < BOARD_SIZE; j++) {
+                rowSum += board[i][j];
+                colSum += board[j][i];
             }
-            // 슬라이딩 윈도우
-            for (int j = N - 1; j < BOARD_SIZE; j++) {
-                sbRow.append(board[i][j]);
-                if (sbRow.toString().equals(sbRow.reverse().toString())) result++;
-                sbRow.reverse();
-                sbRow.deleteCharAt(0);
-
-                sbCol.append(board[j][i]);
-                if (sbCol.toString().equals(sbCol.reverse().toString())) result++;
-                sbCol.reverse();
-                sbCol.deleteCharAt(0);
-            }
+            if (i == j) diagonalASum += board[i][j];
+            if (BOARD_SIZE - i == j - 1) diagonalBSum += board[j - 1][i];
+            max = Math.max(max, Math.max(rowSum, colSum));
         }
-        return result;
+        max = Math.max(max, Math.max(diagonalASum, diagonalBSum));
+        return max;
     }
 }
