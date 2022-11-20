fbd10f660f55e69aec97a73af32b24c67f53f610
HubCreator <chocomilk8604@gmail.com>
2022-11-20 15:04:27 +0900

    공과 잡초

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,36 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
-class Solution {
+public class Solution {
     static StringBuilder answer = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int test_case = 1; test_case <= T; test_case++) {
-            String num = br.readLine();
-            char[] ch = num.toCharArray();
-            int min = Integer.parseInt(num);
-            int max = min;
-
-            for (int i = 0; i < ch.length - 1; i++) {
-                for (int j = i + 1; j < ch.length; j++) {
-                    char tmp = ch[i];
-                    ch[i] = ch[j];
-                    ch[j] = tmp;
-                    if (ch[0] != '0') { // min, max 값 갱신
-                        min = Math.min(min, Integer.parseInt(String.valueOf(ch)));
-                        max = Math.max(max, Integer.parseInt(String.valueOf(ch)));
-                    }
-                    // 원본으로 돌려놓음
-                    tmp = ch[i];
-                    ch[i] = ch[j];
-                    ch[j] = tmp;
-                }
-            }
-            answer.append("#" + test_case + " " + min + " " + max + "\n");
+            String str = br.readLine();
+            answer.append("#" + test_case + " " + solution(str) + "\n");
         }
         System.out.println(answer);
+    }
+
+    private static int solution(String str) {
+        char[] chars = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+
+        boolean flag = false;
+        for (char ch : chars) {
+            if (flag) {
+                if (ch == ')' || ch == '|') result++;
+                flag = false;
+            } else if (ch == '(') flag = true;
+            else if (ch == ')' && (stack.peek() == '|' || stack.peek() == '(')) {
+                result++;
+            }
+            stack.push(ch);
+        }
+        return result;
     }
 }
