681388ceaf7bff949e2a4d795d2b7ba4e7738e76
HubCreator <chocomilk8604@gmail.com>
2022-11-21 10:52:44 +0900

    조교의 성적 매기기

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,72 +3,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Solution {
-    static final int SIZE = 5;
-    static char board[][] = new char[SIZE][SIZE];
-    static Map<Character, int[]> map;
-    static List<String> tokens;
+public class Solution {
+    static int N, K;
+    static final List<String> entry = Arrays.asList("A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0");
+    static List<Double> scores;
+    static StringBuilder answer = new StringBuilder();
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String message = br.readLine();
-        String key = br.readLine();
-        makeBoard(key);
-        transformMessage(message);
-        System.out.println(encode());
-    }
+        int T = Integer.parseInt(br.readLine());
 
-    private static String encode() {
-        StringBuilder result = new StringBuilder();
-
-        for (String token : tokens) {
-            int[] firstCoord = map.get(token.charAt(0));
-            int[] lastCoord = map.get(token.charAt(1));
-            if (firstCoord[0] == lastCoord[0]) { // 같은 행
-                result.append(board[firstCoord[0]][(firstCoord[1] + 1) % SIZE]);
-                result.append(board[lastCoord[0]][(lastCoord[1] + 1) % SIZE]);
-            } else if (firstCoord[1] == lastCoord[1]) { // 같은 열
-                result.append(board[(firstCoord[0] + 1) % SIZE][firstCoord[1]]);
-                result.append(board[(lastCoord[0] + 1) % SIZE][lastCoord[1]]);
-            } else {
-                result.append(board[firstCoord[0]][lastCoord[1]]);
-                result.append(board[lastCoord[0]][firstCoord[1]]);
-            }
+        for (int test_case = 1; test_case <= T; test_case++) {
+            initialize(br);
+            answer.append("#" + test_case + " " + solution() + "\n");
         }
-        return result.toString();
+        System.out.println(answer);
     }
 
-    private static void transformMessage(String message) {
-        int idx = 0;
-        tokens = new ArrayList<>();
-        StringBuilder sb = new StringBuilder(message);
-        while (idx < sb.length() - 2) {
-            if (sb.charAt(idx) == sb.charAt(idx + 1)) {
-                char tmp = 'X';
-                if (sb.charAt(idx) == 'X') tmp = 'Q';
-                sb.insert(idx + 1, tmp);
-                sb.append(tmp);
-            }
-            tokens.add(String.valueOf(sb.charAt(idx)) + sb.charAt(idx + 1));
-            idx += 2;
+    private static String solution() {
+        double target = scores.get(K - 1);
+        Collections.sort(scores, Collections.reverseOrder());
+        return entry.get(scores.indexOf(target) / (N / 10));
+    }
+
+    private static void initialize(BufferedReader br) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        scores = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            double totalScore = getTotalScore(Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken()));
+            scores.add(totalScore);
         }
     }
 
-    private static void makeBoard(String key) {
-        StringBuilder sb = new StringBuilder(key);
-        for (int i = 'A'; i <= 'Z'; i++) sb.append((char) i);
-
-        map = new HashMap();
-        char[] chars = sb.toString().toCharArray();
-        int idx = 0;
-
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                while (map.containsKey(chars[idx]) || chars[idx] == 'J') idx++;
-                map.put(chars[idx], new int[]{i, j});
-                board[i][j] = chars[idx];
-                idx++;
-            }
-        }
+    private static double getTotalScore(int midEx, int finalEx, int hw) {
+        return (midEx * 0.35 + finalEx * 0.45 + hw * 0.2);
     }
 }
