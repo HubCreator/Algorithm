a9d4a13ffd66e7d29719cb408b11874c3e91258e
HubCreator <chocomilk8604@gmail.com>
2022-11-22 20:57:00 +0900

    암호 생성기

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,45 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringJoiner;
 import java.util.StringTokenizer;
 
 public class Solution {
+    static final int SIZE = 8;
+
     static StringBuilder answer = new StringBuilder();
     static StringTokenizer st;
-    static int N;
-    static int[][] board;
+    static Deque<Integer> deque;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int test_case = 1; test_case <= 10; test_case++) {
+        for (int i = 1; i <= 10; i++) {
+            int n = Integer.parseInt(br.readLine());
             initialize(br);
-            answer.append("#" + test_case + " " + solution() + "\n");
+            answer.append("#" + n + " " + solution() + "\n");
         }
         System.out.println(answer);
     }
 
-    private static int solution() {
-        int result = 0;
-        for (int col = 0; col < N; col++) {
-            StringBuilder sb = new StringBuilder();
-            for (int row = 0; row < N; row++) {
-                if (board[row][col] != 0) sb.append(board[row][col]);
-            }
-            for (int i = 0; i < sb.length() - 1; i++) {
-                if (sb.charAt(i) == '1' && sb.charAt(i + 1) == '2') result++;
+    private static String solution() {
+        StringJoiner stringJoiner = new StringJoiner(" ", "", "");
+
+        while (!deque.isEmpty() && deque.peekFirst() > 0) {
+            for (int i = 1; i <= 5; i++) {
+                if (deque.peekFirst() - i <= 0) {
+                    deque.pollFirst();
+                    deque.offerFirst(0);
+                    break;
+                }
+                deque.offerLast(deque.pollFirst() - i);
             }
         }
-        return result;
+        deque.pollFirst();
+        deque.offerLast(0);
+
+        for (Integer x : deque) stringJoiner.add(x + "");
+        return stringJoiner.toString();
     }
 
     private static void initialize(BufferedReader br) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
+        deque = new ArrayDeque<>(SIZE);
+        st = new StringTokenizer(br.readLine());
+        for (int j = 0; j < SIZE; j++) {
+            deque.offerLast(Integer.parseInt(st.nextToken()));
         }
     }
 }
