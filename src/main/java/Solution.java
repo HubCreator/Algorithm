53573023de84712d00b8c655a903ab9066cf2cec
HubCreator <chocomilk8604@gmail.com>
2022-11-24 12:18:34 +0900

    2xN 타일링, 피보나치

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,31 +1,15 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 class Solution {
-    public int solution(int[] cards) {
-        int curBox = 0, curIdx, sum = Arrays.stream(cards).sum();
+    int[] ch;
 
-        List<List<Integer>> list = new ArrayList<>();
+    public int solution(int n) {
+        ch = new int[n + 1];
+        return fibo(n);
+    }
 
-        while (sum != 0) {
-            curIdx = 0;
-            while (cards[curIdx] == 0) curIdx++;
-            list.add(new ArrayList<>());
-
-            while (cards[curIdx] != 0) {
-                list.get(curBox).add(cards[curIdx]);
-                int tmp = curIdx;
-                sum -= cards[curIdx];
-                curIdx = cards[curIdx] - 1;
-                cards[tmp] = 0;
-            }
-            curBox++;
-        }
-
-        Collections.sort(list, (a, b) -> b.size() - a.size());
-        if (list.size() == 1) return 0;
-        return list.get(0).size() * list.get(1).size();
+    private int fibo(int n) {
+        if (ch[n] > 0) return ch[n];
+        if (n == 1) return ch[n] = 1;
+        if (n == 2) return ch[n] = 2;
+        return ch[n] = (fibo(n - 1) + fibo(n - 2)) % 1000000007;
     }
 }
