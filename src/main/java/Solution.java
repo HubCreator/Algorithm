52e6f1a628dc90a5bfa2cd8737d4b292c24d5ec6
HubCreator <chocomilk8604@gmail.com>
2022-11-24 12:01:55 +0900

    숫자 게임

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,42 +1,31 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
-    public static Set<Integer> cached = new HashSet<>();
+    public int solution(int[] cards) {
+        int curBox = 0, curIdx, sum = Arrays.stream(cards).sum();
 
-    public int solution(String begin, String target, String[] words) {
-        return bfs(words, begin, target);
-    }
+        List<List<Integer>> list = new ArrayList<>();
 
-    private int bfs(String[] words, String begin, String target) {
-        Queue<List<Integer>> queue = new ArrayDeque<>();
-        queue.add(getEntry(words, begin));
-        int level = 0;
-        while (!queue.isEmpty()) {
-            int length = queue.size();
-            for (int i = 0; i < length; i++) {
-                List<Integer> poll = queue.poll();
-                for (Integer index : poll) {
-                    if (words[index].equals(target)) return level + 1;
-                    queue.offer(getEntry(words, words[index]));
-                }
+        while (sum != 0) {
+            curIdx = 0;
+            while (cards[curIdx] == 0) curIdx++;
+            list.add(new ArrayList<>());
+
+            while (cards[curIdx] != 0) {
+                list.get(curBox).add(cards[curIdx]);
+                int tmp = curIdx;
+                sum -= cards[curIdx];
+                curIdx = cards[curIdx] - 1;
+                cards[tmp] = 0;
             }
-            level++;
+            curBox++;
         }
-        return 0;
-    }
 
-    private List<Integer> getEntry(String[] words, String input) {
-        List<Integer> idxList = new ArrayList<>();
-
-        for (int idx = 0; idx < words.length; idx++) {
-            String word = words[idx];
-            int cnt = 0;
-            for (int i = 0; i < word.length(); i++) {
-                if (word.charAt(i) == input.charAt(i)) cnt++;
-            }
-            if (cnt == word.length() - 1 && !cached.contains(idx)) idxList.add(idx);
-        }
-        cached.addAll(idxList);
-        return idxList;
+        Collections.sort(list, (a, b) -> b.size() - a.size());
+        if (list.size() == 1) return 0;
+        return list.get(0).size() * list.get(1).size();
     }
 }
