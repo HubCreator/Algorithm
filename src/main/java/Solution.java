172fb905ebc2ccff0aa742436078ad1c14af9cb2
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-11-26 09:16:05 +0900

    귤 고르기

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,46 +1,24 @@
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 class Solution {
-    public String solution(int n, int k, String[] cmd) {
-        StringBuilder answer = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
-        Stack<int[]> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            list.add(i);
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int tmp = 0, answer = 0;
+        for (int x : tangerine) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
-        for (String s : cmd) {
-            StringTokenizer st = new StringTokenizer(s);
-            String command = st.nextToken();
-            if (command.equals("D")) {
-                k += Integer.parseInt(st.nextToken());
-            } else if (command.equals("U")) {
-                k -= Integer.parseInt(st.nextToken());
-            } else if (command.equals("C")) {
-                stack.push(new int[]{k, list.get(k)});
-                list.remove(k);
-                if (k == list.size()) k--;
-            } else if (command.equals("Z")) {
-                int[] pop = stack.pop();
-                list.add(pop[0], pop[1]);
-                if (k >= pop[0]) k++;
-            }
+        Collection<Integer> entry = map.values();
+        List<Integer> result = entry.stream().sorted((a, b) -> b - a).collect(Collectors.toList());
+        for (Integer x : result) {
+            tmp += x;
+            answer++;
+            if (tmp >= k) break;
         }
-
-        for (int i = 0; i < n; i++) {
-            if (list.contains(i)) answer.append('O');
-            else answer.append('X');
-        }
-
-        return answer.toString();
-    }
-
-    public static void main(String[] args) {
-        Solution T = new Solution();
-        int n = 8;
-        int k = 2;
-        String[] arr = new String[]{"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"};
-        System.out.println(T.solution(n, k, arr));
+        return answer;
     }
 }
