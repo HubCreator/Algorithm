4e9610ac440470e14d1f647088c7baa0a022dc6a
HubCreator <chocomilk8604@gmail.com>
2022-12-10 10:41:47 +0900

    스택 기본

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 
+public class Main {
+
+    public static final String DELIMITER = " ";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            validate(stack,br);
+        }
+    }
+
+    private static void validate(ArrayDeque<Integer> stack, BufferedReader br) throws IOException {
+        String input = br.readLine();
+        if (input.startsWith("push")) {
+            stack.offer(Integer.parseInt(input.split(DELIMITER)[1]));
+        }
+        if (input.startsWith("pop")) {
+            if (stack.isEmpty()) {
+                System.out.println(-1);
+                return;
+            }
+            System.out.println(stack.pollLast());
+        }
+        if (input.startsWith("size")) {
+            System.out.println(stack.size());
+        }
+        if (input.startsWith("empty")) {
+            System.out.println(stack.isEmpty() ? 1 : 0);
+        }
+        if (input.startsWith("top")) {
+            if (stack.isEmpty()) {
+                System.out.println(-1);
+                return;
+            }
+            System.out.println(stack.peekLast());
+        }
     }
 }
