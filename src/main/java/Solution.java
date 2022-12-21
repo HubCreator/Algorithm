99205c4dc31dac3a91a3f76e57f48e0733a7872e
HubCreator <chocomilk8604@gmail.com>
2022-12-21 10:20:01 +0900

    3진법 뒤집기

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,25 +1,13 @@
-
 class Solution {
-    public int solution(String input) {
-        for (int window = input.length(); window >= 2; window--) { // 윈도우의 크기
-            for (int index = 0; index <= input.length() - window; index++) { // 슬라이딩
-                if (isPalindrome(input, index, window)) {
-                    return window;
-                }
-            }
-        }
-        return 1;
-    }
 
-    private boolean isPalindrome(String str, int start, int window) {
-        if (window == 2) {
-            return str.charAt(start) == str.charAt(start + 1);
+    private static final int RADIX = 3;
+
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n != 0) {
+            sb.append(n % RADIX);
+            n /= RADIX;
         }
-        for (int i = start; i < start + window / 2; i++) {
-            if (str.charAt(i) != str.charAt(start + (start + window - 1) - i)) {
-                return false;
-            }
-        }
-        return true;
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
