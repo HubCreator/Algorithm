bc369b79b682cedf8c06316e0dc142b0671d906b
HubCreator <chocomilk8604@gmail.com>
2023-05-09 08:23:16 +0900

    리플렉션 예제와 노드 검색 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,64 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+class Myclass {
+    private final String name;
+    private final int age;
+
+    Myclass(final String name, final int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Myclass{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st1.nextToken());
-        int K = Integer.parseInt(st1.nextToken());
+    public static void main(String args[]) throws Exception {
+        final Class<Boolean> clazz = Boolean.class;
+        final Field field = clazz.getDeclaredField("FALSE");
+        field.setAccessible(true);
 
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[N];
-        for (int index = 0; index < N; index++) {
-            arr[index] = Integer.parseInt(st2.nextToken());
-        }
+        final Field modifier = Field.class.getDeclaredField("modifiers");
+        modifier.setAccessible(true);
+        modifier.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        System.out.println("modifier = " + field.getModifiers());
 
-        Map<Integer, Integer> map = new HashMap<>();
-        int rt, lt = 0, answer = Integer.MIN_VALUE;
+        field.set(null, true);
 
-        for (rt = 0; rt < N; rt++) {
-            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
-            if (map.get(arr[rt]) > K) {
-                while (arr[lt] != arr[rt]) {
-                    lt++;
-                }
-                lt++;
-                map.put(arr[rt], map.get(arr[rt]) - 1);
-            }
-            answer = Math.max(answer, rt - lt + 1);
-        }
-        System.out.println(answer);
+        System.out.format("Everything is %s", false); // "Everything is true"
+        System.out.println();
+        System.out.println(false); //false
+        System.out.println(Boolean.FALSE); //true
+    }
+}
+
+
+class MyClass2 {
+    public static final String HELLO = new String("hello");
+    public static final int hi = 10;
+}
+
+class Hello {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        final Class<MyClass2> clazz = MyClass2.class;
+        final Field field = clazz.getDeclaredField("hi");
+        field.setAccessible(true);
+
+        final Field modifier = Field.class.getDeclaredField("modifiers");
+        modifier.setAccessible(true);
+        modifier.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        System.out.println("field.getModifiers() = " + field.getModifiers());
+
+        field.set(null, 1000);
+
+        System.out.println(MyClass2.hi);
     }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,33 +1,184 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
+
+class Vertex {
+    private final String station;
+
+    Vertex(final String station) {
+        this.station = station;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Vertex vertex = (Vertex) o;
+        return Objects.equals(station, vertex.station);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(station);
+    }
+
+    @Override
+    public String
+    toString() {
+        return "Vertex{" +
+                "station='" + station + '\'' +
+                '}';
+    }
+
+    public String getStation() {
+        return station;
+    }
+}
+
+
+class Graph {
+    private final Map<Vertex, List<Vertex>> adjVertices = new HashMap<>();
+
+    public void addVertex(final String station) {
+        adjVertices.putIfAbsent(new Vertex(station), new ArrayList<>());
+    }
+
+    // 양방향
+    public void addEdge(final String from, final String to) {
+        final Vertex fromVertex = new Vertex(from);
+        final Vertex toVertex = new Vertex(to);
+        adjVertices.get(fromVertex).add(toVertex);
+        adjVertices.get(toVertex).add(fromVertex);
+    }
+
+    public void removeEdge(final String from, final String to) {
+        final Vertex fromVertex = new Vertex(from);
+        final Vertex toVertex = new Vertex(to);
+        final List<Vertex> adjFromVertex = adjVertices.get(fromVertex);
+        final List<Vertex> adjToVertex = adjVertices.get(toVertex);
+        if (adjFromVertex != null) {
+            adjFromVertex.remove(toVertex);
+        }
+        if (adjToVertex != null) {
+            adjToVertex.remove(fromVertex);
+        }
+    }
+
+    public List<Vertex> getAdjVertices(final String station) {
+        return adjVertices.get(new Vertex(station));
+    }
+}
+
 
 class Solution {
-    public int solution(int number, int limit, int power) {
-        List<Integer> divisorCountList = getDivisorCountList(number);
-        int result = 0;
-        for (Integer divisor : divisorCountList) {
-            result += divisor > limit ? power : divisor;
-        }
-        return result;
+    public static void main(String[] args) {
+        final Graph graph = initGraph();
+        final List<Vertex> 잠실새내 = graph.getAdjVertices("잠실새내");
+        System.out.println(잠실새내);
+
+        final List<Vertex> 잠실 = graph.getAdjVertices("잠실");
+        System.out.println(잠실);
+
+        System.out.println("------------------------------------------------------------------------");
+
+        final Set<Vertex> dfs = dfs(graph, "잠실나루");
+        System.out.println(dfs);
+
+        System.out.println("------------------------------------------------------------------------");
+
+        final Set<Vertex> shortestBfs = shortestBfs(graph, "잠실나루", "송파");
+        System.out.println(shortestBfs);
+
     }
 
-    private List<Integer> getDivisorCountList(int number) {
-        List<Integer> result = new ArrayList<>(number);
-        for (int index = 1; index <= number; index++) {
-            result.add(getDivisorCount(index));
-        }
-        return result;
-    }
-
-    private int getDivisorCount(int number) {
-        int count = 0;
-        for (int i = 1; i * i <= number; i++) {
-            if (i * i == number) {
-                count++;
-            } else if (number % i == 0) {
-                count += 2;
+    private static Set<Vertex> dfs(final Graph graph, final String root) {
+        final Set<Vertex> visited = new LinkedHashSet<>();
+        final Deque<String> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            final String pop = stack.pop();
+            if (!visited.contains(new Vertex(pop))) {
+                visited.add(new Vertex(pop));
+                for (Vertex adjVertex : graph.getAdjVertices(pop)) {
+                    stack.push(adjVertex.getStation());
+                }
             }
         }
-        return count;
+        return visited;
+    }
+
+    private static Set<Vertex> bfs(final Graph graph, final String root) {
+        final Set<Vertex> visited = new LinkedHashSet<>();
+        Queue<String> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            final String poll = queue.poll();
+            for (Vertex adjVertex : graph.getAdjVertices(poll)) {
+                if (!visited.contains(adjVertex)) {
+                    visited.add(adjVertex);
+                    queue.offer(adjVertex.getStation());
+                }
+            }
+        }
+        return visited;
+    }
+
+    private static Set<Vertex> shortestBfs(final Graph graph, final String root, final String destination) {
+        final Set<Vertex> visited = new LinkedHashSet<>();
+        Queue<String> queue = new ArrayDeque<>();
+        final List<Vertex> adjRoot = graph.getAdjVertices(root);
+        visited.add(new Vertex(root));
+        for (Vertex vertex : adjRoot) {
+            queue.offer(vertex.getStation());
+        }
+
+        while (!queue.isEmpty()) {
+            final int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                final String poll = queue.poll();
+                visited.add(new Vertex(poll));
+                if (poll.equals(destination)) {
+                    return visited;
+                }
+                for (Vertex adjVertex : graph.getAdjVertices(poll)) {
+                    queue.offer(adjVertex.getStation());
+                    visited.add(adjVertex);
+                }
+
+            }
+        }
+
+        return visited;
+    }
+
+    private static Graph initGraph() {
+        final Graph graph = new Graph();
+        // 2호선
+        graph.addVertex("강변");
+        graph.addVertex("잠실나루");
+        graph.addVertex("잠실");
+        graph.addVertex("잠실새내");
+
+        graph.addEdge("강변", "잠실나루");
+        graph.addEdge("잠실나루", "잠실");
+        graph.addEdge("잠실", "잠실새내");
+
+        // 8호선
+        graph.addVertex("몽촌토성");
+        graph.addVertex("석촌");
+        graph.addVertex("송파");
+
+        graph.addEdge("몽촌토성", "잠실");
+        graph.addEdge("잠실", "석촌");
+        graph.addEdge("석촌", "송파");
+
+        return graph;
     }
 }
