3198993297a1ab8fe62e3d0d9e170e93f64746e1
HubCreator <chocomilk8604@gmail.com>
2023-06-29 09:00:06 +0900

    에라토스테네스 체 활용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        int result = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 0) {
+                result++;
+                for (int j = i * 2; j <= n; j += i) {
+                    arr[j] = 1;
+                }
+            }
+        }
+
+
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println(result);
+    }
 }
