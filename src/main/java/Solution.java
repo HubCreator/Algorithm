a43292abc0b5f446b2eb369c4330d7a6b2d99fad
HubCreator <chocomilk8604@gmail.com>
2023-07-09 12:35:42 +0900

    점수 계산

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -11,25 +11,20 @@ class Solution {
         int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            String reversed = new StringBuilder(st.nextToken()).reverse().toString();
-            arr[i] = Integer.parseInt(reversed);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int x : arr) {
-            if (isPrime(x)) {
-                System.out.print(x + " ");
-            }
-        }
-    }
 
-    private static boolean isPrime(int x) {
-        if (x < 2) {
-            return false;
-        }
-        for (int i = 2; i < x; i++) {
-            if (x % i == 0) {
-                return false;
+        int answer = 0;
+        int sum = 0;
+        for (int x : arr) {
+            if (x == 1) {
+                sum++;
+                answer += sum;
+            } else {
+                sum = 0;
             }
         }
-        return true;
+
+        System.out.println(answer);
     }
 }
