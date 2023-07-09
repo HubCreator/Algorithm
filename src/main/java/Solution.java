d591c964f79770a16052da419474095c3d7fbf2c
HubCreator <chocomilk8604@gmail.com>
2023-07-09 12:10:38 +0900

    보이는 학생

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,30 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 class Solution {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        List<Integer> answer = new ArrayList<>();
-        answer.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > arr[i - 1]) {
-                answer.add(arr[i]);
+
+        int max = Integer.MIN_VALUE, answer = 0;
+        for (int x : arr) {
+            if (max < x) {
+                max = x;
+                answer++;
             }
         }
-
-        for (int x : answer) {
-            System.out.print(x + " ");
-        }
+        System.out.println(answer);
     }
 }
