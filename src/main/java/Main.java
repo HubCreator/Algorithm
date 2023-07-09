b8cc107be59801bb95b0f0d75d41bd6ddaca66ef
HubCreator <chocomilk8604@gmail.com>
2023-07-09 18:04:46 +0900

    [B] 차량 번호판 로직 개선

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,47 +4,34 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    private static final int digitCount = 10, charCount = 26;
+    private static final int DIGIT_COUNT = 10, CHAR_COUNT = 26;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        int[] digitArr = new int[input.length()];
-        int[] charArr = new int[input.length()];
-
-        if (input.charAt(0) == 'c') {
-            charArr[0] = charCount;
-        } else {
-            digitArr[0] = digitCount;
-        }
-
-        for (int i = 1; i < input.length(); i++) {
-            if (input.charAt(i) == 'c') {
-                if (charArr[i - 1] != 0) {
-                    charArr[i] = charCount - 1;
-                } else {
-                    charArr[i] = charCount;
-                }
-            } else {
-                if (digitArr[i - 1] != 0) {
-                    digitArr[i] = digitCount - 1;
-                } else {
-                    digitArr[i] = digitCount;
-                }
-            }
-        }
-
         int answer = 1;
         for (int i = 0; i < input.length(); i++) {
-            if (charArr[i] != 0) {
-                answer *= charArr[i];
-            }
-            if (digitArr[i] != 0) {
-                answer *= digitArr[i];
+            if (i == 0) {
+                if (input.charAt(i) == 'd') {
+                    answer *= DIGIT_COUNT;
+                } else {
+                    answer *= CHAR_COUNT;
+                }
+            } else if (input.charAt(i - 1) == input.charAt(i)) {
+                if (input.charAt(i) == 'd') {
+                    answer *= DIGIT_COUNT - 1;
+                } else {
+                    answer *= CHAR_COUNT - 1;
+                }
+            } else {
+                if (input.charAt(i) == 'd') {
+                    answer *= DIGIT_COUNT;
+                } else {
+                    answer *= CHAR_COUNT;
+                }
             }
         }
-
         System.out.println(answer);
     }
 }
