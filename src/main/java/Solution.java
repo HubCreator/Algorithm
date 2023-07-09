cb8abc3f0d05c3285cd6c47f424470b24e35703a
HubCreator <chocomilk8604@gmail.com>
2023-07-09 11:12:56 +0900

    연속 펄스 부분수열의 합 개선

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,16 +1,17 @@
-import java.util.Arrays;
-
 // 시간 복잡도 : O(n)
 // 공간 복잡도 : O(n)
 class Solution {
 
     public long solution(int[] sequence) {
         long[] arr = new long[sequence.length + 1];
+        long min = 0, max = 0;
 
         for (int i = 0; i < sequence.length; i++) {
             arr[i + 1] = arr[i] + sequence[i] * (long) Math.pow(-1, i);
+            min = Math.min(min, arr[i + 1]);
+            max = Math.max(max, arr[i + 1]);
         }
 
-        return Arrays.stream(arr).max().getAsLong() - Arrays.stream(arr).min().getAsLong();
+        return max - min;
     }
 }
