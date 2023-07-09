c5ad3a1a35c098851f462b1258914afd8060cccb
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-07-09 22:51:25 +0900

    양념 반 후라이드 반

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// 시간 복잡도 : O(1)
+// 공간 복잡도 : O(1)
 public class Main {
-
-    private static final int DIGIT_COUNT = 10, CHAR_COUNT = 26;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-
-        int answer = 1;
-        for (int i = 0; i < input.length(); i++) {
-            if (i == 0) {
-                if (input.charAt(i) == 'd') {
-                    answer *= DIGIT_COUNT;
-                } else {
-                    answer *= CHAR_COUNT;
-                }
-            } else if (input.charAt(i - 1) == input.charAt(i)) {
-                if (input.charAt(i) == 'd') {
-                    answer *= DIGIT_COUNT - 1;
-                } else {
-                    answer *= CHAR_COUNT - 1;
-                }
-            } else {
-                if (input.charAt(i) == 'd') {
-                    answer *= DIGIT_COUNT;
-                } else {
-                    answer *= CHAR_COUNT;
-                }
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] price = new int[3];
+        for (int i = 0; i < price.length; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
         }
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int answer = (price[0] * x) + (price[1] * y);
+        if (x < y) {
+            answer = Math.min(answer, (price[2] * 2 * x) + (price[1] * (y - x)));
+        } else if (x > y) {
+            answer = Math.min(answer, (price[2] * 2 * y) + (price[0] * (x - y)));
+        }
+        answer = Math.min(answer, (price[2] * 2) * Math.max(x, y));
         System.out.println(answer);
     }
 }
