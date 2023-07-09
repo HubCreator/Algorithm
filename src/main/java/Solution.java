643966352b467360f28f796bd8d6155a2540a586
HubCreator <chocomilk8604@gmail.com>
2023-07-09 12:13:36 +0900

    피보나치 수열

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,25 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
         }
 
-        int max = Integer.MIN_VALUE, answer = 0;
         for (int x : arr) {
-            if (max < x) {
-                max = x;
-                answer++;
-            }
+            System.out.print(x + " ");
         }
-        System.out.println(answer);
     }
 }
