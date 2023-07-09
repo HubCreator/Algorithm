1cf6bf58edd313118e406f8002782e5d37438b51
HubCreator <chocomilk8604@gmail.com>
2023-07-09 12:17:23 +0900

    소수 갯수 에라토스테네스 체

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -6,15 +6,16 @@ class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        arr[0] = 1;
-        arr[1] = 1;
-        for (int i = 2; i < arr.length; i++) {
-            arr[i] = arr[i - 2] + arr[i - 1];
+        boolean[] arr = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!arr[i]) {
+                count++;
+                for (int j = i; j < n; j += i) {
+                    arr[j] = true;
+                }
+            }
         }
-
-        for (int x : arr) {
-            System.out.print(x + " ");
-        }
+        System.out.println(count);
     }
 }
