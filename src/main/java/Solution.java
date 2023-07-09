0de9ef90d131393d560cfdb2ccc12d62edd81756
HubCreator <chocomilk8604@gmail.com>
2023-07-09 12:05:48 +0900

    큰 수 출력하기

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,17 +1,30 @@
-// 시간 복잡도 : O(n)
-// 공간 복잡도 : O(n)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 class Solution {
 
-    public long solution(int[] sequence) {
-        long[] arr = new long[sequence.length + 1];
-        long min = 0, max = 0;
-
-        for (int i = 0; i < sequence.length; i++) {
-            arr[i + 1] = arr[i] + sequence[i] * (long) Math.pow(-1, i);
-            min = Math.min(min, arr[i + 1]);
-            max = Math.max(max, arr[i + 1]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        List<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                answer.add(arr[i]);
+            }
         }
 
-        return max - min;
+        for (int x : answer) {
+            System.out.print(x + " ");
+        }
     }
 }
