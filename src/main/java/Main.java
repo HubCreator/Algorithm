de8012c710f64990dacb317fb89da538ea05ed16
HubCreator <chocomilk8604@gmail.com>
2023-07-14 10:39:05 +0900

    [I] 모든 아나그램 찾기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,49 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 // 시간 복잡도 : O(n)
 // 공간 복잡도 : O(n)
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st1.nextToken());
-        int k = Integer.parseInt(st1.nextToken());
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
-        }
+        String S = br.readLine();
+        String T = br.readLine();
 
-        for (int x : solution(arr, k)) {
-            System.out.print(x + " ");
-        }
+        System.out.println(solution(S, T));
     }
 
-    private static int[] solution(int[] arr, int k) {
-        int[] result = new int[arr.length - k + 1];
-        int idx = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        // 초기 윈도우 생성
-        for (int i = 0; i < k - 1; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+    private static int solution(String s, String t) {
+        int result = 0, lt = 0;
+        Map<Character, Integer> target = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            target.put(t.charAt(i), target.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        // 슬라이딩 윈도우
-        for (int i = k; i < arr.length; i++, idx++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-            result[idx] = map.size();
-            if (map.get(arr[idx]) == 1) {
-                map.remove(arr[idx]);
+        // 초기 윈도우 설정
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < t.length() - 1; i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = t.length() - 1; i < s.length(); i++, lt++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            if (map.equals(target)) {
+                result++;
+            }
+            if (map.get(s.charAt(lt)) == 1) {
+                map.remove(s.charAt(lt));
             } else {
-                map.put(arr[idx], map.get(arr[idx]) - 1);
+                map.put(s.charAt(lt), map.get(s.charAt(lt)) - 1);
             }
         }
-        result[idx] = map.size();
-
         return result;
     }
 }
