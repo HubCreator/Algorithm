467600cb95f4cac3d42972c87b0a338ebe3c533e
HubCreator <chocomilk8604@gmail.com>
2023-07-14 09:33:19 +0900

    [I] 매출액의 종류

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
-// 시간 복잡도 : O(1)
-// 공간 복잡도 : O(1)
+// 시간 복잡도 : O(n)
+// 공간 복잡도 : O(n)
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] nums = new int[5];
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st1.nextToken());
+        int k = Integer.parseInt(st1.nextToken());
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st2.nextToken());
         }
 
-        int answer = 0;
-        if (nums[0] + nums[1] <= nums[2] * 2) {
-            answer = (nums[0] * nums[3]) + (nums[1] * nums[4]);
-        } else {
-            int min = Math.min(nums[3], nums[4]);
-            int max = Math.max(nums[3], nums[4]);
-            int tmp = min * 2 * nums[2];
-            if (nums[3] < nums[4]) {
-                answer = tmp + (max - min) * nums[1];
-            } else {
-                answer = tmp + (max - min) * nums[0];
-            }
-            answer = Math.min(answer, nums[2] * 2 * max);
+        for (int x : solution(arr, k)) {
+            System.out.print(x + " ");
         }
-        System.out.println(answer);
+    }
+
+    private static int[] solution(int[] arr, int k) {
+        int[] result = new int[arr.length - k + 1];
+        int idx = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // 초기 윈도우 생성
+        for (int i = 0; i < k - 1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        // 슬라이딩 윈도우
+        for (int i = k; i < arr.length; i++, idx++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            result[idx] = map.size();
+            if (map.get(arr[idx]) == 1) {
+                map.remove(arr[idx]);
+            } else {
+                map.put(arr[idx], map.get(arr[idx]) - 1);
+            }
+        }
+        result[idx] = map.size();
+
+        return result;
     }
 }
