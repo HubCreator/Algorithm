01b432776b36ade6b292b89c56327dddac6188d5
HubCreator <chocomilk8604@gmail.com>
2023-07-15 14:34:31 +0900

    [I] 응급실 대기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-// 시간 복잡도 : O(n2)
-// 공간 복잡도 : O(n2)
+// 시간 복잡도 : O(nlogn)
+// 공간 복잡도 : O(n)
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] board = new int[n][n];
-
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st1.nextToken());
+        int m = Integer.parseInt(st1.nextToken());
+        PriorityQueue<Patient> queue = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
+            queue.add(new Patient(i, Integer.parseInt(st2.nextToken())));
         }
-        int count = Integer.parseInt(br.readLine());
-        int[] moves = new int[count];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < count; i++) {
-            moves[i] = Integer.parseInt(st.nextToken()) - 1;
-        }
-
-        System.out.println(solution(board, n, moves));
+        System.out.println(solution(queue, m));
     }
 
-    private static int solution(int[][] board, int n, int[] moves) {
-        int result = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int move : moves) {
-            for (int i = n - 1; i >= 0; i--) {
-                if (board[move][i] != 0) {
-                    if (!stack.isEmpty() && stack.peekLast() == board[move][i]) {
-                        result += 2;
-                        stack.pop();
-                    } else {
-                        stack.push(board[move][i]);
-                        board[move][i] = 0;
-                    }
-                    break;
-                }
-            }
+    private static int solution(PriorityQueue<Patient> queue, int m) {
+        int answer = 1;
+        while (!queue.isEmpty() && queue.peek().index != m) {
+            queue.poll();
+            answer++;
         }
-        return result;
+        return answer;
+    }
+
+    static class Patient implements Comparable<Patient>{
+        public int index;
+        public int value;
+
+        public Patient(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Patient o) {
+            return Objects.compare(this.value, o.value, Integer::compare);
+        }
     }
 }
