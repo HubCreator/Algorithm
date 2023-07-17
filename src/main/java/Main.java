b47714e78139096864a6a68d7e9ff109cc911c36
HubCreator <chocomilk8604@gmail.com>
2023-07-17 16:05:34 +0900

    [I] 공주 구하기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,27 +3,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        System.out.println(T.solution(input));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        System.out.println(T.solution(n, k));
     }
 
-    private int solution(String input) {
-        int answer = 0;
-        Deque<Character> stack = new ArrayDeque<>();
-        for (int i = 0; i < input.length(); i++) {
-            char target = input.charAt(i);
-            if (target == ')') {
-                stack.pop();
-                answer += stack.size();
-            } else {
-                stack.push(target);
-            }
+    private int solution(int n, int k) {
+        int count = 1;
+        Deque<Prince> queue = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            queue.offerLast(new Prince(i));
         }
-        return answer;
+        while (queue.size() != 1) {
+            Prince poll = queue.pollFirst();
+            if (count % k != 0) {
+                queue.addLast(poll);
+            }
+            count++;
+        }
+        return queue.poll().val;
+    }
+
+    static class Prince {
+        final int val;
+
+        public Prince(int val) {
+            this.val = val;
+        }
     }
 }
