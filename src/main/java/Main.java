8a8e3ff2f6304a4ba290cc78362eb5b3984620fc
HubCreator <chocomilk8604@gmail.com>
2023-07-17 10:29:08 +0900

    [I] 세 개의 합을 만족하는 타겟값 구하기(3Sum)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,14 +14,17 @@ public class Main {
         }
 
         int[] answer = T.solution(arr, map, target);
-        System.out.println(answer[0] + " " + answer[1]);
+        System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
     }
 
     private int[] solution(int[] arr, Map<Integer, Integer> map, int target) {
         for (int i = 0; i < arr.length; i++) {
-            int remain = target - arr[i];
-            if (remain != arr[i] && map.containsKey(remain)) {
-                return new int[]{i, map.get(remain)};
+            int remain1 = target - arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                int remain2 = remain1 - arr[j];
+                if (remain1 != arr[i] && remain2 != arr[j] && map.containsKey(remain2) ) {
+                    return new int[]{i, j, map.get(remain2)};
+                }
             }
         }
         return null;
