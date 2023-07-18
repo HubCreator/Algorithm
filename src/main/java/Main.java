06107bdfef2efa4c5f0a80c24e1e02dc7d41cfb7
HubCreator <chocomilk8604@gmail.com>
2023-07-18 09:41:42 +0900

    [I] 이진수 재귀 개선

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,24 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         T.solution(n);
-        System.out.println(answer);
     }
 
-    static int cnt = 0, answer = 0;
+    private void solution(int n) {
+        dfs(n);
+    }
 
-    private int solution(int n) {
-        // 기저 조건
-        if (n < 2) {
-            answer += (n % 2) * (int) Math.pow(10, cnt++);
-            return n;
+    private void dfs(int n) {
+        if (n == 0) {
+            return;
+        } else {
+            dfs(n / 2);
+            System.out.print(n % 2);
         }
-        answer += (n % 2) * (int) Math.pow(10, cnt++);
-        return solution(n / 2);
     }
 }
