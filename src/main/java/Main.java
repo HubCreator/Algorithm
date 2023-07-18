902144af6c6f2cd288caf08553ef65e8167b4c45
HubCreator <chocomilk8604@gmail.com>
2023-07-18 09:36:51 +0900

    [I] 이진수 재귀

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        System.out.println(T.solution(n, k));
+        int n = Integer.parseInt(br.readLine());
+        T.solution(n);
+        System.out.println(answer);
     }
 
-    private int solution(int n, int k) {
-        int count = 1;
-        Deque<Prince> queue = new ArrayDeque<>();
-        for (int i = 1; i <= n; i++) {
-            queue.offerLast(new Prince(i));
-        }
-        while (queue.size() != 1) {
-            Prince poll = queue.pollFirst();
-            if (count % k != 0) {
-                queue.addLast(poll);
-            }
-            count++;
-        }
-        return queue.poll().val;
-    }
+    static int cnt = 0, answer = 0;
 
-    static class Prince {
-        final int val;
-
-        public Prince(int val) {
-            this.val = val;
+    private int solution(int n) {
+        // 기저 조건
+        if (n < 2) {
+            answer += (n % 2) * (int) Math.pow(10, cnt++);
+            return n;
         }
+        answer += (n % 2) * (int) Math.pow(10, cnt++);
+        return solution(n / 2);
     }
 }
