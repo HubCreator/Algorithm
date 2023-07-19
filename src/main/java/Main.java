a4da59036f115e4909a298694fb6bae9528bc179
HubCreator <chocomilk8604@gmail.com>
2023-07-19 18:55:50 +0900

    [I] 피보나치 재귀

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,19 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        System.out.println(T.factorial(n));
+        for (int i = 1; i <= n; i++) {
+            System.out.print(T.dfs(i) + " ");;
+        }
     }
 
-    private int factorial(int n) {
-        if (n == 1) { // 기저 조건
+    private int dfs(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
             return 1;
         } else {
-            return n * factorial(n - 1); // 값을 누적시킴
+            return dfs(n - 1) + dfs(n - 2);
         }
     }
 }
