dcb655a93154beb10e8973004544331f4b5c8315
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-07-30 16:09:50 +0900

    [I] 노드 순회

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-// 시간 복잡도 : O(n)
-// 곤간 복잡도 : O(n)
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st1.nextToken());
-        int m = Integer.parseInt(st1.nextToken());
-        int[] arr = new int[n];
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
-        }
-        System.out.println(T.solution(arr, m));
+        Node node2 = new Node(2, new Node(4, null, null), new Node(5, null, null));
+        Node node3 = new Node(3, new Node(6, null, null), new Node(7, null, null));
+        Node root = new Node(1, node2, node3);
+
+        T.dfs(root);
     }
 
-    private int solution(int[] arr, int m) {
-        int answer = 0;
-        int lt = Arrays.stream(arr).max().getAsInt();
-        int rt = Arrays.stream(arr).sum();
-        while (lt <= rt) {
-            int mid = (lt + rt) / 2;
-            if (getCount(arr, mid) <= m) {
-                answer = mid;
-                rt = mid - 1;
-            } else {
-                lt = mid + 1;
-            }
+    private void dfs(Node root) {
+        if (root.lt != null) {
+            dfs(root.lt);
         }
-        return answer;
+        if (root.rt != null) {
+            dfs(root.rt);
+        }
+        System.out.print(root.value + " ");
     }
 
-    private int getCount(int[] arr, int mid) {
-        int cnt = 1, sum = 0;
-        for (int x : arr) {
-            if (sum + x > mid) {
-                cnt++;
-                sum = x;
-            } else {
-                sum += x;
-            }
+    static class Node {
+        private int value;
+        private Node lt;
+        private Node rt;
+
+        public Node(int value, Node lt, Node rt) {
+            this.value = value;
+            this.lt = lt;
+            this.rt = rt;
         }
-        return cnt;
     }
 }
