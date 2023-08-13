d208bcbf3fe535f5fd2de11ad75e5142c76a132c
HubCreator <chocomilk8604@gmail.com>
2023-08-13 13:59:41 +0900

    [I] 경로 탐색(인접행렬) dfs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-// 시간 복잡도 : O(n)
-// 공간 복잡도 : O(n)
 public class Main {
+    private static int n;
+    private static List<List<Integer>> list = new ArrayList<>();
+    private static boolean[] check;
+    private static int answer = 0;
+
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        check = new boolean[n + 1];
+        int m = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
 
-        System.out.println(T.bfs(s, e));
+        for (int i = 0; i < m; i++) {
+            StringTokenizer input = new StringTokenizer(br.readLine());
+            list.get(Integer.parseInt(input.nextToken())).add(Integer.parseInt(input.nextToken()));
+        }
+        check[1] = true;
+        T.dfs(1);
+        System.out.println(answer);
     }
 
-    private int bfs(int s, int e) {
-        int[] dir = new int[]{-1, 1, 5};
-        int result = 0;
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(s);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            result++;
-            for (int i = 0; i < size; i++) {
-                Integer poll = queue.poll();
-                for (int d : dir) {
-                    if (poll == e) {
-                        return result - 1;
-                    }
-                    queue.offer(poll + d);
+    private void dfs(int level) {
+        if (level == n) {
+            if (check[n]) {
+                answer++;
+            }
+            System.out.println();
+        } else {
+            List<Integer> entry = list.get(level);
+            for (Integer index : entry) {
+                if (!check[index]) {
+                    check[index] = true;
+                    dfs(index);
+                    check[index] = false;
                 }
             }
         }
-        return -1;
     }
 }
+/**
+5 9
+1 2
+1 3
+1 4
+2 1
+2 3
+2 5
+3 4
+4 2
+4 5
+ */
