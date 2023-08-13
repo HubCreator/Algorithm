d489f8767890afaaf89230a9672b67dee4d68e8f
HubCreator <chocomilk8604@gmail.com>
2023-08-13 14:16:21 +0900

    [I] 그래프 최단거리 bfs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,63 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    private static int n;
-    private static List<List<Integer>> list = new ArrayList<>();
-    private static boolean[] check;
-    private static int answer = 0;
+    private static final List<List<Integer>> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        check = new boolean[n + 1];
+        int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
-
         for (int i = 0; i < m; i++) {
-            StringTokenizer input = new StringTokenizer(br.readLine());
-            list.get(Integer.parseInt(input.nextToken())).add(Integer.parseInt(input.nextToken()));
+            StringTokenizer t = new StringTokenizer(br.readLine());
+            list.get(Integer.parseInt(t.nextToken())).add(Integer.parseInt(t.nextToken()));
         }
-        check[1] = true;
-        T.dfs(1);
-        System.out.println(answer);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i + " : " + T.bfs(i));
+        }
     }
 
-    private void dfs(int level) {
-        if (level == n) {
-            if (check[n]) {
-                answer++;
-            }
-            System.out.println();
-        } else {
-            List<Integer> entry = list.get(level);
-            for (Integer index : entry) {
-                if (!check[index]) {
-                    check[index] = true;
-                    dfs(index);
-                    check[index] = false;
+    private int bfs(int target) {
+        Queue<Integer> queue = new ArrayDeque<>(list.get(1));
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Integer poll = queue.poll();
+                if (poll == target) {
+                    return level;
+                }
+                List<Integer> nexts = list.get(poll);
+                for (Integer next : nexts) {
+                    if (!queue.contains(next)) {
+                        queue.offer(next);
+                    }
                 }
             }
+            level++;
         }
+        return -1;
     }
 }
-/**
-5 9
-1 2
+/*
+6 9
 1 3
 1 4
 2 1
-2 3
 2 5
 3 4
-4 2
 4 5
+4 6
+6 2
+6 5
  */
