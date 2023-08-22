13a9ef1f70f3ba2cd23098849ecea67c5d8ecfb9
HubCreator <chocomilk8604@gmail.com>
2023-08-22 14:17:54 +0900

    약수 dfs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,36 +4,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int answer = Integer.MIN_VALUE;
-
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st1.nextToken());
-        int m = Integer.parseInt(st1.nextToken());
-        int[] scores = new int[n];
-        int[] times = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         boolean[] check = new boolean[n];
-        int scoreSum = 0;
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            scores[i] = Integer.parseInt(st.nextToken());
-            times[i] = Integer.parseInt(st.nextToken());
-        }
 
-        T.dfs(scores, times, 0, 0, m, 0);
-        System.out.println(answer);
+        T.dfs(check, m, 0, 0);
     }
 
-    private void dfs(int[] scores, int[] times, int scoreSum, int timeSum, int m, int level) {
-        if (level == scores.length) {
-            if (timeSum <= m) {
-                answer = Math.max(answer, scoreSum);
+    private void dfs(boolean[] check, int m, int count, int level) {
+        if (level == m + 1) {
+            if (count == m) {
+                for (int i = 0; i < check.length; i++) {
+                    if (check[i]) {
+                        System.out.print(i + 1 + " ");
+                    }
+                }
+                System.out.println();
             }
         } else {
-            dfs(scores, times, scoreSum += scores[level], timeSum += times[level], m, level + 1);
-            dfs(scores, times, scoreSum -= scores[level], timeSum -= times[level], m, level + 1);
+            check[level] = true;
+            dfs(check, m, count + 1, level + 1);
+            check[level] = false;
+            dfs(check, m, count, level + 1);
         }
     }
 }
