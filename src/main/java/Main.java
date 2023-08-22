8fbcb6303cbb9028180af3b4af25a48b271791d2
HubCreator <chocomilk8604@gmail.com>
2023-08-23 00:17:49 +0900

    [I] 동전 교환 bfs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] pm = new int[m];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine());
 
-        T.dfs(pm, n, m, 0);
+        System.out.println(T.bfs(arr, n, m));
     }
 
-    private void dfs(int[] pm, int n, int m, int level) {
-        if (level == m) {
-            for (int el : pm) {
-                System.out.print(el + " ");
-            }
-            System.out.println();
-        } else {
-            for (int i = 1; i <= n; i++) {
-                pm[level] = i;
-                dfs(pm, n, m, level + 1);
-            }
+    private int bfs(int[] arr, int n, int m) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        int level = 1;
+        for (int i = 0; i < n; i++) {
+            queue.offer(arr[i]);
         }
+
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            for (int i = 0; i < length; i++) {
+                Integer poll = queue.poll();
+                if (poll == m) {
+                    return level;
+                }
+                for (int j = 0; j < n; j++) {
+                    queue.offer(poll + arr[j]);
+                }
+            }
+            level++;
+        }
+        return -1;
     }
 }
