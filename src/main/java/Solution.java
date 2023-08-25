680a455a0fd9144c4d9131b79a126443489d2e80
HubCreator <chocomilk8604@gmail.com>
2023-08-25 17:03:41 +0900

    [P] 네트워크 연결, dfs

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,10 +1,22 @@
 class Solution {
-    public int solution(int[][] triangle) {
-        for (int i = triangle.length - 1; i >= 0; i--) {
-            for (int j = triangle[i].length - 1; j >= 1; j--) {
-                triangle[i - 1][j - 1] += Math.max(triangle[i][j - 1], triangle[i][j]);
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] check = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!check[i]) {
+                dfs(computers, check, i);
+                answer++;
             }
         }
-        return triangle[0][0];
+        return answer;
+    }
+
+    void dfs(int[][] computers, boolean[] check, int start) {
+        check[start] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[start][i] == 1 && !check[i]) {
+                dfs(computers, check, i);
+            }
+        }
     }
 }
