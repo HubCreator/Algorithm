3823b96b945aeb13120290ae89f28442ef8218aa
HubCreator <chocomilk8604@gmail.com>
2023-08-30 16:30:11 +0900

    [P] 짝지어 제어하기

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,27 +1,23 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 class Solution {
-    Map<Integer, Integer> map = new HashMap<>();
-    public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        for (int t : tangerine) {
-            map.put(t, map.getOrDefault(t, 0) + 1);
-        }
-        List<Integer> values = map.values()
-                .stream()
-                .sorted((a, b) -> b - a)
-                .collect(Collectors.toList());
-
-        for (Integer value : values) {
-            answer += 1;
-            k -= value;
-            if (k <= 0) {
-                break;
+    public int solution(String s) {
+        List<Character> list = new ArrayList<>();
+        list.add(list.get(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (!list.isEmpty() && list.size() > 0 && list.get(list.size() - 1) == s.charAt(i)) {
+                list.remove(list.size() - 1);
+                continue;
             }
+            list.add(s.charAt(i));
         }
-        return answer;
+
+        return list.isEmpty() ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("baabaa"));
     }
 }
