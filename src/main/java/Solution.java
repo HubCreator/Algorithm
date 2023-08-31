9b9389e4586b00efaad7af60ac0f1c685e0aac5a
HubCreator <chocomilk8604@gmail.com>
2023-08-31 14:15:47 +0900

    [P] 기능계발, Stack

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,19 +1,42 @@
-public class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        while (n > 0) {
-            System.out.println(n);
-            if (n % 2 == 1) {
-                answer += 1;
-                n -= 1;
-            }
-            n /= 2;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Work> works = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = progresses.length - 1; i >= 0; i--) {
+            works.add(new Work(progresses[i], speeds[i]));
         }
-        return answer;
+        while (!works.isEmpty()) {
+            for (Work work : works) {
+                work.update();
+            }
+            int tmp = 0;
+            while (!works.isEmpty() && works.get(works.size() - 1).progress >= 100) {
+                works.remove(works.size() - 1);
+                tmp++;
+            }
+            if (tmp > 0) {
+                answer.add(tmp);
+            }
+        }
+
+        return answer.stream().mapToInt(m -> m).toArray();
     }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.solution(5));
+    private static class Work {
+        public int progress;
+        public int speed;
+
+        public Work(int progress, int speed) {
+            this.progress = progress;
+            this.speed = speed;
+        }
+
+        public int update() {
+            return progress += speed;
+        }
     }
 }
