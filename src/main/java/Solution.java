51d4a48a09a0837043b20f72b3b16eca3fb733c2
HubCreator <chocomilk8604@gmail.com>
2023-08-31 16:45:15 +0900

    [P] 피보나치 수, 메모이제이션

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,42 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        List<Work> works = new ArrayList<>();
-        List<Integer> answer = new ArrayList<>();
-
-        for (int i = progresses.length - 1; i >= 0; i--) {
-            works.add(new Work(progresses[i], speeds[i]));
+    public int solution(int n) {
+        int[] cache = new int[n + 1];
+        cache[0] = 0;
+        cache[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            cache[i] = (cache[i - 2] + cache[i - 1]) % 1234567;
         }
-        while (!works.isEmpty()) {
-            for (Work work : works) {
-                work.update();
-            }
-            int tmp = 0;
-            while (!works.isEmpty() && works.get(works.size() - 1).progress >= 100) {
-                works.remove(works.size() - 1);
-                tmp++;
-            }
-            if (tmp > 0) {
-                answer.add(tmp);
-            }
-        }
-
-        return answer.stream().mapToInt(m -> m).toArray();
-    }
-
-    private static class Work {
-        public int progress;
-        public int speed;
-
-        public Work(int progress, int speed) {
-            this.progress = progress;
-            this.speed = speed;
-        }
-
-        public int update() {
-            return progress += speed;
-        }
+        return cache[n];
     }
 }
