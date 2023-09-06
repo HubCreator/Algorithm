f451cef3e4a71e929f8c1b019dedaf1c207da10d
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-09-07 00:12:49 +0900

    [P] PCCP 모의고사 1번

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,21 +1,33 @@
-class Solution {
-    public long solution(int n) {
-        int[] fibo = new int[n + 1];
-        if (n == 1) {
-            return 1;
-        } else if (n == 2) {
-            return 2;
-        } else {
-            makeFibo(fibo);
-            return fibo[n];
-        }
-    }
+import java.util.*;
 
-    private void makeFibo(int[] fibo) {
-        fibo[1] = 1;
-        fibo[2] = 2;
-        for (int i = 3; i < fibo.length; i++) {
-            fibo[i] = (fibo[i - 2] + fibo[i - 1]) % 1234567;
+class Solution {
+    public String solution(String input_string) {
+        Map<Character, Integer> map = new HashMap<>();
+        List<Character> stack = new ArrayList<>();
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < input_string.length(); i++) {
+            char target = input_string.charAt(i);
+            map.put(target, map.getOrDefault(target, 0) + 1);
         }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            Character key = entry.getKey();
+            int start = input_string.indexOf(key);
+            String subString = input_string.substring(start, start + entry.getValue());
+            for (int j = 0; j < subString.length(); j++) {
+                if (subString.charAt(j) != entry.getKey()) {
+                    stack.add(entry.getKey());
+                    break;
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return "N";
+        }
+        Collections.sort(stack);
+        for (Character character : stack) {
+            answer.append(character);
+        }
+        return answer.toString();
     }
 }
