d64517cc9b25b18aa21ab3942b8b99c8e41c5356
HubCreator <chocomilk8604@gmail.com>
2023-09-10 08:51:03 +0900

    [P] 전화번호 목록

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,30 +1,13 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
 
 class Solution {
-    private static final Pattern PATTERN = Pattern.compile("\\{(.*?)\\}");
-
-    public int[] solution(String s) {
-        String substring = s.substring(1, s.length() - 1);
-        List<String> result = new ArrayList<>();
-        List<Integer> answer = new ArrayList<>();
-        Matcher matcher = PATTERN.matcher(substring);
-        while (matcher.find()) {
-            result.add(matcher.group(1));
-        }
-        Collections.sort(result, (a, b) -> a.length() - b.length());
-        for (String line : result) {
-            String[] split = line.split(",");
-            for (String sp : split) {
-                int t = Integer.parseInt(sp);
-                if (answer.isEmpty() || !answer.contains(t)) {
-                    answer.add(t);
-                }
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
         }
-        return answer.stream().mapToInt(m -> m).toArray();
+        return true;
     }
 }
