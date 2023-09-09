e08172dc75ebe28eee4d5dccdf5825d66a53bcdd
HubCreator <chocomilk8604@gmail.com>
2023-09-09 22:17:55 +0900

    [P] 튜플

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Solution {
-    int answer = 0;
+    private static final Pattern PATTERN = Pattern.compile("\\{(.*?)\\}");
 
-    public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
-        return answer;
-    }
-
-    public void dfs(int[] numbers, int target, int sum, int level) {
-        if (numbers.length == level) {
-            if (sum == target) answer += 1;
-        } else {
-            dfs(numbers, target, sum + numbers[level], level + 1);
-            dfs(numbers, target, sum - numbers[level], level + 1);
+    public int[] solution(String s) {
+        String substring = s.substring(1, s.length() - 1);
+        List<String> result = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        Matcher matcher = PATTERN.matcher(substring);
+        while (matcher.find()) {
+            result.add(matcher.group(1));
         }
+        Collections.sort(result, (a, b) -> a.length() - b.length());
+        for (String line : result) {
+            String[] split = line.split(",");
+            for (String sp : split) {
+                int t = Integer.parseInt(sp);
+                if (answer.isEmpty() || !answer.contains(t)) {
+                    answer.add(t);
+                }
+            }
+        }
+        return answer.stream().mapToInt(m -> m).toArray();
     }
 }
