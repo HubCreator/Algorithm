5c964c15d878ea008f9b1db85425827765c185ff
HubCreator <chocomilk8604@gmail.com>
2023-09-10 09:04:27 +0900

    [P] 할인행사

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,13 +1,28 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String, Integer> wishMap = new HashMap<>();
+        Map<String, Integer> discountMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wishMap.put(want[i], number[i]);
+        }
+        for (int i = 0; i < 9; i++) {
+            discountMap.put(discount[i], discountMap.getOrDefault(discount[i], 0) + 1);
+        }
+        int lt = 0;
+        for (int i = 9; i < discount.length; i++, lt++) {
+            discountMap.put(discount[i], discountMap.getOrDefault(discount[i], 0) + 1);
+            if (wishMap.equals(discountMap)) {
+                answer += 1;
+            }
+            discountMap.put(discount[lt], discountMap.get(discount[lt]) - 1);
+            if (discountMap.get(discount[lt]) == 0) {
+                discountMap.remove(discount[lt]);
             }
         }
-        return true;
+        return answer;
     }
 }
