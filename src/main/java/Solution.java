235756fc6fea7b874aefb9a3bd67640f9ff1f957
HubCreator <chocomilk8604@gmail.com>
2023-09-11 21:28:44 +0900

    [P] 던전 피로도, 개선

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -8,25 +8,13 @@ class Solution {
     }
 
     public void dfs(int k, int[][] dungeons, boolean[] check, int level) {
-        if (k < 0) {
-            return;
-        }
-        if (level == check.length) {
-            int count = 0;
-            for (boolean ch : check) {
-                if (ch) count++;
-            }
-            answer = Math.max(answer, count);
-        } else {
-            for (int i = 0; i < check.length; i++) {
-                if (!check[i] && k >= dungeons[i][0]) {
-                    check[i] = true;
-                    dfs(k - dungeons[i][1], dungeons, check, level + 1);
-                    check[i] = false;
-                } else {
-                    dfs(k, dungeons, check, level + 1);
-                }
+        for (int i = 0; i < check.length; i++) {
+            if (!check[i] && k >= dungeons[i][0]) {
+                check[i] = true;
+                dfs(k - dungeons[i][1], dungeons, check, level + 1);
+                check[i] = false;
             }
         }
+        answer = Math.max(answer, level);
     }
 }
