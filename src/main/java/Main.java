8d1045e1c9c949ff1b48dd18d08c2ea0b5226cab
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-09-15 00:46:33 +0900

    [B] 단어 갯수

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,21 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int[][] fibo = new int[41][];
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        initFibo();
-        for (int i = 0; i < t; i++) {
-            int input = Integer.parseInt(br.readLine());
-            int[] result = fibo[input];
-            System.out.println(result[0] + " " + result[1]);
+        String result = br.readLine().strip();
+        if (result.isEmpty()) {
+            System.out.println(0);
+            return;
         }
-    }
-
-    private static void initFibo() {
-        for (int i = 0; i < fibo.length; i++) {
-            if (i == 0) {
-                fibo[i] = new int[]{0, 1};
-            } else if (i == 1) {
-                fibo[i] = new int[]{1, 0};
-            } else {
-                fibo[i] = new int[]{
-                        fibo[i-2][0] + fibo[i-1][0],
-                        fibo[i-2][1] + fibo[i-1][1]
-                };
+        int answer = 1;
+        char[] charArray = result.toCharArray();
+        for (int i = 0; i < charArray.length - 1; i++) {
+            if (charArray[i] == ' ' && charArray[i + 1] != ' ') {
+                answer++;
             }
         }
+        System.out.println(answer);
     }
 }
