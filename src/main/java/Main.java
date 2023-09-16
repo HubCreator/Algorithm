4c6137b63385415e87419a68d810530b24a3a1a2
HubCreator <chocomilk8604@gmail.com>
2023-09-17 00:34:07 +0900

    [B] 보물

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String result = br.readLine().strip();
-        if (result.isEmpty()) {
-            System.out.println(0);
-            return;
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr1 = new Integer[n];
+        Integer[] arr2 = new Integer[n];
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr1[i] = Integer.parseInt(st1.nextToken());
+            arr2[i] = Integer.parseInt(st2.nextToken());
         }
-        int answer = 1;
-        char[] charArray = result.toCharArray();
-        for (int i = 0; i < charArray.length - 1; i++) {
-            if (charArray[i] == ' ' && charArray[i + 1] != ' ') {
-                answer++;
-            }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2, Collections.reverseOrder());
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer += arr1[i] * arr2[i];
         }
         System.out.println(answer);
     }
