6c45b7ed62ad21757c75230ad2cd8bd9317a1570
HubCreator <chocomilk8604@gmail.com>
2023-09-17 00:31:12 +0900

    [B] 보물

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr1 = new Integer[n];
+        Integer[] arr2 = new Integer[n];
         StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st1.nextToken());
-        int m = Integer.parseInt(st1.nextToken());
-        int[] arr = new int[n];
-        int[] pm = new int[n];
-        boolean[] check = new boolean[n];
         StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
+            arr1[i] = Integer.parseInt(st1.nextToken());
+            arr2[i] = Integer.parseInt(st2.nextToken());
         }
-        T.dfs(n, m, arr, pm, check, 0);
-    }
 
-    private void dfs(int n, int m, int[] arr, int[] pm, boolean[] check, int level) {
-        if (level == m) {
-            for (int i = 0; i < m; i++) {
-                System.out.print(pm[i] + " ");
-            }
-            System.out.println();
-        } else {
-            for (int i = 0; i < n; i++) {
-                if (!check[i]) {
-                    check[i] = true;
-                    pm[level] = arr[i];
-                    dfs(n, m, arr, pm, check, level + 1);
-                    check[i] = false;
-                }
-            }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2, Collections.reverseOrder());
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer += arr1[i] * arr2[i];
         }
+        System.out.println(answer);
     }
 }
