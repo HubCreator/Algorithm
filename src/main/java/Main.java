b2fb1052c8b084b7428a40966f2f74499e1e14e3
HubCreator <chocomilk8604@gmail.com>
2023-09-18 01:09:35 +0900

    [B] 눈 치우기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = 0;
         int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        boolean[] rooms = new boolean[n + 1];
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            for (int j = x; j < y; j++) {
-                rooms[j] = true;
-            }
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (boolean room : rooms) {
-            if (!room) {
-                answer++;
+        int result = 0, idx = 0, lastIdx = arr.length - 1;
+        while (arr[lastIdx] != 0) {
+            Arrays.sort(arr);
+            if (idx == lastIdx) {
+                result += arr[lastIdx];
+                break;
             }
+            arr[idx] -= 1;
+            arr[lastIdx] -= 1;
+            if (arr[idx] == 0) {
+                idx += 1;
+            }
+            result += 1;
         }
-        System.out.println(answer - 1);
+        int answer = result > 1440 ?
+                -1 :
+                result;
+        System.out.println(answer);
     }
 }
