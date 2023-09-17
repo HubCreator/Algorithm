f47e03636a55a85ffa0a21f6584bb8a24f3f7333
HubCreator <chocomilk8604@gmail.com>
2023-09-18 00:41:01 +0900

    [B] 동방 프로젝트 small 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = 1;
+        int answer = 0;
         int n = Integer.parseInt(br.readLine());
-        double[] room = new double[n + 1];
-        Set<int[]> behavior = new LinkedHashSet<>();
         int m = Integer.parseInt(br.readLine());
-        if (m == 0) {
-            System.out.println(n);
-            return;
-        }
+        boolean[] rooms = new boolean[n + 1];
         for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            behavior.add(new int[]{x, y});
+            for (int j = x; j < y; j++) {
+                rooms[j] = true;
+            }
         }
-        crushRoom(room, behavior);
-
-        double target = room[1];
-        for (int i = 2; i <= n; i++) {
-            if (target != room[i] || room[i] == 0.0) {
+        for (boolean room : rooms) {
+            if (!room) {
                 answer++;
-                target = room[i];
             }
         }
-        System.out.println(answer);
-    }
-
-    private static void crushRoom(double[] room, Set<int[]> behavior) {
-        for (int[] ints : behavior) {
-            int x = ints[0], y = ints[1];
-            double random = room[x] == 0.0 ?
-                    Math.random() :
-                    room[x];
-            for (int i = x; i <= y; i++) {
-                room[i] = random;
-            }
-        }
+        System.out.println(answer - 1);
     }
 }
