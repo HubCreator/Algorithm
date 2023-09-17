19e155b6acde061f047f31d82f63275986eedd96
HubCreator <chocomilk8604@gmail.com>
2023-09-18 01:36:49 +0900

    [B] 신입사원

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        int result = 0, idx = 0, lastIdx = arr.length - 1;
-        while (arr[lastIdx] != 0) {
-            Arrays.sort(arr);
-            if (idx == lastIdx) {
-                result += arr[lastIdx];
-                break;
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            List<Person> people = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                people.add(new Person(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
             }
-            arr[idx] -= 1;
-            arr[lastIdx] -= 1;
-            if (arr[idx] == 0) {
-                idx += 1;
+            Collections.sort(people);
+            int target = people.get(0).interview;
+            for (int j = 1; j < people.size(); j++) {
+                if (target < people.get(j).interview) {
+                    n -= 1;
+                    continue;
+                }
+                target = people.get(j).interview;
             }
-            result += 1;
+            answer.append(n).append('\n');
         }
-        int answer = result > 1440 ?
-                -1 :
-                result;
-        System.out.println(answer);
+        System.out.print(answer);
+    }
+
+    public static class Person implements Comparable<Person>{
+        private int paper, interview;
+
+        public Person(int paper, int interview) {
+            this.paper = paper;
+            this.interview = interview;
+        }
+
+        @Override
+        public int compareTo(Person o) {
+            if (this.paper == o.paper) {
+                return Integer.compare(this.interview, o.interview);
+            }
+            return Integer.compare(this.paper, o.paper);
+        }
     }
 }
