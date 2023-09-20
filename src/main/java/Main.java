353a2c1b3da1d8ecaf64bd5ad78d4592bd231b08
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-09-21 02:07:36 +0900

    [B] 정원 관리 EASY

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final int START = 0;
-    private static final int END = 1;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = 0;
-        int n = Integer.parseInt(br.readLine());
-        int[][] board = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            board[i][0] = Integer.parseInt(st.nextToken());
-            board[i][1] = Integer.parseInt(st.nextToken());
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st1.nextToken());
+        int m = Integer.parseInt(st1.nextToken());
+        int[][] board = new int[n + 1][m + 1];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            board[i][0] = Integer.parseInt(st2.nextToken());
         }
-        Arrays.sort(board, (o1, o2) -> {
-            if (o1[1] == o2[1]) {
-                return Integer.compare(o1[START], o2[START]);
-            }
-            return Integer.compare(o1[END], o2[END]);
-        });
-        int start, end = Integer.MIN_VALUE;
-        for (int[] ints : board) {
-            if (ints[START] >= end) {
-                start = ints[START];
-                end = ints[END];
-                answer++;
+        StringTokenizer st3 = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= m; i++) {
+            board[0][i] = Integer.parseInt(st3.nextToken());
+        }
+        System.out.println(solution(board));
+    }
+
+    private static int solution(int[][] board) {
+        int i = 0, j = 0;
+        for (i = 1; i < board[0].length; i++) {
+            for (j = 1; j < board.length; j++) {
+                if (board[j][i - 1] == board[j - 1][i]) {
+                    board[j][i] = 0;
+                } else {
+                    board[j][i] = 1;
+                }
             }
         }
-        System.out.println(answer);
+        return board[j - 1][i - 1];
     }
 }
