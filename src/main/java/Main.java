6a1c73bc63ed71f14c64d72064599a99cd983188
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-09-26 21:02:35 +0900

    [B] 스파이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,65 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static int count = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st1 = new StringTokenizer(br.readLine());
-        StringBuilder answer = new StringBuilder();
         int n = Integer.parseInt(st1.nextToken());
         int m = Integer.parseInt(st1.nextToken());
-        int k = Integer.parseInt(st1.nextToken());
-        char[][] board = new char[n][m];
-        for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < line.length(); j++) {
-                board[i][j] = line.charAt(j);
-            }
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        StringTokenizer st3 = new StringTokenizer(br.readLine());
+        int[][] board = new int[2][3];
+        for (int i = 0; i < 3; i++) {
+            board[0][i] = Integer.parseInt(st2.nextToken());
+            board[1][i] = Integer.parseInt(st3.nextToken());
         }
+        solution(board, n, m, 0, Integer.MIN_VALUE, 0);
+        System.out.println(count);
+    }
 
-        int count = 0;
-        char[][] mask = new char[k][k];
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < k; j++) {
-                Map<Character, Integer> map = new HashMap<>();
-                for (int y = i; y < n; y += k) {
-                    for (int x = j; x < m; x += k) {
-                        map.put(board[y][x], map.getOrDefault(board[y][x], 0) + 1);
-                    }
-                }
-                char result = ' ';
-                int max = Integer.MIN_VALUE;
-                for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-                    if (max < entry.getValue()) {
-                        max = entry.getValue();
-                        result = entry.getKey();
-                    }
-                }
-                mask[i][j] = result;
-                count += (n / k) * (m / k) - max;
+    private static void solution(int[][] board, int n, int m, int sum, int last, int level) {
+        if (level == n) {
+            if (sum >= m) {
+                count++;
             }
-        }
-        char[][] resultBoard = new char[n][m];
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < k; j++) {
-                for (int y = i; y < n; y += k) {
-                    for (int x = j; x < m; x += k) {
-                        resultBoard[y][x] = mask[i][j];
+        } else {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if (j == last) {
+                        solution(board, n, m, sum + (board[i][j] / 2), j, level + 1);
+                    } else {
+                        solution(board, n, m, sum + board[i][j], j, level + 1);
                     }
                 }
             }
         }
-        answer.append(count).append('\n');
-        for (char[] chars : resultBoard) {
-            for (char c : chars) {
-                answer.append(c);
-            }
-            answer.append('\n');
-        }
-        System.out.println(answer.deleteCharAt(answer.length() - 1));
     }
 }
