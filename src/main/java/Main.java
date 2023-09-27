c934281cddd6808e45e730c816e2b444e9c880fa
HubCreator <chocomilk8604@gmail.com>
2023-09-28 00:49:41 +0900

    [B] 줄어들지 않아

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-    private static int count = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st1.nextToken());
-        int m = Integer.parseInt(st1.nextToken());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        StringTokenizer st3 = new StringTokenizer(br.readLine());
-        int[][] board = new int[2][3];
-        for (int i = 0; i < 3; i++) {
-            board[0][i] = Integer.parseInt(st2.nextToken());
-            board[1][i] = Integer.parseInt(st3.nextToken());
-        }
-        solution(board, n, m, 0, Integer.MIN_VALUE, 0);
-        System.out.println(count);
-    }
+        StringBuilder answer = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        long[][] dp = new long[65][10];
+        Arrays.fill(dp[1], 1);
 
-    private static void solution(int[][] board, int n, int m, int sum, int last, int level) {
-        if (level == n) {
-            if (sum >= m) {
-                count++;
-            }
-        } else {
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[i].length; j++) {
-                    if (j == last) {
-                        solution(board, n, m, sum + (board[i][j] / 2), j, level + 1);
-                    } else {
-                        solution(board, n, m, sum + board[i][j], j, level + 1);
-                    }
+        for (int i = 2; i < 65; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = j; k < 10; k++) {
+                    dp[i][j] += dp[i - 1][k];
                 }
             }
         }
+        for (int i = 0; i < t; i++) {
+            answer.append(solution(dp, Integer.parseInt(br.readLine()))).append('\n');
+        }
+        System.out.println(answer);
+    }
+
+    private static int solution(long[][] dp, int n) {
+        int answer = 0;
+        for (int i = 0; i < dp[n].length; i++) {
+            answer += dp[n][i];
+        }
+        return answer;
     }
 }
