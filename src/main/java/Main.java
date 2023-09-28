99a1821c967ac90956a9d63f5787d25e8cd2a3fb
HubCreator <chocomilk8604@gmail.com>
2023-09-28 14:05:32 +0900

    [B] 덩치, 그리디

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         StringBuilder answer = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        long[][] dp = new long[65][10];
-        Arrays.fill(dp[1], 1);
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            people.add(new Person(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i));
+        }
+        Collections.sort(people);
 
-        for (int i = 2; i < 65; i++) {
-            for (int j = 0; j < 10; j++) {
-                for (int k = j; k < 10; k++) {
-                    dp[i][j] += dp[i - 1][k];
-                }
-            }
+        for (int i = 0; i < people.size(); i++) {
+            people.get(i).prize = solution(people, i);
         }
-        for (int i = 0; i < t; i++) {
-            answer.append(solution(dp, Integer.parseInt(br.readLine()))).append('\n');
+
+        Collections.sort(people, Comparator.comparingInt(a -> a.index));
+        for (Person person : people) {
+            answer.append(person.prize).append(' ');
         }
+
         System.out.print(answer);
     }
 
-    private static long solution(long[][] dp, int n) {
-        long answer = 0;
-        for (int i = 0; i < 10; i++) {
-            answer += dp[n][i];
+    private static int solution(List<Person> people, int num) {
+        int count = 0;
+        for (int i = 0; i < num; i++) {
+            if (people.get(i).y > people.get(num).y) {
+                count++;
+            }
         }
-        return answer;
+        return count + 1;
+    }
+
+    private static class Person implements Comparable<Person> {
+        int x, y, index, prize;
+
+        public Person(int x, int y, int index) {
+            this.x = x;
+            this.y = y;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Person o) {
+            if (x == o.x) {
+                return Integer.compare(o.y, y);
+            }
+            return Integer.compare(o.x, x);
+        }
     }
 }
