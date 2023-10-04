c8e4a5873b0ea669e37d12882559e74ded888089
HubCreator <chocomilk8604@gmail.com>
2023-10-05 08:56:07 +0900

    [B] 크리 문자열

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,57 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st1.nextToken());
-        int K = Integer.parseInt(st1.nextToken());
-        int M = Integer.parseInt(st1.nextToken());
-        List<List<Integer>> board = new ArrayList<>();
-        for (int i = 0; i <= N + M; i++) {
-            board.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < M; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            for (int j = 0; j < K; j++) {
-                int t = Integer.parseInt(st2.nextToken());
-                board.get(t).add(N + i + 1); // 공간 복잡도 줄이기
-                board.get(N + i + 1).add(t);
-            }
-        }
-        System.out.println(bfs(board, N, M));
-    }
-
-    private static int bfs(List<List<Integer>> board, int N, int M) {
-        boolean[] visited = new boolean[N + M + 1];
-        int[] distance = new int[N + M + 1];
-
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(1);
-        visited[1] = true;
-        distance[1] = 1;
-        while (!queue.isEmpty()) {
-            Integer poll = queue.poll();
-            for (Integer next : board.get(poll)) {
-                if (visited[next]) {
+        String S = br.readLine();
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < S.length(); i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = i; j < S.length(); j++) {
+                list.add(S.charAt(j) - '0');
+                if ((j - i + 1) % 2 != 0) {
                     continue;
                 }
-                visited[next] = true;
-                distance[next] = distance[poll] + 1;
-                queue.offer(next);
+                int sum1 = 0, sum2 = 0;
+                for (int k = 0; k < list.size() / 2; k++) {
+                    sum1 += list.get(k);
+                    sum2 += list.get(list.size() - 1 - k);
+                }
+                if (sum1 == sum2) {
+                    answer = Math.max(answer, j - i + 1);
+                }
             }
         }
-        if (visited[N]) {
-            return distance[N] / 2 + 1;
+        System.out.print(answer);
+    }
+
+    private static int sum(String S) {
+        int result = 0;
+        for (int i = 0; i < S.length(); i++) {
+            result += S.charAt(i) - '0';
         }
-        return -1;
+        return result;
     }
 }
