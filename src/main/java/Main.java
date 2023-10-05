f5a8dc0e1cbc2b783e44467b388371faa3efcfc9
HubCreator <chocomilk8604@gmail.com>
2023-10-05 09:19:34 +0900

    [B] 소가 길을 건나간 이유5 14465

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = initArr(N);
+        for (int i = 0; i < K; i++) {
+            int index = Integer.parseInt(br.readLine());
+            arr[index] = 0;
         }
-        Arrays.sort(arr, (a, b) -> {
-            if (a[0] == b[0]) {
-                return Integer.compare(a[1], b[1]);
-            }
-            return Integer.compare(a[0], b[0]);
-        });
-        int result = 0;
-        for (int[] ints : arr) {
-            if (result < ints[0]) {
-                result = ints[0] + ints[1];
-            } else {
-                result += ints[1];
+
+        int answer = Integer.MAX_VALUE, brokenCount = 0;
+        for (int i = 0; i < B - 1; i++) {
+            if (arr[i] == 0) {
+                brokenCount++;
             }
         }
-        System.out.print(result);
+
+        for (int i = B - 1; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                brokenCount++;
+            }
+            answer = Math.min(answer, brokenCount);
+            if (arr[i - (B - 1)] == 0) {
+                brokenCount--;
+            }
+        }
+
+        System.out.print(answer);
+    }
+
+    private static int[] initArr(int N) {
+        int[] arr = new int[N + 1];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        return arr;
     }
 }
