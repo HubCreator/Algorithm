486b63a9bc7fda17a66120ef3ebd36d83193ab27
HubCreator <chocomilk8604@gmail.com>
2023-10-05 09:07:43 +0900

    [B] 소가 길을 건나간 이유3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
-        int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < S.length(); i++) {
-            List<Integer> list = new ArrayList<>();
-            for (int j = i; j < S.length(); j++) {
-                list.add(S.charAt(j) - '0');
-                if ((j - i + 1) % 2 != 0) {
-                    continue;
-                }
-                int sum1 = 0, sum2 = 0;
-                for (int k = 0; k < list.size() / 2; k++) {
-                    sum1 += list.get(k);
-                    sum2 += list.get(list.size() - 1 - k);
-                }
-                if (sum1 == sum2) {
-                    answer = Math.max(answer, j - i + 1);
-                }
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
+            }
+            return Integer.compare(a[0], b[0]);
+        });
+        int result = 0;
+        for (int[] ints : arr) {
+            if (result < ints[0]) {
+                result = ints[0] + ints[1];
+            } else {
+                result += ints[1];
             }
         }
-        System.out.print(answer);
-    }
-
-    private static int sum(String S) {
-        int result = 0;
-        for (int i = 0; i < S.length(); i++) {
-            result += S.charAt(i) - '0';
-        }
-        return result;
+        System.out.print(result);
     }
 }
