cfc46de6761bd4242479825919ef03e235e68bac
HubCreator <chocomilk8604@gmail.com>
2023-10-05 09:37:39 +0900

    [B] 행복 유치원 13164

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] arr = initArr(N);
-        for (int i = 0; i < K; i++) {
-            int index = Integer.parseInt(br.readLine());
-            arr[index] = 0;
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st1.nextToken());
+        int K = Integer.parseInt(st1.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st2.nextToken());
         }
 
-        int answer = Integer.MAX_VALUE, brokenCount = 0;
-        for (int i = 0; i < B - 1; i++) {
-            if (arr[i] == 0) {
-                brokenCount++;
-            }
+        Queue<Integer> pq = new PriorityQueue<>();
+        for (int i = 1; i < arr.length; i++) {
+            pq.offer(arr[i] - arr[i - 1]);
         }
 
-        for (int i = B - 1; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                brokenCount++;
-            }
-            answer = Math.min(answer, brokenCount);
-            if (arr[i - (B - 1)] == 0) {
-                brokenCount--;
-            }
+        int answer = 0;
+        for (int i = 0; i < N - K; i++) {
+            answer += pq.poll();
         }
 
         System.out.print(answer);
-    }
-
-    private static int[] initArr(int N) {
-        int[] arr = new int[N + 1];
-        for (int i = 1; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        return arr;
     }
 }
