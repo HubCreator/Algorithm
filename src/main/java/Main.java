6c25c4627a28e06947562557eaf6fee5d1061e71
HubCreator <chocomilk8604@gmail.com>
2023-10-09 11:33:28 +0900

    [B] 마트료시카 합치기 25631

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-    private static int count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int row = Integer.parseInt(st.nextToken());
-        int col = Integer.parseInt(st.nextToken());
-        char[][] board = getBoard(br, row, col);
-        int[][] countBoard = new int[row][col];
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for (int i = 0; i < N; i++) {
+            int t = Integer.parseInt(st.nextToken());
+            treeMap.put(t, treeMap.getOrDefault(t, 0) + 1);
+        }
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (countBoard[i][j] == 0) {
-                    count++;
-                    move(countBoard, board, i, j);
+        boolean flag;
+        do {
+            flag = false;
+            for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
+                if (entry.getValue() > 0) {
+                    treeMap.put(entry.getKey(), entry.getValue() - 1);
+                    flag = true;
                 }
             }
-        }
-        System.out.print(count);
-    }
-
-    private static void move(int[][] countBoard, char[][] board, int i, int j) {
-        int row = i, col = j;
-        do {
-            switch (board[row][col]) {
-                case 'U': row--; break;
-                case 'D': row++; break;
-                case 'L': col--; break;
-                case 'R': col++; break;
+            if (flag) {
+                answer++;
             }
-            countBoard[row][col] = count;
-        } while(row != i || col != j);
-    }
-
-    private static char[][] getBoard(BufferedReader br, int row, int col) throws IOException {
-        char[][] board = new char[row][col];
-        for (int i = 0; i < row; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < col; j++) {
-                board[i][j] = line.charAt(j);
-            }
-        }
-        return board;
+        } while (flag);
+        System.out.print(answer);
     }
 }
