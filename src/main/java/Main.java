738546c5d6dadffd8ca4d9bcae8ad95fef3f9707
HubCreator <chocomilk8604@gmail.com>
2023-10-14 12:40:25 +0900

    [B] 음식물 피하기 1743

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int count = 0;
+    private static int[] dy = {-1, 0, 1, 0};
+    private static int[] dx = {0, 1, 0, -1};
+    private static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int t = Integer.parseInt(br.readLine());
-        List<List<Integer>> adjList = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            adjList.add(new ArrayList<>());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        boolean[][] arr = new boolean[N + 1][M + 1];
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st2.nextToken());
+            int c = Integer.parseInt(st2.nextToken());
+            arr[r][c] = true;
         }
-        for (int i = 0; i < t; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            adjList.get(u).add(v);
-            adjList.get(v).add(u);
-        }
-        boolean[] check = new boolean[n + 1];
-        check[1] = true;
-        dfs(adjList, 1, check);
-
-        System.out.print(count);
+        System.out.print(solution(arr));
     }
 
-    private static void dfs(List<List<Integer>> adjList, int curr, boolean[] check) {
-        List<Integer> nodes = adjList.get(curr);
-        for (Integer node : nodes) {
-            if (!check[node]) {
-                check[node] = true;
+    private static int solution(boolean[][] arr) {
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j]) {
+                    boolean[][] check = new boolean[arr.length][arr[i].length];
+                    check[i][j] = true;
+                    count = 1;
+                    dfs(arr, check, i, j);
+                    answer = Math.max(answer, count);
+                }
+            }
+        }
+        return answer;
+    }
+
+    private static void dfs(boolean[][] arr, boolean[][] check, int i, int j) {
+        for (int d = 0; d < 4; d++) {
+            int ny = dy[d] + i;
+            int nx = dx[d] + j;
+            if (ny >= 0 && ny < check.length &&
+                    nx >= 0 && nx < check[0].length &&
+                    arr[ny][nx] && !check[ny][nx]) {
+                check[ny][nx] = true;
                 count++;
-                dfs(adjList, node, check);
+                dfs(arr, check, ny, nx);
             }
         }
     }
+
 }
