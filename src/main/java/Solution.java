dc2650802816c5907a19712985d9a83f9d42a115
HubCreator <chocomilk8604@gmail.com>
2023-10-16 13:52:09 +0900

    [P] 숫자의 표현

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,31 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        int[] students = new int[n + 2];
-        Arrays.fill(students, 1);
-        for (int i = 0; i < reserve.length; i++) {
-            students[reserve[i]] += 1;
-        }
-        for (int i = 0; i < lost.length; i++) {
-            students[lost[i]] -= 1;
-        }
-
-        for (int i = 1; i < students.length - 1; i++) {
-            if (students[i] == 0 && students[i + 1] >= 2) {
-                students[i] += 1;
-                students[i + 1] -= 1;
-            }
-            else if (students[i] >= 2 && students[i + 1] == 0) {
-                students[i] -= 1;
-                students[i + 1] += 1;
-            }
-
-        }
-        for (int i = 1; i < students.length - 1; i++) {
-            if (students[i] >= 1) {
-                answer += 1;
+    public int solution(int n) {
+        int lt = 1, sum = 0, answer = 0;
+        for (int rt = 1; rt <= n; rt++) {
+            sum += rt;
+            if (sum == n) {
+                answer++;
+            } else if (sum > n) {
+                while (sum > n) {
+                    sum -= lt++;
+                }
+                if (sum == n) {
+                    answer++;
+                }
             }
         }
         return answer;
