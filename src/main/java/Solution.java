b19d48b72b7716131e0006c7dd52a7bef5a77ee2
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-10-17 08:15:13 +0900

    [P] 게임 맵 최단 거리 bfs, 효율성 테스트 통과

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -6,18 +6,16 @@ class Solution {
     public int[] dx = new int[]{0, 1, 0, -1};
 
     public int solution(int[][] maps) {
-        int answer = 0, n = maps.length, m = maps[0].length;
-        Queue<int[]> queue = new ArrayDeque<>();
+        int n = maps.length, m = maps[0].length;
+        Queue<int[]> queue = new ArrayDeque<>(n * m);
         queue.offer(new int[]{0, 0});
 
         while(!queue.isEmpty()) {
             int size = queue.size();
-            answer++;
             for (int i = 0; i < size; i++) {
                 int[] poll = queue.poll();
-                maps[poll[0]][poll[1]] = -1;
                 if (poll[0] == n - 1 && poll[1] == m - 1) {
-                    return answer;
+                    return maps[poll[0]][poll[1]];
                 }
                 for (int d = 0; d < 4; d++) {
                     int ny = poll[0] + dy[d];
@@ -25,11 +23,11 @@ class Solution {
                     if (ny >= 0 && ny < n &&
                             nx >= 0 && nx < m &&
                             maps[ny][nx] == 1) {
+                        maps[ny][nx] = maps[poll[0]][poll[1]] + 1;
                         queue.offer(new int[]{ny, nx});
                     }
                 }
             }
-
         }
         return -1;
     }
