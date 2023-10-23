28ac4f914cc2316699a55edf0ac91c68a462561a
HubCreator <chocomilk8604@gmail.com>
2023-10-23 14:48:24 +0900

    [P] 가장 큰 수, comparator

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,25 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
-    private int max = Integer.MIN_VALUE;
-
     public String solution(int[] numbers) {
-        int[] pm = new int[numbers.length];
-        boolean[] check = new boolean[numbers.length];
-        dfs(numbers, pm, check, "", 0);
-        return String.valueOf(max);
-    }
+        StringBuilder answer = new StringBuilder();
+        Arrays.stream(numbers).boxed().sorted((a, b) -> {
+            int s1 = Integer.parseInt(a.toString().concat(b.toString()));
+            int s2 = Integer.parseInt(b.toString().concat(a.toString()));
+            return Integer.compare(s2, s1);
+        }).forEach(answer::append);
 
-    public void dfs(int[] numbers, int[] pm, boolean[] check, String result, int level) {
-        if (pm.length == level) {
-            max = Math.max(max, Integer.parseInt(result));
-        } else {
-            for (int i = 0; i < numbers.length; i++) {
-                if (!check[i]) {
-                    check[i] = true;
-                    pm[level] = numbers[i];
-                    dfs(numbers, pm, check, result + numbers[i], level + 1);
-                    check[i] = false;
-                }
-            }
-        }
+        return Double.parseDouble(answer.toString()) == 0 ? "0" : answer.toString();
     }
 }
