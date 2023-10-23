8e5693c2e47fb2ec3edbd70ea3e4ba56560bc5a2
HubCreator <chocomilk8604@gmail.com>
2023-10-23 14:36:53 +0900

    [P] 가장 큰 수

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,91 +1,25 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, List<Music>> map = new HashMap<>();
-        Map<String, Integer> countMap = new HashMap<>();
+    private int max = Integer.MIN_VALUE;
 
-        PriorityQueue<Genre> queue = new PriorityQueue<>();
+    public String solution(int[] numbers) {
+        int[] pm = new int[numbers.length];
+        boolean[] check = new boolean[numbers.length];
+        dfs(numbers, pm, check, "", 0);
+        return String.valueOf(max);
+    }
 
-        for (int i = 0; i < genres.length; i++) {
-            if (!map.containsKey(genres[i])) {
-                map.put(genres[i], new ArrayList<>());
-            }
-            map.get(genres[i]).add(new Music(i, plays[i]));
-        }
-
-        for (Map.Entry<String, List<Music>> entry : map.entrySet()) {
-            countMap.put(entry.getKey(), countMap.getOrDefault(entry.getKey(), 0) + entry.getValue().stream().map(m -> m.play).reduce(Integer::sum).get());
-        }
-
-        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
-            queue.offer(new Genre(entry.getKey(), map.get(entry.getKey())));
-        }
-
-        while (!queue.isEmpty()) {
-            Genre genre = queue.poll();
-            Collections.sort(genre.musics);
-            int count = 0;
-            for (Music music : genre.musics) {
-                if (count > 1) {
-                    break;
+    public void dfs(int[] numbers, int[] pm, boolean[] check, String result, int level) {
+        if (pm.length == level) {
+            max = Math.max(max, Integer.parseInt(result));
+        } else {
+            for (int i = 0; i < numbers.length; i++) {
+                if (!check[i]) {
+                    check[i] = true;
+                    pm[level] = numbers[i];
+                    dfs(numbers, pm, check, result + numbers[i], level + 1);
+                    check[i] = false;
                 }
-                answer.add(music.index);
-                count++;
             }
-        }
-
-        int[] result = new int[answer.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = answer.get(i);
-        }
-
-        return result;
-    }
-
-    private static class Music implements Comparable<Music> {
-        private int index;
-        private int play;
-
-        public Music(int index, int play) {
-            this.index = index;
-            this.play = play;
-        }
-
-        @Override
-        public int compareTo(Music o) {
-            if (this.play == o.play) {
-                return this.index - o.index;
-            }
-            return o.play - this.play;
-        }
-    }
-
-    private static class Genre implements Comparable<Genre> {
-        private String genre;
-        private List<Music> musics;
-
-        public Genre(String genre, List<Music> musics) {
-            this.genre = genre;
-            this.musics = musics;
-        }
-
-        @Override
-        public int compareTo(Genre o) {
-            int sum1 = 0, sum2 = 0;
-            for (int i = 0; i < this.musics.size(); i++) {
-                sum1 += this.musics.get(i).play;
-            }
-            for (int i = 0; i < o.musics.size(); i++) {
-                sum2 += o.musics.get(i).play;
-            }
-            return sum2 - sum1;
         }
     }
 }
