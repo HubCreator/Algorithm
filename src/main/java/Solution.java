45212dee6769b0f6c6b4a9ff536e71764ee9a700
HubCreator <chocomilk8604@gmail.com>
2023-10-25 08:31:18 +0900

    [P] 뉴스 클러스터링

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,18 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
-    public String solution(int n, int t, int m, int p) {
-        StringBuilder line = new StringBuilder();
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < n * t * m; i++) {
-            line.append(Long.toString(i, n));
-        }
-        for (int i = 0; i < line.length(); i++) {
-            if (answer.length() >= t) {
-                break;
+    public int solution(String str1, String str2) {
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        List<String> union = new ArrayList<>();
+        List<String> intersection = new ArrayList<>();
+
+        init(str1.toUpperCase(), list1);
+        init(str2.toUpperCase(), list2);
+
+        Collections.sort(list1);
+        Collections.sort(list2);
+
+        for (String s : list1) {
+            if (list2.remove(s)) {
+                intersection.add(s);
             }
-            if (i % m == p - 1) {
-                answer.append(Character.toUpperCase(line.charAt(i)));
+            union.add(s);
+        }
+        for (String s : list2) {
+            union.add(s);
+        }
+
+        double similarity;
+        if (union.size() == 0) {
+            similarity = 1;
+        } else {
+            similarity = (double) intersection.size() / (double) union.size();
+        }
+        return (int) (similarity * 65536);
+    }
+
+    private void init(String str1, List<String> list) {
+        for (int i = 0; i < str1.length() - 1; i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str1.charAt(i + 1);
+            if (Character.isAlphabetic(c1) && Character.isAlphabetic(c2)) {
+                String t = c1 + "" + c2;
+                list.add(t);
             }
         }
-        return answer.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("FRANCE ", "french"));
     }
 }
