d6600fa257215ada0469e565e2b7b9379977021a
HubCreator <chocomilk8604@gmail.com>
2023-10-25 23:36:33 +0900

    [B] K 물류창고 23350

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder answer = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            answer.append("Case #").append(i + 1).append(": ").append(solution(n)).append('\n');
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st1.nextToken());
+        int M = Integer.parseInt(st1.nextToken());
+        Queue<int[]> queue = new ArrayDeque<>();
+        List<int[]> area1 = new ArrayList<>();
+        List<int[]> area2 = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            queue.offer(new int[]{Integer.parseInt(st2.nextToken()), Integer.parseInt(st2.nextToken())});
+        }
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            int[] arr = queue.poll();
+            boolean flag = false;
+            for (int[] ints : queue) {
+                if (arr[0] < ints[0]) {
+                    queue.offer(arr);
+                    answer += arr[1];
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) continue;
+            if (area1.isEmpty()) {
+                area1.add(arr);
+                answer += arr[1];
+            } else {
+                while (!area1.isEmpty() &&
+                        area1.get(area1.size() - 1)[0] <= arr[0] &&
+                        area1.get(area1.size() - 1)[1] < arr[1]) {
+                    int[] removed = area1.remove(area1.size() - 1);
+                    answer += removed[1];
+                    area2.add(removed);
+                }
+                area1.add(arr);
+                answer += arr[1];
+                while (!area2.isEmpty()) {
+                    int[] removed = area2.remove(area2.size() - 1);
+                    answer += removed[1];
+                    area1.add(removed);
+                }
+            }
         }
         System.out.println(answer);
-    }
-
-    private static String solution(int n) {
-        if (n == 0) {
-            return "INSOMNIA";
-        }
-        Map<Character, Integer> map = new HashMap<>();
-        int multiply = 1;
-        while (map.size() < 10) {
-            char[] charArray = String.valueOf(n * multiply).toCharArray();
-            for (char c : charArray) {
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            }
-            multiply++;
-        }
-        return String.valueOf(n * (multiply - 1));
     }
 }
