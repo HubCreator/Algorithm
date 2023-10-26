86f8b17c31ed409a9ba44a48d8982aec1d37a2a4
HubCreator <chocomilk8604@gmail.com>
2023-10-26 10:55:38 +0900

    [B] 늑대와 올바른 단어 13022

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st1.nextToken());
-        int M = Integer.parseInt(st1.nextToken());
-        Queue<int[]> queue = new ArrayDeque<>();
-        List<int[]> area1 = new ArrayList<>();
-        List<int[]> area2 = new ArrayList<>();
+        String line = br.readLine();
+        char[] wolf = new char[]{'w', 'o', 'l', 'f'};
+        line += '!';
+        Map<Character, Integer> map = new LinkedHashMap<>();
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            queue.offer(new int[]{Integer.parseInt(st2.nextToken()), Integer.parseInt(st2.nextToken())});
+        char cur = 'w';
+        if (!line.startsWith("w")) {
+            System.out.println(0);
+            return;
         }
-        int answer = 0;
-        while (!queue.isEmpty()) {
-            int[] arr = queue.poll();
-            boolean flag = false;
-            for (int[] ints : queue) {
-                if (arr[0] < ints[0]) {
-                    queue.offer(arr);
-                    answer += arr[1];
-                    flag = true;
-                    break;
+        int index = 0;
+        for (int i = 0; i < line.length() - 1; i++) {
+            char c = line.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            if (c != cur) {
+                cur = c;
+                index++;
+                if (wolf[index % 4] != c) {
+                    System.out.println(0);
+                    return;
                 }
             }
-            if (flag) continue;
-            if (area1.isEmpty()) {
-                area1.add(arr);
-                answer += arr[1];
-            } else {
-                while (!area1.isEmpty() &&
-                        area1.get(area1.size() - 1)[0] <= arr[0] &&
-                        area1.get(area1.size() - 1)[1] < arr[1]) {
-                    int[] removed = area1.remove(area1.size() - 1);
-                    answer += removed[1];
-                    area2.add(removed);
+
+            if (c == 'f' && line.charAt(i + 1) == 'w') {
+                if (!isValid(map)) {
+                    System.out.println(0);
+                    return;
                 }
-                area1.add(arr);
-                answer += arr[1];
-                while (!area2.isEmpty()) {
-                    int[] removed = area2.remove(area2.size() - 1);
-                    answer += removed[1];
-                    area1.add(removed);
-                }
+                map.clear();
             }
         }
-        System.out.println(answer);
+
+        if (!isValid(map)) {
+            System.out.println(0);
+            return;
+        }
+        System.out.println(1);
+    }
+
+    private static boolean isValid(Map<Character, Integer> map) {
+        int sum = getSum(map);
+        StringBuilder sb = new StringBuilder();
+        for (Character character : map.keySet()) {
+            sb.append(character);
+        }
+
+        return sum % 4 == 0 && map.size() % 4 == 0 && sb.toString().equals("wolf");
+    }
+
+    private static int getSum(Map<Character, Integer> map) {
+        int sum = 0;
+        for (Integer value : map.values()) {
+            sum += value;
+        }
+        return sum;
     }
 }
