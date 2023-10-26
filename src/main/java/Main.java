27f9bf279028cc064ad53b91ea6c477d0700b5ac
HubCreator <chocomilk8604@gmail.com>
2023-10-26 11:18:28 +0900

    [B] 비슷한 단어 1411

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,66 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        char[] wolf = new char[]{'w', 'o', 'l', 'f'};
-        line += '!';
-        Map<Character, Integer> map = new LinkedHashMap<>();
-
-        char cur = 'w';
-        if (!line.startsWith("w")) {
-            System.out.println(0);
-            return;
+        int t = Integer.parseInt(br.readLine());
+        String[] arr = new String[t];
+        for (int i = 0; i < t; i++) {
+            arr[i] = br.readLine();
         }
-        int index = 0;
-        for (int i = 0; i < line.length() - 1; i++) {
-            char c = line.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-            if (c != cur) {
-                cur = c;
-                index++;
-                if (wolf[index % 4] != c) {
-                    System.out.println(0);
-                    return;
+        int answer = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (check(arr[i], arr[j])) {
+                    answer++;
                 }
             }
+        }
 
-            if (c == 'f' && line.charAt(i + 1) == 'w') {
-                if (!isValid(map)) {
-                    System.out.println(0);
-                    return;
-                }
-                map.clear();
+        System.out.println(answer);
+    }
+
+    private static boolean check(String s1, String s2) {
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            if (!map1.isEmpty() &&
+                    map1.containsKey(s1.charAt(i)) &&
+                    map1.get(s1.charAt(i)) != s2.charAt(i)) {
+                return false;
             }
+            if (!map2.isEmpty() &&
+                    map2.containsKey(s2.charAt(i)) &&
+                    map2.get(s2.charAt(i)) != s1.charAt(i)) {
+                return false;
+            }
+            map1.put(s1.charAt(i), s2.charAt(i));
+            map2.put(s2.charAt(i), s1.charAt(i));
         }
-
-        if (!isValid(map)) {
-            System.out.println(0);
-            return;
-        }
-        System.out.println(1);
-    }
-
-    private static boolean isValid(Map<Character, Integer> map) {
-        int sum = getSum(map);
-        StringBuilder sb = new StringBuilder();
-        for (Character character : map.keySet()) {
-            sb.append(character);
-        }
-
-        return sum % 4 == 0 && map.size() % 4 == 0 && sb.toString().equals("wolf");
-    }
-
-    private static int getSum(Map<Character, Integer> map) {
-        int sum = 0;
-        for (Integer value : map.values()) {
-            sum += value;
-        }
-        return sum;
+        return true;
     }
 }
