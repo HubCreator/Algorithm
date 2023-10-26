d57d7fd53e73a46c4c35eda139ad79accbde13ae
HubCreator <chocomilk8604@gmail.com>
2023-10-26 14:50:35 +0900

    [B] 늑대와 올바른 단어 13022

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static String wolf = "wolf";
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
@@ -21,35 +23,23 @@ public class Main {
         }
         list.add(sb.toString());
 
-        String wolf = "wolf";
-        char prev = '1';
-        int idx = 0;
         for (String s : list) {
             int n = s.length() / 4;
-            for (int i = 0; i < s.length(); i++) {
-                char curr = s.charAt(i);
-                if (i == 0) {
-                    prev = curr;
-                }
-
-                if (prev != curr) {
-                    if (wolf.charAt((idx + 1) % 4) == curr) {
-                        idx++;
-                        prev = curr;
-                    } else {
-                        System.out.println(0);
-                        return;
-                    }
-                }
-
-                if (s.lastIndexOf(curr) - s.indexOf(curr) + 1 != n) {
-                    System.out.println(0);
-                    return;
-                }
+            if (!s.equals(makeString(n))) {
+                System.out.println(0);
+                return;
             }
-            idx = 0;
         }
-
         System.out.println(1);
+    }
+
+    private static String makeString(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < wolf.length(); i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(wolf.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
