5c0ff2d66356dc655fde61523f151689549e2f56
HubCreator <chocomilk8604@gmail.com>
2023-10-27 13:18:13 +0900

    [B] INK 30036

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,94 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static String wolf = "wolf";
+    private static char[][] board;
+    private static String inkLine;
+    private static int[] boxCoord;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        line += '!';
-        List<String> list = new ArrayList<>();
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int I = Integer.parseInt(st1.nextToken());
+        int N = Integer.parseInt(st1.nextToken());
+        int K = Integer.parseInt(st1.nextToken());
+        board = new char[N][N];
+        inkLine = br.readLine();
+        boxCoord = initBoard(br, N);
+        String commandLine = br.readLine();
+        solution(commandLine);
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < line.length() - 1; i++) {
-            sb.append(line.charAt(i));
-            if (line.charAt(i) == 'f' && line.charAt(i + 1) == 'w') {
-                list.add(sb.toString());
-                sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+        for (char[] chars : board) {
+            for (char aChar : chars) {
+                answer.append(aChar);
             }
+            answer.append('\n');
         }
-        list.add(sb.toString());
-
-        for (String s : list) {
-            int n = s.length() / 4;
-            if (!s.equals(makeString(n))) {
-                System.out.println(0);
-                return;
-            }
-        }
-        System.out.println(1);
+        System.out.print(answer.deleteCharAt(answer.length() - 1));
     }
 
-    private static String makeString(int n) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < wolf.length(); i++) {
-            for (int j = 0; j < n; j++) {
-                sb.append(wolf.charAt(i));
+    private static void solution(String commandLine) {
+        int inkCount = 0, inkIndex = 0;
+        for (int i = 0; i < commandLine.length(); i++) {
+            char command = commandLine.charAt(i);
+            switch (command) {
+                case 'j':
+                    inkCount++;
+                    break;
+                case 'J':
+                    char color = inkLine.charAt(inkIndex);
+                    coloring(color, inkCount);
+                    inkCount = 0;
+                    inkIndex = (inkIndex + 1) % inkLine.length();
+                    break;
+                default:
+                    int y = boxCoord[0], x = boxCoord[1];
+                    if (command == 'U' && boxCoord[0] - 1 >= 0 && board[boxCoord[0] - 1][boxCoord[1]] != '#') {
+                        boxCoord = new int[]{y - 1, x};
+                    }
+                    if (command == 'D' && boxCoord[0] + 1 < board.length && board[boxCoord[0] + 1][boxCoord[1]] != '#') {
+                        boxCoord = new int[]{y + 1, x};
+                    }
+                    if (command == 'L' && boxCoord[1] - 1 >= 0 && board[boxCoord[0]][boxCoord[1] - 1] != '#') {
+                        boxCoord = new int[]{y, x - 1};
+                    }
+                    if (command == 'R' && boxCoord[1] + 1 < board.length && board[boxCoord[0]][boxCoord[1] + 1] != '#') {
+                        boxCoord = new int[]{y, x + 1};
+                    }
+                    break;
             }
         }
-        return sb.toString();
+        board[boxCoord[0]][boxCoord[1]] = '@';
+    }
+
+    private static void coloring(char color, int inkCount) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (Math.abs(boxCoord[0] - i) + Math.abs(boxCoord[1] - j) <= inkCount &&
+                        board[i][j] != '.') {
+                    board[i][j] = color;
+                }
+            }
+        }
+    }
+
+    private static int[] initBoard(BufferedReader br, int N) throws IOException {
+        int[] boxCoord = null;
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+                if (c == '@') {
+                    boxCoord = new int[]{i, j};
+                    board[i][j] = '.';
+                    continue;
+                }
+                board[i][j] = c;
+            }
+        }
+        return boxCoord;
     }
 }
