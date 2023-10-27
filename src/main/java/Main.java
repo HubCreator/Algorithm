69a28053ac0e7e5c366e2dd9723db17bb5bfc62b
HubCreator <chocomilk8604@gmail.com>
2023-10-27 15:54:33 +0900

    [B] 소셜 광고 9335

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static int result;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Queue<Student> queue = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            queue.add(new Student(st.nextToken(), Integer.parseInt(st.nextToken())));
-        }
-
-        while (queue.size() > 1) {
-            Student poll = queue.poll();
-            for (int i = 0; i < poll.num - 1; i++) {
-                queue.offer(queue.poll());
+        StringBuilder answer = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            result = Integer.MAX_VALUE;
+            int N = Integer.parseInt(br.readLine());
+            List<List<Integer>> list = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                list.add(new ArrayList<>());
             }
-            queue.poll();
+            for (int j = 0; j < N; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int d = Integer.parseInt(st.nextToken());
+                for (int k = 0; k < d; k++) {
+                    int t = Integer.parseInt(st.nextToken()) - 1;
+                    list.get(j).add(t);
+                }
+            }
+            boolean[] check = new boolean[N];
+            dfs(list, check, 0);
+            answer.append(result).append('\n');
         }
-
-        System.out.print(queue.poll().name);
+        System.out.print(answer);
     }
 
-    public static class Student {
-        private String name;
-        private int num;
-
-        public Student(String name, int num) {
-            this.name = name;
-            this.num = num;
+    private static void dfs(List<List<Integer>> list, boolean[] check, int level) {
+        if (level == check.length) {
+            boolean[] history = new boolean[check.length];
+            int count = 0;
+            for (int i = 0; i < check.length; i++) {
+                if (check[i]) {
+                    history[i] = true;
+                    for (Integer friend : list.get(i)) {
+                        history[friend] = true;
+                    }
+                    count++;
+                }
+            }
+            int sum = 0;
+            for (boolean b : history) {
+                if (b) {
+                    sum += 1;
+                }
+            }
+            if (sum == check.length) {
+                result = Math.min(result, count);
+            }
+        } else {
+            if (!check[level]) {
+                check[level] = true;
+                dfs(list, check, level + 1);
+                check[level] = false;
+                dfs(list, check, level + 1);
+            }
         }
     }
 }
