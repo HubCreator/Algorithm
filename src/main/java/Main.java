67c59ab78514cc250d8db8d6cccb654bcdc1f6fa
HubCreator <chocomilk8604@gmail.com>
2023-10-27 13:32:40 +0900

    [B] 콰트로피자 27964

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,94 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
-    private static char[][] board;
-    private static String inkLine;
-    private static int[] boxCoord;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int I = Integer.parseInt(st1.nextToken());
-        int N = Integer.parseInt(st1.nextToken());
-        int K = Integer.parseInt(st1.nextToken());
-        board = new char[N][N];
-        inkLine = br.readLine();
-        boxCoord = initBoard(br, N);
-        String commandLine = br.readLine();
-        solution(commandLine);
-
-        StringBuilder answer = new StringBuilder();
-        for (char[] chars : board) {
-            for (char aChar : chars) {
-                answer.append(aChar);
-            }
-            answer.append('\n');
-        }
-        System.out.print(answer.deleteCharAt(answer.length() - 1));
-    }
-
-    private static void solution(String commandLine) {
-        int inkCount = 0, inkIndex = 0;
-        for (int i = 0; i < commandLine.length(); i++) {
-            char command = commandLine.charAt(i);
-            switch (command) {
-                case 'j':
-                    inkCount++;
-                    break;
-                case 'J':
-                    char color = inkLine.charAt(inkIndex);
-                    coloring(color, inkCount);
-                    inkCount = 0;
-                    inkIndex = (inkIndex + 1) % inkLine.length();
-                    break;
-                default:
-                    int y = boxCoord[0], x = boxCoord[1];
-                    if (command == 'U' && boxCoord[0] - 1 >= 0 && board[boxCoord[0] - 1][boxCoord[1]] != '#') {
-                        boxCoord = new int[]{y - 1, x};
-                    }
-                    if (command == 'D' && boxCoord[0] + 1 < board.length && board[boxCoord[0] + 1][boxCoord[1]] != '#') {
-                        boxCoord = new int[]{y + 1, x};
-                    }
-                    if (command == 'L' && boxCoord[1] - 1 >= 0 && board[boxCoord[0]][boxCoord[1] - 1] != '#') {
-                        boxCoord = new int[]{y, x - 1};
-                    }
-                    if (command == 'R' && boxCoord[1] + 1 < board.length && board[boxCoord[0]][boxCoord[1] + 1] != '#') {
-                        boxCoord = new int[]{y, x + 1};
-                    }
-                    break;
+        br.readLine();
+        String line = br.readLine();
+        String[] split = line.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        for (String s : split) {
+            if (s.endsWith("Cheese")) {
+                set.add(s);
             }
         }
-        board[boxCoord[0]][boxCoord[1]] = '@';
-    }
 
-    private static void coloring(char color, int inkCount) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (Math.abs(boxCoord[0] - i) + Math.abs(boxCoord[1] - j) <= inkCount &&
-                        board[i][j] != '.') {
-                    board[i][j] = color;
-                }
-            }
+        if (set.size() >= 4) {
+            System.out.println("yummy");
+        } else {
+            System.out.println("sad");
         }
-    }
 
-    private static int[] initBoard(BufferedReader br, int N) throws IOException {
-        int[] boxCoord = null;
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < line.length(); j++) {
-                char c = line.charAt(j);
-                if (c == '@') {
-                    boxCoord = new int[]{i, j};
-                    board[i][j] = '.';
-                    continue;
-                }
-                board[i][j] = c;
-            }
-        }
-        return boxCoord;
     }
 }
