8f64246d6ee1ea23e1a5b0ad2882ed4c69e11a98
HubCreator <chocomilk8604@gmail.com>
2023-10-29 00:29:52 +0900

    [P] k진수에서 소수 개수 구하기

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,48 +1,34 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    public int solution(String str1, String str2) {
-        List<String> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
-        List<String> union = new ArrayList<>();
-        List<String> intersection = new ArrayList<>();
-
-        init(str1.toUpperCase(), list1);
-        init(str2.toUpperCase(), list2);
-
-        for (String s : list1) {
-            if (list2.remove(s)) {
-                intersection.add(s);
+    public int solution(int n, int k) {
+        int answer = 0;
+        String line = Long.toString(n, k) + "0";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < line.length(); i++) {
+            char curr = line.charAt(i);
+            if (curr != '0') {
+                sb.append(curr);
+            } else {
+                if (sb.length() > 0 && isPrime(sb.toString())) {
+                    answer++;
+                }
+                sb = new StringBuilder();
             }
-            union.add(s);
-        }
-        for (String s : list2) {
-            union.add(s);
         }
 
-        double similarity;
-        if (union.size() == 0) {
-            similarity = 1;
+        return answer;
+    }
+
+    private boolean isPrime(String input) {
+        long i = Long.parseLong(input);
+        if (i == 1) {
+            return false;
         } else {
-            similarity = (double) intersection.size() / (double) union.size();
-        }
-        return (int) (similarity * 65536);
-    }
-
-    private void init(String str1, List<String> list) {
-        for (int i = 0; i < str1.length() - 1; i++) {
-            char c1 = str1.charAt(i);
-            char c2 = str1.charAt(i + 1);
-            if (Character.isAlphabetic(c1) && Character.isAlphabetic(c2)) {
-                String t = c1 + "" + c2;
-                list.add(t);
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    return false;
+                }
             }
+            return true;
         }
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.solution("aa1+aa2", "AAAA12"));
     }
 }
