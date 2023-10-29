0fced3ce30c70440562aff5ea4e9d8d301262ba4
HubCreator <chocomilk8604@gmail.com>
2023-10-29 16:33:08 +0900

    [P] 최고의 집합

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,24 +1,23 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-
 class Solution {
-    public long solution(int n, int[] works) {
-        long answer = 0;
-        long sum = 0;
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for (int work : works) {
-            maxHeap.offer(work);
-            sum += work;
+    public int[] solution(int n, int s) {
+        if (n > s) {
+            return new int[]{-1};
         }
-        if (sum < n) {
-            return 0;
-        }
-        for (int i = 0; i < n; i++) {
-            maxHeap.offer(maxHeap.poll() - 1);
-        }
-        for (Integer integer : maxHeap) {
-            answer += Math.pow(integer, 2);
+        int[] answer = new int[n];
+        int idx = 0;
+        while (n > 0) {
+            int target = s / n;
+            answer[idx++] = target;
+            s -= target;
+            n--;
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        for (int i : solution.solution(2, 9)) {
+            System.out.print(i + " ");
+        }
     }
 }
