ffed4485f3d8e112eb8747f4bede6f14921787bd
HubCreator <chocomilk8604@gmail.com>
2023-10-29 10:30:51 +0900

    [P] 3차 압축

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,34 +3,25 @@ import java.util.List;
 
 class Solution {
     public int[] solution(String msg) {
-        StringBuilder dict1 = new StringBuilder();
-        StringBuilder dict2 = new StringBuilder();
         List<String> dict = new ArrayList<>();
         List<Integer> indexes = new ArrayList<>();
         for (char c = 'A'; c <= 'Z'; c++) {
-            dict1.append(String.valueOf(c));
+            dict.add(String.valueOf(c));
         }
-
-        for (int i = 0; i < msg.length() - 1; i++) {
+        for (int i = 0; i < msg.length(); i++) {
             StringBuilder curr = new StringBuilder();
             curr.append(msg.charAt(i));
 
-            while (i < msg.length() -1 &&
-                    dict2.indexOf(curr.toString() + msg.charAt(i + 1)) != -1) {
+            while (i < msg.length() - 1 && dict.contains(curr.toString() + msg.charAt(i + 1))) {
                 curr.append(msg.charAt(i + 1));
                 i++;
             }
-
             if (i < msg.length() - 1) {
-                char next = msg.charAt(i + 1);
-                dict2.append(curr.toString() + next);
+                dict.add(curr.toString() + msg.charAt(i + 1));
             }
-            if (curr.toString().length() > 1) {
-                indexes.add(dict2.indexOf(curr.toString()) + 27);
-            } else {
-                indexes.add(dict2.indexOf(curr.toString()) + 1);
-            }
+            indexes.add(dict.indexOf(curr.toString()) + 1);
         }
+
         int[] answer = new int[indexes.size()];
         for (int i = 0; i < indexes.size(); i++) {
             answer[i] = indexes.get(i);
@@ -40,7 +31,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        for (int i : solution.solution("ABABABABABABABAB")) {
+        for (int i : solution.solution("KAKAO")) {
             System.out.print(i + " ");
         }
     }
