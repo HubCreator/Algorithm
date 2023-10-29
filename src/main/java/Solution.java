7478b415c71301398d9842ac1a195cefd240aaaf
HubCreator <chocomilk8604@gmail.com>
2023-10-29 15:50:48 +0900

    [P] 야근 지수

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,53 +1,24 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        boolean flag = false;
-        for (String word : words) {
-            if (word.equals(target)) {
-                flag = true;
-            }
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        long sum = 0;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int work : works) {
+            maxHeap.offer(work);
+            sum += work;
         }
-        if (!flag) {
+        if (sum < n) {
             return 0;
         }
-        int answer = 0;
-        boolean[] check = new boolean[words.length];
-        Queue<String> queue = new ArrayDeque<>();
-        queue.offer(begin);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            answer++;
-            for (int i = 0; i < size; i++) {
-                String poll = queue.poll();
-                if (poll.equals(target)) {
-                    return answer - 1;
-                }
-                for (int j = 0; j < words.length; j++) {
-                    if (!check[j] && isValid(poll, words[j])) {
-                        check[j] = true;
-                        queue.offer(words[j]);
-                    }
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            maxHeap.offer(maxHeap.poll() - 1);
         }
-        return 0;
-    }
-
-    private boolean isValid(String word1, String word2) {
-        int count = 0;
-        for (int i = 0; i < word1.length(); i++) {
-            if (word1.charAt(i) != word2.charAt(i)) {
-                count++;
-            }
+        for (Integer integer : maxHeap) {
+            answer += Math.pow(integer, 2);
         }
-        return count == 1;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int answer = solution.solution("hit", "cog", new String[]{"hot", "dot", "dog", "lot", "log", "cog"});
-        System.out.println("answer = " + answer);
+        return answer;
     }
 }
