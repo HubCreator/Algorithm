df15471832a6d877a0a8d5f8ea312b31047aac40
헙크 <chocomilk8604@gmail.com>
2023-11-02 13:55:12 +0900

    [level 2] Title: 요격 시스템, Time: 0.74 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -3,20 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 1;
-        Arrays.sort(targets, (a, b) -> a[0] - b[0]);
+        Arrays.sort(targets, (a, b) -> a[1] - b[1]);
         
-        int[] prev = targets[0];
+        int preS = targets[0][0];
+        int preE = targets[0][1];
+        
         for (int i = 1; i < targets.length; i++) {
-            int s = targets[i][0];
-            int e = targets[i][1];
+            int curS = targets[i][0];
+            int curE = targets[i][1];
             
-            if (prev[0] <= s && s < prev[1]) {
-                prev[0] = Math.max(prev[0], s);
-                prev[1] = Math.min(prev[1], e);
-            } else {
-                prev = targets[i];
-                answer++;
+            if (preE > curS) {
+                continue;
             }
+            answer++;
+            preS = curS;
+            preE = curE;
         }
         
         return answer;
