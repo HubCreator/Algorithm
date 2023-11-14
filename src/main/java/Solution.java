6b991b9138b06fdf09da978e5927a6b5a096b460
hubcreator <chocomilk8604@gmail.com>
2023-11-14 14:39:14 +0900

    [P] 구명보트

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,37 +1,36 @@
+import java.util.Arrays;
+
 class Solution {
-    public long solution(int r1, int r2) {
-        long answer = 0, intersect = 0;
-        int max = Math.max(r1, r2);
-        int min = Math.min(r1, r2);
-        double max2 = Math.pow(max, 2);
-        double min2 = Math.pow(min, 2);
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        boolean flag = false;
+        boolean[] check = new boolean[people.length];
+        Arrays.sort(people);
 
-
-        for (int i = 0; i <= max; i++) {
-            for (int j = 0; j <= max; j++) {
-                double x = Math.pow(i, 2);
-                double y = Math.pow(j, 2);
-                if (x + y <= max2) {
-                    answer++;
-
-                    if (i == 0 || j == 0) {
-                        intersect++;
-                    }
+        for (int i = 0; i < people.length; i++) {
+            if (check[i]) {
+                continue;
+            }
+            for (int j = check.length - 1; j >= 0; j--) {
+                if (check[j]) {
+                    continue;
                 }
-
+                if (people[i] + people[j] <= limit) {
+                    check[i] = true;
+                    check[j] = true;
+                    answer++;
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                check[i] = true;
+                answer++;
+            } else {
+                flag = false;
             }
         }
-        System.out.println(answer - 1);
 
-        return (long) answer;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.solution(0, 10);
+        return answer;
     }
 }
-
-// 2 -> 5 -> 10 -> 16 -> 25 -> 34 -> 44 -> 57 -> 72 -> 89
-//  +3   +5    +6   +9     +9    +10    +13   +20   + 17
-//   +2    +1    +3   +0
