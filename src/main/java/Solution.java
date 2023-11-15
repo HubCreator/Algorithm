6edc94005605f1ba61731b1587d83a8751c37ab3
hubcreator <chocomilk8604@gmail.com>
2023-11-15 19:10:04 +0900

    [P] N개의 최소공배수

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,36 +1,43 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int[] people, int limit) {
-        int answer = 0;
-        boolean flag = false;
-        boolean[] check = new boolean[people.length];
-        Arrays.sort(people);
 
-        for (int i = 0; i < people.length; i++) {
-            if (check[i]) {
-                continue;
-            }
-            for (int j = check.length - 1; j >= 0; j--) {
-                if (check[j]) {
-                    continue;
-                }
-                if (people[i] + people[j] <= limit) {
-                    check[i] = true;
-                    check[j] = true;
-                    answer++;
+    public int solution(int[] arr) {
+        int answer = 1, max = 1;
+        for (int e : arr) {
+            max = Math.max(max, e);
+        }
+
+        for (int i = max; i >= 2; i--) {
+            boolean flag = false;
+            int count = 0;
+            for (int num : arr) {
+                if (count > 1) {
                     flag = true;
                     break;
                 }
+                if (num % i == 0) {
+                    count++;
+                }
             }
-            if (!flag) {
-                check[i] = true;
-                answer++;
-            } else {
-                flag = false;
+            if (flag || count > 1) {
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[j] % i == 0) {
+                        arr[j] /= i;
+                    }
+                }
+                answer *= i;
             }
         }
 
+        for (int num : arr) {
+            answer *= num;
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(new int[]{4, 4});
+        System.out.println("result = " + result);
     }
 }
