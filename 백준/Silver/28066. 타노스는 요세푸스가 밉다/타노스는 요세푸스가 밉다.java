3fdf4768b8fc77d8fcdde5464502ee9dacb6007f
헙크 <chocomilk8604@gmail.com>
2023-11-17 16:56:43 +0900

    [Silver II] Title: 타노스는 요세푸스가 밉다, Time: 224 ms, Memory: 44940 KB -BaekjoonHub

--- 백준/Silver/28066. 타노스는 요세푸스가 밉다/타노스는 요세푸스가 밉다.java
+++ 백준/Silver/28066. 타노스는 요세푸스가 밉다/타노스는 요세푸스가 밉다.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,34 +11,21 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int count = N, index = 0;
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
         }
-        while (count > 1) {
-            if (count < K) {
-                System.out.print(arr[index]);
+
+        while (queue.size() > 1) {
+            if (queue.size() > K) {
+                queue.offer(queue.poll());
+                for (int i = 0; i < K - 1; i++) {
+                    queue.poll();
+                }
+            } else {
+                System.out.print(queue.poll());
                 return;
             }
-            int size = K - 1;
-            index = (index + 1) % N;
-            while (size > 0) {
-                if (arr[index] == -1) {
-                    index = (index + 1) % N;
-                    continue;
-                }
-                arr[index] = -1;
-                count--;
-                size--;
-                index = (index + 1) % N;
-            }
-
-            while (arr[index] == -1) {
-                index = (index + 1) % N;
-            }
         }
-
-        System.out.print(arr[index]);
     }
 }
