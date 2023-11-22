dc5d2cf709095d0ff8841b8da4a2ac02b2e152c4
헙크 <chocomilk8604@gmail.com>
2023-11-22 17:52:24 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long solution(int n) {
+        int[] fibo = new int[n + 1];
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            makeFibo(fibo);
+            return fibo[n];
+        }
+    }
+    
+    private void makeFibo(int[] fibo) {
+        fibo[1] = 1;
+        fibo[2] = 2;
+        for (int i = 3; i < fibo.length; i++) {
+            fibo[i] = (fibo[i - 2] + fibo[i - 1]) % 1234567;
+        }
+    }
+}
