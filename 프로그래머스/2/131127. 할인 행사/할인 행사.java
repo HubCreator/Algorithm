76eb8761ee6b9abcc7eadc8d672ee6ee6606962f
헙크 <chocomilk8604@gmail.com>
2023-11-22 17:52:36 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String, Integer> wishMap = new HashMap<>();
+        Map<String, Integer> discountMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wishMap.put(want[i], number[i]);
+        }
+        for (int i = 0; i < 9; i++) {
+            discountMap.put(discount[i], discountMap.getOrDefault(discount[i], 0) + 1);
+        }
+        int lt = 0;
+        for (int i = 9; i < discount.length; i++, lt++) {
+            discountMap.put(discount[i], discountMap.getOrDefault(discount[i], 0) + 1);
+            if (wishMap.equals(discountMap)) {
+                answer += 1;
+            }
+            discountMap.put(discount[lt], discountMap.get(discount[lt]) - 1);
+            if (discountMap.get(discount[lt]) == 0) {
+                discountMap.remove(discount[lt]);
+            }
+        }
+        return answer;
+    }
+}
