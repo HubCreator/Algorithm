ef62a5c9a3c8c6565e14053c7bf99d4185d26eea
hubcreator <chocomilk8604@gmail.com>
2023-12-04 11:08:07 +0900

    [B] 가장 긴 증가하는 부분 수열 dfs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,42 +4,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static boolean[][] board;
-
+    private static int answer = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = Integer.MAX_VALUE;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        board = new boolean[N][M];
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                board[i][j] = line.charAt(j) == 'W';
-            }
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i <= N - 8; i++) {
-            for (int j = 0; j <= M - 8; j++) {
-                answer = Math.min(answer, solution(i, j));
-            }
+        for (int i = 1; i <= N; i++) {
+            int[] pm = new int[i];
+            boolean[] check = new boolean[N];
+            dfs(i, arr, pm, check, 0);
         }
+
         System.out.print(answer);
     }
 
-    private static int solution(int y, int x) {
-        int count = 0;
-        boolean flag = board[y][x];
-        for (int i = y; i < y + 8; i++) {
-            for (int j = x; j < x + 8; j++) {
-                if (board[i][j] != flag) {
-                    count++;
+    private static void dfs(int len, int[] arr, int[] pm, boolean[] check, int level) {
+        if (len == level) {
+            boolean flag = true;
+            for (int i = 0; i < pm.length - 1; i++) {
+                if (pm[i] >= pm[i + 1]) {
+                    flag = false;
+                    break;
                 }
-                flag = !flag;
             }
-            flag = !board[i][0];
+            if (flag) {
+                answer = Math.max(answer, len);
+            }
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (!check[i]) {
+                    check[i] = true;
+                    pm[level] = arr[i];
+                    dfs(len, arr, pm, check, level + 1);
+                    check[i] = false;
+                }
+            }
         }
-        return Math.min(count, 64 - count);
     }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,43 +1,12 @@
+import java.util.function.Function;
+
 class Solution {
+    Function<Integer, Integer>[] arr = new Function[]{
 
-    public int solution(int[] arr) {
-        int answer = 1, max = 1;
-        for (int e : arr) {
-            max = Math.max(max, e);
-        }
-
-        for (int i = max; i >= 2; i--) {
-            boolean flag = false;
-            int count = 0;
-            for (int num : arr) {
-                if (count > 1) {
-                    flag = true;
-                    break;
-                }
-                if (num % i == 0) {
-                    count++;
-                }
-            }
-            if (flag || count > 1) {
-                for (int j = 0; j < arr.length; j++) {
-                    if (arr[j] % i == 0) {
-                        arr[j] /= i;
-                    }
-                }
-                answer *= i;
-            }
-        }
-
-        for (int num : arr) {
-            answer *= num;
-        }
+    };
+    public int solution(int x, int y, int n) {
+        int answer = 0;
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int result = solution.solution(new int[]{4, 4});
-        System.out.println("result = " + result);
     }
 }
