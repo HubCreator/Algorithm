9af137a6c0dc5b63999dda1d28ed9d7906f82b2f
헙크 <chocomilk8604@gmail.com>
2023-12-21 19:32:26 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 148 ms, Memory: 15944 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st1.nextToken());
+        int M = Integer.parseInt(st1.nextToken());
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int tmp;
+            for (int j = from; j <= (from + to) / 2; j++) {
+                tmp = arr[j];
+                arr[j] = arr[(from + to) - j];
+                arr[(from + to) - j] = tmp;
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
